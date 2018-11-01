74a981157500a3cfb9cb6839c85be9ea1a363549
Tej Chajed <tchajed@mit.edu>
2018-10-31 21:04:21 -0400

    Separate op wp in weakest preconditions

--- src/Spec/WeakestPreconditions.v
+++ src/Spec/WeakestPreconditions.v
@@ -12,14 +12,23 @@ Section Dynamics.
   Notation exec := sem.(exec).
   Notation exec_crash := sem.(exec_crash).
 
-  Fixpoint wp T (p: proc T) (post: T -> State -> Prop) : State -> Prop :=
-    fun s =>
+  Definition precondition T := forall (post: T -> State -> Prop), State -> Prop.
+
+  Context (op_wp: forall T, Op T -> precondition T).
+
+  Definition op_wp_ok :=
+    forall T (op:Op T) post s, op_wp op post s ->
+                          forall s' v, step op s s' v ->
+                                  post v s'.
+
+  Fixpoint wp T (p: proc T)
+    : precondition T :=
+    fun post =>
       match p with
-      | Ret v => post v s
-      | Prim op => forall v s', step op s s' v ->
-                          post v s'
+      | Ret v => post v
+      | Prim op => op_wp op post
       | Bind p rx =>
-        wp p (fun v s' => wp (rx v) post s') s
+        wp p (fun v s' => wp (rx v) post s')
       end.
 
   Ltac cleanup :=
@@ -27,35 +36,61 @@ Section Dynamics.
     unfold pure, and_then, test, rel_or in *;
     propositional.
 
-  Theorem wp_ok T (p: proc T) (post: T -> State -> Prop) :
+  Theorem wp_ok :
+    op_wp_ok ->
+    forall T (p: proc T) (post: T -> State -> Prop),
     forall s, wp p post s ->
          forall s' v, exec p s s' v ->
                  post v s'.
   Proof.
+    intros Hop_wp.
     induction p; cleanup; eauto.
-    eapply IHp in H1; eauto; cleanup.
-    eapply H in H1; eauto.
+    - eapply Hop_wp in H; eauto.
+    - eapply IHp in H1; eauto; cleanup.
+      eapply H in H1; eauto.
   Qed.
 
-  Theorem wp_kat T (p: proc T) (post: T -> State -> Prop) :
+  Theorem wp_op_kat :
+    (forall T (op:Op T) post,
+        test (op_wp op post);; step op --->
+             test (op_wp op post);; v <- step op; test (post v);; pure v) ->
+    op_wp_ok.
+  Proof.
+    unfold op_wp_ok.
+    cleanup.
+    specialize (H _ op post s s' v); simpl in H.
+    match type of H with
+    | ?P -> _ =>
+      let HP := fresh in
+      assert P as HP; [ | specialize (H HP); propositional ]
+    end.
+    descend; intuition eauto.
+  Qed.
+
+  Theorem wp_kat
+          T (p: proc T) (post: T -> State -> Prop) :
+    (forall T (op:Op T) post,
+        test (op_wp op post);; step op --->
+             test (op_wp op post);; v <- step op; test (post v);; pure v) ->
     test (wp p post);; exec p --->
          test (wp p post);; v <- exec p; test (post v);; pure v.
   Proof.
-    cleanup.
-    unfold rimpl; propositional.
+    cleanup; unfold rimpl; propositional.
     descend; intuition eauto.
     descend; intuition eauto.
     descend; intuition eauto.
     eapply wp_ok; eauto.
+    eapply wp_op_kat; eauto.
   Qed.
 
-  Fixpoint wp_crash T (p: proc T) (crash: State -> Prop) : State -> Prop :=
-    fun s =>
+  Definition crashpre := forall (crash: State -> Prop), State -> Prop.
+
+  Fixpoint wp_crash T (p: proc T) : crashpre :=
+    fun crash s =>
       match p with
       | Ret v => crash s
       | Prim op => crash s /\
-                  forall v s', step op s s' v ->
-                          crash s'
+                  op_wp op (fun v s => crash s) s
       | Bind p rx =>
         (* crashing at s is handled by wp_crash p (inductively; the other two
  rules includes this *)
@@ -64,14 +99,17 @@ Section Dynamics.
       end.
 
   Theorem wp_crash_ok T (p: proc T) (crash: State -> Prop) :
+    op_wp_ok ->
     forall s, wp_crash p crash s ->
          forall s' v, exec_crash p s s' v ->
                  crash s'.
   Proof.
+    intros Hop_ok.
     induction p; cleanup; eauto.
     - repeat match goal with
              | [ H: _ \/ _ |- _ ] => destruct H; propositional; eauto
              end.
+      eapply Hop_ok in H1; eauto; cleanup.
     - repeat match goal with
              | [ H: _ \/ _ |- _ ] => destruct H; propositional; eauto
              end.
@@ -81,14 +119,17 @@ Section Dynamics.
   Qed.
 
   Theorem wp_crash_kat T (p: proc T) (crash: State -> Prop) :
+    (forall T (op:Op T) post,
+        test (op_wp op post);; step op --->
+             test (op_wp op post);; v <- step op; test (post v);; pure v) ->
     test (wp_crash p crash);; exec_crash p --->
          test (wp_crash p crash);; exec_crash p;; test crash.
   Proof.
-    cleanup.
-    unfold rimpl; propositional.
+    cleanup; unfold rimpl; propositional.
     descend; intuition eauto.
     descend; intuition eauto.
     eapply wp_crash_ok; eauto.
+    eapply wp_op_kat; eauto.
   Qed.
 
 End Dynamics.
