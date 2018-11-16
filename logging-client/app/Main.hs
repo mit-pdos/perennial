5fa83a12b65b8360454ed0ce1d23bcef07327450
Tej Chajed <tchajed@mit.edu>
2018-11-16 08:49:21 -0500

    Add commands for the entire logging API
    Seems to work!

--- logging-client/app/Main.hs
+++ logging-client/app/Main.hs
@@ -2,23 +2,28 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Data.Semigroup ((<>))
-import Options.Applicative
-import Replication.TwoDiskEnvironment
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Semigroup ((<>))
+import           Options.Applicative
+import           Replication.Logging
+import           Replication.TwoDiskEnvironment
 import qualified Replication.TwoDiskOps as TD
-import Replication.Logging
+import           Transactions.LogEncoding (encodeNum, decodeNum)
+import           TxnDiskAPI (TxnD__WriteStatus(..))
 
-data InitOptions = InitOptions
-  { defaultSizeKB :: Int
-  , initDiskPaths :: (FilePath, FilePath) }
+newtype CommonOpts = CommonOpts
+  { diskPaths :: (FilePath, FilePath) }
 
-data Options = Init InitOptions
+newtype InitOptions = InitOptions
+  { defaultSizeKB :: Int }
 
 parseDiskPaths :: Parser (FilePath, FilePath)
-parseDiskPaths = ((,)
-                <$> argument str (metavar "FILE0")
-                <*> argument str (metavar "FILE1"))
-               <|> pure ("disk0.img", "disk1.img")
+parseDiskPaths = liftA2 (,)
+  (option auto ( long "disk0" <> help "filename for first disk" <> showDefault <> value "disk0.img" ))
+  (option auto ( long "disk1" <> help "filename for second disk" <> showDefault <> value "disk1.img" ))
+
+parseCommonOpts :: Parser CommonOpts
+parseCommonOpts = CommonOpts <$> parseDiskPaths
 
 initOptions :: Parser InitOptions
 initOptions = do
@@ -28,18 +33,69 @@ initOptions = do
       <> showDefault
       <> value (100*1024)
       <> metavar "KB" )
-  initDiskPaths <- parseDiskPaths
   pure InitOptions {..}
+
+data WriteOptions = WriteOptions
+  { writeAddr :: Int
+  , writeValue :: Int }
+
+newtype ReadOptions = ReadOptions
+  { readAddr :: Int }
+
+writeOptions :: Parser WriteOptions
+writeOptions = do
+  writeAddr <- argument auto (metavar "ADDR")
+  writeValue <- argument auto (metavar "VALUE")
+  pure WriteOptions {..}
+
+readOptions :: Parser ReadOptions
+readOptions = do
+  readAddr <- argument auto (metavar "ADDr")
+  pure ReadOptions {..}
+
+data CommitOptions = CommitOptions{}
+
+commitOptions :: Parser CommitOptions
+commitOptions = pure CommitOptions
+
+data RecoverOptions = RecoverOptions{}
+
+recoverOptions :: Parser RecoverOptions
+recoverOptions = pure RecoverOptions
+
+data CmdOpts =
+  Init InitOptions
+  | Write WriteOptions
+  | Read ReadOptions
+  | Commit CommitOptions
+  | Recover RecoverOptions
+
+type Options = (CommonOpts, CmdOpts)
 
 diskDefaultMessage :: String
 diskDefaultMessage = "disks default to disk0.img and disk1.img if not provided"
 
 options :: Parser Options
-options = hsubparser
+options = do
+  common <- parseCommonOpts
+  cmd <- hsubparser
           ( command "init" (info (Init <$> initOptions)
                                (progDesc "initialize replicated disks"
                                <> footer diskDefaultMessage))
+            <> command "write" (info
+                                (Write <$> writeOptions)
+                                (progDesc "add to current transaction"))
+            <> command "read" (info
+                                (Read <$> readOptions)
+                                (progDesc "read from committed state"))
+            <> command "commit" (info
+                                (Commit <$> commitOptions)
+                                (progDesc "commit current transaction"))
+            <> command "recover" (info
+                                (Recover <$> recoverOptions)
+                                (progDesc "run recovery"))
           )
+  pure (common, cmd)
 
 main :: IO ()
 main = execParser opts >>= run
@@ -51,12 +107,30 @@ main = execParser opts >>= run
        )
 
 run :: Options -> IO ()
-run (Init opts) = runInit opts
-
-runInit :: InitOptions -> IO ()
-runInit InitOptions
-  { defaultSizeKB=size,
-    initDiskPaths=(fn0, fn1) } = do
+run (CommonOpts{diskPaths=(fn0, fn1)},
+     Init InitOptions{defaultSizeKB=size}) = do
   env <- TD.init fn0 fn1 (fromIntegral size*1024)
   runTD env . TD.interpret $ logInit
   return ()
+run (opts, Write WriteOptions{..}) =
+  let a = fromIntegral writeAddr
+      v = encodeNum writeValue in
+    runCmd opts $ do
+      s <- TD.interpret (logWrite a v)
+      case s of
+        TxnD__WriteOK -> return ()
+        TxnD__WriteErr -> liftIO $ putStrLn "write failed!"
+run (opts, Read ReadOptions{..}) =
+  let a = fromIntegral readAddr in
+    runCmd opts $ do
+      v <- TD.interpret (logRead a)
+      liftIO $ print (decodeNum v)
+run (opts, Commit CommitOptions) =
+  runCmd opts $ TD.interpret logCommit
+run (opts, Recover RecoverOptions) =
+  runCmd opts $ TD.interpret logRecover
+
+runCmd :: CommonOpts -> Proc a -> IO a
+runCmd CommonOpts{diskPaths=(fn0, fn1)} p = do
+  env <- newEnv fn0 fn1
+  runTD env p
--- logging-client/src/Transactions/LogEncoding.hs
+++ logging-client/src/Transactions/LogEncoding.hs
@@ -1,6 +1,8 @@
 module Transactions.LogEncoding
   ( logHdrFmt
   , descriptorFmt
+  , encodeNum
+  , decodeNum
   ) where
 
 import qualified Data.ByteString.Lazy as BSL
@@ -10,23 +12,24 @@ import Data.Binary.Put (runPut, putWord32le)
 import Data.Binary.Get (runGet, getWord32le)
 import Data.Binary
 import Control.Monad (replicateM)
+import Disk (Coq_block)
 
 import LogEncoding
 
 -- output must be exactly blocksize bytes
-toBlock :: Put -> BS.ByteString
+toBlock :: Put -> Coq_block
 toBlock b = let bs = BSL.toStrict (runPut b)
                 padding = BS.replicate (blocksize - BS.length bs) 0 in
                 if BS.length bs > blocksize
                   then error "string is already too long"
                   else BS.append bs padding
 
-encodeLog :: LogHdr -> BS.ByteString
+encodeLog :: LogHdr -> Coq_block
 encodeLog (Build_LogHdr b n) =
   let binary = putWord8 (if b then 1 else 0) >> putWord32le (fromIntegral n) in
       toBlock binary
 
-decodeLog :: BS.ByteString -> LogHdr
+decodeLog :: Coq_block -> LogHdr
 decodeLog bs =
   let fmt = do
         b <- getWord8
@@ -37,15 +40,22 @@ decodeLog bs =
 logHdrFmt :: Coq_block_encoder LogHdr
 logHdrFmt = Build_block_encoder encodeLog decodeLog
 
-encodeDesc :: Descriptor -> BS.ByteString
+encodeDesc :: Descriptor -> Coq_block
 encodeDesc ds =
   let binary = mapM_ (putWord32le . fromIntegral) ds in
     toBlock binary
 
-decodeDesc :: BS.ByteString -> Descriptor
+decodeDesc :: Coq_block -> Descriptor
 decodeDesc bs =
   let fmt = replicateM (fromIntegral coq_LOG_LENGTH) (fromIntegral <$> getWord32le) in
     runGet fmt (BSL.fromStrict bs)
 
 descriptorFmt :: Coq_block_encoder Descriptor
 descriptorFmt = Build_block_encoder encodeDesc decodeDesc
+
+-- utilities for CLI, not specific to logging
+encodeNum :: Int -> Coq_block
+encodeNum = toBlock . putWord32le . fromIntegral
+
+decodeNum :: Coq_block -> Int
+decodeNum  = runGet (fromIntegral <$> getWord32le) . BSL.fromStrict
