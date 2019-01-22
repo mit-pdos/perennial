88d712632a0336743b7a71f32d6e03d3a5246009
Tej Chajed <tchajed@mit.edu>
2019-01-22 18:32:16 -0500

    Start implementing write-ahead log

---
+++ src/Database/WriteAheadLog.v
@@ -0,0 +1,30 @@
+From RecoveryRefinement Require Import Lib.
+From RecoveryRefinement Require Import Spec.SumProc.
+From RecoveryRefinement Require Import Database.Common.
+From RecoveryRefinement Require Import Database.BinaryEncoding.
+From RecoveryRefinement Require Import Database.Filesys.
+From RecoveryRefinement Require Import Database.Log.
+
+Module WAL.
+  Module Cache.
+    Definition t := HashTable ty.uint64 (ty.option ty.ByteString).
+  End Cache.
+
+  Module DbLog.
+    Record t :=
+      { log: Log.t;
+        cache: Cache.t; }.
+  End DbLog.
+
+  Import ProcNotations.
+  Local Open Scope proc.
+
+  Notation proc := (proc (Data.Op ⊕ FS.Op)).
+
+  Definition create (p: string) : proc DbLog.t :=
+    log <- Log.create p;
+      cache <- Call (inject (Data.NewHashTable _ _));
+      Ret {| DbLog.log := log;
+             DbLog.cache := cache; |}.
+
+End WAL.
