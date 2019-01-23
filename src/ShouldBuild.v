2a4b13e0dd146b722e3d70fd8fc0be7d129fb065
Tej Chajed <tchajed@mit.edu>
2019-01-23 14:43:40 -0500

    Add a file with the current CI targets

---
+++ src/ShouldBuild.v
@@ -0,0 +1,4 @@
+(* This file is a temporary provision to build a subset of the code in CI. *)
+(* TODO: go back to building everything *)
+From RecoveryRefinement Require Import Database.Store.
+From RecoveryRefinement Require Import CSL.RefinementAdequacy.
