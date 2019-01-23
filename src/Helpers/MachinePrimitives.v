c22c53c3065481690f65066877d76937a8470a13
Tej Chajed <tchajed@mit.edu>
2019-01-23 15:34:59 -0500

    Provide an explicit model for ByteString
    Rather than defining them axiomatically and then assuming properties, we
    simply use a model and then carefully interact with an API that at
    extraction time can be replaced by an efficient implementation using
    actual Haskell ByteStrings.
    
    The only problem is that the model of numbers as modular arithmetic is
    extremely painful to prove properties about.

--- src/Helpers/MachinePrimitives.v
+++ src/Helpers/MachinePrimitives.v
@@ -3,7 +3,7 @@ Global Unset Auto Template Polymorphism.
 Global Set Implicit Arguments.
 
 From Coq Require Import Omega.
-From Coq Require Import Nat.
+From Coq Require Import Arith.
 From Classes Require Import EqualDec.
 From stdpp Require Import decidable countable.
 
@@ -26,7 +26,6 @@ Section uint.
   Context {bytes_m1:nat}.
   Notation t := (uint bytes_m1).
   Notation mk := (UInt.mk bytes_m1).
-  Notation val := (@UInt.val bytes_m1).
   Notation bound := (UInt.max bytes_m1).
 
   Implicit Types (x y:t).
@@ -36,6 +35,30 @@ Section uint.
     unfold bound.
     apply Nat.pow_nonzero.
     auto.
+  Qed.
+  Hint Resolve bound_not_0 : core.
+
+  Tactic Notation "mkT" uconstr(x) := refine (mk x _).
+
+  Definition toNum x := x.(UInt.val).
+
+  Definition fromNum (n:nat) : t.
+    mkT (n mod bound).
+    apply Nat.mod_upper_bound; auto.
+  Defined.
+
+  Theorem toNum_inj : forall x y, toNum x = toNum y -> x = y.
+  Proof.
+    intros.
+    destruct x as [x Hx], y as [y Hy]; simpl in *.
+    subst; auto.
+    f_equal.
+    apply proof_irrel.
+  Qed.
+
+  Theorem toNum_fromMum : forall n, toNum (fromNum n) = n mod bound.
+  Proof.
+    unfold toNum, fromNum; simpl; auto.
   Qed.
 
   Lemma bound_gt_1 : 1 < bound.
@@ -56,24 +79,17 @@ Section uint.
     lia.
   Qed.
 
-  Hint Resolve bound_not_0 bound_gt_0 : core.
-
-  Tactic Notation "mkT" uconstr(x) := refine (mk x _).
+  Hint Resolve bound_gt_0 : core.
 
   Definition intPlus x y : t.
-    mkT ((x.(val) + y.(val)) mod bound).
+    mkT ((x.(toNum) + y.(toNum)) mod bound).
     apply Nat.mod_upper_bound; auto.
   Defined.
 
   Definition intSub x y : t.
-    mkT (x.(val) - y.(val)).
+    mkT (x.(UInt.val) - y.(UInt.val)).
     pose proof UInt.val_wf x.
     abstract lia.
-  Defined.
-
-  Definition fromNum (n:nat) : t.
-    mkT (n mod bound).
-    apply Nat.mod_upper_bound; auto.
   Defined.
 
   Definition int_val0 : t.
@@ -87,18 +103,7 @@ Section uint.
   Defined.
 
   Definition intCmp x y : comparison :=
-    Nat.compare x.(val) y.(val).
-
-  Definition toNum x : nat := x.(val).
-
-  Theorem toNum_inj : forall x y, toNum x = toNum y -> x = y.
-  Proof.
-    intros.
-    destruct x as [x Hx], y as [y Hy]; simpl in *.
-    subst; auto.
-    f_equal.
-    apply proof_irrel.
-  Qed.
+    Nat.compare x.(toNum) y.(toNum).
 
   Global Instance uint_eq_dec : EqualDec t.
   apply (EqualDec.inj_eq_dec toNum toNum_inj).
@@ -114,10 +119,10 @@ Definition uint8  := UInt.t (1 - 1).
 Definition uint_val4096 : uint64 := fromNum 4096.
 
 Definition uint64_to_uint16 (x:uint64) : uint16 :=
-  fromNum x.(UInt.val).
+  fromNum x.(toNum).
 
 Definition uint64_to_uint32 (x:uint64) : uint32 :=
-  fromNum x.(UInt.val).
+  fromNum x.(toNum).
 
 Definition upcast_bound {bytes_m1_1 bytes_m1_2} :
   forall x, x < UInt.max bytes_m1_1 ->
@@ -132,29 +137,49 @@ Proof.
 Qed.
 
 Definition uint16_to_uint64 (x:uint16) : uint64.
-  refine (UInt.mk _ x.(UInt.val) _).
+  refine (UInt.mk _ x.(toNum) _).
   apply (upcast_bound x.(UInt.val_wf)); abstract (simpl; repeat constructor).
 Defined.
 
 Definition uint32_to_uint64 (x:uint32) : uint64.
-  refine (UInt.mk _ x.(UInt.val) _).
+  refine (UInt.mk _ x.(toNum) _).
   apply (upcast_bound x.(UInt.val_wf)); abstract (simpl; repeat constructor).
 Defined.
 
-Axiom ByteString : Type.
-Axiom eqdec_ByteString : EqualDec ByteString.
-Existing Instance eqdec_ByteString.
+Axiom byte : Type.
+Axiom byte_eqdec : EqualDec byte.
+Existing Instance byte_eqdec.
+
+Record ByteString :=
+  fromByteList { getBytes: list byte }.
+
+Instance ByteString_eq_dec : EqualDec ByteString.
+Proof.
+  hnf; decide equality.
+  apply (equal getBytes0 getBytes1).
+Defined.
 
 Module BS.
-  Axiom append : ByteString -> ByteString -> ByteString.
-  Axiom length : ByteString -> uint64.
-  (* BS.take n bs ++ BS.drop n bs = bs *)
-  Axiom take : uint64 -> ByteString -> ByteString.
-  Axiom drop : uint64 -> ByteString -> ByteString.
-  Axiom empty : ByteString.
+  Implicit Types (bs:ByteString).
+  Local Coercion getBytes : ByteString >-> list.
+  Definition append bs1 bs2 := fromByteList (bs1 ++ bs2).
+  Definition length bs : uint64 := fromNum (List.length bs).
+  Definition take (n:uint64) bs :=
+    fromByteList (List.firstn n.(toNum) bs).
+  Definition drop (n:uint64) bs :=
+    fromByteList (List.skipn n.(toNum) bs).
+  Definition empty : ByteString := fromByteList [].
 
-  (* TODO: probably best derived from other axioms *)
-  Axiom drop_length : forall n bs, BS.length (BS.drop n bs) = intSub (BS.length bs) n.
+  Theorem drop_length : forall n bs, BS.length (BS.drop n bs) = intSub (BS.length bs) n.
+  Proof.
+    destruct n as [n ?], bs as [bs].
+    apply toNum_inj.
+    unfold drop, length, intSub.
+    rewrite toNum_fromMum.
+    cbn [UInt.val getBytes toNum].
+    rewrite toNum_fromMum.
+    rewrite List.skipn_length.
+  Admitted.
 End BS.
 
 Module BSNotations.
