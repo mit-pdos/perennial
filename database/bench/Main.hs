b04c7e221620ab835723542dca1d3ba4a93ec138
Tej Chajed <tchajed@mit.edu>
2019-01-25 16:02:33 -0500

    Clean up the perBatchEnv wrapper
    A generic newtype wrapper is good enough to dispatch the NFData
    requirement (which we don't need, the environment just holds a reference
    to a mutable object and forcing it is meaningless).

--- database/bench/Main.hs
+++ database/bench/Main.hs
@@ -9,24 +9,27 @@ import Criterion.Main
 import Control.DeepSeq (NFData, rnf)
 import Control.Monad (forM_)
 
-newtype HashTableEnv =
-  HashTableEnv { table :: HashTable Word64 ByteString }
+emptyTbl :: IO (HashTable Word64 ByteString)
+emptyTbl = interpret ex1_setup
 
-instance NFData HashTableEnv where
-  rnf _ = ()
-
-emptyTbl :: IO HashTableEnv
-emptyTbl = HashTableEnv <$> interpret ex1_setup
-
-filledTbl :: IO HashTableEnv
+filledTbl :: IO (HashTable Word64 ByteString)
 filledTbl = do
   h <- emptyTbl
   forM_ [1..1000] $ \i ->
-    interpret $ ex1_write1 i "value" (table h)
+    interpret $ ex1_write1 i "value" h
   return h
 
-tableEnv :: IO HashTableEnv -> (HashTable Word64 ByteString -> IO ()) -> Benchmarkable
-tableEnv mkTable b = perBatchEnv (const mkTable) (b . table)
+newtype EnvWrapper a =
+  EnvWrapper { getEnv :: a }
+
+instance NFData (EnvWrapper a) where
+  rnf _ = ()
+
+-- like perBatchEnv but without any constraints on env
+-- (and somewhat simplified types)
+withEnv :: IO env -> (env -> IO ()) -> Benchmarkable
+withEnv mkTable b =
+  perBatchEnv (const (EnvWrapper <$> mkTable)) (b . getEnv)
 
 benchNum :: (Show int, Integral int) =>
             int -> (int -> Benchmarkable) -> Benchmark
@@ -35,26 +38,26 @@ benchNum iters b = bench (show iters) (b iters)
 main :: IO ()
 main = defaultMain [
     bgroup "write" [
-        bench "1" $ tableEnv emptyTbl $ interpret . ex1_write1 1 "value"
-        , bench "3" $ tableEnv emptyTbl $ interpret . ex1_write3 1 "value"
-        , bench "hs3" $ tableEnv emptyTbl $ \h -> do
+        bench "1" $ withEnv emptyTbl $ interpret . ex1_write1 1 "value"
+        , bench "3" $ withEnv emptyTbl $ interpret . ex1_write3 1 "value"
+        , bench "hs3" $ withEnv emptyTbl $ \h -> do
             interpret $ ex1_write1 1 "value" h
             interpret $ ex1_write1 1 "value" h
             interpret $ ex1_write1 1 "value" h
         ]
   , bgroup "read" [
         bgroup "seq" [
-            benchNum 1 $ \iters -> tableEnv filledTbl $
+            benchNum 1 $ \iters -> withEnv filledTbl $
               interpret . ex2_seq_reads 1 iters
-          , benchNum 1000 $ \iters -> tableEnv filledTbl $
+          , benchNum 1000 $ \iters -> withEnv filledTbl $
             interpret . ex2_seq_reads 1 iters
-          , benchNum 10000 $ \iters -> tableEnv filledTbl $
+          , benchNum 10000 $ \iters -> withEnv filledTbl $
             interpret . ex2_seq_reads 1 iters
           ]
       , bgroup "par" [
-            benchNum 500 $ \iters -> tableEnv filledTbl $
+            benchNum 500 $ \iters -> withEnv filledTbl $
               interpret . ex2_par_reads 1 iters
-          , benchNum 5000 $ \iters -> tableEnv filledTbl $
+          , benchNum 5000 $ \iters -> withEnv filledTbl $
             interpret . ex2_par_reads 1 iters
           ]
       ]
