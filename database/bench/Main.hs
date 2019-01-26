49906649a1e2e8503301d0ad92ceb057cd7d113e
Tej Chajed <tchajed@mit.edu>
2019-01-26 16:15:01 -0500

    Re-format benchmark main

--- database/bench/Main.hs
+++ database/bench/Main.hs
@@ -23,37 +23,39 @@ key :: Word64
 key = 10
 
 hashTableWrites :: [Benchmark]
-hashTableWrites = writeSuite [
-        ("1", run . ht_write1 key "value")
-      , ("3", run . ht_write3 key "value")
-      , ("hs3", \h -> do
-            run $ ht_write1 key "value" h
-            run $ ht_write1 (key+1) "value" h
-            run $ ht_write1 (key+2) "value" h
-            return ()
-        )
-      ]
-  where writeSuite = map (\(name, b) -> bench name $ withEnv emptyTbl b)
+hashTableWrites = writeSuite
+  [ ("1", run . ht_write1 key "value")
+  , ("3", run . ht_write3 key "value")
+  , ("hs3", \h -> do
+        run $ ht_write1 key "value" h
+        run $ ht_write1 (key+1) "value" h
+        run $ ht_write1 (key+2) "value" h
+        return ()
+    )
+  ]
+  where
+    writeSuite = map $ \(name, b) ->
+      bench name $ withEnv emptyTbl b
 
 hashTableReads :: [Benchmark]
-hashTableReads = [
-  bgroup "seq" $ readSuite [
-      (1, \iters -> run . ht_seq_reads key iters)
-      , (1000, \iters -> run . ht_seq_reads key iters)
-      , (10000, \iters -> run . ht_seq_reads key iters)
-      ]
-  , bgroup "par" $ readSuite [
-      (500, \iters -> run . ht_par_reads key iters)
-      , (5000, \iters -> run . ht_par_reads key iters)
-      ]
+hashTableReads =
+  [ bgroup "seq" $ readSuite
+    [ (1, \iters -> run . ht_seq_reads key iters)
+    , (1000, \iters -> run . ht_seq_reads key iters)
+    , (10000, \iters -> run . ht_seq_reads key iters)
+    ]
+  , bgroup "par" $ readSuite
+    [ (500, \iters -> run . ht_par_reads key iters)
+    , (5000, \iters -> run . ht_par_reads key iters)
+    ]
   ]
-  where readSuite = map (\(num, b) ->
-                           benchNum num $ \iters ->
-                            withEnv filledTbl (b iters))
+  where
+    readSuite = map $ \(num, b) ->
+      benchNum num $ \iters -> withEnv filledTbl (b iters)
 
 arrayAppends :: [Benchmark]
-arrayAppends = appendSuite [
-  ("1", run . a_append1)
+arrayAppends = appendSuite
+  [ ("1", run . a_append1)
   , ("3", run . a_append3)
   , ("hs3", \r -> do
         run $ a_append1 r
@@ -64,7 +66,9 @@ arrayAppends = appendSuite [
   , ("10000", run . a_append_many 10000)
   , ("hs10000", replicateM_ 10000 . run . a_append1)
   ]
-  where appendSuite = map (\(name, b) -> bench name $ withEnvRun (run a_setup) b)
+  where
+    appendSuite = map $ \(name, b) ->
+      bench name $ withEnvRun (run a_setup) b
 
 prepareArray :: Word64 -> IO (Array Word64)
 prepareArray n = do
@@ -73,39 +77,43 @@ prepareArray n = do
   return r
 
 arrayReads :: [Benchmark]
-arrayReads = [
-  bench "read1" $ withEnvRun (prepareArray 1) $ void . run . a_read
-  , bgroup "seq" $ readSuite [
-      (1, run . a_readall)
-      , (1000, run . a_readall)
-      , (10000, run . a_readall)
-      ]
-  , bgroup "par" $ readSuite [
-      (500, run . a_readall_par)
-      , (5000, run . a_readall_par)
-      ]
+arrayReads =
+  [ bench "read1" $ withEnvRun (prepareArray 1) $ void . run . a_read
+  , bgroup "seq" $ readSuite
+    [ (1, run . a_readall)
+    , (1000, run . a_readall)
+    , (10000, run . a_readall)
     ]
-  where readSuite = map (\(num, b) -> benchNum num $ \iters -> withEnvRun (prepareArray iters) b)
+  , bgroup "par" $ readSuite
+    [ (500, run . a_readall_par)
+    , (5000, run . a_readall_par)
+    ]
+  ]
+  where
+    readSuite = map $ \(num, b) ->
+      benchNum num $ \iters -> withEnvRun (prepareArray iters) b
 
 prepareRef :: IO (IORef ByteString)
 prepareRef = run (ref_setup "string")
 
 iorefBenches :: [Benchmark]
-iorefBenches = [
-  bench "write" $ withEnv prepareRef $ run . ref_write "new string"
-  , bgroup "read" [
-      bench "1" $ withEnv prepareRef $ run . ref_read
-      , bgroup "seq" $ iorefSuite [
-          (1, \iters -> run . ref_read_seq iters)
-          , (100000, \iters -> run . ref_read_seq iters)
+iorefBenches =
+  [ bench "write" $ withEnv prepareRef $ run . ref_write "new string"
+  , bgroup "read"
+    [ bench "1" $ withEnv prepareRef $ run . ref_read
+    , bgroup "seq" $ iorefSuite
+      [ (1, \iters -> run . ref_read_seq iters)
+      , (100000, \iters -> run . ref_read_seq iters)
       ]
-      , bgroup "par" $ iorefSuite [
-          (1, \iters -> run . ref_read_par iters)
-          , (50000, \iters -> run . ref_read_par iters)
-          ]
+    , bgroup "par" $ iorefSuite
+      [ (1, \iters -> run . ref_read_par iters)
+      , (50000, \iters -> run . ref_read_par iters)
       ]
+    ]
   ]
-  where iorefSuite = map (\(num, b) -> benchNum num $ \iters -> withEnv prepareRef (b iters))
+  where
+    iorefSuite = map $ \(num, b) ->
+      benchNum num $ \iters -> withEnv prepareRef (b iters)
 
 filledTbl :: IO (HashTable Word64 ByteString)
 filledTbl = do
@@ -115,14 +123,14 @@ filledTbl = do
   return h
 
 main :: IO ()
-main = defaultMain [
-  bgroup "ioref" iorefBenches
-  , bgroup "hashtable" [
-      bgroup "write" hashTableWrites
-      , bgroup "read" hashTableReads
-      ]
-  , bgroup "array" [
-      bgroup "append" arrayAppends
-      , bgroup "reads" arrayReads
-      ]
+main = defaultMain
+  [ bgroup "ioref" iorefBenches
+  , bgroup "hashtable"
+    [ bgroup "write" hashTableWrites
+    , bgroup "read" hashTableReads
+    ]
+  , bgroup "array"
+    [ bgroup "append" arrayAppends
+    , bgroup "reads" arrayReads
+    ]
   ]
