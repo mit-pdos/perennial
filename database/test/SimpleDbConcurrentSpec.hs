19176cad49a175756c13f54c924944743e5201e5
Tej Chajed <tchajed@mit.edu>
2019-02-04 13:16:27 -0500

    Add more involved concurrency tests

--- database/test/SimpleDbConcurrentSpec.hs
+++ database/test/SimpleDbConcurrentSpec.hs
@@ -1,13 +1,15 @@
 module SimpleDbConcurrentSpec(spec) where
 
-import Prelude hiding (read)
+import Prelude hiding (read, reads)
 
 import Control.Concurrent.Forkable
 import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
-import Control.Monad (forM, forM_, replicateM_, mapM, void)
+import Control.Monad (forM, forM_, replicateM_, mapM, void, when)
 import Control.Monad.IO.Class
-import Test.Hspec
+import Test.Hspec hiding (shouldReturn)
 
+import Filesys.Memory (MemFilesysM)
+import Lib (ByteString)
 import SimpleDbTesting
 
 {-# ANN module ("HLint: ignore Redundant do" :: String) #-}
@@ -23,18 +25,55 @@ runAll acts = do
 runAll_ :: (ForkableMonad m, MonadIO m) => [m a] -> m ()
 runAll_ acts = void $ runAll acts
 
+writes :: DbT -> MemFilesysM ()
+writes db = forM_ [1..1000] $ \k -> write db k "val"
+
+reads :: DbT -> [Maybe ByteString] -> MemFilesysM ()
+reads db allowed = forM_ [1..1000] $ \k -> do
+  v <- read db k
+  liftIO $ v `shouldSatisfy` (`elem` allowed)
+
+compactions :: DbT -> MemFilesysM ()
+compactions = replicateM_ 100 . compact
+
+readsVal :: DbT -> MemFilesysM ()
+readsVal db = reads db [Just "val"]
+
 spec :: Spec
 spec = do
   describe "concurrent operations should not crash" $ do
-    let writeThread db = forM_ [1..100] $ \k -> write db k "val"
-    let readThread db = forM_ [1..100] $ read db
-    let compactThread db = replicateM_ 100 $ compact db
     it "should handle concurrent reads" $ withDb $ \db -> do
-      writeThread db
-      runAll_ $ replicate 10 $ readThread db
+      writes db
+      compact db
+      runAll_ $ replicate 10 $ readsVal db
     it "should handle concurrent writes" $ withDb $ \db -> do
-      runAll_ $ replicate 10 $ writeThread db
+      runAll_ $ replicate 10 $ writes db
     it "should handle mixed reads, writes" $ withDb $ \db -> do
-      runAll_ $ concat $ replicate 10 [readThread db, writeThread db]
+      runAll_ $ concat $ replicate 10 [reads db [Nothing, Just "val"], writes db]
     it "should handle reads, writes, and compactions" $ withDb $ \db -> do
-      runAll_ $ [readThread db, compactThread db] ++ replicate 10 (writeThread db)
+      runAll_ $ [reads db [Nothing, Just "val"], compactions db] ++ replicate 10 (writes db)
+  describe "readers observe writes" $ do
+    it "should ensure readers observe old results" $ withDb $ \db -> do
+      let writer = forM_ [1001..2000] $ \k -> write db k "new val"
+      writes db
+      compact db
+      runAll_ $ replicate 10 (readsVal db) ++ replicate 10 writer
+    it "should ensure compactions do not impact readers" $ withDb $ \db -> do
+      let reader = forM_ [1..100] $ \k -> read db k `shouldReturn` Just "val"
+      writes db
+      runAll_ $ replicate 10 reader ++ replicate 10 (compact db)
+    it "should ensure compactions with writers do not impact readers" $ withDb $ \db -> do
+      let reader = reads db [Just "val", Just "new val"]
+      let writer = forM_ [1..100] $ \k -> write db k "new val"
+      writes db
+      runAll_ $ replicate 10 reader ++ [writer] ++ replicate 10 (compact db)
+  describe "compaction should preserve writes" $ do
+    it "should ensure all writes are persisted" $ withDb $ \db -> do
+      let writer i = forM_ [1..100] $ \k -> do
+            when (k `mod` 10 == 0) $ compact db
+            write db (i*100 + k) "val"
+      runAll_ $ map writer [0..9]
+      compact db
+      compact db
+      forM_ [1..1000] $ \k ->
+        read db k `shouldReturn` Just "val"
