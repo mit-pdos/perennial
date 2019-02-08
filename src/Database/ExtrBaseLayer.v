ce1f2244a3b50a8360a414235831d9c3bd3a97b0
Tej Chajed <tchajed@mit.edu>
2019-02-07 20:48:09 -0500

    Inline injections to clean up Haskell profiles

--- src/Database/ExtrBaseLayer.v
+++ src/Database/ExtrBaseLayer.v
@@ -1,6 +1,7 @@
 From Coq Require Extraction.
 From RecoveryRefinement Require Import Database.DataStructures.
 From RecoveryRefinement Require Import Database.BaseLayer.
+From RecoveryRefinement Require Import Spec.InjectOp.
 
 Extraction Language Haskell.
 
@@ -8,5 +9,9 @@ Extract Constant IORef "a" => "Lib.IORef a".
 Extract Constant Array "a" => "Lib.Array a".
 Extract Constant HashTable "v" => "Lib.HashTable Lib.Word64 v".
 Extract Constant LockRef => "Lib.LockRef".
+
+Extraction Inline inject.
+Extraction Inline data_inj.
+Extraction Inline fs_inj.
 
 (* TODO: extract proc to some monad *)
