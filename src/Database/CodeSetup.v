4f10bc43a1fdc448812354377f0eb0e123179d9e
Tej Chajed <tchajed@mit.edu>
2019-02-10 14:21:33 -0500

    Add a single import to prepare code parsing

---
+++ src/Database/CodeSetup.v
@@ -0,0 +1,12 @@
+From RecoveryRefinement Require Export Database.Base.
+From RecoveryRefinement Require Import Spec.Proc.
+From RecoveryRefinement Require Import Helpers.MachinePrimitives.
+From Classes Require Import EqualDec.
+
+Export ProcNotations.
+Open Scope proc.
+
+Export UIntNotations.
+Open Scope u64.
+
+Export EqualDecNotation.
--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -3,6 +3,7 @@
 From RecoveryRefinement Require Import Database.Store.
 From RecoveryRefinement Require Import Database.Simple.SimpleDb.
 From RecoveryRefinement Require Import Database.Simple.SimpleDbProof.
+From RecoveryRefinement Require Import Database.CodeSetup.
 From RecoveryRefinement Require Import Mail.Impl Mail.API.
 From RecoveryRefinement Require Import CSL.RefinementAdequacy.
 From RecoveryRefinement Require Import CSL.Lifting.
