95ac9cee7cb5d960ad3dd1063ff5bf18fa4e6087
Tej Chajed <tchajed@mit.edu>
2019-02-15 13:56:42 -0500

    Add goose output for goose/examples

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -1,12 +1,21 @@
 (* This file is a temporary provision to build a subset of the code in CI. *)
 (* TODO: go back to building everything *)
+
+(* simple databases *)
 From RecoveryRefinement Require Import Database.Store.
 From RecoveryRefinement Require Import Database.Simple.SimpleDb.
 From RecoveryRefinement Require Import Database.Simple.SimpleDbProof.
-From RecoveryRefinement Require Import Database.CodeSetup.
+
+(* mail server experiment *)
 From RecoveryRefinement Require Import Mail.Impl Mail.API.
+
+(* concurrency framework *)
 From RecoveryRefinement Require Import CSL.RefinementAdequacy.
 From RecoveryRefinement Require Import CSL.Lifting.
 From RecoveryRefinement Require Import Examples.ExMach.WeakestPre.
 From RecoveryRefinement Require Import Examples.ExMach.RefinementAdequacy.
 From RecoveryRefinement Require Import Examples.StatDb.Refinement.
+
+(* goose output *)
+From RecoveryRefinement Require Import Database.Simple.GoSimpleDb.
+From RecoveryRefinement Require Import Examples.GooseUnitTests.
