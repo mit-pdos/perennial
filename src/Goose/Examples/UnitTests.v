d364c8104d39237d3137eb1cd0bb9b0965c8d250
Tej Chajed <tchajed@mit.edu>
2019-03-03 15:46:50 -0500

    Update goose unit tests

--- src/Goose/Examples/UnitTests.v
+++ src/Goose/Examples/UnitTests.v
@@ -16,36 +16,36 @@ End importantStruct.
 Definition doSubtleThings  : proc unit :=
   Ret tt.
 
-Definition TypedLiteral  : proc uint64 :=
+Definition typedLiteral  : proc uint64 :=
   Ret 3.
 
-Definition LiteralCast  : proc uint64 :=
+Definition literalCast  : proc uint64 :=
   let x := 2 in
   Ret (x + 2).
 
-Definition CastInt (p:slice.t byte) : proc uint64 :=
+Definition castInt (p:slice.t byte) : proc uint64 :=
   Ret (slice.length p).
 
-Definition StringToByteSlice (s:string) : proc (slice.t byte) :=
+Definition stringToByteSlice (s:string) : proc (slice.t byte) :=
   p <- Data.stringToBytes s;
   Ret p.
 
-Definition ByteSliceToString (p:slice.t byte) : proc string :=
+Definition byteSliceToString (p:slice.t byte) : proc string :=
   s <- Data.bytesToString p;
   Ret s.
 
-Definition UseSlice  : proc unit :=
+Definition useSlice  : proc unit :=
   s <- Data.newSlice byte 1;
   s1 <- Data.sliceAppendSlice s s;
   FS.atomicCreate "file" s1.
 
-Definition UseSliceIndexing  : proc uint64 :=
+Definition useSliceIndexing  : proc uint64 :=
   s <- Data.newSlice uint64 2;
   _ <- Data.sliceWrite s 1 2;
   x <- Data.sliceRead s 0;
   Ret x.
 
-Definition UseMap  : proc unit :=
+Definition useMap  : proc unit :=
   m <- Data.newMap (slice.t byte);
   _ <- Data.mapAlter m 1 (fun _ => Some (slice.nil _));
   let! (x, ok) <- Data.mapGet m 2;
@@ -53,13 +53,13 @@ Definition UseMap  : proc unit :=
   then Ret tt
   else Data.mapAlter m 3 (fun _ => Some x).
 
-Definition UsePtr  : proc unit :=
+Definition usePtr  : proc unit :=
   p <- Data.newPtr uint64;
   _ <- Data.writePtr p 1;
   x <- Data.readPtr p;
   Data.writePtr p x.
 
-Definition IterMapKeysAndValues (m:Map uint64) : proc uint64 :=
+Definition iterMapKeysAndValues (m:Map uint64) : proc uint64 :=
   sumPtr <- Data.newPtr uint64;
   _ <- Data.mapIter m (fun k v =>
     sum <- Data.readPtr sumPtr;
@@ -67,7 +67,7 @@ Definition IterMapKeysAndValues (m:Map uint64) : proc uint64 :=
   sum <- Data.readPtr sumPtr;
   Ret sum.
 
-Definition IterMapKeys (m:Map uint64) : proc (slice.t uint64) :=
+Definition iterMapKeys (m:Map uint64) : proc (slice.t uint64) :=
   keysSlice <- Data.newSlice uint64 0;
   keysRef <- Data.newPtr (slice.t uint64);
   _ <- Data.writePtr keysRef keysSlice;
@@ -78,14 +78,14 @@ Definition IterMapKeys (m:Map uint64) : proc (slice.t uint64) :=
   keys <- Data.readPtr keysRef;
   Ret keys.
 
-Definition GetRandom  : proc uint64 :=
+Definition getRandom  : proc uint64 :=
   r <- Data.randomUint64;
   Ret r.
 
-Definition Empty  : proc unit :=
+Definition empty  : proc unit :=
   Ret tt.
 
-Definition EmptyReturn  : proc unit :=
+Definition emptyReturn  : proc unit :=
   Ret tt.
 
 Module allTheLiterals.
@@ -112,10 +112,11 @@ Definition oddLiterals  : proc allTheLiterals.t :=
          allTheLiterals.s := "backquote string";
          allTheLiterals.b := false; |}.
 
+(* DoSomething is an impure function *)
 Definition DoSomething (s:string) : proc unit :=
   Ret tt.
 
-Definition ConditionalInLoop  : proc unit :=
+Definition conditionalInLoop  : proc unit :=
   Loop (fun i =>
         _ <- if compare_to i 3 Lt
         then DoSomething ("i is small")
@@ -124,17 +125,18 @@ Definition ConditionalInLoop  : proc unit :=
         then LoopRet tt
         else Continue (i + 1)) 0.
 
-Definition ReturnTwo (p:slice.t byte) : proc (uint64 * uint64) :=
+Definition returnTwo (p:slice.t byte) : proc (uint64 * uint64) :=
   Ret (0, 0).
 
-Definition ReturnTwoWrapper (data:slice.t byte) : proc (uint64 * uint64) :=
-  let! (a, b) <- ReturnTwo data;
+Definition returnTwoWrapper (data:slice.t byte) : proc (uint64 * uint64) :=
+  let! (a, b) <- returnTwo data;
   Ret (a, b).
 
+(* Skip is a placeholder for some impure code *)
 Definition Skip  : proc unit :=
   Ret tt.
 
-Definition SimpleSpawn  : proc unit :=
+Definition simpleSpawn  : proc unit :=
   l <- Data.newLock;
   v <- Data.newPtr uint64;
   _ <- Spawn (_ <- Data.lockAcquire l Reader;
@@ -150,7 +152,7 @@ Definition SimpleSpawn  : proc unit :=
 Definition threadCode (tid:uint64) : proc unit :=
   Ret tt.
 
-Definition LoopSpawn  : proc unit :=
+Definition loopSpawn  : proc unit :=
   _ <- Loop (fun i =>
         if compare_to i 10 Gt
         then LoopRet tt
@@ -158,11 +160,12 @@ Definition LoopSpawn  : proc unit :=
           _ <- Spawn (threadCode i);
           Continue (i + 1)) 0;
   Loop (fun dummy =>
-        Continue dummy) true.
+        Continue (negb dummy)) true.
 
-Definition StringAppend (s:string) (x:uint64) : proc string :=
+Definition stringAppend (s:string) (x:uint64) : proc string :=
   Ret ("prefix " ++ s ++ " " ++ uint64_to_string x).
 
+(* DoSomeLocking uses the entire lock API *)
 Definition DoSomeLocking (l:LockRef) : proc unit :=
   _ <- Data.lockAcquire l Writer;
   _ <- Data.lockRelease l Writer;
@@ -171,6 +174,6 @@ Definition DoSomeLocking (l:LockRef) : proc unit :=
   _ <- Data.lockRelease l Reader;
   Data.lockRelease l Reader.
 
-Definition MakeLock  : proc unit :=
+Definition makeLock  : proc unit :=
   l <- Data.newLock;
   DoSomeLocking l.
