688a976b987eb14d075ba519ace703ae1758ed32
Joseph Tassarotti <jtassaro@andrew.cmu.edu>
2019-03-02 20:19:42 -0500

    Clean up proofs for ptrStore; add ptrDeref. Add pts-to definition for slices and read triple.

--- src/Goose/Proof/Interp.v
+++ src/Goose/Proof/Interp.v
@@ -39,6 +39,16 @@ Instance gooseG_irisG `{gooseG Σ} : irisG GoLayer.Op GoLayer.Go.l Σ :=
     state_interp := goose_interp
   }.
 
+Class GenericMapsTo `{gooseG Σ} (Addr:Type) :=
+  { ValTy : Type;
+    generic_mapsto : Addr -> nat → ValTy -> iProp Σ; }.
+
+Notation "l ↦{ q } v" := (generic_mapsto l q v)
+                      (at level 20) : bi_scope.
+Notation "l ↦ v" := (generic_mapsto l 0 v)
+                      (at level 20) : bi_scope.
+
+
 Definition ptr_mapsto `{gooseG Σ} {T} (l: ptr T) (q: nat) (v: Datatypes.list T) : iProp Σ :=
    mapsto (existT (Ptr.Heap T) l) q (existT (Ptr.Heap T) (Unlocked, v)).
 
@@ -50,19 +60,14 @@ Definition fd_mapsto `{baseG Σ} (fh: Fd) (s:Path * FS.OpenMode) : iProp Σ.
 Admitted.
 *)
 
-Class GenericMapsTo `{gooseG Σ} (Addr:Type) :=
-  { ValTy : Type;
-    generic_mapsto : Addr -> nat → ValTy -> iProp Σ; }.
-
 Instance ptr_gen_mapsto `{gooseG Σ} T : GenericMapsTo (ptr T)
   := {| generic_mapsto := ptr_mapsto; |}.
 
-Notation "l ↦{ q } v" := (generic_mapsto l q v)
-                      (at level 20) : bi_scope.
-Notation "l ↦ v" := (generic_mapsto l 0 v)
-                      (at level 20) : bi_scope.
+Definition slice_mapsto `{gooseG Σ} {T} (l: slice.t T) q (vs: Datatypes.list T) : iProp Σ :=
+  (∃ vs', ⌜ getSliceModel l vs' = Some vs ⌝ ∗ l.(slice.ptr) ↦{q} vs')%I.
 
-
+Instance slice_gen_mapsto `{gooseG Σ} T : GenericMapsTo (slice.t T)
+  := {| generic_mapsto := slice_mapsto; |}.
 
 Import Reg_wp.
 Section lifting.
@@ -92,6 +97,57 @@ Lemma wp_wait s E :
   {{{ ▷ Registered }}} Wait @ s; E {{{ RET tt; AllDone }}}.
 Proof. eapply wp_wait; eauto using thread_reg1, thread_reg2. Qed.
 
+Lemma readSome_Err_inv {A T : Type} (f : A → option T) (s : A) :
+  readSome f s Err → f s = None.
+Proof. rewrite /readSome. destruct (f s); auto; congruence. Qed.
+
+Lemma readSome_Some_inv {A T : Type} (f : A → option T) (s : A) s' t :
+  readSome f s (Val s' t) → s = s' ∧ f s = Some t.
+Proof. rewrite /readSome. destruct (f s); auto; try inversion 1; subst; split; congruence. Qed.
+
+Ltac inj_pair2 :=
+  repeat match goal with
+         | [ H: existT ?x ?o1 = existT ?x ?o2 |- _ ] =>
+           apply Eqdep.EqdepTheory.inj_pair2 in H; subst
+         end.
+
+Ltac trans_elim P :=
+  match goal with
+  |[ H1 : P = ?y, H2 : P = ?z |- _ ] => rewrite H1 in H2
+  end.
+
+Ltac inv_step :=
+  repeat match goal with
+         | [ H : Data.step _ _ Err  |- _ ] =>
+           let Hhd_err := fresh "Hhd_err" in
+           let Hhd := fresh "Hhd" in
+           let Htl_err := fresh "Htl_err" in
+           inversion H as [Hhd_err|(?&?&Hhd&Htl_err)]; clear H
+         | [ H : and_then _ _ _ Err  |- _ ] =>
+           let Hhd_err := fresh "Hhd_err" in
+           let Hhd := fresh "Hhd" in
+           let Htl_err := fresh "Htl_err" in
+           inversion H as [Hhd_err|(?&?&Hhd&Htl_err)]; clear H
+         | [ H : Data.step _ _ (Val _ _)  |- _ ] =>
+           let Hhd := fresh "Hhd" in
+           let Htl := fresh "Htl" in
+           destruct H as (?&?&Hhd&Htl)
+         | [ H : and_then _ _ _ (Val _ _)  |- _ ] =>
+           let Hhd := fresh "Hhd" in
+           let Htl := fresh "Htl" in
+           destruct H as (?&?&Hhd&Htl)
+         | [ H : readSome _ _ Err |- _ ] =>
+           apply readSome_Err_inv in H
+         | [ H : readSome _ _ (Val _ _) |- _ ] =>
+           apply readSome_Some_inv in H as (?&?); subst
+         | [ H : context[getAlloc ?p ?σ] |- _ ] =>
+           unfold getAlloc in H; try erewrite (SemanticsHelpers.getDyn_lookup) in H; eauto; []
+         | [ H : context[let '(s, alloc) := ?x in _] |- _] => destruct x
+         | [ H : lock_acquire _ Unlocked = Some _ |- _ ] => inversion H; subst; clear H
+         | [ H : Some ?x = Some ?x |- _] => clear H
+         | [ H : Some _ = Some _ |- _] => inversion H; subst
+         end.
+
 Lemma wp_ptrStore_start {T} s E (p: ptr T) off l l' v :
   list_nth_upd l off v = Some l' →
   {{{ ▷ p ↦ l }}}
@@ -105,48 +161,20 @@ Proof.
   iDestruct (@gen_heap_valid with "Hσ Hi") as %?.
   simpl in H0.
   iModIntro. iSplit.
-  { destruct s; auto. iPureIntro. intros Hsnd.
-    rewrite /snd_lift in Hsnd.
-    inversion Hsnd. subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H1.
-    subst. inversion H3.
-    rewrite /readSome in H1.
-    rewrite /getAlloc in H1.
-    erewrite SemanticsHelpers.getDyn_lookup in H1; eauto.
-    congruence.
-    destruct H1 as ((?&?)&?&?&?).
-    rewrite /readSome in H1.
-    rewrite /getAlloc in H1.
-    erewrite SemanticsHelpers.getDyn_lookup in H1; eauto.
-    inversion H1. subst.
-    inversion H2.
-    rewrite /readSome in H4.
-    simpl in H4. congruence.
-    destruct H4 as (?&?&?&?).
-    rewrite //= in H4.
+  { destruct s; auto. iPureIntro.
+    apply snd_lift_non_err => Herr.
+    inversion Herr; inj_pair2; subst.
+    inv_step; try congruence.
+    rewrite //= in Hhd_err.
   }
   iIntros (e2 (n', σ2) Hstep) "!>".
   inversion Hstep; subst.
   inversion H1. subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H2.
-    subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H7.
-    subst.
-  destruct H6 as (?&?&?&?).
-  rewrite /readSome in H2. destruct x.
-  destruct H3 as (?&?&?&?).
-  rewrite /getAlloc in H2.
-    erewrite SemanticsHelpers.getDyn_lookup in H2; eauto.
-    inversion H2; subst.
-    inversion H4. subst.
-    simpl in *.
-    destruct Hstep. rewrite /goose_interp. iFrame.
-    rewrite /heap_interp//=.
-  inversion H3. subst.
-
-  unshelve (iMod (@gen_heap_update with "Hσ Hi") as "[$ Hi]").
-  iModIntro.
-  iApply "HΦ"; eauto.
+  inj_pair2.
+  inv_step.
+  inversion Htl0; subst.
+  unshelve (iMod (@gen_heap_update with "Hσ Hi") as "[$ Hi]"); simpl.
+  iFrame. iApply "HΦ"; eauto.
 Qed.
 
 Lemma wp_ptrStore {T} s E (p: ptr T) off l l' v :
@@ -160,63 +188,91 @@ Proof.
   iApply wp_lift_call_step.
   iIntros ((n, σ)) "(?&Hσ)".
   iDestruct (@gen_heap_valid with "Hσ Hi") as %?.
-  simpl in H0.
   iModIntro. iSplit.
-  { destruct s; auto. iPureIntro. intros Hsnd.
-    rewrite /snd_lift in Hsnd.
-    inversion Hsnd. subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H1.
-    subst. inversion H3.
-    rewrite /readSome in H1.
-    rewrite /getAlloc in H1.
-    erewrite SemanticsHelpers.getDyn_lookup in H1; eauto.
-    congruence.
-    destruct H1 as ((?&?)&?&?&?).
-    rewrite /readSome in H1.
-    rewrite /getAlloc in H1.
-    erewrite SemanticsHelpers.getDyn_lookup in H1; eauto.
-    inversion H1. subst.
-    inversion H2.
-    rewrite /readSome in H4.
-    simpl in H4. congruence.
-    destruct H4 as (?&?&?&?).
-    rewrite //= in H4.
-    rewrite /readSome in H4. inversion H4; subst.
-    inversion H5.
-    rewrite /readSome in H6.
-    rewrite Hupd in H6. congruence.
-    destruct H6 as (?&?&?&?).
-    rewrite /readSome in H6.
-    rewrite Hupd in H6. inversion H6. subst.
-    inversion H7.
+  { destruct s; auto. iPureIntro.
+    apply snd_lift_non_err => Herr.
+    inversion Herr; inj_pair2; subst.
+    inv_step; try congruence.
+    rewrite //= in Hhd_err.
   }
   iIntros (e2 (n', σ2) Hstep) "!>".
   inversion Hstep; subst.
   inversion H1. subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H2.
-    subst.
-    apply Eqdep.EqdepTheory.inj_pair2 in H7.
-    subst.
-  destruct H6 as (?&?&?&?).
-  rewrite /readSome in H2. destruct x.
-  destruct H3 as (?&?&?&?).
-  rewrite /getAlloc in H2.
-    erewrite SemanticsHelpers.getDyn_lookup in H2; eauto.
-    inversion H2; subst.
-    inversion H4. subst.
-    simpl in *.
-    destruct Hstep. rewrite /goose_interp. iFrame.
-    rewrite /heap_interp//=.
-  inversion H3. subst.
-  destruct H5 as (?&?&?).
-  rewrite /readSome in H5. rewrite Hupd in H5. inversion H5. subst.
-  rewrite /updAllocs in H8. inversion H8. subst. simpl in *.
+  inj_pair2.
+  inv_step.
+  inversion Htl; subst; simpl in *.
   unshelve (iMod (@gen_heap_update with "Hσ Hi") as "[$ Hi]").
-  iModIntro. by iApply "HΦ".
+  iModIntro. iFrame.
+  trans_elim (list_nth_upd l1 off v). inv_step.
+    by iApply "HΦ".
 Qed.
 
 Lemma wp_writePtr {T} s E (p: ptr T) v' l v :
   {{{ ▷ p ↦ (v' :: l) }}} writePtr p v @ s; E {{{ RET tt; p ↦ (v :: l) }}}.
 Proof. iIntros (Φ) ">Hi HΦ". iApply (wp_ptrStore with "Hi"); eauto. Qed.
+
+Lemma wp_ptrDeref {T} s E (p: ptr T) off l v :
+  List.nth_error l off = Some v →
+  {{{ ▷ p ↦ l }}} ptrDeref p off @ s; E {{{ RET v; p ↦ l }}}.
+Proof.
+  intros Hupd.
+  iIntros (Φ) ">Hi HΦ". rewrite /ptrDeref.
+  iApply wp_lift_call_step.
+  iIntros ((n, σ)) "(?&Hσ)".
+  iDestruct (@gen_heap_valid with "Hσ Hi") as %?.
+  simpl in H0.
+  iModIntro. iSplit.
+  { destruct s; auto. iPureIntro.
+    apply snd_lift_non_err => Herr.
+    inversion Herr; inj_pair2; subst.
+    inv_step; try congruence.
+    rewrite //= in Hhd_err.
+  }
+  iIntros (e2 (n', σ2) Hstep) "!>".
+  inversion Hstep; subst.
+  inversion H1. subst.
+  inj_pair2.
+  inv_step.
+  inversion Htl; subst.
+  iFrame.
+  trans_elim (nth_error l1 off). inv_step.
+    by iApply "HΦ".
+Qed.
+
+Lemma wp_readPtr {T} s E (p: ptr T) v l :
+  {{{ ▷ p ↦ (v :: l) }}} readPtr p @ s; E {{{ RET v; p ↦ (v :: l) }}}.
+Proof. iIntros (Φ) ">Hi HΦ". iApply (wp_ptrDeref with "Hi"); eauto. Qed.
+
+Lemma nth_error_lookup {A :Type} (l: Datatypes.list A) (i: nat) :
+  nth_error l i = l !! i.
+Proof.
+  revert l.
+  induction i => l.
+  * destruct l; auto.
+  * move: IHi. simpl. destruct l => //=.
+Qed.
+
+Lemma wp_sliceRead {T} s E (p: slice.t T) off l v :
+  List.nth_error l off = Some v →
+  {{{ ▷ p ↦ l }}} sliceRead p off @ s; E {{{ RET v; p ↦ l }}}.
+Proof.
+  iIntros (Hnth Φ) ">Hp HΦ".
+  iDestruct "Hp" as (vs Heq) "Hp".
+  rewrite /getSliceModel/sublist_lookup/mguard/option_guard in Heq.
+  destruct (decide_rel); last by congruence.
+  inversion Heq. subst.
+  iApply (wp_ptrDeref with "Hp").
+  rewrite nth_error_lookup in Hnth *.
+  assert (Hlen: off < length (take p.(slice.length) (drop p.(slice.offset) vs))).
+  { eapply lookup_lt_is_Some_1. eauto. }
+  rewrite lookup_take in Hnth. rewrite lookup_drop in Hnth.
+  rewrite nth_error_lookup. eauto.
+  { rewrite take_length in Hlen. lia. }
+  iIntros "!> ?".
+  iApply "HΦ". iExists _; iFrame.
+  rewrite /getSliceModel/sublist_lookup/mguard/option_guard.
+  destruct (decide_rel); last by congruence.
+  eauto.
+Qed.
 
 End lifting.
