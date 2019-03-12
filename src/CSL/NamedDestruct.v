61b9cea0fd11ac7acc8cbaea83e0849f9744946f
Joseph Tassarotti <jtassaro@andrew.cmu.edu>
2019-03-11 20:32:38 -0400

    Experimenting with tactics to more quickly frame a named list of hypotheses.

--- src/CSL/NamedDestruct.v
+++ src/CSL/NamedDestruct.v
@@ -22,6 +22,9 @@ Definition named_eq {PROP : bi} : @named PROP = @named_def PROP := (@named_aux P
 Global Instance named_timeless {PROP: sbi} i (P: PROP) : Timeless P → Timeless (named i P).
 Proof. rewrite named_eq /named_def//=. Qed.
 
+Global Instance named_absorbing {PROP: bi} i (P: PROP) : Absorbing P → Absorbing (named i P).
+Proof. rewrite named_eq /named_def//=. Qed.
+
 Global Instance named_frame_here :
   ∀ (PROP : bi) i (p : bool) (R : PROP), Frame p (named i R) R emp | 100.
 Proof. intros ???. rewrite named_eq /named_def //=//=. apply _. Qed.
@@ -115,6 +118,26 @@ Arguments IntoSepEnv {_} _%I _%I : simpl never.
 Arguments into_sep_env {_} _%I _%I {_}.
 Hint Mode IntoSepEnv + ! - : typeclass_instances.
 
+Class FrameSepEnv {PROP : bi} (Δ Δ' : envs PROP) (P Q : PROP) :=
+  frame_env : ∃ R, (of_envs Δ ⊢ (R ∗ of_envs Δ')) ∧ (R -∗ Q -∗ P).
+Arguments FrameSepEnv {_} _%I _%I _%I _%I.
+Arguments frame_env {_} _%I _%I _%I _%I {_}.
+Hint Mode FrameSepEnv + + - + - : typeclass_instances.
+
+(* Above we pass in the context and cancel as we go, but then the context
+   re-appears repeatedly, and you can't abstract it because of an evar. Maybe we
+   should instead generate a term with holes in it, along with a list of
+   names/props, and then check each name to see if it is in the context or not
+   with the appropriate form, and then fill in the holes based on whether we
+   could cancel?  *)
+
+(*
+Class FrameSepEnv {PROP : bi} (Rs : prop_list PROP) (P Q : PROP) := frame_env : [∗] (pl2p Rs) ∗ Q ⊢ P.
+Arguments FrameSepEnv {_} _%I _%I _%I.
+Arguments frame_env {_} _%I _%I _%I {_}.
+Hint Mode FrameSepEnv + - ! + : typeclass_instances.
+*)
+
 Definition envs_set_counter {PROP} (Δ : envs PROP) (p: positive) : envs PROP :=
   Envs (env_intuitionistic Δ) (env_spatial Δ) p.
 Lemma envs_set_counter_equiv {PROP} (Δ : envs PROP) p : envs_Forall2 (⊣⊢) Δ (envs_set_counter Δ p).
@@ -145,6 +168,133 @@ Proof. rewrite /IntoSepEnv//=. iIntros (H1) "(?&?)". iFrame. by iApply H1. Qed.
 Global Instance sep_into_sep_single_anon (P: PROP) :
   IntoSepEnv P [(LAnon, P)] | 100.
 Proof. rewrite /IntoSepEnv//=. iFrame. eauto. Qed.
+
+Implicit Types P Q R : PROP.
+
+Global Instance frame_env_here_absorbing Δ Δ' i (R: PROP) :
+  Absorbing R →
+  envs_lookup_delete false i Δ = Some (false, R, Δ') →
+  FrameSepEnv Δ Δ' (named i R) True | 0.
+Proof.
+  rewrite /FrameSepEnv => ? Hsound.
+  exists R; split.
+  * rewrite envs_lookup_delete_sound //=; simpl; auto.
+  * iIntros "??". rewrite named_eq. iFrame.
+Qed.
+Global Instance frame_env_here Δ Δ' i (R: PROP) :
+  envs_lookup_delete false i Δ = Some (false, R, Δ') →
+  FrameSepEnv Δ Δ' (named i R) emp | 1.
+Proof.
+  rewrite /FrameSepEnv => Hsound.
+  exists R; split.
+  * rewrite envs_lookup_delete_sound //=; simpl; auto.
+  * iIntros "??". rewrite named_eq. iFrame.
+Qed.
+
+Global Instance frame_env_sep Δ Δ' Δ'' P1 P2 Q1 Q2 :
+  FrameSepEnv Δ Δ' P1 Q1 →
+  FrameSepEnv Δ' Δ'' P2 Q2 →
+  FrameSepEnv Δ Δ'' (P1 ∗ P2) (Q1 ∗ Q2).
+Proof.
+  rewrite /FrameSepEnv.
+  intros (R1&HR1&HR1').
+  intros (R2&HR2&HR2').
+  exists (R1 ∗ R2)%I.
+  split.
+  - rewrite HR1 HR2. by rewrite assoc.
+  - rewrite HR1' HR2'. iIntros "(Hw1&Hw2) (H1&H2)".
+    iSplitL "Hw1 H1".
+    * by iApply "Hw1".
+    * by iApply "Hw2".
+Qed.
+
+Global Instance frame_env_sep_miss Δ P:
+  FrameSepEnv Δ Δ P P | 100.
+Proof.
+  rewrite /FrameSepEnv. exists emp%I. split; eauto.
+Qed.
+
+Lemma tac_frame_env Δ Δ' P Q :
+  FrameSepEnv Δ Δ' P Q →
+  envs_entails Δ' Q →
+  envs_entails Δ P.
+Proof.
+  rewrite /FrameSepEnv.
+  intros (R&HR&HR').
+  rewrite envs_entails_eq HR HR' => ->.
+  apply wand_elim_l.
+Qed.
+
+(*
+Global Instance frame_env_sep Δ Δ' P1 Q1 Q2 :
+  FrameSepEnv Δ Δ' P1 Q1 →
+  FrameSepEnv Δ Δ' (P1 ∗ Q2) (Q1 ∗ Q2).
+Proof.
+  rewrite /FrameSepEnv => H.
+  destruct H as (R&H1&H2).
+  exists R. split; auto.
+  rewrite H2. iIntros "Hwand (?&?)".
+  iFrame. by iApply "Hwand".
+Qed.
+*)
+
+(*
+Global Instance frame_env_here_absorbing Δ Δ' i (R: PROP) :
+  Absorbing R →
+  envs_lookup_delete false i Δ = Some (false, R, Δ') →
+  FrameSepEnv Δ Δ' (named i R) True | 0.
+Proof.
+  intros. rewrite /FrameSepEnv.
+  rewrite envs_lookup_delete_sound //=; simpl.
+  rewrite -assoc sep_elim_l named_eq //=.
+Qed.
+Global Instance frame_env_here Δ Δ' i (R: PROP) :
+  envs_lookup_delete false i Δ = Some (false, R, Δ') →
+  FrameSepEnv Δ Δ' (named i R) emp | 1.
+Proof.
+  intros. rewrite /FrameSepEnv.
+  rewrite envs_lookup_delete_sound //=; simpl.
+  rewrite -assoc wand_elim_r right_id named_eq //=.
+Qed.
+
+Global Instance frame_env_sep Δ Δ' P1 Q1 Q2 :
+  FrameSepEnv Δ Δ' P1 Q1 →
+  FrameSepEnv Δ Δ' (P1 ∗ Q2) (Q1 ∗ Q2).
+Proof.
+  rewrite /FrameSepEnv. iIntros (H1) "H".
+  iDestruct "H" as "(HΔ&Hwand)".
+  assert (
+  iPoseProof (H1 with "[-]") as "HP1".
+  iFrame. iIntros "HΔ'".
+
+*)
+
+(*
+Global Instance frame_env_sep Δ Δ' Δ'' P1 P2 Q1 Q2 :
+  FrameSepEnv Δ Δ' P1 Q1 →
+  FrameSepEnv Δ' Δ'' P2 Q2 →
+  FrameSepEnv Δ Δ'' (P1 ∗ P2) (Q1 ∗ Q2).
+Proof.
+  rewrite /FrameSepEnv. iIntros (H1 H2) "H".
+  iDestruct "H" as "(HΔ&Hwand)".
+  iPoseProof (H1 with "[-]") as "HP1".
+  iFrame. iIntros "HΔ'".
+*)
+
+(*
+Global Instance frame_env_here_absorbing i (R: PROP) :
+  Absorbing R → FrameSepEnv [(LNamed i, R)] (named i R) True | 0.
+Proof. intros. by rewrite /FrameSepEnv//= sep_elim_l right_id. Qed.
+Global Instance frame_env_here i R : FrameSepEnv [(LNamed i, R)] (named i R) emp | 1.
+Proof. intros. by rewrite /FrameSepEnv//= sep_elim_l right_id. Qed.
+Global Instance frame_env_sep Rs1 Rs2 P1 P2 Q1 Q2 :
+  FrameSepEnv Rs1 P1 Q1 →
+  FrameSepEnv Rs2 P2 Q2 →
+  FrameSepEnv (Rs1 ++ Rs2) (P1 ∗ P2) (Q1 ∗ Q2).
+Proof. rewrite /FrameSepEnv//=. /MakeSep => <- <-. by rewrite assoc. Qed.
+Global Instance frame_env_miss R : FrameSepEnv [] R R | 100.
+Proof. intros. by rewrite /FrameSepEnv//= left_id. Qed.
+*)
 
 Lemma tac_sep_list_destruct Δ Δ' i Γ bump (P: PROP) (Ps: prop_list PROP) Q :
   envs_lookup i Δ = Some (false, P) →
@@ -190,49 +340,88 @@ Ltac iLIntro :=
 
 Section test.
 Context {PROP: bi}.
-Context {P P' : PROP}.
+Context {P P' : nat → PROP}.
 
-Context (HPP': P ⊢ P').
+Context (HPP': ∀ n, P n ⊢ P' n).
 
-Definition test_list := pl2p [ (LNamed "Hfoo", P); (LAnon, P')  ].
-Fixpoint bigterm_list (P: PROP) n :=
+Definition test_list := pl2p [ (LNamed "Hfoo", P O); (LAnon, P' O)  ].
+Fixpoint bigterm_list (P: nat → PROP) s n :=
   match n with
   | O => []
-  | S n' => (LAnon, P) :: bigterm_list P n'
+  | S n' => (LNamed s, P (S n')) :: bigterm_list P (String "a" s) n'
   end.
 
-Lemma HPP_destruct_named : True ∗ [∗] test_list ∗ True ⊢ P -∗ P -∗ P -∗ P -∗ [∗] test_list.
+Lemma HPP_destruct_named : True ∗ [∗] test_list ∗ True ⊢ P O -∗ P O -∗ P O -∗ P O -∗ [∗] test_list.
 Proof.
   iIntros "(?&H0&H1)".
   iLDestruct "H0".
 Abort.
 
-Lemma HPP_intro_named : [∗] test_list ⊢ P -∗ P -∗ P -∗ P -∗ [∗] test_list.
+Lemma HPP_intro_named : [∗] test_list ⊢ P O -∗ P O -∗ P O -∗ P O -∗ [∗] test_list.
 Proof.
   iLIntro.
 Abort.
 
-Lemma Hbig : [∗] (pl2p (bigterm_list P 20)) ⊢ True.
+Lemma Hbig : [∗] (pl2p (bigterm_list P "a" 20)) ⊢ True.
 Proof.
   iLIntro.
   auto.
 Qed.
 
+
 (*
-Lemma Hbig_slow : [∗] (pl2p (bigterm_list P 20)) ⊢ True.
+Lemma Hbig_slow : [∗] (pl2p (bigterm_list P "a" 20)) ⊢ True.
 Proof.
   simpl.
   iDestructIntro.
   auto.
-Qed.
+Time Qed.
 *)
 
-Lemma Hbig_combined : [∗] (pl2p (bigterm_list P 20)) -∗ [∗] (pl2p (bigterm_list P 20)) -∗ True.
+Lemma Hbig_combined : [∗] (pl2p (bigterm_list P "a" 20)) -∗ [∗] (pl2p (bigterm_list P "b" 20)) -∗ True.
+Proof.
   iLIntro.
   iLIntro.
   auto.
 Qed.
+Hint Extern 10 (envs_lookup_delete _ _ _ = Some _) => (compute; reflexivity) : typeclass_instances.
 
+(*
+Lemma Hbig_frame : [∗] (pl2p (bigterm_list P "a" 20)) ⊢ [∗] (pl2p (bigterm_list P "a" 20)).
+Proof.
+  simpl. iLIntro.
+  (*
+  iNamed (iFrame).
+  Time Qed.
+   *)
+  (*
+  evar (Δ : envs PROP).
+  match goal with
+  | [ |- envs_entails ?Γ _ ] => assert ((envs_lookup_delete false "a" Γ = Some (false, P 20, Δ)))
+  end.
+   *)
+  Print Esnoc.
+  match goal with
+  | [ |- context [(Esnoc _ (IAnon ?x) _)]] => idtac x
+  end.
+  eapply tac_frame_env.
+  apply _.
+  abstract(
+  assert (
+      emp -∗
+  (emp : PROP)
+  ∗ emp
+    ∗ emp
+      ∗ emp
+        ∗ emp
+          ∗ emp
+          ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp ∗ emp);
+  abstract (rewrite ?left_id //=);
+  by iApply H).
+Time Qed.
+*)
+
+(*
 Lemma HPP_named i : named i P ⊢ named i P'.
 Proof using HPP'. by unbundle_names. Qed.
 
@@ -248,4 +437,7 @@ Lemma test2 Q: (named "HP" P) ∗ (named "HQ" Q) ⊢ P ∗ Q.
 Proof using HPP'.
   iDestructIntro. iFrame "HP". iFrame "HQ".
 Qed.
+*)
 End test.
+
+Hint Extern 10 (envs_lookup_delete _ _ _ = Some _) => (compute; reflexivity) : typeclass_instances.
