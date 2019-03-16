f879e2572376e9ac41d9aa4b61c5a4a08617ec9c
Sydney <gibsons@mit.edu>
2019-03-16 17:11:06 -0400

    attempt to get a better refl wrapper

--- src/Goose/Interpret.v
+++ src/Goose/Interpret.v
@@ -16,18 +16,17 @@ Fixpoint rtermDenote A B T (r: rterm A B T) : relation A B T :=
   | AndThen r1 f => and_then (rtermDenote r1) (fun x => (rtermDenote (f x)))
   end.
 
-Definition ex : rterm nat nat nat := AndThen (Pure nat 3) (fun n => Reads (fun x => x+n)).
-Definition denote_ex : relation nat nat nat := and_then (pure 3) (fun x : nat => reads (fun x0 : nat => x0 + x)).
-Eval cbv [rtermDenote ex] in (rtermDenote ex).
-
 Ltac refl' e :=
   match eval simpl in e with
   | fun x : ?T => @pure ?A _ (@?E x) =>
     constr:(fun x => Pure A (E x))
 
-  | fun x : ?T => @reads ?A ?T0 (@?f x) =>
-    constr: (fun x => Reads A T0 (f x))
+  | fun x : ?T => @reads ?A ?T0 (fun (y: ?A) => (@?f x y)) =>
+    constr: (fun x => Reads (f x))
               
+  | fun x : ?T => @readSome ?A ?T0 (fun (y: ?A) => (@?f x y)) =>
+    constr: (fun x => ReadSome (f x))
+
   | fun x: ?T => @and_then ?A ?B ?C ?T1 ?T2 (@?r1 x) (fun (y: ?T1) => (@?r2 x y)) =>
     let f1 := refl' r1 in
     let f2 := refl' (fun (p: T * T1) => (r2 (fst p) (snd p))) in
@@ -35,27 +34,32 @@ Ltac refl' e :=
               
   end.
 
-Ltac refl1 := let t := refl' constr:(fun _: unit => pure (A:=unit) 1) in idtac t.
-Ltac refl2 := let t := refl' constr:(fun _: unit => reads (A:=nat) (fun x : nat => x + 3)) in idtac t.
-Ltac refl3 := let t := refl' constr:(fun _: unit => and_then (pure 3) (fun x : nat => reads (A:=nat) (T:=nat) (fun x0 : nat => x0 + x))) in idtac t.
-Ltac refl4 := let t := refl' constr:(fun _: unit => and_then (pure 3) (fun _: nat => pure (A:=nat) 1)) in idtac t.
+Ltac refl e :=
+  let t := refl' constr:(fun _ : unit => e) in idtac t.
 
-Goal (pure nat 1) = (pure nat 2). 
-  refl2.
+(*                                                       
+Ltac refl e :=
+  let t := refl' constr:(fun _ : unit => e) in
+  eval cbv [rtermDenote t] in (rtermDenote (t tt)).
+ *)
+
+Ltac reflPure := refl (pure (A:=unit) 1).
+Ltac reflReads := refl (reads (A:=nat) (fun x : nat => x + 3)).
+Ltac reflReadSome := refl (and_then (pure 3) (fun x : nat => readSome (A:=nat) (T:=nat) (fun x0 : nat => Some (x0 + x)))).
+Ltac reflAndThen := refl (and_then (pure 3) (fun _: nat => pure (A:=nat) 1)).
+
+Goal False.
+  reflPure.
+  reflReads.
+  reflReadSome.
+  reflAndThen.
 Abort.
+
+Definition ex : rterm nat nat nat := AndThen (Pure nat 3) (fun n => ReadSome (fun x => Some (x+n))).
+Eval cbv [rtermDenote ex] in (rtermDenote ex).
+
+Definition refl2_out := (fun x : unit => Reads ((fun (_ : unit) (x0 : nat) => x0 + 3) x)). 
+Eval cbv [rtermDenote refl2_out] in (rtermDenote (refl2_out tt)).
 
 Definition refl4_out := (fun x : unit => AndThen ((fun H : unit => Pure nat ((fun _ : unit => 3) H)) x) (fun y : nat => (fun p : unit * nat => Pure nat ((fun _ : unit * nat => 1) p)) (x, y))).
 Eval cbv [rtermDenote refl4_out] in (rtermDenote (refl4_out tt)).
-
-(*
-Ltac refl :=
-  match goal with
-    | [ |- ?E1 = ?E2 ] =>
-      let E1' := refl' (fun _ : unit => E1) in
-      let E2' := refl' (fun _ : unit => E2) in
-      change (rtermDenote (E1' tt) = rtermDenote (E2' tt))
-  end.
-*)
-
-Eval cbv [rtermDenote ex] in (rtermDenote ex).
-Compute (rtermDenote ex).
