54e01958395bd631621a513592911cbd7c9ed314
Tej Chajed <tchajed@mit.edu>
2019-04-19 10:16:30 -0400

    Simplify ShouldBuild.v

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -1,24 +1,13 @@
-(* concurrency framework *)
-From RecoveryRefinement Require Import CSL.RefinementAdequacy.
-From RecoveryRefinement Require Import CSL.Lifting.
-From RecoveryRefinement Require Import CSL.Count_Heap.
-From RecoveryRefinement Require Import CSL.BigDynOp.
-
 (* examples on top of concurrency framework *)
-From RecoveryRefinement Require Import Examples.ExMach.WeakestPre.
-From RecoveryRefinement Require Import Examples.ExMach.RefinementAdequacy.
 From RecoveryRefinement Require Import Examples.StatDb.Refinement.
 From RecoveryRefinement Require Import Examples.AtomicPair.RefinementShadow.
 From RecoveryRefinement Require Import Examples.AtomicPair.RefinementLog.
 From RecoveryRefinement Require Import Examples.Logging.LogRefinement.
-From RecoveryRefinement Require Import Examples.MailServer.MailAPI.
+
+(* mailboat proof *)
 From RecoveryRefinement Require Import Examples.MailServer.MailRefinement.
 
 (* goose output *)
 From RecoveryRefinement Require Import Goose.Examples.UnitTests.
 From RecoveryRefinement Require Import Goose.Examples.SimpleDb.
 From RecoveryRefinement Require Import Goose.Examples.MailServer.
-
-(* goose proof rules *)
-From RecoveryRefinement Require Import Goose.Proof.Interp.
-From RecoveryRefinement Require Import Goose.Proof.RefinementAdequacy.
