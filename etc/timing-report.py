a4551052d058a588c6032de21bc1dadcc40058d5
Tej Chajed <tchajed@mit.edu>
2019-04-26 17:05:27 -0400

    Add timing report to summarize results
    Current results (my laptop, parallel build):
    
    total        2837.0
     Iris         296.3
     stdpp        110.2
     vendor        11.4
    Qed total    1008.0
    
    slow files:
                                                     fname        time  qed_time
    111           src/Examples/MailServer/MailRefinement.v  577.970405   450.716
    112              src/Examples/MailServer/MailTriples.v  287.023604    82.263
    108               src/Examples/Logging/LogRefinement.v  280.732961    98.786
    100            src/Examples/AtomicPair/RefinementLog.v  213.011746    73.951
    115  src/Examples/ReplicatedDisk/ReplicatedDiskRefi...  174.571014    54.582
    124                           src/Goose/Proof/Interp.v  156.998441    39.955
    125               src/Goose/Proof/RefinementAdequacy.v  105.951106    39.307
    101         src/Examples/AtomicPair/RefinementShadow.v   90.147974    28.513
    110                 src/Examples/MailServer/MailHeap.v   55.141519    17.987
    109            src/Examples/MailServer/MailAPILemmas.v   52.072117    12.094
    
    slow QEDs:
                                                      fname                        ident     time
    6055           src/Examples/MailServer/MailRefinement.v    mail_crash_refinement_seq  313.866
    6054           src/Examples/MailServer/MailRefinement.v              recovery_triple  136.494
    5997            src/Examples/AtomicPair/RefinementLog.v  exmach_crash_refinement_seq   52.530
    6008  src/Examples/ReplicatedDisk/ReplicatedDiskRefi...  exmach_crash_refinement_seq   40.406
    6006               src/Goose/Proof/RefinementAdequacy.v  exmach_crash_refinement_seq   36.383
    6039               src/Examples/Logging/LogRefinement.v  exmach_crash_refinement_seq   36.176
    6049              src/Examples/MailServer/MailTriples.v            pickup_refinement   35.094
    5993               src/Examples/Logging/LogRefinement.v            commit_refinement   22.586
    6009                 src/Examples/MailServer/MailHeap.v           data_op_refinement   17.332
    6040              src/Examples/MailServer/MailTriples.v              open_refinement   14.154

---
+++ etc/timing-report.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+import argparse
+import sqlite3
+import pandas
+
+
+def read_db(fname):
+    conn = sqlite3.connect(fname)
+    conn.row_factory = sqlite3.Row
+    qed_timings = []
+    for row in conn.execute("select fname, ident, time from qed_timings"):
+        qed_timings.append((row["fname"], row["ident"], row["time"]))
+
+    file_timings = []
+    for row in conn.execute(
+        """SELECT file_timings.fname as fname, time, qed_time FROM """
+        + """(file_timings INNER JOIN """
+        + """(SELECT fname, sum(time) AS qed_time FROM qed_timings group by fname) file_qeds """
+        + """on file_timings.fname = file_qeds.fname)"""
+    ):
+        file_timings.append((row["fname"], row["time"], row["qed_time"]))
+
+    conn.close()
+
+    qed_df = pandas.DataFrame(qed_timings, columns=["fname", "ident", "time"])
+    file_df = pandas.DataFrame(file_timings, columns=["fname", "time", "qed_time"])
+    return qed_df, file_df
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("db", help="sqlite database of timing info")
+
+    args = parser.parse_args()
+
+    qed_df, file_df = read_db(args.db)
+    print("{:12s} {:>6.1f}".format("total", file_df["time"].sum()))
+    print(
+        "{:12s} {:>6.1f}".format(
+            " Iris",
+            file_df[file_df["fname"].str.contains("^external/iris-coq")]["time"].sum(),
+        )
+    )
+    print(
+        "{:12s} {:>6.1f}".format(
+            " stdpp",
+            file_df[file_df["fname"].str.contains("^external/stdpp")]["time"].sum(),
+        )
+    )
+    print(
+        "{:12s} {:>6.1f}".format(
+            " vendor", file_df[file_df["fname"].str.contains("^vendor/")]["time"].sum()
+        )
+    )
+    print("{:12s} {:>6.1f}".format("Qed total", file_df["qed_time"].sum()))
+
+    print()
+    print("slow files:")
+    print(file_df.sort_values("time", ascending=False)[:10])
+
+    print()
+    print("slow QEDs:")
+    print(qed_df.sort_values("time", ascending=False)[:10])
