64c24c60d40a7670065be4ed815e696d860c2aee
Tej Chajed <tchajed@mit.edu>
2019-05-01 08:19:18 -0400

    Clean up code in timing-report.py

--- etc/timing-report.py
+++ etc/timing-report.py
@@ -2,9 +2,9 @@
 
 from __future__ import print_function
 
-import argparse
 import sqlite3
 import pandas as pd
+
 
 def read_db(fname):
     conn = sqlite3.connect(fname)
@@ -14,9 +14,7 @@ def read_db(fname):
         qed_timings.append((row["fname"], row["ident"], row["time"]))
 
     file_timings = []
-    for row in conn.execute(
-        """SELECT fname, time FROM file_timings"""
-    ):
+    for row in conn.execute("""SELECT fname, time FROM file_timings"""):
         file_timings.append((row["fname"], row["time"]))
     conn.close()
 
@@ -24,45 +22,50 @@ def read_db(fname):
     qed_sum = qed_df.groupby("fname").sum()
     raw_file_df = pd.DataFrame(file_timings, columns=["fname", "time"])
     file_df = raw_file_df.join(qed_sum, rsuffix="_qed", on="fname")
-    file_df.rename(mapper={"time_qed": "qed_time"}, axis='columns', inplace=True)
+    file_df.rename(mapper={"time_qed": "qed_time"}, axis="columns", inplace=True)
     return qed_df, file_df
 
 
+def filter_df(df, col=None, pat=None):
+    return df[df[col].str.contains(pat)]
+
+
 if __name__ == "__main__":
+    import argparse
+
     parser = argparse.ArgumentParser()
 
+    parser.add_argument(
+        "--max", type=int, default=10, help="number of slowest files and proofs to show"
+    )
     parser.add_argument("db", help="sqlite database of timing info")
 
     args = parser.parse_args()
 
     qed_df, file_df = read_db(args.db)
-    pd.set_option('display.max_rows', 100)
-    pd.set_option('display.max_columns', 10)
-    pd.set_option('display.width', 300)
+    pd.set_option("display.max_rows", 100)
+    pd.set_option("display.max_columns", 10)
+    pd.set_option("display.width", 300)
     print("{:12s} {:>6.1f}".format("total", file_df["time"].sum()))
     print(
         "{:12s} {:>6.1f}".format(
             " Iris",
-            file_df[file_df["fname"].str.contains("^external/iris-coq")]["time"].sum(),
+            filter_df(file_df, col="fname", pat="^external/iris-coq")["time"].sum(),
         )
     )
     print(
         "{:12s} {:>6.1f}".format(
             " stdpp",
-            file_df[file_df["fname"].str.contains("^external/stdpp")]["time"].sum(),
-        )
-    )
-    print(
-        "{:12s} {:>6.1f}".format(
-            " vendor", file_df[file_df["fname"].str.contains("^vendor/")]["time"].sum()
+            filter_df(file_df, col="fname", pat="^external/stdpp")["time"].sum(),
         )
     )
     print("{:12s} {:>6.1f}".format("Qed total", file_df["qed_time"].sum()))
 
-    print()
-    print("slow files:")
-    print(file_df.sort_values("time", ascending=False)[:10])
+    if args.max > 0:
+        print()
+        print("slow files:")
+        print(file_df.nlargest(args.max, "time"))
 
-    print()
-    print("slow QEDs:")
-    print(qed_df.sort_values("time", ascending=False)[:10])
+        print()
+        print("slow QEDs:")
+        print(qed_df.nlargest(args.max, "time"))
