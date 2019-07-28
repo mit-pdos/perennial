39b09ebf0e0f9459504dc48c1eb369c213a2a2cc
Tej Chajed <tchajed@mit.edu>
2019-07-28 16:12:56 -0400

    Remove broken part of VM setup script
    Installing opam is annoyingly difficult in Ubuntu 19.04; need this
    workaround:
    
    ```
    sudo apt install mccs
    opam init --solver=mccs
    ```

--- artifact/vm/scripts/setup.sh
+++ artifact/vm/scripts/setup.sh
@@ -9,9 +9,3 @@ sudo apt-get install -y opam cloc sqlite3 zip
 sudo apt-get install -y virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-utils vim firefox
 # CoqIDE dependencies
 sudo apt-get install -y pkg-config libgtksourceview2.0-dev
-
-# install Coq
-opam init --auto-setup
-eval $(opam config env)
-opam repo add coq-released https://coq.inria.fr/opam/released/
-opam install -j2 -y coq.8.9.1 coqide
