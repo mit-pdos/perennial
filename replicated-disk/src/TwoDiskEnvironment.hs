3ae33c7916f550bfcfaafc10d33c9f52b924d31d
Tej Chajed <tchajed@mit.edu>
2019-08-30 12:08:24 -0400

    Add tests for the two disk operations

--- replicated-disk/src/TwoDiskEnvironment.hs
+++ replicated-disk/src/TwoDiskEnvironment.hs
@@ -3,6 +3,8 @@
 module TwoDiskEnvironment
   ( Env
   , newEnv
+  , closeEnv
+  , deleteEnv
   , Proc
   , runProc
   , forkProc
@@ -51,18 +53,38 @@ data Env =
 
 type Proc = ReaderT Env IO
 
-newDisk :: FilePath -> IO CachedHandle
-newDisk path = do
-  fd <- openFd path ReadWrite Nothing defaultFileFlags
-  setFdSize fd (8 * fromIntegral numVars)
-  return (path, Just fd)
+openDisk :: FilePath -> IO CachedHandle
+openDisk path = do
+  exists <- doesFileExist path
+  if exists
+    then do
+      fd <- openFd path ReadWrite Nothing defaultFileFlags
+      return (path, Just fd)
+    else return (path, Nothing)
 
 newEnv :: FilePath -> IO Env
 newEnv path = do
   vars <- replicateM 1000 (newMVar 0)
-  d0   <- newDisk (path ++ ".0")
-  d1   <- newDisk (path ++ ".1")
+  let fn0 = path ++ ".0"
+      fn1 = path ++ ".1"
+  exists0 <- doesFileExist fn0
+  exists1 <- doesFileExist fn1
+  when (not exists0 && not exists1) $ forM_ [fn0, fn1] $ \p -> do
+    fd <- createFile p 0o700
+    setFdSize fd (8 * fromIntegral numVars)
+  d0 <- openDisk fn0
+  d1 <- openDisk fn1
   return $ Env vars d0 d1
+
+closeEnv :: Env -> IO ()
+closeEnv env = forM_ [disk0Handle env, disk1Handle env] $ \disk -> do
+  fd <- getFd disk
+  mapM_ closeFd fd
+
+deleteEnv :: Env -> IO ()
+deleteEnv env = forM_ [disk0Handle env, disk1Handle env] $ \(p, _) -> do
+  exists <- doesFileExist p
+  when exists (removeLink p)
 
 runProc :: Env -> Proc a -> IO a
 runProc env p = runReaderT p env
@@ -96,7 +118,7 @@ readDiskRange :: TwoDisk__Coq_diskId -> Int -> Int -> Proc (Maybe ByteString)
 readDiskRange did off len = do
   mfd <- getDisk did
   case mfd of
-    Just fd -> lift $ Just <$> fdPread fd (fromIntegral off) (fromIntegral len)
+    Just fd -> lift $ Just <$> fdPread fd (fromIntegral len) (fromIntegral off)
     Nothing -> return Nothing
 
 writeDiskRange :: TwoDisk__Coq_diskId -> Int -> ByteString -> Proc ()
@@ -146,7 +168,7 @@ readDisk did i = if not (0 <= i && i < numVars)
     case mbs of
       Just bs -> case decodeValue bs of
         Just v  -> return (Just v)
-        Nothing -> error "bug in decoding integer"
+        Nothing -> error $ "bug in decoding integer " ++ show bs
       Nothing -> return Nothing
 
 writeDisk :: TwoDisk__Coq_diskId -> Int -> Value -> Proc ()
--- replicated-disk/test/TwoDiskEnvironmentSpec.hs
+++ replicated-disk/test/TwoDiskEnvironmentSpec.hs
@@ -1,13 +1,33 @@
 module TwoDiskEnvironmentSpec where
 
 import           TwoDiskEnvironment
+import           TwoDiskAPI                     ( TwoDisk__Coq_diskId(..) )
 import           Test.Hspec
+import           Control.Exception
+import           Control.Monad.Trans
+import           System.Directory
 
 {-# ANN module "HLint: ignore Redundant do" #-}
 
 shouldEncodeDecode
   :: (Show a, Eq a) => (a -> b) -> (b -> Maybe a) -> a -> Expectation
 shouldEncodeDecode enc dec x = dec (enc x) `shouldBe` Just x
+
+shouldProduce :: (Show a, Eq a) => Proc a -> a -> Proc ()
+shouldProduce comp x = do
+  actual <- comp
+  liftIO $ actual `shouldBe` x
+
+withEnv :: Proc a -> IO a
+withEnv act = bracket (newEnv "/tmp/disk")
+                      (\env -> closeEnv env >> deleteEnv env)
+                      (\env -> runProc env act)
+
+d0 :: TwoDisk__Coq_diskId
+d0 = TwoDisk__Coq_d0
+
+d1 :: TwoDisk__Coq_diskId
+d1 = TwoDisk__Coq_d1
 
 spec :: Spec
 spec = do
@@ -24,3 +44,47 @@ spec = do
         , maxBound
         , maxBound - 1
         ]
+  describe "two disk environment" $ do
+    it "can be setup and closed" $ withEnv $ do
+      return ()
+    it "can handle reads and writes" $ withEnv $ do
+      readMem 0 `shouldProduce` 0
+      writeMem 0 32
+      writeMem 1 7
+      readMem 0 `shouldProduce` 32
+      writeMem 0 3
+      readMem 0 `shouldProduce` 3
+      readMem 1 `shouldProduce` 7
+    it "can compare and swap" $ withEnv $ do
+      writeMem 0 100
+      casMem 0 0 1 `shouldProduce` 100
+      readMem 0 `shouldProduce` 100
+      casMem 0 100 1 `shouldProduce` 100
+      readMem 0 `shouldProduce` 1
+    it "can handle disk reads and writes" $ withEnv $ do
+      readDisk d0 0 `shouldProduce` Just 0
+      writeDisk d0 0 32
+      writeDisk d1 0 132
+      writeDisk d0 1 7
+      writeDisk d1 1 107
+      readDisk d0 0 `shouldProduce` Just 32
+      writeDisk d0 0 3
+      readDisk d0 0 `shouldProduce` Just 3
+      readDisk d1 0 `shouldProduce` Just 132
+      readDisk d0 1 `shouldProduce` Just 7
+      readDisk d1 1 `shouldProduce` Just 107
+    it "can write to 999" $ withEnv $ do
+      writeMem 999 100
+      readMem 999 `shouldProduce` 100
+      writeDisk d0 999 100
+      writeDisk d1 999 100
+      readDisk d0 999 `shouldProduce` Just 100
+      readDisk d1 999 `shouldProduce` Just 100
+    it "can handle disk failures" $ withEnv $ do
+      writeDisk d0 0 32
+      writeDisk d1 0 132
+      readDisk d0 0 `shouldProduce` Just 32
+      readDisk d1 0 `shouldProduce` Just 132
+      liftIO $ removeFile "/tmp/disk.0"
+      readDisk d0 0 `shouldProduce` Nothing
+      readDisk d1 0 `shouldProduce` Just 132
