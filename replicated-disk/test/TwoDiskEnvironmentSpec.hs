d77ea6cb37430611d5400174db4c07b8c01d845f
Tej Chajed <tchajed@mit.edu>
2019-08-30 10:36:13 -0400

    Fix intero setup

--- replicated-disk/test/TwoDiskEnvironmentSpec.hs
+++ replicated-disk/test/TwoDiskEnvironmentSpec.hs
@@ -1,27 +1,26 @@
--- |
-
 module TwoDiskEnvironmentSpec where
 
 import           TwoDiskEnvironment
 import           Test.Hspec
 
-{-# ANN module "HLint: ingore Redundant do" #-}
+{-# ANN module "HLint: ignore Redundant do" #-}
 
-should_encode_decode
+shouldEncodeDecode
   :: (Show a, Eq a) => (a -> b) -> (b -> Maybe a) -> a -> Expectation
-should_encode_decode enc dec x = dec (enc x) `shouldBe` Just x
+shouldEncodeDecode enc dec x = dec (enc x) `shouldBe` Just x
 
 spec :: Spec
 spec = do
   describe "value encoder" $ do
     it "encode >>> decode = id" $ do
-      mapM_ (should_encode_decode encodeValue decodeValue)
-        $ [ minBound
-          , minBound + 1
-          , 3
-          , 1231
-          , 138741923804719470
-          , 2 ^ 63
-          , maxBound
-          , maxBound - 1
-          ]
+      mapM_
+        (shouldEncodeDecode encodeValue decodeValue)
+        [ minBound
+        , minBound + 1
+        , 3
+        , 1231
+        , 138741923804719470
+        , 2 ^ 63
+        , maxBound
+        , maxBound - 1
+        ]
