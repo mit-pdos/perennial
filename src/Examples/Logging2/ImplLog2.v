76a38acfd7127f918dcf594722d86651c6ffbc4c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-10-03 18:31:35 -0400

    implementation for group commit in Logging2

--- src/Examples/Logging2/ImplLog2.v
+++ src/Examples/Logging2/ImplLog2.v
@@ -7,9 +7,16 @@ Definition log_start := 1.
 
 (* Memory addresses *)
 Definition log_lock := 0.
+Definition mem_lock := 1.
+Definition mem_count_in_memory := 2.
+Definition mem_count_on_disk := 3.
+Definition mem_start := 4.
 
 Definition log_data (pos : nat) :=
   log_start + pos.
+
+Definition mem_data (pos : nat) :=
+  mem_start + pos.
 
 Fixpoint write_blocks (l : list nat) (pos : nat) :=
   (
@@ -22,17 +29,68 @@ Fixpoint write_blocks (l : list nat) (pos : nat) :=
     end
   )%proc.
 
-Definition append (l : list nat) :=
+Fixpoint write_mem_blocks (l : list nat) (pos : nat) :=
+  (
+    match l with
+    | nil =>
+      Ret tt
+    | b :: l' =>
+      _ <- write_mem (mem_data pos) b;
+      write_mem_blocks l' (pos+1)
+    end
+  )%proc.
+
+Fixpoint read_mem_blocks (count : nat) (pos : nat) (res : list nat) :=
+  (
+    match count with
+    | 0 => Ret res
+    | S count' =>
+      b <- read_mem (mem_data pos);
+      read_mem_blocks count' (pos+1) (res ++ [b])
+    end
+  )%proc.
+
+Definition disk_append :=
   ( _ <- lock log_lock;
-    len <- read_disk log_commit;
-    if gt_dec (len + length l) log_size then
+
+    _ <- lock mem_lock;
+    memlen <- read_mem mem_count_in_memory;
+    l <- read_mem_blocks memlen 0 nil;
+    _ <- write_mem mem_count_in_memory 0;
+
+    disklen <- read_mem mem_count_on_disk;
+    _ <- write_mem mem_count_on_disk (disklen+memlen);
+    _ <- unlock mem_lock;
+
+    _ <- write_blocks l disklen;
+    _ <- write_disk log_commit (disklen+memlen);
+
+    _ <- unlock log_lock;
+    Ret tt
+  )%proc.
+
+Definition mem_append (l : list nat) :=
+  ( _ <- lock mem_lock;
+    memlen <- read_mem mem_count_in_memory;
+    disklen <- read_mem mem_count_on_disk;
+    if gt_dec (disklen + memlen + length l) log_size then
       _ <- unlock log_lock;
       Ret false
     else
-      _ <- write_blocks l len;
-      _ <- write_disk log_commit (len+length l);
-      _ <- unlock log_lock;
+      _ <- write_mem_blocks l memlen;
+      _ <- write_mem mem_count_in_memory (memlen + length l);
+      _ <- unlock mem_lock;
       Ret true
+  )%proc.
+
+Definition append (l : list nat) :=
+  (
+    ok <- mem_append l;
+    if ok then
+      _ <- disk_append;
+      Ret true
+    else
+      Ret false
   )%proc.
 
 Fixpoint read_blocks (count : nat) (pos : nat) (res : list nat) :=
