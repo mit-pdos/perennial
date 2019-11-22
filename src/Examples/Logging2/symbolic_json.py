5c40fe7c5ea22cd03dca22766a592ad9d77f8abd
Nickolai Zeldovich <nickolai@csail.mit.edu>
2019-11-22 14:24:36 -0500

    checkpoint more python symbolic spec evaluator

--- src/Examples/Logging2/symbolic_json.py
+++ src/Examples/Logging2/symbolic_json.py
@@ -47,6 +47,7 @@ class Module(object):
       return mod._execute(dbody, namespace)
 
     if name in mod.constructors:
+      print "Looking up constructor", name
       c = mod.constructors[name]
       t = mod.z3_sort(c['typename'])
       idx = self._constructor_by_name(t, name)
@@ -55,13 +56,29 @@ class Module(object):
 
     print "execute(): unknown name", name, "in module", mod.modname
 
+  def proc_expr(self, name, args, state):
+    mod, name = self.context.scope_name(name, self)
+    if name in mod.decls:
+      d = mod.decls[name]
+
+      namespace = {}
+      dvalue = d['value']
+
+      for (idx, argname) in enumerate(dvalue['argnames']):
+        namespace[argname] = args[idx]
+
+      dbody = dvalue['body']
+      return mod._proc_expr(dbody, namespace, state)
+
+    print "proc_expr(): unknown name", name, "in module", mod.modname
+
   def _type_var_lookup(self, typeargnames, x):
     if x['what'] == 'type:var':
       return typeargnames[x['name']]
     return x
 
   def _z3_sort(self, json_type, typeargs = [], typeargnames = {}):
-    print "Looking up type %s (%s, %s): %s" % (json_type['name'], typeargs, typeargnames, json_type)
+    # print "Looking up type %s (%s, %s): %s" % (json_type['name'], typeargs, typeargnames, json_type)
 
     ## Hard-coded Z3 correspondence for some types
     if json_type['name'] == 'nat':
@@ -92,10 +109,9 @@ class Module(object):
 
     datatype = z3.Datatype(str(json_type['name']))
     typeargnames = {}
-    if 'argnames' in json_type:
-      for i in range(0, len(json_type['argnames'])):
-        typeargname = json_type['argnames'][i]
-        typeargnames[typeargname] = typeargs[i]
+    for i in range(0, len(typeargs)):
+      typeargname = json_type['argnames'][i]
+      typeargnames[typeargname] = typeargs[i]
     for c in json_type['constructors']:
       cname = str(c['name'])
       datatype.declare(cname, *[(cname + '_%d' % idx, self._z3_sort(argtype, [], typeargnames))
@@ -113,6 +129,11 @@ class Module(object):
     return None
 
   def _execute(self, expr, namespace):
+    print "NAMESPACE: f =", namespace.get('f', None)
+
+    while expr['what'] == 'expr:coerce':
+      expr = expr['value']
+
     if expr['what'] == 'expr:case':
       victim = self._execute(expr['expr'], namespace)
       symExpr = None
@@ -142,8 +163,129 @@ class Module(object):
 
       args = [self._execute(a, namespace) for a in expr['args']]
       return self.execute(expr['name'], args)
+    elif expr['what'] == 'expr:apply':
+      ## Chase down the func
+      f = expr['func']
+      print "APPLY", f, "TO", expr['args'], "IN NAMESPACE", namespace
+      while True:
+        if f['what'] == 'expr:global':
+          if f['name'] == 'lookup0':
+            break
+          mod, name = self.context.scope_name(f['name'], self)
+          f = mod.decls[name]
+        elif f['what'] == 'expr:coerce':
+          f = f['value']
+        elif f['what'] == 'decl:term':
+          f = f['value']
+        elif f['what'] == 'expr:rel':
+          f = namespace[f['name']]
+        else:
+          break
+
+      posargs = []
+      for i in range(len(expr['args'])):
+        a = expr['args'][i]
+        while True:
+          if type(a) != dict:
+            break
+          if a['what'] == 'expr:rel':
+            a = namespace[a['name']]
+          elif a['what'] == 'expr:coerce':
+            a = a['value']
+          else:
+            break
+        posargs.append(a)
+
+      if f['what'] == 'expr:global':
+        if f['name'] == 'lookup0':
+          m = self._execute(posargs[2], namespace)
+          k = posargs[1]
+          return m[k]
+
+      assert(f['what'] == 'expr:lambda')
+      apply_namespace = {k: v for k, v in namespace.items()}
+      assert(len(expr['args']) == len(f['argnames']))
+      for i in range(len(posargs)):
+        apply_namespace[f['argnames'][i]] = posargs[i]
+
+      return self._execute(f['body'], apply_namespace)
+    else:
+      print "_execute unmatched: %s" % expr['what']
 
     print "_execute(%s) returning None" % str(expr)
+    return None
+
+  def _proc_expr(self, expr, namespace, state):
+    print "PROC_EXPR: %s" % expr
+    print "NAMESPACE: f =", namespace.get('f', None)
+
+    while expr['what'] == 'expr:coerce':
+      expr = expr['value']
+
+    if expr['what'] == 'expr:apply':
+      ## Chase down the func
+      f = expr['func']
+      while True:
+        if f['what'] == 'expr:global':
+          mod, name = self.context.scope_name(f['name'], self)
+          f = mod.decls[name]
+        elif f['what'] == 'expr:coerce':
+          f = f['value']
+        elif f['what'] == 'decl:term':
+          f = f['value']
+        else:
+          break
+
+      assert(f['what'] == 'expr:lambda')
+
+      print "NAMESPACE: f =", namespace.get('f', None)
+      apply_namespace = {k: v for k, v in namespace.items()}
+      print "APPLY_NAMESPACE: f =", apply_namespace.get('f', None)
+      print "LAMBDA:", f
+      print "LAMBDA ARGNAMES:", f['argnames']
+      assert(len(expr['args']) == len(f['argnames']))
+
+      for i in range(len(expr['args'])):
+        a = expr['args'][i]
+        while True:
+          if type(a) != dict:
+            break
+          if a['what'] == 'expr:rel':
+            a = namespace[a['name']]
+          elif a['what'] == 'expr:coerce':
+            a = a['value']
+          else:
+            break
+        apply_namespace[f['argnames'][i]] = a
+
+      print "APPLY_NAMESPACE: f =", apply_namespace.get('f', None)
+      return self._proc_expr(f['body'], apply_namespace, state)
+    elif expr['what'] == 'expr:constructor':
+      if expr['name'] == 'Bind':
+        p1 = expr['args'][0]
+        p2 = expr['args'][1]
+        state1, res1 = self._proc_expr(p1, namespace, state)
+        p2apply = {
+          'what': 'expr:apply',
+          'func': p2,
+          'args': [res1],
+        }
+        print "BIND PART 1 STATE:", state1
+        print "BIND PART 1 RES:", res1
+        return self._proc_expr(p2apply, namespace, state1)
+      elif expr['name'] == 'Ret':
+        res = self._execute(expr['args'][0], namespace)
+        return state, res
+      elif expr['name'] == 'Call':
+        argname = expr['args'][0]['name']
+        if argname == 'Reads':
+          return state, state
+        else:
+          print "Unsupported call op %s" % argname
+      else:
+        print "Unsupported constructor %s" % expr['name']
+
+    print "_proc_expr(%s) returning None" % expr['what']
     return None
 
 class SymbolicJSON(object):
@@ -171,6 +313,7 @@ class SymbolicJSON(object):
 if __name__ == "__main__":
   test = SymbolicJSON()
   test.load('NFS3API.json')
-  print test.modules['Main'].z3_sort('state')
   s = z3.Const('s', test.modules['Main'].z3_sort('state'))
-  # print test.modules['Main'].execute('getFst_sem', [p])
+  fh = z3.Const('fh', test.modules['Main'].z3_sort('fh'))
+
+  print test.modules['Main'].proc_expr('getattr_step', [fh], s)
