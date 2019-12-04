54a7f5ae026443ee738f9b6e708d3657f0a10b40
Tej Chajed <tchajed@mit.edu>
2019-12-03 20:26:34 -0500

    Stop compiling shallow embedding code by default
    This makes builds significantly faster.
    
    You can still compile everything with `make all`.

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -3,30 +3,8 @@ From Perennial Require Import program_logic.crash_weakestpre.
 From Perennial Require Import program_logic.recovery_adequacy.
 From Perennial Require Import program_logic.crash_inv.
 
-
-(* examples on top of concurrency framework *)
-From Perennial Require Import Examples.StatDbDataRefinement.Impl.
-From Perennial Require Import Examples.StatDb.Refinement.
-From Perennial Require Import Examples.AtomicPair.RefinementShadow.
-From Perennial Require Import Examples.AtomicPair.RefinementLog.
-(* From Perennial Require Import Examples.Logging.LogRefinement. *)
-(* From Perennial Require Import Examples.Logging2.RefinementLog2. *)
-From Perennial Require Import Examples.ReplicatedDisk.ReplicatedDiskRefinement.
-
-(* mailboat proof *)
-From Perennial Require Import Examples.MailServer.MailRefinement.
-
-(* work-in-progress on goose *)
-From Perennial Require Import Goose.TypeSystem.
-
-(* work-in-progress on Go deep embedding based on heap_lang *)
 From Perennial.go_lang Require Import
      adequacy total_adequacy lib.spin_lock.
 (* goose deep output *)
 From Perennial.go_lang.examples Require Import
      goose_unittest append_log wal mailserver simpledb logging2 rfc1813.
-
-(* goose output *)
-From Perennial.Goose Require Import ExplicitModel.
-From Perennial.Goose.Examples Require Import
-     UnitTests SimpleDb MailServer WAL.
