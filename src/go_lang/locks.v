86732b02f9d74032613553bbba4adf34dedfdd7b
Tej Chajed <tchajed@mit.edu>
2020-01-16 10:10:42 -0500

    Fix cond var model to apply to *sync.Cond

--- src/go_lang/locks.v
+++ src/go_lang/locks.v
@@ -3,6 +3,7 @@ From Perennial.go_lang Require Import lifting.
 From iris.program_logic Require Import weakestpre.
 From Perennial.go_lang Require Import proofmode.
 From Perennial.go_lang Require Import typing.
+From Perennial.go_lang Require Import basic_triples.
 Import uPred.
 
 Definition lockRefT {ext} {ext_ty: ext_types ext} := refT boolT.
@@ -18,61 +19,71 @@ Module lock.
   Definition acquire := spin_lock.acquire.
   Definition release := spin_lock.release.
 
-  (* a cond var is modeled with the underlying lock *)
-  Definition newCond: val := λ: "l", "l".
+  (* a cond var (a Go *sync.Cond) is modeled as a pointer to the underlying
+  lock *)
+  Definition newCond: val := λ: "l", ref "l".
   (* no-op in the model, only affects scheduling *)
   Definition condSignal: val := λ: "l", #().
   Definition condBroadcast: val := λ: "l", #().
-  Definition condWait: val := λ: "l", release "l";;
+  Definition condWait: val := λ: "l", release !"l";;
                                       (* actual cond var waits for a signal
                                       here, but the model does not take this
                                       into account *)
-                                      acquire "l".
+                                      acquire !"l".
+
+  Definition is_cond N γ (c: loc) R: iProp Σ :=
+    ∃ lk, c ↦ Free lk ∗ is_lock N γ lk R.
 
   Theorem wp_newCond N γ lk R :
     {{{ is_lock N γ lk R }}}
       newCond lk
-      {{{ RET lk; is_lock N γ lk R }}}.
+      {{{ c, RET #c; is_cond N γ c R }}}.
   Proof.
     iIntros (Φ) "Hl HΦ".
     wp_call.
+    wp_alloc c as "Hc".
     iApply "HΦ".
-    iFrame.
+    iExists _; iFrame.
   Qed.
 
-  Theorem wp_condSignal N γ lk R :
-    {{{ is_lock N γ lk R }}}
-      condSignal lk
-      {{{ RET #(); is_lock N γ lk R }}}.
+  Theorem wp_condSignal N γ c R :
+    {{{ is_cond N γ c R }}}
+      condSignal #c
+      {{{ RET #(); is_cond N γ c R }}}.
   Proof.
     iIntros (Φ) "Hc HΦ".
     wp_call.
     iApply ("HΦ" with "[$]").
   Qed.
 
-  Theorem wp_condBroadcast N γ lk R :
-    {{{ is_lock N γ lk R }}}
-      condBroadcast lk
-      {{{ RET #(); is_lock N γ lk R }}}.
+  Theorem wp_condBroadcast N γ c R :
+    {{{ is_cond N γ c R }}}
+      condBroadcast #c
+      {{{ RET #(); is_cond N γ c R }}}.
   Proof.
     iIntros (Φ) "Hc HΦ".
     wp_call.
     iApply ("HΦ" with "[$]").
   Qed.
 
-  Theorem wp_condWait N γ lk R :
-    {{{ is_lock N γ lk R ∗ spin_lock.locked γ ∗ R }}}
-      condWait lk
-      {{{ RET #(); is_lock N γ lk R ∗ spin_lock.locked γ ∗ R }}}.
+  Theorem wp_condWait N γ c R :
+    {{{ is_cond N γ c R ∗ spin_lock.locked γ ∗ R }}}
+      condWait #c
+      {{{ RET #(); is_cond N γ c R ∗ spin_lock.locked γ ∗ R }}}.
   Proof.
-    iIntros (Φ) "(#Hc&Hlocked&HR) HΦ".
+    iIntros (Φ) "(Hc&Hlocked&HR) HΦ".
+    iDestruct "Hc" as (lk) "(Hcptr&#Hc)".
     wp_call.
+    wp_load.
     wp_apply (release_spec with "[$Hc $Hlocked $HR]").
     iIntros "_".
     wp_pures.
+    wp_load.
     wp_apply (acquire_spec with "[$Hc]").
     iIntros "(Hlocked&HR)".
-    iApply ("HΦ" with "[$]").
+    iApply "HΦ".
+    iSplitR "Hlocked HR"; last by iFrame.
+    iExists lk; iFrame "#∗".
   Qed.
 
   End go_lang.
