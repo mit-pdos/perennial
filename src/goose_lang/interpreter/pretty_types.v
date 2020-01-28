3b72d52fb5d120b504c114f32f4259723ceb5328
Tej Chajed <tchajed@mit.edu>
2020-01-28 13:51:27 -0500

    Print literals more compactly in interpreter

--- src/goose_lang/interpreter/pretty_types.v
+++ src/goose_lang/interpreter/pretty_types.v
@@ -16,27 +16,29 @@ Instance pretty_u32 : Pretty Integers.u32 :=
 Instance pretty_loc : Pretty loc :=
   fun x => pretty x.(loc_car).
 
+Definition single_quote: string := String (Ascii.ascii_of_N 34%N) EmptyString.
+Definition quoted (s:string) : string := (single_quote ++ s ++ single_quote)%string.
+
 Instance pretty_lit : Pretty base_lit :=
   fun x => match x with
-        | LitInt n => ("LitInt(" ++ pretty n ++ ")")%string
-        | LitInt32 n => ("LitInt32" ++ pretty n ++ ")")%string
-        | LitBool true => "LitBool #t"
-        | LitBool false => "LitBool #f"
+        | LitInt n => "#" ++ pretty n
+        | LitInt32 n => "#(U32 " ++ pretty n ++ ")"
+        | LitBool b => if b then "#t" else "f"
         | LitByte b => "LitByte"
-        | LitString s => "LitString"
-        | LitUnit => "LitUnit"
+        | LitString s => "str" ++ quoted s
+        | LitUnit => "#()"
         | LitPoison => "LitPoison"
-        | LitLoc l => ("LitLoc" ++ pretty l ++ ")")%string
+        | LitLoc l => "#(loc " ++ pretty l ++ ")"
         | LitProphecy p => "LitProphecy"
-        end.
+        end%string.
 
 Instance pretty_un_op : Pretty un_op :=
   fun x => match x with
         | NegOp => "NegOp"
         | MinusUnOp => "MinusUnOp"
-        | ToUInt64Op => "ToUInt64Op"
-        | ToUInt32Op => "ToUInt32Op"
-        | ToUInt8Op => "ToUInt8Op"
+        | ToUInt64Op => "to_u64"
+        | ToUInt32Op => "to_u32"
+        | ToUInt8Op => "to_u8"
         end.
 
 Instance pretty_bin_op : Pretty bin_op :=
