2f025c591e6ef39739d0a80f37f9194864e296cc
Tej Chajed <tchajed@mit.edu>
2020-01-31 14:32:34 -0500

    Remove most of the vendor dependencies
    stdpp covers most of what we need

--- src/Helpers/RecordZoom.v
+++
@@ -1,9 +0,0 @@
-From Transitions Require Import Relations.
-From RecordUpdate Require Import RecordSet.
-
-Generalizable All Variables.
-
-Definition _zoom `{Setter A C proj} T (r: relation C C T) : relation A A T :=
-  zoom proj (fun c => set proj (fun _ => c)) r.
-
-Arguments _zoom {A C} proj {H} {T} r.
