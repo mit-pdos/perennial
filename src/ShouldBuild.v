add82513078b1452e9b4f3455c1e15588b3c4606
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2020-02-16 17:08:08 -0500

    Basic definitions for logical relation.

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -7,7 +7,7 @@ From Perennial Require Import program_logic.crash_inv.
 From Perennial Require Import heap_lang.crash_lock.
 
 From Perennial.goose_lang Require Import
-     adequacy recovery_adequacy spec_assert lib.spin_lock refinement refinement_adequacy.
+     adequacy recovery_adequacy spec_assert lib.spin_lock refinement refinement_adequacy logical_reln.
 From Perennial.goose_lang Require Import map_triples.
 From Perennial.program_proof Require Import
      marshal_proof
---
+++ src/goose_lang/logical_reln.v
@@ -0,0 +1,55 @@
+From iris.proofmode Require Import tactics.
+From iris.algebra Require Import auth.
+From iris.base_logic.lib Require Import proph_map.
+From iris.program_logic Require Export weakestpre adequacy.
+From Perennial.algebra Require Import proph_map.
+From Perennial.goose_lang Require Import proofmode notation.
+From Perennial.program_logic Require Import recovery_weakestpre recovery_adequacy spec_assert.
+From Perennial.goose_lang Require Import typing adequacy refinement.
+From Perennial.goose_lang Require Export recovery_adequacy spec_assert refinement_adequacy.
+
+Set Default Proof Using "Type".
+
+Section reln.
+Context {ext: ext_op}.
+Context {ffi: ffi_model}.
+Context {ffi_semantics: ext_semantics ext ffi}.
+Context `{interp: !ffi_interp ffi}.
+Context `{interp_adeq: !ffi_interp_adequacy}.
+Context (impl_ty: ext_types ext).
+
+Context {spec_ext: spec_ext_op}.
+Context {spec_ffi: spec_ffi_model}.
+Context {spec_ffi_semantics: spec_ext_semantics spec_ext spec_ffi}.
+Context `{spec_interp: @spec_ffi_interp spec_ffi}.
+Context `{spec_adeq: !spec_ffi_interp_adequacy}.
+Context (spec_ty: ext_types (@spec_ext_op_field spec_ext)).
+
+Context `{Hhpre: @heapPreG ext ffi ffi_semantics interp _ Σ}.
+Context `{Hcpre: @cfgPreG spec_lang Σ}.
+Context `{Hrpre: @refinement_heapPreG spec_ext spec_ffi spec_interp _ spec_adeq Σ}.
+
+Context {MAX: nat}.
+
+Notation sexpr := (@expr (@spec_ext_op_field spec_ext)).
+Notation sval := (@val (@spec_ext_op_field spec_ext)).
+Notation iexpr := (@expr ext).
+Notation ival := (@val ext).
+
+Existing Instances spec_ffi_model_field spec_ext_op_field spec_ext_semantics_field spec_ffi_interp_field
+         spec_ffi_interp_adequacy_field.
+
+Definition val_semTy := ∀ `(hG: !heapG Σ) (hRG: refinement_heapG Σ),
+  sval → ival → iProp Σ.
+
+Record spec_valTy_model :=
+  { spec_val_semTy : @ext_tys (@val_tys _ spec_ty) → val_semTy }.
+
+Definition has_semTy `{hG: !heapG Σ} {hRG: refinement_heapG Σ} {hC: crashG Σ}
+           (es: sexpr) (e: iexpr) (vty: val_semTy) : iProp Σ :=
+  (∀ (j: nat) (K: sexpr → sexpr) {CTX: LanguageCtx K},
+      j ⤇ K es -∗ WPC e @ NotStuck; MAX; ⊤; (⊤ ∖ ↑sN) {{ v, ∃ vs, j ⤇ K (of_val vs)
+                                                                    ∗ vty hG hRG vs v }}
+                                                      {{ True }})%I.
+
+End reln.
