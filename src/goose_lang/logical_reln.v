c4141e654e3236bf04ca37dcc52a2754c7b33199
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2020-02-16 17:44:52 -0500

    Start val type model.

--- src/goose_lang/logical_reln.v
+++ src/goose_lang/logical_reln.v
@@ -16,7 +16,6 @@ Context {ffi: ffi_model}.
 Context {ffi_semantics: ext_semantics ext ffi}.
 Context `{interp: !ffi_interp ffi}.
 Context `{interp_adeq: !ffi_interp_adequacy}.
-Context (impl_ty: ext_types ext).
 
 Context {spec_ext: spec_ext_op}.
 Context {spec_ffi: spec_ffi_model}.
@@ -35,6 +34,7 @@ Notation sexpr := (@expr (@spec_ext_op_field spec_ext)).
 Notation sval := (@val (@spec_ext_op_field spec_ext)).
 Notation iexpr := (@expr ext).
 Notation ival := (@val ext).
+Notation sty := (@ty (@val_tys _ spec_ty)).
 
 Existing Instances spec_ffi_model_field spec_ext_op_field spec_ext_semantics_field spec_ffi_interp_field
          spec_ffi_interp_adequacy_field.
@@ -43,7 +43,7 @@ Definition val_semTy := ∀ `(hG: !heapG Σ) (hRG: refinement_heapG Σ),
   sval → ival → iProp Σ.
 
 Record spec_valTy_model :=
-  { spec_val_semTy : @ext_tys (@val_tys _ spec_ty) → val_semTy }.
+  { spec_val_interp : @ext_tys (@val_tys _ spec_ty) → val_semTy }.
 
 Definition has_semTy `{hG: !heapG Σ} {hRG: refinement_heapG Σ} {hC: crashG Σ}
            (es: sexpr) (e: iexpr) (vty: val_semTy) : iProp Σ :=
@@ -51,5 +51,37 @@ Definition has_semTy `{hG: !heapG Σ} {hRG: refinement_heapG Σ} {hC: crashG Σ}
       j ⤇ K es -∗ WPC e @ NotStuck; MAX; ⊤; (⊤ ∖ ↑sN) {{ v, ∃ vs, j ⤇ K (of_val vs)
                                                                     ∗ vty hG hRG vs v }}
                                                       {{ True }})%I.
+
+Definition base_ty_interp `{hG: !heapG Σ} {hRG : refinement_heapG Σ} (t: base_ty) :=
+  λ (v1: sval) (v2: ival),
+  match t with
+    | uint64BT => (∃ x, ⌜ v1 = LitV $ LitInt x ∧ v2 = LitV $ LitInt x ⌝ : iProp Σ)%I
+    | uint32BT => (∃ x, ⌜ v1 = LitV $ LitInt32 x ∧ v2 = LitV $ LitInt32 x ⌝ : iProp Σ)%I
+    | byteBT => (∃ x, ⌜ v1 = LitV $ LitByte x ∧ v2 = LitV $ LitByte x ⌝ : iProp Σ)%I
+    | boolBT => (∃ x, ⌜ v1 = LitV $ LitBool x ∧ v2 = LitV $ LitBool x ⌝ : iProp Σ)%I
+    | unitBT => (⌜ v1 = LitV $ LitUnit ∧ v2 = LitV $ LitUnit ⌝ : iProp Σ)%I
+    | stringBT => (∃ x, ⌜ v1 = LitV $ LitString x ∧ v2 = LitV $ LitString x ⌝ : iProp Σ)%I
+  end.
+
+Fixpoint val_interp_aux `{hG: !heapG Σ} {hRG : refinement_heapG Σ}
+         (smodel: spec_valTy_model) (t: sty) (vs: sval) (v: ival) :=
+  match t with
+  | baseT bt => base_ty_interp bt vs v
+  | prodT t1 t2 => (∃ v1 v2 vs1 vs2, ⌜ v = (v1, v2)%V ∧
+                                       vs = (vs1, vs2)%V⌝
+                   ∗ val_interp_aux smodel t1 vs1 v1
+                   ∗ val_interp_aux smodel t2 vs2 v2)%I
+  | sumT t1 t2 => ((∃ v' vs', ⌜ v = InjLV v' ∧
+                                vs = InjLV vs'⌝
+                              ∗ val_interp_aux smodel t1 vs' v')
+                     ∨
+                   (∃ v' vs', ⌜ v = InjRV v' ∧
+                                vs = InjRV vs'⌝
+                              ∗ val_interp_aux smodel t2 vs' v'))%I
+  | _ => False%I
+  end.
+
+Definition val_interp (smodel: spec_valTy_model) (t: sty) : val_semTy :=
+  λ `(hG: !heapG Σ) (hRG: refinement_heapG Σ), val_interp_aux smodel t%I.
 
 End reln.
