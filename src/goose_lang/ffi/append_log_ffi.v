225880cfbb264c6d7c51bef02ee0b32649f094a9
Tej Chajed <tchajed@mit.edu>
2020-02-26 12:52:43 -0500

    Add log pointer to append_log FFI

--- src/goose_lang/ffi/append_log_ffi.v
+++ src/goose_lang/ffi/append_log_ffi.v
@@ -5,49 +5,55 @@ From Perennial.goose_lang Require Import lang lifting slice typing.
 From Perennial.goose_lang Require ffi.disk.
 
 (* TODO: move this out, it's completely general *)
-Inductive RecoverableState {Σ: Type} :=
-| UnInit
-| Closed (s:Σ)
-| Opened (s:Σ)
-.
+Section recoverable.
+  Context {Σ:Type}.
+  Inductive RecoverableState :=
+    | UnInit
+    | Closed (s:Σ)
+    | Opened (s:Σ) (l:loc)
+  .
+
+  Definition recoverable_model : ffi_model :=
+    mkFfiModel (RecoverableState) (populate UnInit).
+
+  Local Existing Instance recoverable_model.
+
+  Context {ext:ext_op}.
+
+  Definition openΣ : transition state (Σ*loc) :=
+    bind (reads id) (λ rs, match rs.(world) with
+                           | Opened s l => ret (s,l)
+                           | _ => undefined
+                           end).
+
+  Definition modifyΣ (f:Σ -> Σ) : transition state unit :=
+    bind openΣ (λ '(s, l), modify (set world (λ _, Opened (f s) l))).
+
+  (* TODO: generalize to a transition to construct the initial value, using a zoom *)
+  Definition initTo (init:Σ) (l:loc) : transition state unit :=
+    bind (reads id) (λ rs, match rs.(world) with
+                           | UnInit => modify (set world (fun _ => Opened init l))
+                           | _ => undefined
+                           end).
+
+  Definition open (l:loc) : transition state Σ :=
+    bind (reads id) (λ rs, match rs.(world) with
+                           | Closed s => bind (modify (set world (fun _ => Opened s l)))
+                                             (fun _ => ret s)
+                           | _ => undefined
+                           end).
+
+  Definition close : transition (RecoverableState) unit :=
+    bind (reads id) (fun s => match s with
+                           | Opened s _ => modify (fun _ => Closed s)
+                           | _ => undefined
+                           end).
+
+  Global Instance Recoverable_inhabited : Inhabited RecoverableState := populate UnInit.
+End recoverable.
+
 Arguments RecoverableState Σ : clear implicits.
-
-Definition recoverable_model (Σ: Type) : ffi_model :=
-  mkFfiModel (RecoverableState Σ) (populate UnInit).
-
-Definition openΣ {ext:ext_op} {Σ: Type} : transition (@state ext (recoverable_model Σ)) Σ :=
-  bind (reads id) (λ (rs: @state _ (recoverable_model Σ)), match rs.(world) with
-                         | Opened s => ret s
-                         | _ => undefined
-                         end).
-
-Definition modifyΣ {ext:ext_op} {Σ: Type} (f:Σ -> Σ) : transition (@state ext (recoverable_model Σ)) unit :=
-  bind (reads id) (λ (rs: @state _ (recoverable_model Σ)), match rs.(world) with
-                         | Opened s => modify (set world (fun (_:@ffi_state (recoverable_model Σ)) => Opened (f s)))
-                         | _ => undefined
-                         end).
-
-(* TODO: generalize to a transition to construct the initial value, using a zoom *)
-Definition initTo {ext:ext_op} {Σ: Type} (init:Σ) : transition (@state ext (recoverable_model Σ)) unit :=
-  bind (reads id) (λ (rs: @state _ (recoverable_model Σ)), match rs.(world) with
-                         | UnInit => modify (set world (fun (_:@ffi_state (recoverable_model Σ)) => Opened init))
-                         | _ => undefined
-                         end).
-
-Definition open {ext:ext_op} {Σ: Type} : transition (@state ext (recoverable_model Σ)) Σ :=
-  bind (reads id) (λ (rs: @state _ (recoverable_model Σ)), match rs.(world) with
-                         | Closed s => bind (modify (set world (fun (_:@ffi_state (recoverable_model Σ)) => Opened s)))
-                                      (fun _ => ret s)
-                         | _ => undefined
-                         end).
-
-Definition close {ext:ext_op} {Σ: Type} : transition (RecoverableState Σ) unit :=
-  bind (reads id) (fun s => match s with
-                         | Opened s => modify (fun _ => Closed s)
-                         | _ => undefined
-                         end).
-
-Instance Recoverable_inhabited state : Inhabited (RecoverableState state) := populate UnInit.
+Arguments recoverable_model Σ : clear implicits.
 
 Definition ty_ := forall (val_ty:val_types), @ty val_ty.
 (* TODO: slice should not require an entire ext_ty *)
@@ -119,26 +125,36 @@ Section log.
   (* TODO: implement *)
   Definition to_block (l: list val): option disk.Block := None.
 
+  Definition allocIdent: transition state loc :=
+    l ← allocateN 1;
+    modify (set heap <[l := Free #()]>);;
+    ret l.
+
   Definition log_step (op:LogOp) (v:val) : transition state val :=
     match op, v with
-    | GetOp, LitV (LitInt a) =>
-      log ← openΣ;
+    | GetOp, PairV (LitV (LitLoc logPtr)) (LitV (LitInt a)) =>
+      openΣ ≫= λ '(log, logPtr_),
+      check (logPtr = logPtr_);;
       b ← unwrap (log !! int.nat a);
       l ← allocateN 4096;
       modify (state_insert_list l (disk.Block_to_vals b));;
       ret $ #(LitLoc l)
-    | ResetOp, LitV LitUnit =>
+    | ResetOp, PairV (LitV (LitLoc logPtr)) (LitV LitUnit) =>
+      openΣ ≫= λ '(_, logPtr_),
+      check (logPtr = logPtr_);;
       modifyΣ (fun _ => []);;
       ret $ #()
     | InitOp, LitV LitUnit =>
-      initTo [];;
-      (* TODO: initialize log with a pointer to a unit *)
-      ret $ #()
+      logPtr ← allocIdent;
+      initTo [] logPtr;;
+      ret $ LitV $ LitLoc logPtr
     | OpenOp, LitV LitUnit =>
-      s ← open;
-      (* TODO: initialize log with a pointer to a unit *)
-      ret $ #()
-    | AppendOp, v =>
+      logPtr ← allocIdent;
+      s ← open logPtr;
+      ret $ LitV $ LitLoc logPtr
+    | AppendOp, PairV (LitV (LitLoc logPtr)) v =>
+      openΣ ≫= λ '(_, logPtr_),
+      check (logPtr = logPtr_);;
       (* FIXME: append should be non-atomic in the spec because it needs to read
          an input slice (and the slices the input points to). *)
       (* this is absolutely horrendous to reason about *)
