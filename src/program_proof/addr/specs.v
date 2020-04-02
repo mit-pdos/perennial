af1fcf388695abc9fa5b25b80441cdc1ea46cda4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-04-02 11:50:46 -0400

    fully prove addr

--- src/program_proof/addr/specs.v
+++ src/program_proof/addr/specs.v
@@ -32,12 +32,56 @@ Qed.
 Definition addr2val (a : addr) : val :=
   (#a.(addrBlock), (#a.(addrOff), #())).
 
-Definition addr2flat (a : addr) : u64 :=
+Definition addr2flat_z (a : addr) : Z :=
   int.val a.(addrBlock) * block_bytes * 8 + int.val a.(addrOff).
+
+Definition addr2flat (a : addr) : u64 :=
+  addr2flat_z a.
 
 Definition valid_addr (a : addr) : Prop :=
   int.val a.(addrOff) < block_bytes*8 ∧
-  int.val a.(addrBlock) * block_bytes * 8 < 2^64.
+  int.val a.(addrBlock) * block_bytes < 2^64 ∧
+  int.val a.(addrBlock) * block_bytes * 8 < 2^64 ∧
+  addr2flat_z a < 2^64.
+
+Theorem addr2flat_z_eq `(valid_addr a0) `(valid_addr a1) :
+  addr2flat a0 = addr2flat a1 →
+  addr2flat_z a0 = addr2flat_z a1.
+Proof.
+  destruct a0, a1.
+  unfold addr2flat, valid_addr, addr2flat_z, block_bytes in *.
+  simpl in *; intuition.
+  apply (f_equal int.val) in H.
+  revert H.
+  word.
+Qed.
+
+Theorem addr2flat_eq `(valid_addr a0) `(valid_addr a1) :
+  addr2flat a0 = addr2flat a1 →
+  a0 = a1.
+Proof.
+  intros.
+  eapply addr2flat_z_eq in H; eauto.
+  destruct a0, a1.
+  unfold valid_addr, addr2flat_z, block_bytes in *.
+  simpl in *.
+  intuition.
+  replace addrOff0 with addrOff1 in *.
+  { replace addrBlock0 with addrBlock1; auto.
+    assert (int.val addrBlock0 * Z.to_nat 4096 * 8 = int.val addrBlock1 * Z.to_nat 4096 * 8) by lia.
+    word.
+  }
+  word.
+Qed.
+
+Theorem addr2flat_ne `(valid_addr a0) `(valid_addr a1) :
+  a0 ≠ a1 ->
+  addr2flat a0 ≠ addr2flat a1.
+Proof.
+  intros; intuition.
+  apply H.
+  eapply addr2flat_eq; eauto.
+Qed.
 
 Set Implicit Arguments.
 
@@ -67,14 +111,26 @@ Section flatid2addr.
     valid_addr a ->
     flatid_addr_map (<[addr2flat a := v]> fm) (<[a := v]> am).
   Proof.
-  Admitted.
+    rewrite /flatid_addr_map /valid_addr; intros.
+    destruct (decide (a = a0)); subst.
+    - repeat rewrite lookup_insert; eauto.
+    - rewrite -> lookup_insert_ne by eauto.
+      rewrite -> lookup_insert_ne by (apply addr2flat_ne; eauto).
+      eauto.
+  Qed.
 
   Theorem flatid_addr_delete fm am a :
     flatid_addr_map fm am ->
     valid_addr a ->
     flatid_addr_map (delete (addr2flat a) fm) (delete a am).
   Proof.
-  Admitted.
+    rewrite /flatid_addr_map /valid_addr; intros.
+    destruct (decide (a = a0)); subst.
+    - repeat rewrite lookup_delete; eauto.
+    - rewrite -> lookup_delete_ne by eauto.
+      rewrite -> lookup_delete_ne by (apply addr2flat_ne; eauto).
+      eauto.
+  Qed.
 
   Theorem flatid_addr_empty :
     flatid_addr_map ∅ ∅.
@@ -85,6 +141,12 @@ Section flatid2addr.
   Qed.
 
 End flatid2addr.
+
+
+Hint Unfold block_bytes : word.
+Hint Unfold valid_addr : word.
+Hint Unfold addr2flat : word.
+Hint Unfold addr2flat_z : word.
 
 Theorem wp_Addr__Flatid a :
   {{{
@@ -100,7 +162,9 @@ Proof.
   iApply "HΦ".
   iPureIntro.
 
-  rewrite /addr2flat.
-Admitted.
+  word_cleanup.
+  rewrite ?word.unsigned_mul. (* XXX why is this needed? *)
+  word_cleanup.
+Qed.
 
 End heap.
