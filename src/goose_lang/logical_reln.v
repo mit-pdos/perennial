4f6adfda634d8581a818d728243ae928981d8a61
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2020-04-06 14:57:02 -0400

    More fundamental lemma cases.

--- src/goose_lang/logical_reln.v
+++ src/goose_lang/logical_reln.v
@@ -408,8 +408,45 @@ Proof using spec_op_trans.
     { iFrame. iDestruct "Hspec" as "($&?)". }
     { set_solver+. }
     { intros; eapply stuck_Panic. }
-  - admit.
-  - admit.
+  - subst.
+    iIntros (j K Hctx) "Hj". simpl.
+    iApply wp_wpc.
+    iApply wp_fupd.
+    iApply wp_ArbitraryInt; auto.
+    iNext.
+    iIntros. iMod (ghost_step_lifting_puredet with "[Hj]") as "(Hj&Hchild)"; swap 1 3.
+    { iFrame. iDestruct "Hspec" as "($&?)". }
+    { set_solver+. }
+    { intros ?. eexists. simpl.
+      apply head_prim_step. repeat econstructor; eauto.
+    }
+    iModIntro. iExists _. iFrame. iExists _. iPureIntro; eauto.
+  - subst.
+    iIntros (j K Hctx) "Hj". simpl.
+    wpc_bind (subst_map _ e2).
+    iPoseProof (IHHtyping with "[//] [$] [$] [$] [$]") as "H".
+    iSpecialize ("H" $! j (λ x, K (ectx_language.fill [UnOpCtx _] x)) with "[] Hj").
+    { iPureIntro. apply comp_ctx; last done. apply ectx_lang_ctx. }
+    iApply (wpc_mono' with "[] [] H"); last done.
+    iIntros (v1) "H". iDestruct "H" as (vs1) "(Hj&Hv1)".
+    iAssert (∃ (vres: u64), ⌜ un_op_eval ToUInt64Op v1 = Some #vres ∧
+                            un_op_eval ToUInt64Op vs1 = Some #vres ⌝)%I with "[Hv1]" as %Hres.
+    {
+      destruct t; try inversion e;
+      destruct t; try congruence;
+      rewrite /val_interp//=;
+      iDestruct "Hv1" as (?) "(%&%)"; subst;
+      iPureIntro; eexists; eauto.
+    }
+    destruct Hres as (x&Heq1&Heq2).
+      iMod (ghost_step_lifting_puredet with "[Hj]") as "(Hj&Hchild)"; swap 1 3.
+      { iFrame. iDestruct "Hspec" as "($&?)". }
+      { set_solver+. }
+      { intros ?. eexists. simpl.
+        apply head_prim_step. repeat econstructor; eauto.
+        rewrite Heq2; eauto. econstructor; eauto.
+      }
+      wpc_pures; eauto.
   - admit.
   - admit.
   - admit.
