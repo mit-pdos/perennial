5e1f13a31ad2a1f231da9135c1701b71ea432806
Nickolai Zeldovich <nickolai@csail.mit.edu>
2020-04-09 01:56:11 -0400

    must ensure buf offsets are properly aligned for their kinds
    e.g., inode offsets must be a multiple of inode_bytes*8

---
+++ src/program_proof/buf/defs.v
@@ -0,0 +1,49 @@
+From RecordUpdate Require Import RecordSet.
+Import RecordSetNotations.
+
+
+From Perennial.program_proof Require Import proof_prelude.
+From Perennial.program_proof.addr Require Import specs.
+
+Definition inode_bytes := Z.to_nat 128.
+Definition inode_buf := vec u8 inode_bytes.
+Definition inode_to_vals {ext: ext_op} (i:inode_buf) : list val :=
+  fmap b2val (vec_to_list i).
+
+Inductive bufDataKind :=
+| KindBit
+| KindInode
+| KindBlock
+.
+
+Global Instance bufDataKind_eq_dec : EqDecision bufDataKind.
+Proof.
+  solve_decision.
+Defined.
+
+Inductive bufDataT : forall {K : bufDataKind}, Type :=
+| bufBit (b : bool) : @bufDataT KindBit
+| bufInode (i : inode_buf) : @bufDataT KindInode
+| bufBlock (b : Block) : @bufDataT KindBlock
+.
+
+Definition bufSz K : nat :=
+  match K with
+  | KindBit => 1
+  | KindInode => inode_bytes*8
+  | KindBlock => block_bytes*8
+  end.
+
+Record buf := {
+  bufKind : bufDataKind;
+  bufData : @bufDataT bufKind;
+  bufDirty : bool;
+}.
+
+Definition get_bit (b0 : u8) (off : u64) : bool :=
+  if decide (U8 1 = word.and (word.sru b0 (u8_from_u64 off)) (U8 1))
+  then true
+  else false.
+
+Definition valid_off K (off : u64) : Prop :=
+  int.val off `mod` bufSz K = 0.
--- src/program_proof/buf/specs.v
+++ src/program_proof/buf/specs.v
@@ -5,6 +5,7 @@ Import RecordSetNotations.
 From Perennial.program_proof Require Import proof_prelude.
 From Goose.github_com.mit_pdos.goose_nfsd Require Import buf.
 From Perennial.program_proof Require Import util_proof.
+From Perennial.program_proof.buf Require Import defs.
 From Perennial.program_proof.addr Require Import specs.
 From Perennial.program_proof.wal Require Import abstraction.
 
@@ -14,46 +15,6 @@ Context `{!lockG Σ}.
 
 Implicit Types s : Slice.t.
 Implicit Types (stk:stuckness) (E: coPset).
-
-Definition inode_bytes := Z.to_nat 128.
-Definition inode_buf := vec u8 inode_bytes.
-Definition inode_to_vals {ext: ext_op} (i:inode_buf) : list val :=
-  fmap b2val (vec_to_list i).
-
-Inductive bufDataKind :=
-| KindBit
-| KindInode
-| KindBlock
-.
-
-Global Instance bufDataKind_eq_dec : EqDecision bufDataKind.
-Proof.
-  solve_decision.
-Defined.
-
-Inductive bufDataT : forall {K : bufDataKind}, Type :=
-| bufBit (b : bool) : @bufDataT KindBit
-| bufInode (i : inode_buf) : @bufDataT KindInode
-| bufBlock (b : Block) : @bufDataT KindBlock
-.
-
-Definition bufSz K : nat :=
-  match K with
-  | KindBit => 1
-  | KindInode => inode_bytes*8
-  | KindBlock => block_bytes*8
-  end.
-
-Record buf := {
-  bufKind : bufDataKind;
-  bufData : @bufDataT bufKind;
-  bufDirty : bool;
-}.
-
-Definition get_bit (b0 : u8) (off : u64) : bool :=
-  if decide (U8 1 = word.and (word.sru b0 (u8_from_u64 off)) (U8 1))
-  then true
-  else false.
 
 Definition is_buf_data {K} (s : Slice.t) (d : @bufDataT K) (a : addr) : iProp Σ :=
   match d with
@@ -69,7 +30,7 @@ Definition is_buf (bufptr : loc) (a : addr) (o : buf) : iProp Σ :=
     bufptr ↦[Buf.S :: "Sz"] #sz ∗
     bufptr ↦[Buf.S :: "Data"] (slice_val data) ∗
     bufptr ↦[Buf.S :: "dirty"] #o.(bufDirty) ∗
-    ⌜ valid_addr a ⌝ ∗
+    ⌜ valid_addr a ∧ valid_off o.(bufKind) a.(addrOff) ⌝ ∗
     ⌜ sz = bufSz o.(bufKind) ⌝ ∗
     ⌜ #bufptr ≠ #null ⌝ ∗
     is_buf_data data o.(bufData) a.
@@ -140,7 +101,7 @@ Proof using.
 
   wp_call.
   wp_loadField.
-  wp_apply wp_Addr__Flatid; eauto. iIntros (?) "->".
+  wp_apply wp_Addr__Flatid; intuition eauto. iIntros (?) "->".
   wp_loadField.
   wp_apply (wp_MapInsert with "Hmap"); iIntros "Hmap".
   iApply "HΦ".
@@ -155,14 +116,18 @@ Proof using.
     iApply "Hacc".
     iExists _.
     iSplitR; eauto.
-    iExists _, _. iFrame. done.
+    iExists _, _. iFrame.
+    iDestruct "Hdata" as "(% & % & Hdata)". iFrame.
+    done.
 
   - iDestruct (big_sepM2_lookup_1_none with "Ham") as "%"; eauto.
     iApply big_sepM2_insert; eauto.
     iFrame "Ham".
     iExists _.
     iSplitR; eauto.
-    iExists _, _. iFrame. done.
+    iExists _, _. iFrame.
+    iDestruct "Hdata" as "(% & % & Hdata)". iFrame.
+    done.
 Qed.
 
 Theorem wp_BufMap__Del l m a :
@@ -345,8 +310,9 @@ Proof.
 Admitted.
 
 Definition is_bufData_at_off {K} (b : Block) (off : u64) (d : @bufDataT K) : Prop :=
+  valid_off K off ∧
   match d with
-  | bufBlock d => off = 0 ∧ b = d
+  | bufBlock d => b = d
   | bufInode i => extract_nth b inode_bytes (int.nat off) = Some i
   | bufBit d => ∃ (b0 : u8), extract_nth b 1 ((int.nat off)/8) = Some (Vector.of_list [b0]) ∧
       get_bit b0 (word.modu off 8) = d
@@ -355,7 +321,7 @@ Definition is_bufData_at_off {K} (b : Block) (off : u64) (d : @bufDataT K) : Pro
 Theorem wp_MkBuf K a data (bufdata : @bufDataT K) :
   {{{
     is_buf_data data bufdata a ∗
-    ⌜ valid_addr a ⌝
+    ⌜ valid_addr a ∧ valid_off K a.(addrOff) ⌝
   }}}
     MkBuf (addr2val a) #(bufSz K) (slice_val data)
   {{{
@@ -399,6 +365,7 @@ Proof using.
   iIntros (Φ) "(Hs & % & %) HΦ".
   wp_call.
   iDestruct (is_slice_small_sz with "Hs") as "%".
+  destruct H as [Hoff Hatoff].
 
   wp_apply (wp_SliceSubslice_small with "[$Hs]").
   { rewrite /valid_addr in H0.
@@ -424,8 +391,8 @@ Proof using.
   iFrame.
   iSplitR; first done.
   iSplitR; first done.
-  destruct bufdata; cbn; cbn in H.
-  - destruct H; intuition.
+  destruct bufdata; cbn; cbn in Hatoff.
+  - destruct Hatoff; intuition.
     iSplitR.
     { iPureIntro. congruence. }
     iExists _.
@@ -480,8 +447,8 @@ Proof using.
         apply Zdiv_le_upper_bound; lia.
     }
 
-    eapply extract_nth_skipn_firstn in H.
-    rewrite /inode_to_vals H.
+    eapply extract_nth_skipn_firstn in Hatoff.
+    rewrite /inode_to_vals Hatoff.
     f_equal.
     { word. }
     f_equal.
@@ -490,11 +457,12 @@ Proof using.
   - intuition subst.
     iSplitR.
     { iPureIntro. congruence. }
-    rewrite H3.
-    rewrite /block_bytes.
-    destruct s; cbn in *.
-    rewrite length_Block_to_vals in H1.
     iExactEq "Hs".
+
+    assert (a.(addrOff) = 0) as Hoff0.
+    { admit. }
+    rewrite Hoff0.
+    unfold block_bytes in *.
     replace (word.divu 0 8) with (U64 0) by reflexivity.
     replace (word.add 0 (Z.to_nat 4096 * 8)%nat) with (U64 (4096 * 8)) by reflexivity.
     replace (word.sub (4096 * 8) 1) with (U64 32767) by reflexivity.
@@ -529,7 +497,7 @@ Proof.
   wp_loadField.
 
   destruct b; simpl in *.
-  destruct bufData0.
+  destruct bufData.
   - wp_pures.
     wp_loadField.
     wp_loadField.
@@ -552,8 +520,20 @@ Proof.
     wp_loadField.
     wp_pures.
 
-    (* XXX how do we know the addr offset is properly aligned
-      for KindInode? *)
+    destruct (bool_decide (#(word.modu a.(addrOff) 8 : u64) = #0)) eqn:Hd;
+      wp_pures.
+    2: {
+      (* contradiction with [valid_off] in H2. *)
+      admit.
+    }
+
+    wp_loadField.
+    wp_loadField.
+    wp_loadField.
+    wp_call.
+
+    (* XXX need WP for SliceCopy, and more specialized WPs for
+      SliceSkip and SliceTake in terms of is_slice_small *)
     admit.
 
   - wp_pures.
@@ -561,8 +541,15 @@ Proof.
     wp_loadField.
     wp_pures.
 
-    (* XXX how do we know the addr offset is byte-aligned for
-      KindBlock? *)
+    destruct (bool_decide (#(word.modu a.(addrOff) 8 : u64) = #0)) eqn:Hd;
+      wp_pures.
+    2: {
+      (* contradiction with [valid_off] in H. *)
+      admit.
+    }
+
+    wp_loadField.
+    (* XXX stack overflow........ *)
     admit.
 Admitted.
 
