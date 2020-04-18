e3f2a1586455d339c204c4ff90d628afd62bb05e
Lily Tsai <tslilyai@mit.edu>
2020-04-18 18:02:46 -0400

    finally write down triples, figured out how to relate kvpair types to kvpair slices

--- src/program_proof/kvs/specs.v
+++ src/program_proof/kvs/specs.v
@@ -12,6 +12,13 @@ From Perennial.program_proof Require Import proof_prelude.
 From Perennial.program_proof Require Import disk_lib.
 From Perennial.program_proof Require Import marshal_proof.
 
+Module kvpair.
+  Record t :=
+    mk { key: u64;
+         val: Block; }.
+  Global Instance _eta: Settable _ := settable! mk <key; val>.
+End kvpair.
+
 Section heap.
 Context `{!heapG Σ}.
 Context `{!lockG Σ}.
@@ -39,44 +46,68 @@ match sz with
 end.
 Definition kvs_init_s sz : gmap u64 Block := init_kvs ∅ sz.
 
-Definition is_kvs (kvsl: loc) (s :gmap u64 Block) (sz : nat): iProp Σ :=
-  ( ∃ (l : loc) (keys: list u64) γ,
+(*XXXway to generalize from wal/abstractions.v?*)
+Definition kvpair_val (pair : u64*Slice.t): val :=
+  (#(fst pair), (slice_val (snd pair), #()))%V.
+
+(* Links a list of kvpairs to a slice *)
+Definition kvpairs_slice (val_s: Slice.t) (pairs: list kvpair.t): iProp Σ :=
+  ∃ bks, is_slice val_s (struct.t KVPair.S) 1 (kvpair_val <$> bks) ∗
+   [∗ list] _ ↦ b_kvp;kvp ∈ bks;pairs, let '(kvpair.mk a b) := kvp in
+                                     is_block (snd b_kvp) b ∗
+                                              ⌜fst b_kvp = a⌝.
+
+Definition kvpairs_match (s: gmap u64 Block) (pairs: list kvpair.t): iProp Σ :=
+  [∗ list] kvp ∈ pairs, let '(kvpair.mk a b) := kvp in (⌜(s !! a = Some b)⌝)%I.
+
+Definition is_kvs' (s: gmap u64 Block) (sz : nat) :=
+  (∃ keys,
+    ⌜keys = init_keys [] sz⌝ ∗
+    [∗ list] k ∈ keys, (∃ v : Block, ⌜s !! k = Some v⌝ ∗ k k↦ v))%I.
+
+Definition ptsto_kvs (kvsl: loc) (s : gmap u64 Block) (sz : nat): iProp Σ :=
+  ( ∃ (l : loc) γ,
       kvsl↦[KVS.S :: "txn"] #l ∗
       kvsl ↦[KVS.S :: "sz"] #(U64 (Z.of_nat sz)) ∗
       is_txn l γ ∗
-      ⌜keys = init_keys [] sz⌝ ∗
-      [∗ list] k ∈ keys, (∃ v : Block, ⌜s !! k = Some v⌝ ∗ k k↦ v))%I.
+      is_kvs' s sz)%I.
 
-Definition is_block (s:Slice.t) (b:Block) :=
-  is_slice_small s byteT 1 (Block_to_vals b).
+Definition crashed_kvs s sz kvp_ls : iProp Σ :=
+  is_kvs' s sz ∗ kvpairs_match s kvp_ls.
 
-Definition is_kvpair (l: loc) (key : u64) (b: Block) : iProp Σ :=
-      ∃ bs, (l↦[KVPair.S :: "Key"] #key ∗
+Definition ptsto_kvpair (l: loc) (pair: kvpair.t) : iProp Σ :=
+      ∃ bs, (l↦[KVPair.S :: "Key"] #pair.(kvpair.key) ∗
       l ↦[KVPair.S :: "Val"] (slice_val bs) ∗
-      is_block bs b)%I.
+      is_block bs pair.(kvpair.val))%I.
 
 Theorem wpc_MkKVS d (sz: nat) k E1 E2:
   {{{ True }}}
     MkKVS #d #(U64(Z.of_nat sz)) @ NotStuck; k; E1; E2
-  {{{ kvsl, RET #kvsl; is_kvs kvsl (kvs_init_s sz) sz}}}
+  {{{ kvsl, RET #kvsl; ptsto_kvs kvsl (kvs_init_s sz) sz}}}
   {{{ True }}}.
 Proof.
 Admitted.
 
 Theorem wpc_KVS__Get kvsl s sz key v stk E:
-  {{{ is_kvs kvsl s sz ∗ ⌜s !! key = Some v⌝ }}}
+  {{{ ptsto_kvs kvsl s sz ∗ ⌜s !! key = Some v⌝ }}}
     KVS__Get #kvsl #key @ stk; E (* Crashes don't matter to state here *)
-  {{{ (pairl: loc), RET #pairl; (is_kvs kvsl s sz ∗ is_kvpair pairl key v)}}}.
+  {{{(pairl: loc), RET #pairl;
+     ptsto_kvs kvsl s sz ∗ ptsto_kvpair pairl (kvpair.mk key v)
+  }}}.
 Proof.
 Admitted.
 
-(*TODO put actually modifies *)
-(*Would it just be easier to not deal with kvpairs?*)
-Theorem wpc_KVS__MultiPut kvsl s kvpairsl stk k E1 E2:
-  {{{ is_kvs kvsl s }}}
-    KVS__MultiPut #kvsl #kvpairsl @ stk; k; E1; E2
-  {{{ (ok: bool), RET #ok; (is_kvs kvsl s)}}}
-  {{{ True }}}.
+Theorem wpc_KVS__MultiPut kvsl s sz kvp_ls_before kvp_slice kvp_ls stk k E1 E2:
+  {{{
+       ptsto_kvs kvsl s sz
+                 ∗ kvpairs_match s kvp_ls_before
+                 ∗ kvpairs_slice kvp_slice kvp_ls
+  }}}
+    KVS__MultiPut #kvsl (slice_val kvp_slice) @ stk; k; E1; E2
+  {{{ (ok: bool), RET #ok;
+      ptsto_kvs kvsl s sz ∗ kvpairs_match s kvp_ls
+  }}}
+  {{{ crashed_kvs s sz kvp_ls ∨ crashed_kvs s sz kvp_ls_before }}}.
 Proof.
 Admitted.
 End heap.
