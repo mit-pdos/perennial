114305f51246c8b47aaf4f3590dc4c2528b5a574
Tej Chajed <tchajed@mit.edu>
2020-04-19 11:53:38 -0500

    Generalize readonly support

--- src/goose_lang/lib/persistent_readonly.v
+++ src/goose_lang/lib/persistent_readonly.v
@@ -1,15 +1,17 @@
 From iris.base_logic.lib Require Import invariants.
 From iris.bi.lib Require Import fractional.
 From Perennial.Helpers Require Import iris.
+From Perennial.algebra Require Import deletable_heap.
 From Perennial.goose_lang Require Import notation.
 From Perennial.goose_lang Require Import lifting.
 From Perennial.goose_lang Require Import proofmode.
 
 Section goose_lang.
-  Context `{ffi_sem: ext_semantics} `{!ffi_interp ffi} `{!heapG Σ}.
+  Context `{!invG Σ}.
+  Context {L V: Type}.
 
   Class AsMapsTo (P: iProp Σ)
-        (Φ : loc -> Qp -> val -> iProp Σ) (l: loc) (v: val) :=
+        (Φ : L -> Qp -> V -> iProp Σ) (l: L) (v: V) :=
     { as_mapsto : P ≡ Φ l 1%Qp v;
       as_mapsto_fractional :> forall l v, Fractional (λ q, Φ l q v);
       as_mapsto_timeless :> forall l q v, Timeless (Φ l q v);
@@ -63,15 +65,22 @@ Section goose_lang.
     auto.
   Qed.
 
-  Global Instance heap_mapsto_AsMapsTo l v :
-    AsMapsTo (l ↦ v) (λ l q v, heap_mapsto l q v) l v.
-  Proof.
-    split; [done|apply _|apply _].
-  Qed.
-
 End goose_lang.
 
-Hint Mode AsMapsTo - - + - - - : typeclass_instances.
+Instance heap_mapsto_AsMapsTo `{ext: !ext_op} `{!na_heapG loc Z val Σ}
+         (l: loc) (v: val) :
+  AsMapsTo (l ↦ v) (λ l q v, l ↦{q} v)%I l v.
+Proof.
+  split; [done|apply _|apply _].
+Qed.
+
+Instance mapsto_AsMapsTo `{ffi_sem: ext_semantics} `(hG: gen_heapG loc val Σ) l v :
+  AsMapsTo (mapsto (hG:=hG) l 1 v) (λ l q v, mapsto (hG:=hG) l q v) l v.
+Proof.
+  split; [done|apply _|apply _].
+Qed.
+
+Hint Mode AsMapsTo - - - + - - - : typeclass_instances.
 
 Typeclasses Opaque readonly.
 Opaque readonly.
