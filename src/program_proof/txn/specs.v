6572da4e3152edb9143fcbb85d739b2ecc01883a
Frans Kaashoek <kaashoek@mit.edu>
2020-04-22 09:13:02 -0400

    Checkpoint bufsByBlock_wf

--- src/program_proof/txn/specs.v
+++ src/program_proof/txn/specs.v
@@ -427,28 +427,24 @@ Definition is_txn_buf  bufptrval (a : addr) (buf : buf) gData : iProp Σ :=
     is_buf bufptr a buf ∗
     @mapsto_txn buf.(bufKind) gData a v0.
 
-Definition bufsby_slice_wf blkno val gData bufamap: iProp Σ :=
-  ∃ s bbblist,
+Definition bufsby_slice_wf blkno bbblist val gData bufamap: iProp Σ :=
+  ∃ s,
     ⌜val = slice_val s⌝ ∗ is_slice s (slice.T (refT (struct.t buf.Buf.S))) 1 bbblist ∗
     [∗ maplist] a ↦ buf; bufptrval ∈ bufamap; bbblist,
-      is_txn_buf bufptrval a buf gData ∗ ⌜a.(addrBlock) = blkno⌝.
+  is_txn_buf bufptrval a buf gData ∗ ⌜a.(addrBlock) = blkno⌝.
 
-Definition bufsByBlock_wf bufsByBlock_l (bufsByBlock:loc) bbbmap gData bufamap: iProp Σ :=
-  bufsByBlock_l ↦[mapT (slice.T (refT (struct.t buf.Buf.S)))] #bufsByBlock ∗
-  is_map bufsByBlock (bbbmap, zero_val (slice.T (struct.ptrT buf.Buf.S))) ∗
-  [∗ map] blkno ↦ val ∈ bbbmap, bufsby_slice_wf blkno val gData bufamap.
+Definition bufsByBlock_wf bufsByBlock_l (bufsByBlock:loc) gData bufamap buflist: iProp Σ :=
+  ∃ bbbmap buflist',
+    bufsByBlock_l ↦[mapT (slice.T (refT (struct.t buf.Buf.S)))] #bufsByBlock ∗
+    is_map bufsByBlock (bbbmap, zero_val (slice.T (struct.ptrT buf.Buf.S))) ∗
+    ([∗ map] blkno ↦ val ∈ bbbmap,
+     ∃ bbblist,
+       bufsby_slice_wf blkno bbblist val gData bufamap ∗ ⌜ bbblist ∈ buflist' ⌝)
+    ∗ ⌜ concat buflist' ≡ₚ buflist ⌝.
 
-(* XXX connect buf list and all bufs in bbbmap *)
-Definition bufsByBlock_buflist bufsByBlock_l (bufsByBlock:loc) bbbmap gData bufamap buflist : iProp Σ :=
-  bufsByBlock_wf bufsByBlock_l (bufsByBlock:loc) bbbmap gData bufamap ∗
-  [∗ maplist] a ↦ buf; bufptrval ∈ bufamap; buflist,
-     is_txn_buf bufptrval a buf gData.
-
-(* XXX pass in (take i buflist)? *)
+(* XXX pass in (take i buflist) and filter bufamp? *)
 Definition loop_inv bufsByBlock_l (bufsByBlock:loc) gData bufamap buflist: iProp Σ :=
-  ∃ bbbmap,
-    bufsByBlock_wf bufsByBlock_l bufsByBlock bbbmap gData bufamap ∗
-    bufsByBlock_buflist bufsByBlock_l bufsByBlock bbbmap gData bufamap buflist.
+  bufsByBlock_wf bufsByBlock_l bufsByBlock gData bufamap buflist.
   
 Theorem wp_txn__installBufs l q gData mData walHeap γMaps bufs buflist (bufamap : gmap addr buf) :
   {{{ is_txn l gData ∗
@@ -517,7 +513,7 @@ Opaque struct.t.
   {
     iIntros (i b).
     iIntros (Φ') "!> [Hloop HP] HΦ'".
-    iDestruct "Hloop" as (bbbmap) "((HbufsByBlock&Hismap&Hslice_wf) & Hbbb_buflist)".
+    iDestruct "Hloop" as (bbbmap buflist') "(HbufsByBlock&Hismap&Hbbb&Hbuflist)".
     wp_lam.
     wp_pures.
     iDestruct "HP" as (a buf) "Htxnbuf".
@@ -538,7 +534,7 @@ Opaque struct.t.
       wp_apply (wp_SliceAppend_to_zero).
       1: admit.
       1: admit.
-      iIntros (s) "His_slice0".
+      iIntros (s) "Hslice".
       wp_loadField.
       wp_pures.
       wp_load.
