2993dbfc14df697473aa972e7cd371b2155ee83b
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2020-05-07 21:23:09 -0400

    Split up log relation into separate files.

---
+++ src/goose_lang/logical_reln_adeq.v
@@ -0,0 +1,175 @@
+From iris.proofmode Require Import tactics.
+From iris.algebra Require Import auth excl.
+From iris.base_logic.lib Require Import proph_map.
+From iris.program_logic Require Export weakestpre adequacy.
+From Perennial.algebra Require Import proph_map frac_count big_op.
+From Perennial.goose_lang Require Import proofmode notation wpc_proofmode.
+From Perennial.program_logic Require Import recovery_weakestpre recovery_adequacy spec_assert language_ctx.
+From Perennial.goose_lang Require Import typing typed_translate adequacy refinement.
+From Perennial.goose_lang Require Export recovery_adequacy spec_assert refinement_adequacy.
+From Perennial.goose_lang Require Import metatheory.
+From Perennial.Helpers Require Import Qextra.
+From Perennial.Helpers Require List.
+From Perennial.goose_lang Require Import logical_reln_defns logical_reln_fund.
+
+Section adeq.
+
+Existing Instances spec_ffi_model_field spec_ext_op_field spec_ext_semantics_field spec_ffi_interp_field
+         spec_ffi_interp_adequacy_field.
+
+Context {ext: ext_op}.
+Context {ffi: ffi_model}.
+Context {ffi_semantics: ext_semantics ext ffi}.
+Context `{interp: !ffi_interp ffi}.
+Context `{interp_adeq: !ffi_interp_adequacy}.
+Context (impl_ty: ext_types ext).
+
+Context {spec_ext: spec_ext_op}.
+Context {spec_ffi: spec_ffi_model}.
+Context {spec_ffi_semantics: spec_ext_semantics spec_ext spec_ffi}.
+Context `{spec_interp: @spec_ffi_interp spec_ffi}.
+Context `{spec_adeq: !spec_ffi_interp_adequacy}.
+Context (spec_ty: ext_types (@spec_ext_op_field spec_ext)).
+
+Notation sstate := (@state (@spec_ext_op_field spec_ext) (spec_ffi_model_field)).
+Notation sexpr := (@expr (@spec_ext_op_field spec_ext)).
+Notation sval := (@val (@spec_ext_op_field spec_ext)).
+Notation istate := (@state ext).
+Notation iexpr := (@expr ext).
+Notation ival := (@val ext).
+Notation sty := (@ty (@val_tys _ spec_ty)).
+
+Context `{hsT_model: !specTy_model spec_ty} (spec_trans: sval → ival → Prop).
+Context (upd: specTy_update hsT_model).
+
+Existing Instance sty_inv_persistent.
+
+Section pre_assumptions.
+
+Context `{Hhpre: @heapPreG ext ffi ffi_semantics interp _ Σ}.
+Context `{Hcpre: @cfgPreG spec_lang Σ}.
+Context `{Hrpre: @refinement_heapPreG spec_ext spec_ffi spec_interp _ spec_adeq Σ}.
+Context `{Hcrashpre: crashPreG Σ}.
+Context `{Hstypre: !sty_preG (hsT_model := hsT_model) (specTy_update := upd) Σ}.
+
+Definition sty_derived_crash_condition :=
+    (λ (hG: heapG Σ) (hC: crashG Σ) (hRG: refinement_heapG Σ), ∃ hS,
+      ▷ ∀ (hG': heapG Σ), |={⊤}=>
+      ∀ (hC': crashG Σ) σs,
+      (∃ σ0 σ1, ffi_restart (heapG_ffiG) σ1.(world) ∗
+      ffi_crash_rel Σ (heapG_ffiG (hG := hG)) σ0.(world) (heapG_ffiG (hG := hG')) σ1.(world)) -∗
+      ffi_ctx (refinement_spec_ffiG) σs.(world) -∗
+      ∃ (σs': sstate) (HCRASH: crash_prim_step (spec_crash_lang) σs σs'),
+      ffi_ctx (refinement_spec_ffiG) σs.(world) ∗
+      ∀ (hRG': refinement_heapG Σ),
+      ffi_crash_rel Σ (refinement_spec_ffiG (hRG := hRG)) σs.(world)
+                      (refinement_spec_ffiG (hRG := hRG')) σs'.(world) -∗
+      ffi_restart (refinement_spec_ffiG) σs'.(world) -∗
+      |={styN}=> ∃ (new: sty_names), sty_init (sty_update Σ hS new))%I.
+
+Lemma sty_inv_to_wpc hG hC hRG hS es e τ j:
+  expr_transTy _ _ _ spec_trans ∅ es e τ →
+  sty_crash_inv_obligation →
+  sty_crash_obligation →
+  sty_rules_obligation spec_trans →
+  spec_ctx -∗
+  trace_ctx -∗
+  sty_init hS -∗
+  j ⤇ es -∗
+  WPC e @ sty_lvl_init; ⊤;⊤ ∖ ↑sN {{ _, True }}{{sty_derived_crash_condition hG hC hRG}}.
+Proof.
+  iIntros (Htype Hsty_crash_inv Hsty_crash Hsty_rules) "#Hspec #Htrace Hinit Hj".
+    rewrite /sty_crash_obligation in Hsty_crash.
+  iAssert (|={⊤}=> sty_inv hS ∗ WPC e @ sty_lvl_init; ⊤;⊤ ∖ ↑sN ∖ styN {{ _, True }}{{sty_crash_cond hS}})%I with "[-]" as ">(#Hinv&H)".
+  {
+    rewrite /sty_crash_inv_obligation in Hsty_crash_inv.
+    iApply (Hsty_crash_inv with "[$] [$] [Hj]").
+    { iIntros "#Hinv'".
+      iPoseProof (sty_fundamental_lemma _ _ Hsty_rules ∅ _ _ _ Htype) as "H"; eauto.
+      iSpecialize ("H" $! ∅ with "[] [$] [$] [$] []").
+      { iPureIntro. apply: fmap_empty. }
+      { by rewrite big_sepM_empty. }
+      rewrite /has_semTy.
+      iSpecialize ("H" $! j id with "[] [Hj]").
+      { iPureIntro. apply _. }
+      { simpl. by rewrite fmap_empty subst_map_empty. }
+      rewrite fmap_empty subst_map_empty.
+      iApply (wpc_strong_mono _ _ _ _ _ _ _ _ _ _ (λ _, True%I) with "[$]"); eauto.
+      iSplit.
+      - eauto.
+      - eauto. rewrite difference_diag_L.
+        simpl. replace (sty_lvl_ops - sty_lvl_ops)%nat with O by lia. eauto.
+    }
+  }
+  iApply (wpc_strong_mono with "[$]"); eauto.
+  { solve_ndisj. }
+  iSplit.
+  - eauto.
+  - iIntros.
+    simpl. replace (sty_lvl_init - sty_lvl_init)%nat with O by lia. simpl.
+    replace (⊤ ∖ ↑sN ∖ (⊤ ∖ ↑sN ∖ styN)) with (styN); last first.
+    {
+      rewrite difference_difference_remainder_L; auto.
+      clear. generalize (styN_disjoint). solve_ndisj.
+    }
+    iMod (Hsty_crash with "[$] [$]").
+    iModIntro. iModIntro. iExists _. iFrame.
+Qed.
+End pre_assumptions.
+
+Existing Instances subG_cfgG subG_refinement_heapPreG subG_crashG.
+Definition logical_relnΣ := #[styΣ; heapΣ; @cfgΣ spec_lang; refinement_heapΣ; crashΣ].
+
+Lemma sty_adequacy es σs e σ τ initP:
+  sty_init_obligation1 upd initP →
+  sty_init_obligation2 initP →
+  sty_crash_inv_obligation →
+  sty_crash_obligation →
+  sty_rules_obligation spec_trans →
+  expr_transTy _ _ _ spec_trans ∅ es e τ →
+  σ.(trace) = σs.(trace) →
+  σ.(oracle) = σs.(oracle) →
+  initP σ σs →
+  trace_refines e e σ es es σs.
+Proof.
+  intros Hsty_init1 Hsty_init2 Hsty_crash_inv Hsty_crash Hsty_rules Htype Htrace Horacle Hinit.
+  eapply @heap_wpc_refinement_adequacy with (spec_ext := spec_ext) (Σ := logical_relnΣ)
+           (Φ := λ _ _ _ _, True%I) (Φc := sty_derived_crash_condition)
+           (k := sty_lvl_init) (initP := initP); eauto.
+  { apply _. }
+  { apply _. }
+  { apply _. }
+  { apply _. }
+  { clear dependent σ σs. rewrite /wpc_init. iIntros (hG hC hRG σ σs Hinit) "Hffi Hffi_spec".
+    rewrite /sty_init_obligation1 in Hsty_init1.
+    rewrite /wpc_obligation.
+    iIntros "Hj #Hspec #Htrace".
+    iApply fupd_wpc.
+    iPoseProof (Hsty_init1 _ _ _ _  with "[$] [$]") as "H"; first auto.
+    iApply (fupd_mask_mono styN); first by set_solver+.
+    iMod "H" as (names) "Hinit".
+    iModIntro.
+    iApply (sty_inv_to_wpc with "[$] [$] [$]"); eauto.
+  }
+  { clear dependent σ σs.
+    rewrite /wpc_post_crash.
+    iIntros (???) "H". iDestruct "H" as (hS') "H". iNext.
+    iIntros (hG'). iMod ("H" $! hG') as "H". iModIntro.
+    iIntros. iSpecialize ("H" with "[$] [$]").
+    iDestruct "H" as (σs' Hcrash) "(Hctx&Hrest)".
+    iExists σs', Hcrash. iFrame. iIntros (hRG') "Hcrash_rel Hrestart".
+    iSpecialize ("Hrest" $! hRG' with "[$] [$]").
+    rewrite /wpc_obligation.
+    iIntros "Hj #Hspec #Htrace".
+    iApply fupd_wpc.
+    iApply (fupd_mask_mono styN); first by set_solver+.
+    iMod "Hrest" as (names) "Hinv".
+    iModIntro.
+    iApply (sty_inv_to_wpc _ _ _ (sty_update logical_relnΣ hS' names) with "[$] [$] [$]"); eauto.
+  }
+  Grab Existential Variables.
+  apply subG_styPreG, _.
+Qed.
+
+
+End adeq.
---
+++ src/program_proof/append_log_refinement_triples.v
@@ -0,0 +1,182 @@
+From Perennial.goose_lang.examples Require Import append_log.
+From Perennial.goose_lang.lib Require Import encoding crash_lock.
+From Perennial.program_proof Require Import proof_prelude.
+From Perennial.program_proof Require Import disk_lib.
+From Perennial.program_proof Require Import append_log_hocap.
+From Perennial.goose_lang.ffi Require Import append_log_ffi.
+From Perennial.goose_lang Require Import spec_assert refinement_adequacy.
+From Perennial.program_logic Require Import ghost_var.
+
+Existing Instances log_spec_ext log_spec_ffi_model log_spec_ext_semantics log_spec_ffi_interp log_spec_interp_adequacy.
+
+Section refinement.
+Context `{!heapG Σ}.
+Context `{!crashG Σ}.
+Context `{!refinement_heapG Σ}.
+Context `{!lockG Σ, stagedG Σ}.
+Existing Instance logG0.
+Context `{Hin: inG Σ (authR (optionUR (exclR log_stateO)))}.
+Context `{Hin_nat_ctx: inG Σ (authR (optionUR (exclR (leibnizO (nat * (spec_lang.(language.expr) →
+                                                                       spec_lang.(language.expr)))))))}.
+Context (SIZE: nat).
+Context (SIZE_nonzero: 0 < SIZE).
+Context (SIZE_bounds: int.nat SIZE = SIZE).
+Existing Instances spec_ffi_model_field spec_ext_op_field spec_ext_semantics_field (* spec_ffi_interp_field  *) spec_ffi_interp_adequacy_field.
+
+Notation sstate := (@state (@spec_ext_op_field log_spec_ext) (spec_ffi_model_field)).
+Notation sexpr := (@expr (@spec_ext_op_field log_spec_ext)).
+Notation sval := (@val (@spec_ext_op_field log_spec_ext)).
+
+Definition Nlog := nroot.@"log".
+
+Definition thread_tok_frag γ j K :=
+  own γ (◯ Excl' ((j, K) : leibnizO (nat * (sexpr → sexpr)))).
+Definition thread_tok_auth γ j K :=
+  own γ (● Excl' ((j, K) : leibnizO (nat * (sexpr → sexpr)))).
+Definition thread_tok_full γ j K :=
+ (thread_tok_auth γ j K ∗ thread_tok_frag γ j K)%I.
+
+Definition P (γ: gname) (s: log_state) :=
+  match s with
+  | UnInit => log_uninit_frag ∗ log_frag [] ∗ thread_tok_full γ 0 id
+  | Initing  => log_uninit_frag ∗ log_frag []
+  | Closed vs => log_closed_frag ∗ log_frag vs ∗ thread_tok_full γ 0 id
+  | Opening vs => log_closed_frag ∗ log_frag vs
+  | Opened vs l => (∃ l', log_open l' ∗ log_frag vs)
+  end%I.
+
+Definition POpened := (∃ l, log_open l)%I.
+Definition PStartedOpening γ :=
+  (∃ j (K: spec_lang.(language.expr) → spec_lang.(language.expr)) (Hctx: LanguageCtx K),
+      j ⤇ K (ExternalOp (ext := spec_ext_op_field) OpenOp #())
+      ∗ thread_tok_auth γ j K)%I.
+Definition PStartedIniting γ :=
+  (∃ j (K: spec_lang.(language.expr) → spec_lang.(language.expr)) (Hctx: LanguageCtx K),
+      j ⤇ K (ExternalOp (ext := spec_ext_op_field) InitOp #())
+      ∗ thread_tok_auth γ j K)%I.
+
+Lemma PStartedOpening_Timeless γ : Timeless (PStartedOpening γ).
+Proof. apply _. Qed.
+Lemma PStartedIniting_Timeless γ : Timeless (PStartedIniting γ).
+Proof. apply _. Qed.
+
+Definition log_inv γ : nat → iProp Σ :=
+  log_inv Nlog (P γ) (POpened) (PStartedOpening γ) (PStartedIniting γ) SIZE.
+Definition is_log γ : nat → loc → iProp Σ :=
+  is_log Nlog (P γ) (POpened) (PStartedOpening γ) (PStartedIniting γ) SIZE.
+
+Theorem wpc_Init j γ K `{LanguageCtx _ K} k k' E2:
+  (S k < k')%nat →
+  {{{ spec_ctx ∗ log_inv γ k' ∗ j ⤇ K (ExternalOp (ext := spec_ext_op_field) InitOp #()) }}}
+    Init #SIZE @ NotStuck; LVL (S (S (S k))); ⊤; E2
+  {{{ l, RET (#l, #true);  is_log γ k' l ∗ (∃ (l': loc), j ⤇ K (of_val (#l', #true) : sexpr) ∗ log_open l')}}}
+  {{{ True }}}.
+Proof using SIZE_nonzero SIZE_bounds.
+  iIntros (? Φ Φc) "(#Hspec&#Hinv&Hj) HΦ".
+  unshelve
+    wpc_apply (wpc_Init _ _ _ _ _ _ _ _ _ _ _ _ (True%I)
+                        (λ l, (∃ (l': loc), j ⤇ K (of_val (#l', #true)) ∗ log_open l'))%I
+               with "[Hj]"); try iFrame "Hinv"; eauto.
+  { apply _. }
+  iSplit; [| iSplit].
+  - iIntros (vs); simpl P.
+    iIntros "(Hclosed&Hlist&Htok)".
+    iMod (log_closed_init_false with "[$] [$] [$] Hj") as %[].
+    { solve_ndisj. }
+  - iIntros "[Hiniting|[Hopening|Hopened]]".
+    * iDestruct "Hiniting" as (j' K' Hctx') "(Hj'&_)".
+      iMod (log_init_init_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+    * iDestruct "Hopening" as (j' K' Hctx') "(Hj'&_)".
+      iMod (log_init_open_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+    * iDestruct "Hopened" as (l) "Hopen".
+      iMod (log_opened_init_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+  - iSplit; last done. simpl.
+    iIntros "(Huninit_frag&Hvals_frag&(Hthread_auth&Hthread_frag))".
+    iMod (ghost_var_update _ ((j, K) : leibnizO (nat * (sexpr → sexpr))) with "[$] [$]")
+         as "(Hthread_auth&Hthread_frag)".
+    iModIntro.
+    iSplitL "Hthread_auth Hj".
+    { unshelve (iExists _, _, _; iFrame); eauto. }
+    iFrame. iSplit; first done.
+    iIntros (l) "(Huninit&Hvals) Hthread".
+    iDestruct "Hthread" as (j' K' ?) "(Hj&Hthread_auth)".
+    rewrite /thread_tok_auth.
+    unify_ghost.
+    iMod (ghost_step_log_init with "[$] [$] [$] [$]") as (l') "(Hj&#Hopen&Hvals)".
+    { solve_ndisj. }
+    iModIntro. iSplitR "Hvals".
+    * iExists _. iFrame. iFrame "Hopen".
+    * iFrame. iSplitL ""; iExists _; iFrame "Hopen".
+Qed.
+
+Theorem wpc_Open j γ K `{LanguageCtx _ K} k k' E2:
+  (S k < k')%nat →
+  {{{ spec_ctx ∗ log_inv γ k' ∗ j ⤇ K (ExternalOp (ext := spec_ext_op_field) OpenOp #()) }}}
+    Open #() @ NotStuck; LVL (S (S (S k))); ⊤; E2
+  {{{ l, RET #l;  is_log γ k' l ∗ (∃ (l': loc), j ⤇ K (of_val #l': sexpr) ∗ log_open l')}}}
+  {{{ True }}}.
+Proof using SIZE_bounds.
+  iIntros (? Φ Φc) "(#Hspec&#Hinv&Hj) HΦ".
+  unshelve
+  wpc_apply (wpc_Open _ _ _ _ _ _ _ _ _ _ (True%I) (λ l, (∃ (l': loc), j ⤇ K (of_val #l') ∗ log_open l'))%I
+               with "[Hj]"); try iFrame "Hinv"; eauto.
+  { apply _. }
+  iSplit; [| iSplit].
+  - iIntros "(Huninit&Hlist&Htok)".
+    iMod (log_uninit_open_false with "[$] [$] [$] Hj") as %[].
+    { solve_ndisj. }
+  - iIntros "[Hiniting|[Hopening|Hopened]]".
+    * iDestruct "Hiniting" as (j' K' Hctx') "(Hj'&_)".
+      iMod (log_init_open_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+    * iDestruct "Hopening" as (j' K' Hctx') "(Hj'&_)".
+      iMod (log_open_open_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+    * iDestruct "Hopened" as (l) "Hopen".
+      iMod (log_opened_open_false with "[$] [$] [$]") as %[].
+      { solve_ndisj. }
+  - iSplit; last done. simpl.
+    iIntros (vs) "(Hclosed_frag&Hvals_frag&(Hthread_auth&Hthread_frag))".
+    iMod (ghost_var_update _ ((j, K) : leibnizO (nat * (sexpr → sexpr))) with "[$] [$]")
+         as "(Hthread_auth&Hthread_frag)".
+    iModIntro.
+    iSplitL "Hthread_auth Hj".
+    { unshelve (iExists _, _, _; iFrame); eauto. }
+    iFrame. iSplit; first done.
+    iIntros (l) "(Huninit&Hvals) Hthread".
+    iDestruct "Hthread" as (j' K' ?) "(Hj&Hthread_auth)".
+    rewrite /thread_tok_auth.
+    unify_ghost.
+    iMod (ghost_step_log_open with "[$] [$] [$] [$]") as (l') "(Hj&#Hopen&Hvals)".
+    { solve_ndisj. }
+    iModIntro. iSplitR "Hvals".
+    * iExists _. iFrame. iFrame "Hopen".
+    * iFrame. iSplitL ""; iExists _; iFrame "Hopen".
+Qed.
+
+Theorem wpc_Log__Reset j γ γ0 K `{LanguageCtx _ K} k k' E2 (l l': loc):
+  (S (S k) < k')%nat →
+  {{{ spec_ctx ∗ log_inv γ0 k' ∗ j ⤇ K (ExternalOp (ext := spec_ext_op_field) (ResetOp) #l') ∗
+               is_log γ k' l ∗ log_open l'
+  }}}
+    Log__Reset #l @ NotStuck; (LVL (S (S (S k)))); ⊤; E2
+  {{{ RET #(); j ⤇ K (of_val #(): sexpr)}}}
+  {{{ True }}}.
+Proof using SIZE_bounds.
+  iIntros (? Φ Φc) "(#Hspec&#Hinv&Hj&His_log&#Hopen) HΦ".
+  wpc_apply (wpc_Log__Reset _ _ _ _ _ _ _ _ _ _ (j ⤇ K (of_val #()))%I True%I
+               with "[Hj His_log Hopen]"); try iFrame "His_log"; eauto.
+  iSplit; last done.
+  iIntros (bs).
+  iIntros "Hopened". iDestruct "Hopened" as (?) "(_&Hfrag)".
+  iMod (ghost_step_log_reset with "[$] [$] [$] [$]") as "(Hj&Hvals)".
+  { solve_ndisj. }
+  iModIntro. iFrame. iExists _. eauto.
+Qed.
+
+
+End refinement.
+
