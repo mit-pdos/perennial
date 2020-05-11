8ae8517e93d98eba820be53e7e273ab2d32a2f18
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2020-05-11 13:05:06 -0400

    More wal recovery proof.

--- src/program_proof/wal/recovery_proof.v
+++ src/program_proof/wal/recovery_proof.v
@@ -28,12 +28,20 @@ Theorem wp_MkLog_init d (bs: list Block) :
 Proof.
 Admitted.
 
-Theorem wpc_mkLog_recover stk k E1 E2 d γ σ :
-  {{{ is_wal_inner_durable γ σ }}}
-    mkLog #d @ stk; k; E1; E2
-  {{{ σ' γ' l, RET #l;
+(* XXX: this used to have a postcondition that would give you some σ' which was
+   the crash of σ:
+
       ⌜relation.denote (log_crash) σ σ' tt⌝ ∗
-       is_wal_inv_pre l γ' σ' ∗
+
+   However, I think simulating the crash to σ' should be
+   done using post_crash modality at the time when we initially
+   crashed. mkLog_recover is itself a no-op at the spec level. *)
+
+Theorem wpc_mkLog_recover k E2 d γ σ :
+  {{{ is_wal_inner_durable γ σ }}}
+    mkLog #d @ NotStuck; k; ⊤; E2
+  {{{ γ' l, RET #l;
+       is_wal_inv_pre l γ' σ ∗
        (* XXX whatever it is that background threads needs *)
        True}}}
   {{{ ∃ γ', is_wal_inner_durable γ' σ }}}.
@@ -59,7 +67,8 @@ Proof.
   { iIntros "Hcirc". show_crash2. }
 
   iNext. iIntros (γ' c diskStart diskEnd bufSlice upds).
-  iIntros "(Hupd_slice&Hcirc&Happender&Hstart&Hdisk&%&%)".
+  iIntros "(Hupd_slice&Hcirc&Happender&Hstart&Hdisk&%&%&%)".
+  iDestruct (is_circular_state_wf with "Hcirc") as %Hwf_circ.
 
   wpc_frame_compl "Hupd_slice".
   { crash_case. iExists (set circ_name (λ _, γ') γ). destruct γ.
@@ -68,17 +77,34 @@ Proof.
     (* XXX: need to somehow show that all of these predicates do not depend on the circ_name... *)
     admit.
   }
-  (* XXX: these tuples are parenthesized the wrong way for these projections *)
-  wp_pure _.
-  wp_pure _.
-  wp_pure _.
-  wp_pure _.
-  wp_pure _.
-  wp_pure _.
   wp_pures.
-  iDestruct "Hupd_slice" as (?) "(Hbufsloc&Hupds)".
-  (* How can end be the Fst of #bufSlice.(Slice.cap) which appears
-     to be a u64 ??? *)
+  wp_apply (wp_new_free_lock); iIntros (ml) "Hlock".
+  wp_pures.
+  iDestruct (updates_slice_to_frag with "[$]") as "Hupd_slice".
+  wp_apply (wp_mkSliding with "[$]").
+  { destruct Hwf_circ as (?&?). subst; lia. }
+  iIntros (lslide) "Hsliding".
+  wp_apply wp_allocStruct; first by auto.
+  iIntros (st) "Hwal_state".
+  wp_pures.
+  iMod (alloc_lock _ _ _ (wal_linv st γ) with "[$] [-]").
+  { rewrite /wal_linv.
+    iExists {| diskEnd := diskEnd; memLog := _ |}. iSplitL "Hwal_state Hsliding".
+    { iExists {| memLogPtr := _; shutdown := _; nthread := _ |}.
+      iDestruct (struct_fields_split with "Hwal_state") as "Hwal_state".
+      iDestruct "Hwal_state" as "(?&?&?&?&_)".
+      iFrame. iPureIntro. rewrite /locked_wf//=.
+      { destruct Hwf_circ as (?&?). subst. split.
+        * split; first lia.
+          (* XXX: This constraint seems unprovable (?) but maybe I need to exploit the fact
+             that we should assume the is_wal_state in the precondition is a post-crash state ?? *)
+          admit.
+        * rewrite /slidingM.wf //=; split; try lia.
+      }
+    }
+    (* XXX: need to create a whole bunch of ghost state earlier *)
+    admit.
+  }
 Abort.
 
 
