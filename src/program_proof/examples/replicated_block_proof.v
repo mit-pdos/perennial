826ba38588b054912950b9791395c5cc23933fe3
Tej Chajed <tchajed@mit.edu>
2020-06-03 15:49:10 -0500

    Implement some reasonable crash condition caching

--- src/program_proof/examples/replicated_block_proof.v
+++ src/program_proof/examples/replicated_block_proof.v
@@ -234,6 +234,108 @@ Section goose.
     iApply "HΦ"; iFrame.
   Admitted.
 
+  Record cache {PROP:bi} (R: PROP) :=
+    Cache { cache_prop :> env PROP;
+            (* TODO: make this a string pattern *)
+            cache_names: list ident; }.
+
+  Arguments Cache {PROP}.
+  Arguments cache_names {PROP R} c.
+  Arguments cache_prop {PROP R} c.
+
+  Definition cached_def {R} (c: cache R): iProp Σ :=
+    □ ([∗] c -∗ R).
+  Definition cached_aux : seal (@cached_def). by eexists. Qed.
+  Definition cached := unseal cached_aux.
+  Definition cached_eq : @cached = @cached_def := seal_eq cached_aux.
+  Arguments cached {R} c.
+
+  Ltac unseal := rewrite cached_eq /cached_def.
+
+  Global Instance cached_Persistent : Persistent (@cached R c).
+  Proof. unseal; apply _. Qed.
+
+  Theorem tac_cached_use {Δ: envs (iPropI Σ)} i {R} (c: cache R) :
+    envs_lookup i Δ = Some (true, cached c) →
+    match envs_split base.Left c.(cache_names) Δ with
+    | Some (Γs, _) => Γs.(env_spatial) = c.(cache_prop)
+    | None => False
+    end →
+    envs_entails Δ R.
+  Proof.
+    rewrite cached_eq.
+    iIntros (Hlookup Hsubenv).
+    destruct_with_eqn (envs_split base.Left c.(cache_names) Δ); [ | contradiction ].
+    destruct p as [Γs Γ'].
+    rewrite envs_entails_eq.
+    iIntros "HΔ".
+    iDestruct (envs_lookup_intuitionistic_sound _ _ _ Hlookup with "HΔ") as
+        "[#Hcache HΔ]".
+    iDestruct (envs_split_sound with "HΔ") as "[HΔ1 HΔ2]"; eauto.
+    iDestruct (envs_clear_spatial_sound with "HΔ1") as "(HΔ'&HΔs)".
+    iApply "Hcache".
+    rewrite -Hsubenv.
+    iAssumption.
+  Qed.
+
+  Theorem cached_make R (c: cache R) :
+    □ (env_to_named_prop c -∗ R) -∗
+    cached c.
+  Proof.
+    unseal.
+    iIntros "#HR !>".
+    rewrite env_to_named_prop_unname.
+    rewrite env_to_prop_sound.
+    auto.
+  Qed.
+
+  Ltac iCache_go P Hs pat :=
+  let Hs := words Hs in
+  let Hs := eval vm_compute in (INamed <$> Hs) in
+  let Δ := iGetCtx in
+  let js := reduction.pm_eval (envs_split base.Left Hs Δ) in
+  match js with
+  | Some (?Δ, _) => let Γs := (eval cbv [env_spatial] in Δ.(env_spatial)) in
+                    iAssert (cached (Cache P Γs Hs)) as pat;
+                    [ iApply cached_make; iModIntro;
+                      cbv [env_to_named_prop env_to_named_prop_go cache_prop];
+                      iNamed 1
+                    | ]
+  | None => fail 1 "hypotheses not found"
+  end.
+
+  Tactic Notation "iCache" "with" constr(Hs) :=
+    lazymatch goal with
+    | [ |- envs_entails _ (wpc _ _ _ _ _ _ ?Φc) ] =>
+         iCache_go Φc Hs "#?"
+    end.
+
+  Ltac iFromCache :=
+    lazymatch goal with
+    | [ |- envs_entails (Envs ?Γp _ _) ?P ] =>
+      match Γp with
+      | context[Esnoc _ ?i (@cached P ?c)] =>
+        apply (tac_cached_use i c);
+        [ reflexivity (* lookup should always succeed, found by context match *)
+        | reduction.pm_reduce;
+          reflexivity ]
+      end
+    end.
+
+  (* this is attractively simple but probably doesn't correctly backtrack in
+  iAssumptionCore (for using different caches) *)
+  Ltac iFromCache' :=
+    eapply tac_cached_use;
+    [ iAssumptionCore (* find a cache for the goal *)
+    | cbv [cache_prop cache_names env_to_named_prop env_to_named_prop_go];
+      reduction.pm_reduce;
+      try reflexivity
+    ].
+
+  Declare Scope cache_hide_scope.
+  Notation "'cache_for!' P 'with' Hs" := (@cached _ (Cache P _ Hs)) (at level 29, only printing) : cache_hide_scope.
+  Open Scope cache_hide_scope.
+
   Theorem wpc_RepBlock__Write (Q: iProp Σ) {k E2} γ l k' addr (s: Slice.t) q (b: Block) :
     (S k < k')%nat →
     {{{ "Hrb" ∷ is_rblock γ k' l addr ∗
@@ -254,21 +356,25 @@ Section goose.
     wp_apply (crash_lock.acquire_spec with "Hlock"); auto.
     iIntros "His_locked". iDestruct "His_locked" as (γlk) "His_locked".
     iNamed 1.
-    wpc_pures; auto.
+
+    iCache with "HΦ".
+    { by crash_case. }
+
+    wpc_pures; first by auto.
     wpc_bind_seq.
     iApply (use_crash_locked with "His_locked"); auto.
     iSplit; first by crash_case.
     iNamed 1.
-    wpc_call.
+    iNamed "Hlkinv".
+
+    iCache with "HP Hprimary Hbackup HΦ".
     { iSplitL "HΦ"; first by crash_case.
       iExists _; iFrame.
-      iApply rblock_linv_to_cinv; iFrame. }
-    iNamed "Hlkinv".
+      iExists _; iFrame. }
+
+    wpc_call; first by iFromCache.
     wpc_bind (struct.loadF _ _ _).
-    wpc_frame "HΦ Hprimary Hbackup HP".
-    { iSplitL "HΦ"; first by crash_case.
-      iExists _; iFrame "HP".
-      iApply rblock_linv_to_cinv; iFrame. }
+    wpc_frame "HΦ Hprimary Hbackup HP"; first by iFromCache.
     wp_loadField.
     iNamed 1.
     wpc_apply (wpc_Write_fupd (⊤ ∖ ↑N) ("Hprimary" ∷ int.val addr d↦ b ∗
@@ -291,22 +397,17 @@ Section goose.
       - iExists _; iFrame.
         iExists _; iFrame. }
     iIntros "(Hb&Hpost)"; iNamed "Hpost".
+    iCache with "HΦ Hprimary Hbackup HP".
+    { iSplitL "HΦ"; first by crash_case.
+      iExists _; iFrame.
+      iExists _; iFrame. }
 
-    wpc_pures.
-    { iSplitL "HΦ"; first by crash_case.
-      iExists _; iFrame.
-      iExists _; iFrame. }
+    wpc_pures; first by iFromCache.
     wpc_bind (struct.loadF _ _ _).
-    wpc_frame "HΦ Hprimary Hbackup HP".
-    { iSplitL "HΦ"; first by crash_case.
-      iExists _; iFrame.
-      iExists _; iFrame. }
+    wpc_frame "HΦ Hprimary Hbackup HP"; first by iFromCache. (* TODO: can we get these from a cache? *)
     wp_loadField.
     iNamed 1.
-    wpc_pures.
-    { iSplitL "HΦ"; first by crash_case.
-      iExists _; iFrame.
-      iExists _; iFrame. }
+    wpc_pures; first by iFromCache.
     wpc_apply (wpc_Write with "[$Hb Hbackup]").
     { iExists _; iFrame. }
     iSplit; [ | iNext ].
@@ -318,7 +419,7 @@ Section goose.
     iSplitR "Hprimary Hbackup HP"; last first.
     { iExists _; iFrame. }
     iIntros "His_locked".
-    iSplit; last by crash_case.
+    iSplit; last by iFromCache.
     wpc_pures; first by crash_case.
     wpc_frame "HΦ"; first by crash_case.
     wp_loadField.
