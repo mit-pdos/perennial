9a4b9ca8ec6ac423fd7b8451a607bfc1d80a6890
Tej Chajed <tchajed@mit.edu>
2020-06-11 15:27:11 -0500

    Fix how ghost state works in single inode proof
    Not necessarily better for this proof but closer to how the directory
    proof will work, when allocator and abstract state is spread out over
    all the inodes.

--- src/program_proof/examples/single_inode_proof.v
+++ src/program_proof/examples/single_inode_proof.v
@@ -12,12 +12,15 @@ Module s_inode.
   Definition t := list Block.
 End s_inode.
 
+Definition listBlockO := discreteO (list Block).
+
 Section goose.
   Context `{!heapG Σ}.
   Context `{!lockG Σ}.
   Context `{!crashG Σ}.
   Context `{!allocG Σ}.
   Context `{!stagedG Σ}.
+  Context `{!inG Σ (ghostR listBlockO)}.
 
   Implicit Types (l:loc) (σ: s_inode.t) (γ: gname).
 
@@ -25,22 +28,22 @@ Section goose.
   Let allocN := nroot.@"allocator".
   Context (P: s_inode.t → iProp Σ).
 
-  (* TODO: is γused the right strategy for connecting the used blocks? *)
-  (* (more technically, is it ok that γused is existentially quantified? I only
-  imagine using it for one step, so it doesn't matter if it changes. *)
-
   Definition is_single_inode l (sz: Z) k' k'' : iProp Σ :=
-    ∃ (inode_ref alloc_ref: loc) γinode γalloc γused,
+    ∃ (inode_ref alloc_ref: loc) γinode γalloc γused γblocks,
       l ↦[SingleInode.S :: "i"] #inode_ref ∗
       l ↦[SingleInode.S :: "alloc"] #alloc_ref ∗
       is_inode inode_ref k' γinode
-        (λ s, P s.(inode.blocks) ∗
-              own γused (●{1/2} Excl' s.(inode.addrs))) (U64 0) ∗
+        (λ s, own γblocks (◯ Excl' (s.(inode.blocks): listBlockO)) ∗
+              own γused (●{1/2} Excl' s.(inode.addrs)))
+        (U64 0) ∗
       is_allocator
         (λ s, own γused (●{1/2} Excl' (alloc.used s)))
         (λ a, ∃ b, int.val a d↦ b)
         allocN alloc_ref (rangeSet 1 (sz-1)) γalloc k'' ∗
-      inv N (∃ (used:gset u64), own γused (◯ Excl' used))
+      inv N (∃ (blocks: list Block) (used:gset u64),
+                own γblocks (● Excl' (blocks: listBlockO)) ∗
+                P blocks ∗
+                own γused (◯ Excl' used))
   .
 
 End goose.
