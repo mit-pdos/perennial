94422eb77e16bb37c3cbbd2d60371521acb14e40
Tej Chajed <tchajed@mit.edu>
2020-07-03 08:26:58 -0500

    Make assumptions printing look nicer

--- src/program_proof/examples/assumptions.v
+++ src/program_proof/examples/assumptions.v
@@ -1,4 +1,7 @@
-(** Just testing which admits are needed by some top-level theorems *)
+(** Test which admits are needed by some top-level theorems *)
+(* importing the prelude ensures notations are in scope for these lemma
+statements *)
+From Perennial.program_proof Require Import proof_prelude.
 From Perennial.program_proof.examples Require Import
      replicated_block_proof single_inode_proof dir_proof.
 
