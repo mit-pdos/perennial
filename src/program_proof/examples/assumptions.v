92bef83d9247c51e95a624f51408701f39a58990
Ralf Jung <research@ralfj.de>
2020-07-04 10:46:43 +0200

    add alloc and init_zero lemmas to assumptions check

--- src/program_proof/examples/assumptions.v
+++ src/program_proof/examples/assumptions.v
@@ -5,10 +5,17 @@ From Perennial.program_proof Require Import proof_prelude.
 From Perennial.program_proof.examples Require Import
      replicated_block_proof single_inode_proof dir_proof.
 
+Print Assumptions replicated_block_proof.init_zero_cinv. (* not used by [OpenRead] *)
 Print Assumptions replicated_block_proof.OpenRead_adequate.
+
+(* FIXME: also prove and check zero_init lemma. *)
 Print Assumptions single_inode_proof.wpc_Open.
+Print Assumptions single_inode_proof.is_single_inode_alloc.
 Print Assumptions single_inode_proof.wpc_Read.
 Print Assumptions single_inode_proof.wpc_Append.
+
+(* FIXME: also prove and check zero_init lemma. *)
 Print Assumptions dir_proof.wpc_Open.
+Print Assumptions dir_proof.is_dir_alloc.
 Print Assumptions dir_proof.wpc_Read.
 Print Assumptions dir_proof.wpc_Append.
