16cc8813714f090a12311ba7b68ce707dee6f6e6
Tej Chajed <tchajed@mit.edu>
2020-07-07 16:34:43 -0500

    Make print assumptions slightly faster

--- src/program_proof/examples/print_assumptions.v
+++ src/program_proof/examples/print_assumptions.v
@@ -2,34 +2,40 @@
 (* importing the prelude ensures notations are in scope for these lemma
 statements *)
 From Perennial.program_proof Require Import proof_prelude.
-From Perennial.program_proof.examples Require Import
+From Perennial.program_proof.examples Require
      replicated_block_proof single_inode_proof dir_proof.
+
+Definition replicated_block_theorems :=
+  (@replicated_block_proof.init_zero_cinv,  (* not used by [OpenRead] *)
+   @replicated_block_proof.OpenRead_adequate).
+
+Definition single_inode_theorems :=
+  (@single_inode_proof.init_single_inode,
+   @single_inode_proof.wpc_Open,
+   @single_inode_proof.is_single_inode_alloc,
+   @single_inode_proof.wpc_Read,
+   @single_inode_proof.wpc_Append).
+
+Definition dir_theorems :=
+  (@dir_proof.init_dir,
+   @dir_proof.wpc_Open,
+   @dir_proof.is_dir_alloc,
+   @dir_proof.wpc_Read,
+   @dir_proof.wpc_Size,
+   @dir_proof.wpc_Append,
+   @dir_proof.wpr_Open).
 
 Goal True.
 
 idtac "replicated block:".
-Print Assumptions replicated_block_proof.init_zero_cinv. (* not used by [OpenRead] *)
-Print Assumptions replicated_block_proof.OpenRead_adequate.
+Print Assumptions replicated_block_theorems.
 
 idtac "".
-idtac "single inode initialization:".
-Print Assumptions single_inode_proof.init_single_inode.
-Print Assumptions single_inode_proof.wpc_Open.
-Print Assumptions single_inode_proof.is_single_inode_alloc.
-idtac "single inode operations:".
-Print Assumptions single_inode_proof.wpc_Read.
-Print Assumptions single_inode_proof.wpc_Append.
+idtac "single inode:".
+Print Assumptions single_inode_theorems.
 
 idtac "".
-idtac "directory initialization:".
-Print Assumptions dir_proof.init_dir.
-Print Assumptions dir_proof.wpc_Open.
-Print Assumptions dir_proof.is_dir_alloc.
-idtac "directory operations:".
-Print Assumptions dir_proof.wpc_Read.
-Print Assumptions dir_proof.wpc_Size.
-Print Assumptions dir_proof.wpc_Append.
-idtac "directory wpr:".
-Print Assumptions dir_proof.wpr_Open.
+idtac "directory:".
+Print Assumptions dir_theorems.
 
 Abort. (* leave [Goal] *)
