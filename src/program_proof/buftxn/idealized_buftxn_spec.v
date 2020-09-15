ef6f65ccc0616ca986c852d27fd8f4980fbf6656
Tej Chajed <tchajed@mit.edu>
2020-09-15 15:11:03 -0500

    Switch gamma and gamma-txn
    gamma-txn is now used for the ghost name specific to a txn

--- src/program_proof/buftxn/idealized_buftxn_spec.v
+++ src/program_proof/buftxn/idealized_buftxn_spec.v
@@ -24,80 +24,83 @@ Definition objData (obj: object): bufDataT (objKind obj) := projT2 obj.
 Class buftxnG Σ :=
   { buftxn_buffer_inG :> mapG Σ addr object; }.
 
+Record buftxn_names Σ :=
+  { buftxn_txn_names : @txn_names Σ; }.
+
 Section goose_lang.
   Context `{!heapG Σ}.
   Context `{!buftxnG Σ}.
 
-  Implicit Types (l: loc) (γtxn: @txn_names Σ) (γ: gname).
+  Implicit Types (l: loc) (γ: buftxn_names Σ) (γtxn: gname).
   Implicit Types (obj: object).
 
   (* this is for the entire txn manager, and relates it to some ghost state *)
-  Definition is_txn l γtxn : iProp Σ.
+  Definition is_txn l γ : iProp Σ.
   Admitted.
 
   (* TODO: eventually need a proper name for this; I think of it as "the right
   to use address [a] in a transaction", together with the fact that the current
   disk value is obj *)
   (* this is probably just [mspec.mapsto_txn] *)
-  Definition modify_token γtxn (a: addr) obj: iProp Σ.
+  Definition modify_token γ (a: addr) obj: iProp Σ.
   Admitted.
 
   (* TODO: I think this just connects to γUnified.(crash_states), which is the
   name of an auth log_heap; when maintaining synchrony the ownership is really
   simple since we only fully own the latest and forward value *)
-  Definition stable_maps_to γtxn (a:addr) obj: iProp Σ.
+  Definition stable_maps_to γ (a:addr) obj: iProp Σ.
   Admitted.
 
   (* TODO: name this better *)
-  Definition buftxn_ctx γ (bufs: gmap addr buf) : iProp Σ :=
+  Definition buftxn_ctx γtxn (bufs: gmap addr buf) : iProp Σ :=
     ∃ (owned_bufs: gmap addr object),
       (* NOTE: this data is a superset of what's actually in the buftxn; can do
       an update to move ownership of an address into the transaction, which
       makes it available for reading (actually materializing the value) and
       writing (including overwrites without reading) *)
       "%Hown_super" ∷ ⌜(λ bf, existT bf.(bufKind) bf.(bufData)) <$> bufs ⊆ owned_bufs⌝ ∗
-      "Howned●" ∷ map_ctx γ owned_bufs.
+      "Howned●" ∷ map_ctx γtxn owned_bufs.
 
   (* this is for a single buftxn (transaction) - not persistent, buftxn's are
   not shareable *)
   (* TODO: this is re-deriving mspec.is_buftxn; build directly on top, but
   replace mT argument with a ghost name *)
-  Definition is_buftxn l γtxn γ : iProp Σ :=
+  Definition is_buftxn l γ γtxn : iProp Σ :=
     ∃ (txn_l: loc) (bufs_l: loc) (bufs: gmap addr buf),
       "txn" ∷ readonly (l ↦[BufTxn.S :: "txn"] #txn_l) ∗
       "bufs" ∷ readonly (l ↦[BufTxn.S :: "bufs"] #bufs_l) ∗
-      "Htxn" ∷ is_txn txn_l γtxn ∗
+      "Htxn" ∷ is_txn txn_l γ ∗
       "Hbufs" ∷ is_bufmap bufs_l bufs ∗
-      "Hctx" ∷ buftxn_ctx γ bufs ∗
-      "Hold_stable" ∷ ([∗ map] a↦_ ∈ bufs, ∃ obj0, modify_token γtxn a obj0).
+      "Hctx" ∷ buftxn_ctx γtxn bufs ∗
+      "Hold_stable" ∷ ([∗ map] a↦_ ∈ bufs, ∃ obj0, modify_token γ a obj0).
 
-  Definition buftxn_maps_to γ (a: addr) obj : iProp Σ :=
-    ∃ q, ptsto γ a q obj.
+  Definition buftxn_maps_to γtxn (a: addr) obj : iProp Σ :=
+    ∃ q, ptsto γtxn a q obj.
 
-  Theorem lift_into_txn γtxn γ bufs a obj :
-    buftxn_ctx γ bufs -∗
-    modify_token γtxn a obj ==∗
-    buftxn_maps_to γ a obj ∗ buftxn_ctx γ bufs.
+  Theorem lift_into_txn γ γtxn bufs a obj :
+    buftxn_ctx γtxn bufs -∗
+    modify_token γ a obj ==∗
+    buftxn_maps_to γtxn a obj ∗ buftxn_ctx γtxn bufs.
   Proof.
     (* TODO: allocate into buftxn_ctx, consume modify_token into
     buftxn_ctx *)
   Admitted.
 
-  Lemma modify_token_conflicting γtxn :
-    Conflicting (modify_token γtxn) (modify_token γtxn).
+  Lemma modify_token_conflicting γ :
+    Conflicting (modify_token γ) (modify_token γ).
   Proof.
   Admitted.
 
   Theorem lift_liftable_into_txn `{!Liftable P}
-          γtxn γ bufs :
-    buftxn_ctx γ bufs -∗
-    P (modify_token γtxn) ==∗
-    P (buftxn_maps_to γ) ∗ buftxn_ctx γ bufs.
+          γ γtxn bufs :
+    buftxn_ctx γtxn bufs -∗
+    P (modify_token γ) ==∗
+    P (buftxn_maps_to γtxn) ∗ buftxn_ctx γtxn bufs.
   Proof.
     iIntros "Hctx HP".
     iDestruct (liftable (P:=P) with "HP") as (m) "[Hm HP]".
     { apply modify_token_conflicting. }
-    iSpecialize ("HP" $! (buftxn_maps_to γ)).
+    iSpecialize ("HP" $! (buftxn_maps_to γtxn)).
     iInduction m as [|i x m] "IH" using map_ind.
     - iModIntro.
       setoid_rewrite big_sepM_empty.
@@ -119,9 +122,9 @@ Section goose_lang.
                        bufData := objData obj;
                        bufDirty := dirty |}.
 
-  Theorem wp_BufTxn__ReadBuf l γtxn γ (a: addr) (sz: u64) obj :
+  Theorem wp_BufTxn__ReadBuf l γ γtxn (a: addr) (sz: u64) obj :
     bufSz (objKind obj) = int.nat sz →
-    {{{ is_buftxn l γtxn γ ∗ buftxn_maps_to γ a obj }}}
+    {{{ is_buftxn l γ γtxn ∗ buftxn_maps_to γtxn a obj }}}
       BufTxn__ReadBuf #l (addr2val a) #sz
     {{{ (l:loc), RET #l; is_object l a obj }}}.
   Proof.
@@ -136,13 +139,13 @@ Section goose_lang.
     | bufBlock b => vec_to_list b = data
     end.
 
-  Theorem wp_BufTxn__OverWrite l γtxn γ (a: addr) (sz: u64)
+  Theorem wp_BufTxn__OverWrite l γ γtxn (a: addr) (sz: u64)
           (data_s: Slice.t) q (data: list byte) obj0 obj :
     bufSz (objKind obj) = int.nat sz →
     data_has_obj data obj →
-    {{{ is_buftxn l γtxn γ ∗ buftxn_maps_to γ a obj0 ∗ is_slice data_s byteT q data }}}
+    {{{ is_buftxn l γ γtxn ∗ buftxn_maps_to γtxn a obj0 ∗ is_slice data_s byteT q data }}}
       BufTxn__OverWrite #l (addr2val a) (slice_val data_s)
-    {{{ RET #(); buftxn_maps_to γ a obj }}}.
+    {{{ RET #(); buftxn_maps_to γtxn a obj }}}.
   Proof.
   Admitted.
 
@@ -154,9 +157,9 @@ Section goose_lang.
   predicates; TODO: won't it be difficult to establish that the footprint of P
   hasn't changed in the invariant? it hasn't because we've had it locked, but we
   don't have ownership over it... *)
-  Theorem wp_BufTxn__CommitWait l γtxn γ E `{!Liftable P0, !Liftable P} Q :
-    {{{ is_buftxn l γtxn γ ∗ P (buftxn_maps_to γ) ∗
-        |={⊤,E}=> (P0 (stable_maps_to γtxn) ∗ (P (stable_maps_to γtxn) ={E,⊤}=∗ Q))  }}}
+  Theorem wp_BufTxn__CommitWait l γ γtxn E `{!Liftable P0, !Liftable P} Q :
+    {{{ is_buftxn l γ γtxn ∗ P (buftxn_maps_to γtxn) ∗
+        |={⊤,E}=> (P0 (stable_maps_to γ) ∗ (P (stable_maps_to γ) ={E,⊤}=∗ Q))  }}}
       BufTxn__CommitWait #l #true
     {{{ (n:u64), RET #n; Q }}}.
   Proof.
