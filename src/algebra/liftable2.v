07fd568de5e2059eeafea673656b0ccb1f051d53
Tej Chajed <tchajed@mit.edu>
2020-09-17 17:29:37 -0500

    Work through more of Lifted

--- src/algebra/liftable2.v
+++ src/algebra/liftable2.v
@@ -4,10 +4,10 @@ From Perennial.Helpers Require Import NamedProps ipm gset.
 From Perennial.algebra Require Import big_op.
 
 Set Default Proof Using "Type".
+Set Default Goal Selector "!".
 
 Section bi.
   Context {PROP:bi}.
-  Context `{BiInternalEq PROP}.
 
   Context `{L : Type}.
   Context `{V : Type}.
@@ -21,10 +21,10 @@ Section bi.
   Class Lifted P d := lifted :
       ∀ mapsto1, Conflicting mapsto1 →
     P mapsto1 -∗
-      ∃ m, "%Hdom" ∷ ⌜dom _ m = d⌝ ∗
+      ∃ m, "%Hdom" ∷ <affine> ⌜dom _ m = d⌝ ∗
            "Hm" ∷ ([∗ map] a↦v ∈ m, mapsto1 a v) ∗
            "Hmapsto2" ∷
-           ∀ mapsto2, ⌜Conflicting mapsto2⌝ -∗
+           ∀ mapsto2, <affine> ⌜Conflicting mapsto2⌝ -∗
                        (([∗ map] a↦v ∈ m, mapsto2 a v) -∗ P mapsto2).
 
   Theorem lifted_acc `{!Lifted P d} mapsto1 mapsto2 `{!Conflicting mapsto1, !Conflicting mapsto2} :
@@ -43,7 +43,29 @@ Section bi.
     iApply "Hmapsto2"; iFrame "%∗".
   Qed.
 
-  Instance star_lifted `{!Lifted P d1, !Lifted Q d2} `{BiAffine PROP} :
+  Theorem lifted_strong P d :
+    (∃ Q m,
+        ∀ mapsto, Conflicting mapsto →
+              P mapsto ⊣⊢ Q ∗ <affine> ⌜dom _ m = d⌝ ∗ ([∗ map] a↦v ∈ m, mapsto a v)) →
+    Lifted P d.
+  Proof.
+    destruct 1 as (Q&m&Hequiv).
+    iIntros (mapsto ?) "HP".
+    iExists m; iFrame "%∗".
+    iDestruct (Hequiv with "HP") as "(HQ&%Hdom&Hm)".
+    iFrame "%∗".
+    iIntros (mapsto2 ?) "Hm".
+    iApply Hequiv; iFrame "%∗".
+  Qed.
+
+  Global Instance Lifted_proper :
+    Proper (pointwise_relation _ (⊣⊢) ==> (=) ==> (↔)) Lifted.
+  Proof.
+    intros P1 P2 ? d d2 <-.
+    rewrite /Lifted. setoid_rewrite H. auto.
+  Qed.
+
+  Global Instance star_lifted `{!Lifted P d1, !Lifted Q d2} `{BiAffine PROP} :
     Lifted (fun mapsto => P mapsto ∗ Q mapsto)%I (d1 ∪ d2).
   Proof.
     rewrite /Lifted.
@@ -69,7 +91,113 @@ Section bi.
     - iApply "Hm2_acc"; iFrame "%∗".
 
     Grab Existential Variables.
-    apply H1.
+    assumption.
+  Qed.
+
+  Global Instance independent_lifted (P:PROP) :
+    Lifted (fun _ => P) ∅.
+  Proof.
+    apply lifted_strong.
+    exists P, ∅; intros.
+    rewrite big_sepM_empty right_id.
+    rewrite dom_empty_L.
+    iSplit; auto.
+    iIntros "[$ _]".
+  Qed.
+
+  Global Instance exists_lifted T Φ d :
+    (forall x, Lifted (Φ x) d) ->
+    Lifted (fun h => ∃ (x : T), Φ x h)%I d.
+  Proof.
+    rewrite /Lifted; intros.
+    iDestruct 1 as (x) "HΦ".
+    iDestruct (H with "HΦ") as "H"; iNamed "H".
+    iExists m.
+    iFrame "%∗".
+    iIntros (??) "Hm".
+    iExists x.
+    iApply "Hmapsto2"; auto.
+  Qed.
+
+  Global Instance singleton_lifted a v :
+    Lifted (fun mapsto => mapsto a v) {[a]}.
+  Proof.
+    apply lifted_strong.
+    exists emp%I, {[a := v]}.
+    intros.
+    rewrite big_sepM_singleton.
+    rewrite dom_singleton_L.
+    rewrite left_id.
+    iSplit; by [iIntros "$" | iIntros "[_ $]"].
+  Qed.
+
+  Lemma dom_map_to_list m :
+    dom (gset L) m = list_to_set (map_to_list m).*1.
+  Proof.
+    induction m as [|l v m] using map_ind.
+    - rewrite dom_empty_L map_to_list_empty //.
+    - rewrite map_to_list_insert //.
+      rewrite dom_insert_L.
+      rewrite fmap_cons /=.
+      rewrite -IHm //.
+  Qed.
+
+  Lemma dom_singleton_inv m a :
+    dom (gset _) m = {[a]} →
+    ∃ v, m = {[a := v]}.
+  Proof.
+    intros.
+    assert (∃ v, map_to_list m = [(a, v)]).
+    { rewrite dom_map_to_list in H.
+      generalize dependent (map_to_list m); intros es.
+      destruct es; simpl.
+      - intros H%(f_equal elements).
+        rewrite elements_empty elements_singleton in H; congruence.
+      - intros.
+        destruct es.
+        + simpl in H.
+          rewrite right_id_L in H.
+          destruct p; simpl in *.
+          apply (f_equal elements) in H.
+          rewrite !elements_singleton in H.
+          inversion H; eauto.
+        + (* this destruct is the wrong strategy; list_to_set could in principle have many duplicates *)
+          admit.
+    }
+    rewrite -(list_to_map_to_list m).
+    destruct H0 as [v ->].
+    exists v; auto.
+  Admitted.
+
+  Global Instance map_singleton_lifted m Φ :
+    (∀ a v, Lifted (Φ a v) {[a]}) →
+    Lifted (fun mapsto => [∗ map] a ↦ v ∈ m, (Φ a v mapsto))%I (dom _ m).
+  Proof.
+    intros LiftedΦ.
+    iIntros (??) "Hm".
+    iInduction m as [|l v m] "IH" using map_ind.
+    - iExists ∅.
+      setoid_rewrite big_sepM_empty.
+      eauto with iFrame.
+    - setoid_rewrite big_sepM_insert; auto.
+      iDestruct "Hm" as "[HΦ Hm]".
+      iDestruct (LiftedΦ with "HΦ") as (ml) "(%Hdom & Hml & HΦ)".
+      apply dom_singleton_inv in Hdom as [v' ->].
+      setoid_rewrite big_sepM_singleton.
+      iDestruct ("IH" with "Hm") as (m') "(%Hdom'&Hm'&Hmapsto2)".
+      iExists (<[l:=v']> m').
+      iSplitR; [ iPureIntro | ].
+      { rewrite !dom_insert_L; congruence. }
+      assert (m' !! l = None).
+      { apply not_elem_of_dom. rewrite Hdom'. apply not_elem_of_dom.
+        auto. }
+      rewrite big_sepM_insert //.
+      iFrame.
+      iIntros (??) "Hm".
+      rewrite !big_sepM_insert //.
+      iDestruct "Hm" as "[Hl Hm']".
+      iDestruct ("HΦ" with "[% //] Hl") as "$".
+      iApply "Hmapsto2"; auto.
   Qed.
 
 End bi.
