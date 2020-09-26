110c2322a8e40a3e77bf1a42abf5709dd1ab4f35
Tej Chajed <tchajed@mit.edu>
2020-09-26 09:50:11 -0500

    Extend fractional_weaken to AsFractional
    The AsFractional version will unify better (the AsFractional instance
    will resolve correctly given only the premise of the entailment, whereas
    the fractional instance can't be resolved since that would require
    higher-order unification).

--- src/Helpers/Fractional.v
+++ src/Helpers/Fractional.v
@@ -4,16 +4,33 @@ From iris.base_logic.lib Require Import iprop.
 From iris.bi.lib Require Import fractional.
 From Perennial.Helpers Require Import Qextra.
 
-Theorem fractional_weaken {Σ} (Φ : Qp -> iProp Σ) `{fractional.Fractional _ Φ} (q1 q2 : Qp) :
-  (q1 ≤ q2)%Qc ->
-  Φ q2 -∗ Φ q1.
-Proof.
-  iIntros (Hle) "H".
-  edestruct (Qcanon.Qcle_lt_or_eq); eauto.
-  - edestruct Qp_split_lt; eauto.
-    rewrite -H1.
-    rewrite fractional.
-    iDestruct "H" as "[$ _]".
-  - apply Qp_eq in H0.
-    subst; done.
-Qed.
+Section bi.
+  Context {PROP:bi} `{!BiAffine PROP}.
+  Set Default Proof Using "All".
+
+  Implicit Types (P:PROP) (Φ: Qp → PROP) (q: Qp).
+
+  Theorem fractional_weaken Φ `{fractional.Fractional _ Φ} q1 q2 :
+    (q1 ≤ q2)%Qc ->
+    Φ q2 -∗ Φ q1.
+  Proof.
+    iIntros (Hle) "H".
+    edestruct (Qcanon.Qcle_lt_or_eq); eauto.
+    - edestruct Qp_split_lt; eauto.
+      rewrite -H1.
+      rewrite fractional.
+      iDestruct "H" as "[$ _]".
+    - apply Qp_eq in H0.
+      subst; done.
+  Qed.
+
+  Theorem as_fractional_weaken `{!fractional.AsFractional P Φ q2} q1 :
+    (q1 ≤ q2)%Qc →
+    P -∗ Φ q1.
+  Proof.
+    iIntros (?) "HP".
+    iDestruct (as_fractional (AsFractional:=H) with "HP") as "HΦ".
+    iApply fractional_weaken; eauto.
+  Qed.
+
+End bi.
