9c7efc02a7340d482eb2ba7d0381077fd715ab07
Ralf Jung <research@ralfj.de>
2020-09-29 17:42:28 +0200

    update Iris and std++; ghost-var upstreaming is complete

--- src/program_logic/ghost_var.v
+++ src/program_logic/ghost_var.v
@@ -10,30 +10,6 @@ Section lemmas.
   Global Instance ghost_var_discrete γ q a : Discretizable (ghost_var γ q a).
   Proof. rewrite /ghost_var. apply _. Qed.
 
-  (* TODO: upstream this *)
-  Lemma ghost_var_agree γ a1 q1 a2 q2 :
-    ghost_var γ q1 a1 -∗ ghost_var γ q2 a2 -∗ ⌜a1 = a2⌝.
-  Proof.
-    iIntros "Hvar1 Hvar2".
-    iDestruct (ghost_var_op_valid with "Hvar1 Hvar2") as %[_ ?]. done.
-  Qed.
-  Lemma frac_agree_op q1 q2 (a : leibnizO A) :
-    to_frac_agree (q1 + q2) a ≡ to_frac_agree q1 a ⋅ to_frac_agree q2 a.
-  Proof. rewrite /to_frac_agree -pair_op agree_idemp //. Qed.
-  Lemma ghost_var_update_2 b γ a1 q1 a2 q2 :
-    (q1 + q2 = 1)%Qp →
-    ghost_var γ q1 a1 -∗ ghost_var γ q2 a2 ==∗ ghost_var γ q1 b ∗ ghost_var γ q2 b.
-  Proof.
-    iIntros (Hq) "H1 H2".
-    iDestruct (ghost_var_op_valid with "H1 H2") as %[_ ->].
-    iCombine "H1 H2" as "H". rewrite Hq.
-    iMod (ghost_var_update with "H") as "H".
-    rewrite -Hq. iApply ghost_var_split. done.
-  Qed.
-  Lemma ghost_var_update_halves b γ a1 a2 :
-    ghost_var γ (1/2) a1 -∗ ghost_var γ (1/2) a2 ==∗ ghost_var γ (1/2) b ∗ ghost_var γ (1/2) b.
-  Proof. iApply ghost_var_update_2. apply Qp_half_half. Qed.
-
 End lemmas.
 
 
