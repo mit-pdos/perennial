484d6b9f901e19e0564ac3b3e79278b8418a0932
Upamanyu Sharma <upamanyu@mit.edu>
2021-02-15 17:28:11 -0500

    Figured out 2pc invariant (hopefully);
    The initial plan was to have four states, unprepared, prepared,
    committed, and aborted.
    Then, participants transition from unprepared to prepared, while the
    transaction coordinator would transition from (all) prepared ->
    committed, as well as unprepared -> abort and prepared -> abort.
    
    That approach was troublesome, and I couldn't tell why at first.
    The problem is that if the TC transitions from unprepared to aborted,
    there might still be a in-flightprepare request that never reached its
    destination, and so a participant might run the prepare method and want
    to logically go from Unprepared -> Prepared, but would be unable to
    because the TC already aborted. Indeed, even after the TC aborts, a
    participant might actually prepare if stuff gets reordered.
    
    So, now, each participant makes the logical transition from
    unprepared -> aborted or prepared -> aborted, instead of the TC doing
    it, and it seems like it might work. Each participant keeps a "finished"
    token that it keeps for all TIDs that it has not aborted or committed
    (including TIDs that haven't been seen at all yet). In order to
    transition to the Aborted state, you must have this finished token, so
    the TC is now prevented from making the abort transition for a
    participant.

--- src/program_proof/lockservice/two_pc_example.v
+++ src/program_proof/lockservice/two_pc_example.v
@@ -1,7 +1,6 @@
 From Perennial.algebra Require Import auth_map.
 From Perennial.program_proof Require Import proof_prelude.
 From Goose.github_com.mit_pdos.lockservice Require Import lockservice.
-(* From Perennial.program_proof.lockservice Require Import *)
 Require Import Decimal Ascii String DecimalString.
 From Perennial.goose_lang Require Import ffi.grove_ffi.
 From Perennial.program_proof Require Import lockmap_proof.
@@ -23,21 +22,77 @@ Record tpc_names :=
 mk_tpc_names  {
   committed_gn : gname ; (* tid -> option bool *)
   prepared_gn : gname ;  (* (tid, pid) -> () *)
+  finish_token_gn : gname ;
 }.
 
 Implicit Type γtpc : tpc_names.
 Implicit Type γ : participant_names.
 
+(* The participant keeps the other half *)
+Definition unprepared γtpc (tid pid:u64) : iProp Σ := (tid, pid)[[γtpc.(prepared_gn)]]↦ ().
 Definition prepared γtpc (tid pid:u64) : iProp Σ := (tid, pid)[[γtpc.(prepared_gn)]]↦ro ().
+Definition finish_token γtpc (tid pid:u64) : iProp Σ := (tid, pid)[[γtpc.(finish_token_gn)]]↦ ().
+
+Definition undecided γtpc tid : iProp Σ := tid [[γtpc.(committed_gn)]]↦ None.
+Definition aborted γtpc tid : iProp Σ := tid [[γtpc.(committed_gn)]]↦ro Some false.
+Definition committed γtpc tid : iProp Σ := tid [[γtpc.(committed_gn)]]↦ro Some true.
+
+Definition tpc_inv_single γtpc tid pid R R' : iProp Σ :=
+  unprepared γtpc tid pid ∨
+  prepared γtpc tid pid ∗ R ∨
+  committed γtpc tid ∗ prepared γtpc tid pid ∗ (R' ∨ finish_token γtpc tid pid) ∨
+  aborted γtpc tid ∗ finish_token γtpc tid pid
+.
 
 Definition tpc_inv γtpc tid R0 R1 : iProp Σ :=
-  (tid [[γtpc.(committed_gn)]]↦ None ∗
-   (prepared γtpc tid 0 ∨ R0) ∗
-   (prepared γtpc tid 1 ∨ R1)) ∨
-  ∃ d, tid [[γtpc.(committed_gn)]]↦ Some d.
+  tpc_inv_single γtpc tid 0 R0 R0 ∗
+  tpc_inv_single γtpc tid 1 R1 R1
+.
 
 Definition tpcN := nroot .@ "tpc".
-Definition is_txn (tid:u64) γtpc R0 R1 : iProp Σ := inv tpcN (tpc_inv γtpc tid R0 R1).
+Definition is_txn_single γtpc (tid pid:u64) R R': iProp Σ := inv tpcN (tpc_inv_single γtpc tid pid R R').
+Definition is_txn γtpc (tid:u64) R0 R1 : iProp Σ := inv tpcN (tpc_inv γtpc tid R0 R1).
+
+Lemma participant_prepare E γtpc tid pid R R':
+  ↑tpcN ⊆ E →
+  is_txn_single γtpc tid pid R R' -∗ finish_token γtpc tid pid-∗ R ={E}=∗ prepared γtpc tid pid ∗ finish_token γtpc tid pid.
+Proof.
+  intros ?.
+  iIntros "His_txn Hfinish_tok HR".
+  iInv "His_txn" as "[>Hunprep|[[>#Hprep _]|[Hcommitted|Haborted]]]" "Htclose".
+  {
+    (* TODO: don't use auth_map; want just mapUR *)
+    iMod (map_freeze with "[] Hunprep") as "[_ #Hprep]".
+    { admit. }
+
+    iFrame "Hprep Hfinish_tok".
+    iMod ("Htclose" with "[HR]"); last done.
+    iNext.
+    iRight.
+    iLeft.
+    iFrame "#∗".
+  }
+  { (* Already prepared *)
+    iFrame "Hfinish_tok".
+    iMod ("Htclose" with "[HR]"); last done.
+    iRight. iLeft. iFrame "#∗".
+  }
+  { (* Committed *)
+    iDestruct "Hcommitted" as "(>Hcom & >#Hprep & Hescrow)".
+    iFrame "#∗".
+    iMod ("Htclose" with "[Hcom $Hprep Hescrow]"); last done.
+    iNext.
+    iRight.
+    iRight.
+    iLeft.
+    iFrame.
+  }
+  { (* Aborted *)
+    iDestruct "Haborted" as "[_ >Hfinish_tok2]".
+    iDestruct (ptsto_valid_2 with "Hfinish_tok2 Hfinish_tok") as %Hval.
+    contradiction.
+  }
+Admitted.
 
 Record TransactionC :=
 mkTransactionC {
@@ -72,7 +127,7 @@ Definition ps_mu_inv (ps:loc) γ γtpc pid : iProp Σ :=
     "HtxnsMap" ∷ is_map (txns_ptr) txnsM ∗
     "Hkvs_ctx" ∷ map_ctx γ.(ps_kvs) 1 kvsM ∗
     "#HlockMap" ∷ is_lockMap lockMap_ptr γ.(ps_ghs) (fin_to_set u64) (λ x, x [[γ.(ps_kvs)]]↦{1/2} (map_get kvsM x).1) ∗
-    "Htxnx_prepared" ∷ [∗ map] tid ↦ txn ∈ txnsM, (prepared γtpc tid pid)
+    "#Htxnx_prepared" ∷ [∗ map] tid ↦ txn ∈ txnsM, (prepared γtpc tid pid)
 .
 
 Definition participantN := nroot .@ "participant".
@@ -142,6 +197,7 @@ Proof.
 
   (* Get prepared token *)
   iInv "Htxn" as ">Ht" "Htclose".
+  iDestruct "Ht" as "[Ht Ht2]".
   iDestruct "Ht" as "[Ht|Ht]".
   {
     iDestruct "Ht" as "(Hundecided & [Hprep | Hptsto2] & Hirrelevant)"; last first.
