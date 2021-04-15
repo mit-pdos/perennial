a264b86b774909ff529fab8ac978135aa198498b
Ralf Jung <research@ralfj.de>
2021-04-15 13:41:06 +0200

    remove old version of dist_lang

--- src/program_logic/dist_lang.v
+++ src/program_logic/dist_lang.v
@@ -3,32 +3,6 @@ derived notions/lemmas. *)
 From Perennial.program_logic Require Export language crash_lang.
 From iris.prelude Require Import options.
 
-Module dist_language1.
-  Context {Λ: language}.
-  Context {CS: crash_semantics Λ}.
-
-  Definition dist_cfg (nm : nat) : Type :=
-    vec (list (expr Λ) * state Λ) nm * global_state Λ.
-
-  (** A step of the distributed system is parameterized by the number of
-  machines and their respective "boot code", i.e., recovery procedure. *)
-  Inductive dist_step (nm : nat) (boot : vec (expr Λ) nm) :
-    dist_cfg nm → list (observation Λ) → dist_cfg nm → Prop :=
-  | dist_step_machine ρ1 κs ρ2 m e1 σ1 e2 σ2 efs t1 t2 :
-      ρ1.1 !!! m = (t1 ++ e1 :: t2, σ1) →
-      ρ2.1 = vinsert m (t1 ++ e2 :: t2 ++ efs, σ2) ρ1.1 →
-      prim_step e1 σ1 ρ1.2 κs e2 σ2 ρ2.2 efs →
-      dist_step nm boot ρ1 κs ρ2
-  | dist_step_crash ρ1 κs ρ2 m σ1 σ2 :
-      (ρ1.1 !!! m).2 = σ1 →
-      ρ2.1 = vinsert m ([boot !!! m], σ2) ρ1.1 →
-      ρ2.2 = ρ1.2 →
-      crash_prim_step CS σ1 σ2 →
-      dist_step nm boot ρ1 κs ρ2.
-
-End dist_language1.
-
-
 Section dist_language.
   Context {Λ: language}.
   Context {CS: crash_semantics Λ}.
