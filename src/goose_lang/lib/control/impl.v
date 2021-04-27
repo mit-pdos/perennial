06244aba84e7fbfcf67cec4ec674f364a5899009
Ralf Jung <research@ralfj.de>
2021-04-27 15:15:06 +0200

    docs for Assume/Assert

--- src/goose_lang/lib/control/impl.v
+++ src/goose_lang/lib/control/impl.v
@@ -3,10 +3,14 @@ From Perennial.goose_lang Require Import lang notation.
 Section goose_lang.
 Context {ext: ext_op}.
 
+(** [Assume c] goes into an endless loop if [c] does not hold. So proofs can
+assume that it holds. *)
 Definition Assume: val :=
   λ: "cond", if: Var "cond" then #()
              else (rec: "loop" <> := Var "loop" #()) #().
 
+(** [Assert c] raises UB (program gets stuck via [Panic]) if [c] does not
+hold. So proofs have to show it always holds. *)
 Definition Assert: val :=
   λ: "cond", if: Var "cond" then #()
              else Panic "assert failed".
