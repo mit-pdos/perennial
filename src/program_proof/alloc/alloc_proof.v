7ace7c7405caab5cf4d011e13365d144ba26bd1f
Tej Chajed <tchajed@mit.edu>
2021-04-30 11:04:21 -0400

    Prove wp_NumFree

--- src/program_proof/alloc/alloc_proof.v
+++ src/program_proof/alloc/alloc_proof.v
@@ -1,8 +1,25 @@
 From Perennial.program_proof Require Import disk_prelude.
 From Perennial.program_proof Require Import util_proof.
+From Perennial.Helpers Require Import bytes.
 From Goose.github_com.mit_pdos.goose_nfsd Require Import alloc.
 
 (* TODO: this file isn't using typed_slice, should fix that *)
+
+Lemma Z_u8_to_u64 x : int.Z (u8_to_u64 x) = int.Z x.
+Proof.
+  rewrite /u8_to_u64 /U64.
+  rewrite /U64.
+  rewrite word.unsigned_of_Z_nowrap //.
+  pose proof (word.unsigned_range x).
+  lia.
+Qed.
+
+Lemma and_1_u8 (x: u8) : 0 ≤ int.Z (word.and x (U8 1)) ≤ 1.
+Proof.
+  assert (Lt ≠ Gt) by congruence.
+  assert (Eq ≠ Gt) by congruence.
+  byte_cases x; vm_compute; auto.
+Qed.
 
 Section proof.
 Context `{!heapG Σ}.
@@ -328,6 +345,104 @@ Proof.
   { contradiction Hnonzero. word. }
   wp_apply (wp_freeBit with "H"); eauto.
   by iApply "HΦ".
+Qed.
+
+Lemma wp_popCnt (b: u8) :
+  {{{ True }}}
+    popCnt #b
+  {{{ (n: u64), RET #n; ⌜int.Z n ≤ 8⌝ }}}.
+Proof.
+  iIntros (Φ) "_ HΦ".
+  wp_call.
+  wp_apply (typed_mem.wp_AllocAt uint64T); auto.
+  iIntros (count_l) "Hcount".
+  wp_pures.
+  wp_apply wp_ref_to; auto.
+  iIntros (x_l) "Hx".
+  wp_pures.
+  wp_apply wp_ref_to; auto.
+  iIntros (i_l) "Hi".
+  wp_pures.
+  wp_apply (wp_forUpto (λ i, ∃ (x: u8) (count: u64),
+                           "Hx" ∷ x_l ↦[byteT] #x ∗
+                           "Hcount" ∷ count_l ↦[uint64T] #count ∗
+                           "%Hcount_bound" ∷ ⌜int.Z count ≤ int.Z i⌝)%I
+              with "[] [Hx Hcount $Hi]").
+  - word.
+  - clear.
+    iIntros (i Φ) "!> Hpre HΦ".
+    iDestruct "Hpre" as "(Hpre & Hi & %Hi)".
+    iNamed "Hpre".
+    wp_pures.
+    wp_load. wp_store. wp_load. wp_store.
+    iModIntro.
+    iApply "HΦ".
+    iFrame "Hi".
+    iExists _, _; iFrame.
+    iPureIntro.
+    word_cleanup.
+    rewrite Z_u8_to_u64.
+    pose proof (and_1_u8 b).
+    rewrite wrap_small; lia.
+  - iExists _, _; iFrame.
+    iPureIntro.
+    word.
+  - iIntros "[Hinv _]".
+    iNamed "Hinv".
+    wp_pures. wp_load.
+    iModIntro.
+    iApply "HΦ".
+    iPureIntro. word.
+Qed.
+
+Lemma wp_NumFree max l :
+  {{{ is_alloc max l }}}
+    Alloc__NumFree #l
+  {{{ (n:u64), RET #n; ⌜0 ≤ int.Z n ≤ int.Z max⌝}}}.
+Proof.
+  iIntros (Φ) "H HΦ". iNamed "H".
+  wp_call.
+  wp_loadField.
+  wp_apply (acquire_spec with "[$]").
+  iIntros "[Hlocked Hlinv]".
+  wp_pures.
+  iNamed "Hlinv".
+  wp_apply (typed_mem.wp_AllocAt uint64T); first by val_ty.
+  iIntros (count_l) "Hcount".
+  wp_pures.
+  wp_loadField.
+
+  iDestruct (is_slice_small_sz with "Hbits") as %Hsz.
+  rewrite fmap_length in Hsz.
+
+  wp_apply (wp_forSlice (λ i, ∃ (count: u64),
+                            "Hcount" ∷ count_l ↦[uint64T] #count ∗
+                            "%Hcount_bound" ∷ ⌜int.Z count ≤ 8 * int.Z i⌝)%I
+           with "[] [Hcount $Hbits]").
+  - clear Φ.
+    iIntros (i x Φ) "!> [Hinv [%Hbound %Hlookup]] HΦ". iNamed "Hinv".
+    fmap_Some in Hlookup as b.
+    wp_pures.
+    wp_load.
+    wp_apply wp_popCnt.
+    iIntros (popcnt Hpop_bound).
+    wp_store.
+    iModIntro.
+    iApply "HΦ".
+    iExists _; iFrame.
+    iPureIntro.
+    word.
+  - iExists _; iFrame.
+    iPureIntro; word.
+  - iIntros "[Hinv Hbits]". iNamed "Hinv".
+    wp_pures.
+    wp_loadField.
+    wp_apply (release_spec with "[$His_lock $Hlocked next bitmap Hbits]").
+    { iExists _, _, _; iFrame "∗%". }
+    wp_load.
+    iModIntro.
+    iApply "HΦ".
+    iPureIntro. word.
 Qed.
 
 End proof.
