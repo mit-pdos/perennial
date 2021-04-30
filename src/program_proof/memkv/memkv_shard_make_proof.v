b52259b46c06aeb7e4dbb0c34f022068c537a7a3
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2021-04-29 20:22:52 -0400

    Checkpoint Make for shard servers

--- src/program_proof/memkv/memkv_shard_make_proof.v
+++ src/program_proof/memkv/memkv_shard_make_proof.v
@@ -2,7 +2,7 @@ From Perennial.program_proof Require Import dist_prelude.
 From Goose.github_com.mit_pdos.gokv Require Import memkv.
 From Perennial.program_proof.lockservice Require Import rpc.
 
-From Perennial.program_proof.memkv Require Export memkv_shard_definitions memkv_put_proof memkv_conditional_put_proof memkv_get_proof memkv_install_shard_proof memkv_getcid_proof memkv_move_shard_proof common_proof.
+From Perennial.program_proof.memkv Require Export memkv_shard_definitions common_proof.
 
 Section memkv_shard_make_proof.
 
@@ -43,6 +43,8 @@ Proof.
   iIntros (kvss_sl) "Hkvss_sl".
   Transparent slice.T. wp_storeField. Opaque slice.T.
   replace (ref (InjLV #null))%E with ((NewMap (struct.ptrT MemKVShardClerk))) by auto.
+  remember (replicate (int.nat 65536) IntoVal_def) as initShardMapping eqn:Heq_initShardMapping.
+  remember (replicate (int.nat 65536) (@zero_val grove_op grove_ty KvMap)) as init_kvs_ptrs eqn:Heq_init_kvs_ptrs.
   wp_apply (wp_NewMap).
   iIntros (peers_ptr) "HpeersMap".
   wp_storeField.
@@ -50,13 +52,23 @@ Proof.
   { repeat econstructor. }
   iIntros (iptr) "Hi".
   wp_pures.
-  (*
-  wp_apply (wp_forUpto (λ i,
-      "shardMap" ∷ srv ↦[MemKVShardServer :: "shardMap"] (slice_val shardMap_sl) ∗
-      "HshardMap_sl" ∷ typed_slice.is_slice shardMap_sl boolT 1
-                                            (replicate (int.nat i) b ++
-                                             replicate (int.nat (U64 65536) - int.nat i) IntoVal_def))%I
-          with "[] [$Hi HshardMap_sl shardMap]").
+  wp_apply (wp_forUpto (λ i, ∃ shardMapping kvs_ptrs,
+  "%HshardMapping_dom" ∷ ⌜ (∀ i : u64, int.Z i < int.Z uNSHARD → is_Some (shardMapping !! int.nat i)) ⌝ ∗
+  "%Hkvss_dom" ∷ ⌜ (∀ i : u64, int.Z i < int.Z uNSHARD →
+                               is_Some ((fmap (λ x : loc, #x) kvs_ptrs) !! int.nat i)) ⌝ ∗
+  "kvss" ∷ srv ↦[MemKVShardServer :: "kvss"] (slice_val kvss_sl) ∗
+  "Hkvss_sl" ∷ slice.is_slice kvss_sl (mapT (slice.T byteT)) 1%Qp (fmap (λ x:loc, #x) kvs_ptrs) ∗
+  "shardMap" ∷ srv ↦[MemKVShardServer :: "shardMap"] (slice_val shardMap_sl) ∗
+  "HshardMap_sl" ∷  typed_slice.is_slice shardMap_sl boolT 1 shardMapping ∗
+  "HownShards" ∷ ([∗ set] sid ∈ (fin_to_set u64),
+                  ⌜(shardMapping !! (int.nat sid)) ≠ Some true⌝ ∨
+                  (∃ (kvs_ptr:loc) (m:gmap u64 (list u8)) (mv:gmap u64 goose_lang.val),
+                      own_shard γ.(kv_gn) sid m ∗ (* own shard *)
+                      ⌜kvs_ptrs !! (int.nat sid) = Some kvs_ptr⌝ ∗
+                      map.is_map kvs_ptr 1 (mv, (slice_val Slice.nil)) ∗
+                      ([∗ set] k ∈ (fin_to_set u64),
+                       ⌜shardOfC k ≠ sid⌝ ∨ (∃ vsl, ⌜default (slice_val Slice.nil) (mv !! k) = (slice_val vsl)⌝ ∗ typed_slice.is_slice vsl byteT (1%Qp) (default [] (m !! k))))
+                  )))%I with "[] [$Hi HshardMap_sl shardMap]").
   { word. }
   { iIntros (i Φ') "!# H HΦ".
     iDestruct "H" as "(H1&H2)".
@@ -66,16 +78,122 @@ Proof.
     wp_apply (wp_LoadAt with "[$Hi]").
     iIntros "Hi".
     wp_loadField.
-    simpl in Hbound.
-    admit.
+    iDestruct (typed_slice.is_slice_small_acc with "HshardMap_sl") as "(HshardMap_sl&HshardMap_sl_close)".
+    wp_apply (typed_slice.wp_SliceSet with "[$HshardMap_sl]").
+    { eauto. }
+    iIntros "HshardMap_sl".
+    iDestruct ("HshardMap_sl_close" with "[$HshardMap_sl]") as "HshardMap_sl".
+    wp_pures.
+    wp_if_destruct.
+    {
+      wp_pures.
+      replace (ref (InjLV (#null, #0, #0)))%E with ((NewMap (slice.T byteT))) by auto.
+      wp_apply (map.wp_NewMap). iIntros (mv) "Hmv".
+      wp_apply (wp_LoadAt with "[$Hi]").
+      iIntros "Hi".
+      wp_loadField.
+      iDestruct (is_slice_small_acc with "Hkvss_sl") as "(Hkvss_sl&Hkvss_sl_close)".
+      wp_apply (wp_SliceSet with "[$Hkvss_sl]").
+      { iPureIntro; split; eauto. }
+      iIntros "Hkvss_sl".
+      iDestruct ("Hkvss_sl_close" with "[$Hkvss_sl]") as "Hkvss_sl".
+      (*
+        edestruct (Hkvss_dom) as (?&Heq); first eassumption. eexists.
+        Search lookup fmap. eapply lookup_fmap_Some; eauto. } *)
+      wp_pures. iModIntro. iApply "HΦ".
+      { iFrame. iExists _, (<[int.nat i := mv]>kvs_ptrs). iFrame.
+        iSplit.
+        { iPureIntro. intros.
+          destruct (decide (int.nat i0 = int.nat i)) as [->|Hneq].
+          { eexists. apply list_lookup_insert. eapply lookup_lt_is_Some_1; eauto. }
+          rewrite list_lookup_insert_ne; auto.
+        }
+        assert ((int.nat i < length kvs_ptrs)%nat).
+        { erewrite <-fmap_length. eapply lookup_lt_is_Some_1; eauto. }
+        iSplit.
+        { iPureIntro. intros.
+          rewrite list_lookup_fmap.
+          destruct (decide (int.nat i0 = int.nat i)) as [->|Hneq].
+          { rewrite fmap_is_Some. eexists. apply list_lookup_insert; eauto. }
+          rewrite list_lookup_insert_ne; auto.
+          rewrite -list_lookup_fmap. eauto. }
+        iSplitL "Hkvss_sl".
+        { rewrite list_fmap_insert. eauto. }
+        assert (i ∈ (fin_to_set u64 : gset u64)).
+        { apply elem_of_fin_to_set. }
+        assert (fin_to_set u64 = {[i]} ∪ ((fin_to_set u64 : gset u64) ∖ {[i]})) as Heq_diff.
+        { apply union_difference_singleton_L; eauto. }
+        iEval (rewrite {2}Heq_diff) in "HownShards".
+        iEval (rewrite {2}Heq_diff).
+        iApply big_sepS_union.
+        { set_solver. }
+        iDestruct (big_sepS_union with "HownShards") as "(Hi&HownShards)".
+        { set_solver. }
+        iSplitL "Hi Hmv".
+        { rewrite ?big_sepS_singleton.
+          iRight. iExists _, ∅, _. iFrame. iSplitL "".
+          { admit. (* MISSING INTIAL RESOURCE *) }
+          iSplit.
+          { iPureIntro. rewrite list_lookup_insert; eauto. }
+          iPoseProof (big_sepS_intro_emp (fin_to_set u64)) as "Hemp".
+          iApply (big_sepS_mono with "Hemp").
+          { iIntros (??) "_".
+            destruct (decide (shardOfC x = i)); last by eauto.
+            { iRight. iExists _. rewrite ?lookup_empty //=.
+              iSplit; first eauto. iApply typed_slice.is_slice_zero. }
+          }
+        }
+        iApply (big_sepS_mono with "HownShards").
+        { iIntros (??) "H".
+          assert (int.nat i ≠ int.nat x).
+          { cut (i ≠ x).
+            { intros. intros Heq. apply Z2Nat.inj in Heq; try word.
+               apply int_Z_inj in Heq; eauto with *.
+            }
+            set_solver. }
+          rewrite ?list_lookup_insert_ne //.
+        }
+      }
+    }
+    {
+      wp_pures. iModIntro. iApply "HΦ".
+      { iFrame. iExists _, kvs_ptrs. iFrame.
+        iSplit.
+        { iPureIntro. intros.
+          destruct (decide (int.nat i0 = int.nat i)) as [->|Hneq].
+          { eexists. apply list_lookup_insert. eapply lookup_lt_is_Some_1; eauto. }
+          rewrite list_lookup_insert_ne; auto.
+        }
+        assert ((int.nat i < length kvs_ptrs)%nat).
+        { erewrite <-fmap_length. eapply lookup_lt_is_Some_1; eauto. }
+        iSplit.
+        { iPureIntro. eauto. }
+        assert (i ∈ (fin_to_set u64 : gset u64)).
+        { apply elem_of_fin_to_set. }
+        assert (fin_to_set u64 = {[i]} ∪ ((fin_to_set u64 : gset u64) ∖ {[i]})) as Heq_diff.
+        { apply union_difference_singleton_L; eauto. }
+        iEval (rewrite {2}Heq_diff) in "HownShards".
+        iEval (rewrite {2}Heq_diff).
+        iApply big_sepS_union.
+        { set_solver. }
+        iDestruct (big_sepS_union with "HownShards") as "(Hi&HownShards)".
+        { set_solver. }
+        iSplitL "Hi".
+        { rewrite ?big_sepS_singleton.
+          iLeft. rewrite list_lookup_insert //. eapply lookup_lt_is_Some_1; eauto. }
+        iApply (big_sepS_mono with "HownShards").
+        { iIntros (??) "H".
+          assert (int.nat i ≠ int.nat x).
+          { cut (i ≠ x).
+            { intros. intros Heq. apply Z2Nat.inj in Heq; try word.
+               apply int_Z_inj in Heq; eauto with *.
+            }
+            set_solver. }
+          rewrite ?list_lookup_insert_ne //.
+        }
+      }
+    }
   }
-  { iFrame "shardMap".
-    change (int.nat 0) with 0%nat.
-    rewrite replicate_0 app_nil_l.
-    iFrame "HshardMap_sl". }
-  iIntros "(H&Hi)". iNamed "H".
-  wp_pures.
-   *)
 Abort.
 
 End memkv_shard_make_proof.
