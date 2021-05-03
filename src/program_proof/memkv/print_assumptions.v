047e84261ab3e73f59dea9f01c4d8e3b5c18c0ce
Ralf Jung <research@ralfj.de>
2021-05-03 12:45:56 +0200

    also print client assumptions

--- src/program_proof/memkv/print_assumptions.v
+++ src/program_proof/memkv/print_assumptions.v
@@ -1,4 +1,9 @@
 From Perennial.program_proof Require Import proof_prelude.
-From Perennial.program_proof.memkv Require Import closed.
+From Perennial.program_proof.memkv Require Import closed memkv_clerk_proof.
 
+(* Closed proof for shard server *)
 Print Assumptions shard_boot1.
+
+(* Client proofs -- not yet closed *)
+Definition client_theorems := (@KVClerk__Get, @KVClerk__Put, @KVClerk__ConditionalPut).
+Print Assumptions client_theorems.
