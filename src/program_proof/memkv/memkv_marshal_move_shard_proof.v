ebd82129d6c81cbdfc0880ccb77c78da677802d1
Ralf Jung <research@ralfj.de>
2021-05-06 15:00:03 +0200

    verify MoveShard marshaling

--- src/program_proof/memkv/memkv_marshal_move_shard_proof.v
+++ src/program_proof/memkv/memkv_marshal_move_shard_proof.v
@@ -15,8 +15,8 @@ Record MoveShardRequestC := mkMoveShardRequestC {
   MR_Dst : u64
 }.
 
-Definition has_encoding_MoveShardRequest (data:list u8) (args:MoveShardRequestC) : Prop.
-Admitted.
+Definition has_encoding_MoveShardRequest (data:list u8) (args:MoveShardRequestC) : Prop :=
+  has_encoding data [ EncUInt64 args.(MR_Sid) ; EncUInt64 args.(MR_Dst) ].
 
 Context `{!heapG Σ}.
 
@@ -25,20 +25,49 @@ Definition own_MoveShardRequest args_ptr args : iProp Σ :=
   "HDst" ∷ args_ptr ↦[MoveShardRequest :: "Dst"] #args.(MR_Dst)
 .
 
-(* Pre: "HownShard" ∷ own_shard γ.(kv_gn) sid m *)
-
 Lemma wp_encodeMoveShardRequest args_ptr args :
   {{{
        own_MoveShardRequest args_ptr args
   }}}
     encodeMoveShardRequest #args_ptr
   {{{
-       (reqData:list u8) req_sl, RET (slice_val req_sl); ⌜has_encoding_MoveShardRequest reqData args⌝ ∗
-                                               typed_slice.is_slice req_sl byteT 1%Qp reqData ∗
-                                               own_MoveShardRequest args_ptr args
+     (reqData:list u8) req_sl, RET (slice_val req_sl);
+       ⌜has_encoding_MoveShardRequest reqData args⌝ ∗
+        typed_slice.is_slice req_sl byteT 1%Qp reqData ∗
+        own_MoveShardRequest args_ptr args
   }}}.
 Proof.
-Admitted.
+  iIntros (Φ) "Hrep HΦ".
+
+  wp_lam.
+  wp_pures.
+  iNamed "Hrep".
+
+  wp_apply (wp_new_enc).
+  iIntros (enc) "Henc".
+  wp_pures.
+
+  wp_loadField.
+  wp_apply (wp_Enc__PutInt with "Henc").
+  { word. }
+  iIntros "Henc".
+  wp_pures.
+
+  wp_loadField.
+  wp_apply (wp_Enc__PutInt with "Henc").
+  { word. }
+  iIntros "Henc".
+  wp_pures.
+
+  wp_apply (wp_Enc__Finish with "Henc").
+  iIntros (rep_sl repData).
+  iIntros "(%Henc & %Hlen & Hrep_sl)".
+  iApply "HΦ".
+  iFrame.
+  iPureIntro.
+  rewrite /has_encoding_MoveShardRequest.
+  done.
+Qed.
 
 Lemma wp_decodeMoveShardRequest args args_sl argsData :
   {{{
@@ -51,6 +80,34 @@ Lemma wp_decodeMoveShardRequest args args_sl argsData :
        own_MoveShardRequest args_ptr args
   }}}.
 Proof.
-Admitted.
+  iIntros (Φ) "[Hsl %Henc] HΦ".
+  wp_lam.
+  wp_apply (wp_allocStruct).
+  {
+    naive_solver.
+  }
+  iIntros (rep_ptr) "Hrep".
+  iDestruct (struct_fields_split with "Hrep") as "HH".
+  iNamed "HH".
+  wp_pures.
+
+  iDestruct (typed_slice.is_slice_small_acc with "Hsl") as "[Hsl _]".
+  wp_apply (wp_new_dec with "[$Hsl]").
+  { done. }
+  iIntros (?) "Hdec".
+  wp_pures.
+
+  wp_apply (wp_Dec__GetInt with "[$Hdec]").
+  iIntros "Hdec".
+  wp_storeField.
+
+  wp_apply (wp_Dec__GetInt with "[$Hdec]").
+  iIntros "Hdec".
+  wp_storeField.
+
+  iApply "HΦ".
+  iModIntro.
+  iFrame.
+Qed.
 
 End memkv_marshal_move_shard_proof.
