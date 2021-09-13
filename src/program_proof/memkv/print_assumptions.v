9d552d192e78454a237762b204ec89d605dcbbc6
Ralf Jung <research@ralfj.de>
2021-09-13 14:47:18 -0400

    update print_assumptions

--- src/program_proof/memkv/print_assumptions.v
+++ src/program_proof/memkv/print_assumptions.v
@@ -5,5 +5,5 @@ From Perennial.program_proof.memkv Require Import closed memkv_clerk_proof.
 Print Assumptions shard_coord_boot.
 
 (* Client proofs -- not yet closed *)
-Definition client_theorems := (@KVClerk__Get, @KVClerk__Put, @KVClerk__ConditionalPut).
+Definition client_theorems := (@KVClerk__Get, @KVClerk__MGet, @KVClerk__Put, @KVClerk__ConditionalPut).
 Print Assumptions client_theorems.
