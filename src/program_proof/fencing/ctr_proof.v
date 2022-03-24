5dca83919555a6d7924bd4a5d2feb4dbc293ec19
Upamanyu Sharma <upamanyu@mit.edu>
2022-03-24 18:24:59 -0400

    Checkpoint frontend FetchAndIncrement proof

--- src/program_proof/fencing/ctr_proof.v
+++ src/program_proof/fencing/ctr_proof.v
@@ -10,69 +10,43 @@ Context `{!heapGS Σ}.
 Record ctr_names :=
   {}.
 
-Definition epoch_ptsto (e:u64) : iProp Σ.
+Definition own_epoch (e:u64) (q:Qp): iProp Σ.
 Admitted.
 
-Definition kv_epoch_ptsto (k e:u64) (v:option u64) : iProp Σ.
+Definition own_val (e:u64) (v:option u64) (q:Qp): iProp Σ.
 Admitted.
 
-Definition kv_ptsto (k v:u64) : iProp Σ.
+Global Instance own_epoch_timeless e q : Timeless (own_epoch e q).
+Admitted.
+
+Global Instance own_val_timeless e v q : Timeless (own_val e v q).
 Admitted.
 
 Definition own_Clerk (ck:loc) : iProp Σ.
 Admitted.
 
-
-(* |={Eo,Ei}=> ∃ vold, kv_ptsto k vold ∗ (kv_ptsto k vnew ={Ei,Eo}=∗ Q) *)
-
-(* XXX: the current Clerk is a sequential clerk: can only do one operation at a
-   time with it. Will need to have multiple CIDs to do multiple operations. *)
-
-Lemma wp_Clerk__Put_typical {Eo Ei:coPset} Φ (k v:u64):
-  (|={Eo,Ei}=> ∃ vold, kv_ptsto k vold ∗ (kv_ptsto k v ={Ei,Eo}=∗ Φ #v)) -∗
-    WP Clerk__Put #() @ Eo {{ Φ }}.
-Proof.
-Admitted.
-
-(*
-Lemma wp_Clerk__Put {Eo Ei:coPset} Φ (k e v:u64):
-  (|={Eo,Ei}=> ∃ vold, kv_ptsto k vold ∗ kv_epoch_ptsto k e vold ∗
-    (kv_ptsto k v ∗ kv_epoch_ptsto k e v ={Ei,Eo}=∗ Φ #v)) -∗
-    WP Clerk__Put #() @ Eo {{ Φ }}.
-Proof.
-Admitted.
-
-Lemma wp_Clerk__Put_unknown {Eo Ei:coPset} Φ (k e v:u64):
-  (|={Eo,Ei}=> ∃ vold, kv_ptsto k vold ∗ kv_epoch_unknown k e ∗
-    (kv_ptsto k v ∗ kv_epoch_ptsto k e v ={Ei,Eo}=∗ Φ #v)) -∗
-    WP Clerk__Put #() @ Eo {{ Φ }}.
-Proof.
-Admitted.
-*)
-
-Lemma wp_Clerk__Get {Eo Ei Ei2:coPset} ck (k e:u64) :
+Lemma wp_Clerk__Get {Eo Ei Ei2:coPset} ck (e:u64) :
   ∀ Φ,
   own_Clerk ck -∗
-  (|={Eo,Ei}=> ∃ latestEpoch, epoch_ptsto latestEpoch ∗
+  (|={Eo,Ei}=> ∃ latestEpoch, own_epoch latestEpoch (1/2)%Qp ∗
     (*(⌜int.Z latestEpoch > int.Z e⌝ → epoch_ptsto latestEpoch ={Ei, Eo}=∗ Φ #EStale) ∗*) (* XXX: program exits in this case *)
-    (⌜int.Z latestEpoch ≤ int.Z e⌝ → epoch_ptsto e ={Ei}=∗ (* XXX: one inner mask is probably enough, thhough we could have multiple invariants *)
-     (∃ oldv, kv_epoch_ptsto k e oldv ∗ (∃ v, ⌜is_Some oldv → oldv = Some v⌝ →
-                                               kv_epoch_ptsto k e (Some v) ={Ei2,Eo}=∗
+    (⌜int.Z latestEpoch ≤ int.Z e⌝ → own_epoch e (1/2)%Qp ={Ei}=∗ (* XXX: one inner mask is probably enough, thhough we could have multiple invariants *)
+     (∃ oldv, own_val e oldv (1/2)%Qp ∗ (∃ v, ⌜is_Some oldv → oldv = Some v⌝ →
+                                               own_val e (Some v) (1/2)%Qp ={Ei2,Eo}=∗
                                                               (own_Clerk ck -∗ Φ #v))))) -∗
     WP Clerk__Get #ck #e @ Eo {{ Φ }}.
 Proof.
 Admitted.
 
-(*
-Lemma wp_Server__Put {Eo Ei:coPset} Φ (k e v:u64) (s args:loc):
-  (|={Eo,Ei}=> ∃ vold, kv_ptsto k vold ∗ kv_ptsto_epoch k e vold ∗
-    (kv_ptsto k v ∗ kv_ptsto_epoch k e v ={Ei,Eo}=∗ Φ #v)) -∗
-    WP Server__Put #s #args @ Eo {{ Φ }}.
+Lemma wp_Clerk__Put {Eo Ei Ei2:coPset} ck (k e v:u64) :
+  ∀ Φ,
+  own_Clerk ck -∗
+  (|={Eo,Ei}=> ∃ latestEpoch, own_epoch latestEpoch (1/2)%Qp ∗
+    (*(⌜int.Z latestEpoch > int.Z e⌝ → epoch_ptsto latestEpoch ={Ei, Eo}=∗ Φ #EStale) ∗*) (* XXX: program exits in this case *)
+    (⌜int.Z latestEpoch ≤ int.Z e⌝ → own_epoch e (1/2)%Qp ={Ei}=∗ (* XXX: one inner mask is probably enough, thhough we could have multiple invariants *)
+     (∃ oldv, own_val e oldv (1/2)%Qp ∗ (own_val e (Some v) (1/2)%Qp ={Ei2,Eo}=∗ (own_Clerk ck -∗ Φ #v))))) -∗
+    WP Clerk__Get #ck #e @ Eo {{ Φ }}.
 Proof.
-  iIntros "Hpre".
-  wp_lam.
-  wp_pures.
-  (* wp_loadField. *)
-Admitted. *)
+Admitted.
 
 End ctr_definitions.
--- src/program_proof/fencing/frontend_proof.v
+++ src/program_proof/fencing/frontend_proof.v
@@ -2,17 +2,26 @@ From Perennial.program_proof Require Import grove_prelude.
 From Goose.github_com.mit_pdos.gokv.fencing Require Import frontend.
 From Perennial.program_proof Require Import grove_prelude.
 From Perennial.program_proof.memkv Require Export rpc_lib rpc_proof rpc_spec.
+From Perennial.program_proof.fencing Require Export ctr_proof.
 
 Section frontend_proof.
 
 Context `{!heapGS Σ}.
 
-Definition own_Server (s:loc) epoch : iProp Σ :=
-  ∃ ck1 ck2,
+Local Definition q2 := (1/2)%Qp.
+Local Definition q4 := (1/4)%Qp.
+
+Definition own_Server (s:loc) (epoch:u64) : iProp Σ :=
+  ∃ (ck1 ck2:loc),
   "#Hepoch" ∷ readonly (s ↦[frontend.Server :: "epoch"] #epoch) ∗
   "Hck1" ∷ s ↦[frontend.Server :: "ck1"] #ck1 ∗
   "Hck2" ∷ s ↦[frontend.Server :: "ck1"] #ck2 ∗
-  (* TODO: add ownership of clerks *)
+  "Hck1_own" ∷ own_Clerk ck1 ∗
+  "Hck2_own" ∷ own_Clerk ck2 ∗
+
+  "Hghost1" ∷ (∃ ov, own_val epoch ov q4) ∗
+  (* either have 1/2, or have 1/4 and have witness that latestEpoch >= epoch *)
+  (* TODO: add second value *)
   "_" ∷ True.
 
 Definition frontendN := nroot .@ "frontend".
@@ -23,9 +32,28 @@ Definition is_Server s : iProp Σ :=
   "HmuInv" ∷ is_lock frontendN mu (∃ epoch, own_Server s epoch)
 .
 
+Definition k0 := (U64 0).
+Definition k1 := (U64 1).
+
+Definition kv_ptsto (k v:u64) : iProp Σ.
+Admitted.
+
+Global Instance kv_ptsto_timeless k v : Timeless (kv_ptsto k v).
+Admitted.
+
+Definition frontend_inv_def: iProp Σ :=
+  ∃ (latestEpoch v:u64),
+  "HlatestEpoch" ∷ own_epoch latestEpoch q2 ∗
+  "Hval" ∷ own_val latestEpoch (Some v) q4 ∗
+  "Hkv" ∷ kv_ptsto k0 v.
+
+Definition frontend_inv : iProp Σ :=
+  inv frontendN (frontend_inv_def).
+
 Lemma wp_FetchAndIncrement (s:loc) (key:u64) :
   key = 0 ∨ key = 1 →
   is_Server s -∗
+  frontend_inv -∗
   {{{
         True
   }}}
@@ -35,7 +63,7 @@ Lemma wp_FetchAndIncrement (s:loc) (key:u64) :
   }}}.
 Proof.
   intros Hkey.
-  iIntros "#Hsrv !#" (Φ) "_ HΦ".
+  iIntros "#Hsrv #Hinv !#" (Φ) "_ HΦ".
   wp_lam.
   wp_pures.
   iNamed "Hsrv".
@@ -52,6 +80,15 @@ Proof.
   {
     wp_loadField.
     wp_loadField.
-    wp_apply (wp_Clerk__Get_unknown).
+    wp_apply (wp_Clerk__Get with "Hck2_own").
+    iInv "Hinv" as ">Hown" "Hclose".
+    iNamed "Hown".
+    iModIntro.
+    iExists _; iFrame.
+    iIntros "%Hfresh HlatestEpoch".
+    iModIntro.
+    admit.
   }
+Admitted.
+
 End frontend_proof.
