6edf7c8cd7f4f20d0c42c2426f9cf5e7cd1bec44
Ralf Jung <research@ralfj.de>
2022-03-28 18:58:39 -0400

    fix outdated comment

--- src/goose_lang/ffi/grove_ffi.v
+++ src/goose_lang/ffi/grove_ffi.v
@@ -502,8 +502,7 @@ Section grove.
   Existing Instances grove_op grove_model grove_ty grove_semantics grove_interp goose_groveGS.
   Local Coercion Var' (s:string) : expr := Var s.
 
-  (** We only use these types behind a ptr indirection so their size should not matter. *)
-  (* FIXME: This is a bit strange; not sure how to think about "axiomatizing" a struct *)
+  (** [extT] have size 1 so this fits with them being pointers in Go. *)
   Definition Listener : ty := extT GroveListenTy.
   Definition Connection : ty := extT GroveConnectionTy.
   Definition Address : ty := uint64T.
