8f65b33056af2ffe661f3961e24f0a8af0889f0f
Yun-Sheng Chang <yschang@mit.edu>
2022-07-19 18:15:06 -0400

    Temp remove txn_proof from CI

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -53,7 +53,7 @@ From Perennial.program_proof.ctrexample Require
 (* MVCC *)
 From Perennial.program_proof.mvcc Require
      mvcc_ghost
-     txn_proof
+     (* txn_proof *)
      index_proof
      gc_proof
      tuple_proof.
