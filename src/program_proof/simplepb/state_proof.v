7a924e6cdaaaf5bf0f0cfbcc05109e3171315ce1
Upamanyu Sharma <upamanyu@mit.edu>
2022-08-08 11:03:49 -0400

    Refactor simplepb; better invariant namespace management; add to ShouldBuild

--- src/program_proof/simplepb/state_proof.v
+++ src/program_proof/simplepb/state_proof.v
@@ -1,6 +1,6 @@
 From Perennial.program_proof Require Import grove_prelude.
 From Goose.github_com.mit_pdos.gokv.simplepb Require Import state.
-From Perennial.program_proof.simplepb Require Import pb_definitions.
+From Perennial.program_proof.simplepb Require Import pb_definitions pb_apply_proof.
 From Perennial.program_proof Require Import marshal_stateless_proof.
 From iris.base_logic Require Import ghost_map.
 
@@ -10,7 +10,9 @@ Context `{!heapGS Σ, !pbG Σ}.
 
 Implicit Type σ : list (list u8).
 
+Context `{!pbG Σ}.
 Context `{!ghost_mapG Σ u64 (list u8)}.
+
 Definition own_kvs (γkv:gname) σ : iProp Σ :=
   (* TODO: m has to agree with σ *)
   ∃ m (ops:list (u64 * (list u8))),
@@ -22,7 +24,7 @@ Definition own_kvs (γkv:gname) σ : iProp Σ :=
 Definition stateN := nroot .@ "state".
 
 Definition sys_inv γ γkv : iProp Σ :=
-  inv stateN ( ∃ σ, own_ghost γ σ ∗ own_kvs γkv σ).
+  inv stateN ( ∃ σ, own_log γ σ ∗ own_kvs γkv σ).
 
 Definition kv_ptsto γkv (k:u64) (v:list u8): iProp Σ :=
   k ↪[γkv] v.
@@ -36,11 +38,11 @@ Context `{!urpc_proof.urpcregG Σ}.
 Context `{!stagedG Σ}.
 Lemma wp_Clerk__PrimaryApply γ ck op_sl op (Φ:val → iProp Σ) :
 is_slice op_sl byteT 1 op -∗
-(|={⊤,∅}=> ∃ σ, own_ghost γ σ ∗
-  (own_ghost γ (σ ++ [op]) ={∅,⊤}=∗
-(∀ reply_sl, is_slice reply_sl byteT 1 (replyFn σ) -∗
-    Φ (slice_val reply_sl))))
-    -∗
+(|={⊤∖↑pbN,∅}=> ∃ σ, own_log γ σ ∗
+  (own_log γ (σ ++ [op]) ={∅,⊤∖↑pbN}=∗
+     (∀ reply_sl, is_slice reply_sl byteT 1 (replyFn σ op) -∗ Φ (#(U64 0), slice_val reply_sl)%V)))
+∧
+(∀ (err:u64) unused_sl, ⌜err ≠ 0⌝ -∗ Φ (#err, (slice_val unused_sl))%V ) -∗
 WP Clerk__PrimaryApply #ck (slice_val op_sl) {{ Φ }}.
 Proof.
 Admitted.
@@ -49,8 +51,8 @@ Lemma wp_Clerk__FetchAndAppend ck γ γkv key val_sl value Φ:
   sys_inv γ γkv -∗
   is_Clerk ck -∗
   is_slice val_sl byteT 1 value -∗
-  (|={⊤,↑stateN}=> ∃ old_value, kv_ptsto γkv key old_value ∗
-    (kv_ptsto γkv key (old_value ++ value) ={↑stateN,⊤}=∗
+  (|={⊤∖↑pbN,↑stateN}=> ∃ old_value, kv_ptsto γkv key old_value ∗
+    (kv_ptsto γkv key (old_value ++ value) ={↑stateN,⊤∖↑pbN}=∗
   (∀ reply_sl, is_slice reply_sl byteT 1 old_value -∗
       Φ (slice_val reply_sl))))
   -∗
@@ -91,26 +93,31 @@ Proof.
   wp_loadField.
 
   wp_apply (wp_Clerk__PrimaryApply with "Hop_sl").
-  iMod "Hupd".
-  iInv "Hinv" as ">Hown" "Hclose".
-  replace (↑_∖_) with (∅:coPset); last set_solver.
-  iModIntro.
+  iSplit.
+  { (* case: successful response *)
+    iMod "Hupd".
+    iInv "Hinv" as ">Hown" "Hclose".
+    replace (↑_∖_) with (∅:coPset); last set_solver.
+    iModIntro.
 
-  iDestruct "Hown" as (?) "[Hown Hkvs]".
-  iExists _.
-  iFrame.
+    iDestruct "Hupd" as (?) "[Hkvptsto Hupd]".
+    iDestruct "Hown" as (?) "[Hown Hkvs]".
+    iExists _.
+    iFrame.
 
-  iIntros "Hghost".
-  (* need to update own_kvs; need to fire Hupd *)
-  iMod ("Hclose" with "[Hghost Hkvs]") as "HH".
-  {
-    iNext.
-    iExists _; iFrame "Hghost".
-    admit.
-  }
-  iModIntro.
-  iIntros (reply_sl) "Hreply_sl".
-  admit.
+    iIntros "Hghost".
+    (* need to update own_kvs; need to fire Hupd *)
+    iMod ("Hclose" with "[Hghost Hkvs]") as "HH".
+    {
+      iNext.
+      iExists _; iFrame "Hghost".
+      admit.
+    }
+    iMod ("Hupd" with "[Hkvptsto]") as "Hupd".
+    { admit. } (* FIXME: do the update *)
+    iModIntro.
+    iIntros (reply_sl) "Hreply_sl".
+    (* FIXME: incorporate errors into spec *)
 Admitted.
 
 End state_proof.
