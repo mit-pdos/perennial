81af5fde0d9aa2b4cba3bbe574a9bca06c8ac1c9
Yun-Sheng Chang <yschang@mit.edu>
2022-08-23 15:54:10 -0400

    Checkpoint [wp_wrbuf__OpenTuples] again

--- src/program_proof/mvcc/wrbuf_open_tuples.v
+++ src/program_proof/mvcc/wrbuf_open_tuples.v
@@ -75,9 +75,6 @@ Proof.
   }
   iIntros "HP". subst P. simpl.
   iNamed "HP".
-  replace (take (int.nat i) ents) with ents in Htpls; last first.
-  { admit. }
-
   (**
    * Get [mods_token] and [is_tuple] for each element in [ents].
    *)
@@ -105,8 +102,11 @@ Proof.
   (* for _, ent := range ents {                              *)
   (*     ent.tpl.WriteLock()                                 *)
   (* }                                                       *)
-  (* return true                                             *)
   (***********************************************************)
+  iDestruct (is_slice_sz with "HentsS") as "%HentsLen".
+  rewrite fmap_length in HentsLen.
+  replace (take (int.nat i) ents) with ents in Htpls; last first.
+  { apply Znot_lt_ge in Heqb. symmetry. apply take_ge. word. }
   iDestruct (is_slice_small_acc with "HentsS") as "[HentsS HentsC]".
   set P := (λ (i : u64),
               let tpls_take := (list_to_map (wrent_to_key_tpl <$> (take (int.nat i) ents))) in
@@ -114,7 +114,7 @@ Proof.
               "Htokens"  ∷ ([∗ map] k ↦ _ ∈ tpls_drop, mods_token γ k (int.nat tid)) ∗
               "HtplsOwn" ∷ own_tuples_locked (int.nat tid) tpls_take γ
            )%I.
-  wp_apply (wp_forSlice P with "[] [$HentsS]").
+  wp_apply (wp_forSlice P with "[] [$HentsS Htokens]").
   { (* Loop body. *)
     clear Φ.
     iIntros (j e).
@@ -131,18 +131,67 @@ Proof.
       apply elem_of_list_lookup_2, (elem_of_list_fmap_1 wrent_to_key_tpl) in Hlookup.
       done.
     }
+    (**
+     * Deduce [k ∉ (drop (S (int.nat i)) ents).*1.*1.*1], which we need in [big_sepM_insert].
+     * Deduce [k ∉ (take (int.nat i) ents).*1.*1.*1], which we need in [list_to_map_snoc] and [big_sepM_insert].
+     *)
+    apply take_drop_middle in Hlookup as Eqents.
+    rewrite -Eqents in HNoDup.
+    do 3 rewrite fmap_app in HNoDup.
+    do 3 rewrite fmap_cons in HNoDup.
+    simpl in HNoDup.
+    apply NoDup_app_comm in HNoDup as HNoDup'.
+    apply NoDup_app in HNoDup as (_ & _ & HNoDup).
+    apply NoDup_cons in HNoDup as [Hnotin _].
+    apply NoDup_app in HNoDup' as (_ & Hnotin' & _).
+    specialize (Hnotin' k).
+    pose proof (elem_of_list_here k (drop (S (int.nat j)) ents).*1.*1.*1) as Helem.
+    specialize (Hnotin' Helem).
+    (* Q: How to rewrite [P -> Q] to [Q] and prove [P]. *)
+    (* specialize (Hnotin' elem_of_list_here). doesn't work. *)
     (* Retrieve [mods_token] of key [k]. *)
     rewrite (drop_S _ _ _ Hlookup).
-    rewrite fmap_cons.
+    rewrite fmap_cons list_to_map_cons. simpl.
+    rewrite big_sepM_insert; last first.
+    { apply not_elem_of_list_to_map_1. set_solver. }
+    iDestruct "Htokens" as "[Htoken Htokens]".
 
-    wp_apply (wp_tuple__WriteLock with "HtplRP").
-    { admit. }
-    admit.
+    wp_apply (wp_tuple__WriteLock with "HtplRP Htoken").
+    iIntros (phys) "Htpl".
+    iApply "HΦ".
+    replace (int.nat (word.add _ _)) with (S (int.nat j)) by word.
+    iFrame "Htokens".
+    rewrite (take_S_r _ _ _ Hlookup).
+    rewrite fmap_snoc list_to_map_snoc; last first.
+    { simpl. rewrite -list_fmap_compose. set_solver. }
+    unfold named. rewrite {2} /own_tuples_locked.
+    rewrite big_sepM_insert; last first.
+    { apply not_elem_of_list_to_map_1. set_solver. }
+    iFrame "HtplsOwn".
+    by iExists phys.
   }
   { (* Loop entry. *)
-    admit.
+    subst P. simpl.
+    replace (int.nat 0) with 0%nat by word.
+    rewrite drop_0 take_0 -Htpls. simpl.
+    iFrame "Htokens".
+    by iApply big_sepM_empty.
   }
-  admit.
+  iIntros "[HP HentsS]".
+  subst P. simpl.
+  iNamed "HP".
+  iDestruct ("HentsC" with "HentsS") as "HentsS".
+  wp_pures.
+
+  (***********************************************************)
+  (* return true                                             *)
+  (***********************************************************)
+  iApply "HΦ".
+  iFrame "Hactive".
+  iExists _.
+  rewrite -HentsLen firstn_all.
+  iFrame "HtplsOwn".
+  eauto 10 with iFrame.
 Admitted.
 
 End heap.
