697048577d0c8ba6b9cb84658870e11b0d789151
Yun-Sheng Chang <yschang@mit.edu>
2022-08-31 19:31:12 -0400

    Update MVCC print_assumptions

--- src/program_proof/mvcc/mvcc_proof.v
+++ src/program_proof/mvcc/mvcc_proof.v
@@ -1,14 +1,15 @@
 From Perennial.program_proof.mvcc Require Export
-     mvcc_ghost
+     mvcc_misc mvcc_action mvcc_tuplext mvcc_ghost mvcc_inv
      proph_proof
      index_proof
      tid_proof
      (* txnmgr *)
-     txnmgr_mk txnmgr_new txnmgr_get_min_active_tid
+     txnmgr_repr txnmgr_mk txnmgr_new
      txnmgr_activate txnmgr_deactivate
+     txnmgr_get_min_active_tid txnmgr_activate_gc
      (* txn *)
-     txn_begin txn_commit txn_get txn_put txn_delete
-     txn_do_txn
+     txn_repr txn_begin txn_commit txn_acquire txn_do_txn
+     txn_get txn_put txn_delete txn_do_txn
      (* tuple *)
      tuple_prelude tuple_repr
      tuple_append_version tuple_kill_version tuple_read_version
--- src/program_proof/mvcc/print_assumptions.v
+++ src/program_proof/mvcc/print_assumptions.v
@@ -2,6 +2,9 @@ From Perennial.program_proof Require Import proof_prelude.
 From Perennial.program_proof.mvcc Require Import mvcc_proof.
 
 (* Core Txn machinery *)
-Definition mvcc_theorems := (@wp_txn__DoTxn, @wp_txn__Get, @wp_txn__Put).
+Definition mvcc_theorems := (
+	@wp_txn__DoTxn, @wp_txn__Get, @wp_txn__Put, @wp_txn__Delete,
+	@wp_MkTxnMgr, @wp_txnMgr__New, @wp_txnMgr__ActivateGC
+).
 Print Assumptions mvcc_theorems.
 
