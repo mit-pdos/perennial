1a2d494428fe1fa230cba05fbf70a5d524298dd4
Yun-Sheng Chang <yschang@mit.edu>
2022-08-31 13:50:46 -0400

    Fix build

--- external/Goose/github_com/tchajed/goose/internal/examples/unittest.v
+++ external/Goose/github_com/tchajed/goose/internal/examples/unittest.v
@@ -1015,6 +1015,13 @@ Definition makeLock: val :=
     DoSomeLocking "l";;
     #().
 
+(* time.go *)
+
+Definition sleep: val :=
+  rec: "sleep" <> :=
+    time.Sleep #1000;;
+    #().
+
 (* trailing_call.go *)
 
 Definition mkInt: val :=
