4c0371cb77ca55da7ab851c886680fa986b1428f
Joseph Tassarotti <jt4767@nyu.edu>
2022-12-13 07:16:51 -0500

    Print assumptions for some kv Clerk methods

--- src/program_proof/simplepb/apps/print_assumptions.v
+++ src/program_proof/simplepb/apps/print_assumptions.v
@@ -5,7 +5,10 @@ From Perennial.program_proof.simplepb.apps Require kv_proof closed_proof.
 Definition lemmas :=
   (@config_proof.wp_MakeServer, @config_proof.wp_Server__Serve,
   @admin_proof.wp_Reconfig,
-  @kv_proof.wp_Start).
+  @kv_proof.wp_Start,
+  @kv_proof.wp_Clerk__Put,
+  @kv_proof.wp_Clerk__Get).
+
 Print Assumptions lemmas.
 
 Print Assumptions closed_proof.closed.kv_pb_boot.
