f40f3a371f8e07697bfab6089188f86d6d9fcbbf
Ralf Jung <research@ralfj.de>
2022-12-13 11:10:38 +0100

    complete U64 map lemmas

--- src/program_proof/map_marshal_proof.v
+++ src/program_proof/map_marshal_proof.v
@@ -110,18 +110,18 @@ Proof.
   }
 Qed.
 
-Local Definition encode_maplist (l:list (u64 *list u8)) : list u8 :=
+Local Definition encode_byte_maplist (l:list (u64 * list u8)) : list u8 :=
   flat_map (λ u, (u64_le u.1) ++ (u64_le (int.Z (length (u.2)))) ++ u.2) l.
 
-Local Lemma encode_maplist_cons k data l :
-  encode_maplist ((k, data)::l) = (u64_le k) ++ (u64_le (int.Z (length data))) ++ data ++ encode_maplist l.
+Local Lemma encode_byte_maplist_cons k data l :
+  encode_byte_maplist ((k, data)::l) = (u64_le k) ++ (u64_le (int.Z (length data))) ++ data ++ encode_byte_maplist l.
 Proof. done. Qed.
 
 Local Definition has_partial_byte_map_encoding (enc:list u8) (fullsize: u64) (m:gmap u64 (list u8)) : Prop :=
   ∃ l,
   NoDup l.*1 ∧
   (list_to_map l) = m ∧
-  enc = (u64_le fullsize) ++ encode_maplist l.
+  enc = (u64_le fullsize) ++ encode_byte_maplist l.
 
 Definition has_byte_map_encoding (enc:list u8) (m:gmap u64 (list u8)) : Prop :=
   int.Z (size m) = size m ∧ has_partial_byte_map_encoding enc (size m) m.
@@ -193,7 +193,7 @@ Proof.
        change data with ((λ v _, v) data v).
        rewrite map_insert_zip_with. rewrite insert_id //.
      - rewrite Hdata Henc. cbn. rewrite -!app_assoc. repeat f_equal.
-       rewrite /encode_maplist flat_map_app. f_equal. cbn. rewrite -!app_assoc app_nil_r. repeat f_equal.
+       rewrite /encode_byte_maplist flat_map_app. f_equal. cbn. rewrite -!app_assoc app_nil_r. repeat f_equal.
        move:Hsz. rewrite Hdata Hm. erewrite lookup_union_Some_l by done. cbn. intros. word.
   }
   iIntros "[Hkvs_map HI]". iNamed "HI".
@@ -229,7 +229,7 @@ Proof.
   wp_apply (wp_forUpto (λ i, ∃ s,
               "Hm" ∷ own_byte_map mptr (list_to_map (take (int.nat i) ls)) ∗
               "Hl" ∷ l ↦[slice.T byteT] (slice_val s) ∗
-              "Hs" ∷ is_slice_small s byteT q (encode_maplist (drop (int.nat i) ls) ++ enc_rest)
+              "Hs" ∷ is_slice_small s byteT q (encode_byte_maplist (drop (int.nat i) ls) ++ enc_rest)
   )%I with "[] [$Hli Hm Hl Hs]"); first word.
   2:{ iExists _. iFrame. }
   { (* core loop *)
@@ -245,7 +245,7 @@ Proof.
     set ls_head := take (int.nat i) ls.
     set ls_tail := drop (1+int.nat i) ls.
     intros Hnodup.
-    rewrite encode_maplist_cons -!app_assoc.
+    rewrite encode_byte_maplist_cons -!app_assoc.
     wp_load.
     wp_apply (wp_ReadInt with "Hs"). iIntros (s') "Hs".
     wp_apply (wp_ReadInt with "Hs"). iIntros (s'') "Hs".
@@ -272,13 +272,21 @@ Proof.
   done.
 Qed.
 
-Definition has_u64_map_encoding (enc:list u8) (m:gmap u64 u64) : Prop :=
+Local Definition encode_u64_maplist (l:list (u64 * u64)) : list u8 :=
+  flat_map (λ u, (u64_le u.1) ++ (u64_le u.2)) l.
+
+Local Lemma encode_u64_maplist_cons k data l :
+  encode_u64_maplist ((k, data)::l) = (u64_le k) ++ (u64_le data) ++ encode_u64_maplist l.
+Proof. done. Qed.
+
+Local Definition has_partial_u64_map_encoding (enc:list u8) (fullsize: u64) (m:gmap u64 u64) : Prop :=
   ∃ l,
-  (int.Z (size m) = size m) ∧
   NoDup l.*1 ∧
   (list_to_map l) = m ∧
-  enc = (u64_le (size m)) ++
-      (flat_map (λ u, (u64_le u.1) ++ (u64_le u.2)) l).
+  enc = (u64_le fullsize) ++ encode_u64_maplist l.
+
+Definition has_u64_map_encoding (enc:list u8) (m:gmap u64 u64) : Prop :=
+  (int.Z (size m) = size m) ∧ has_partial_u64_map_encoding enc (size m) m.
 
 Lemma wp_EncodeMapU64ToU64 mptr m :
   {{{
@@ -292,7 +300,59 @@ Lemma wp_EncodeMapU64ToU64 mptr m :
         ⌜has_u64_map_encoding enc m⌝
   }}}.
 Proof.
-Admitted.
+  iIntros "%Φ H HΦ". iNamed "H". wp_call.
+  wp_apply wp_NewSlice. iIntros (s) "Hs".
+  wp_apply wp_ref_to; first by val_ty. iIntros (l) "Hl".
+  wp_apply (wp_MapLen with "Hmap"). iIntros "[%Hmsize Hmap]".
+  wp_load.
+  wp_apply (wp_WriteInt with "Hs"). iIntros (s') "Hs".
+  rewrite replicate_0 /=. wp_store. clear s.
+  wp_apply (wp_MapIter_2 _ _ _ _ _
+    (λ mtodo mdone, ∃ (s : Slice.t) enc,
+      "%Hm" ∷ ⌜m = mtodo ∪ mdone⌝ ∗
+      "%Hdisk" ∷ ⌜dom mtodo ## dom mdone⌝ ∗
+      "%Henc" ∷ ⌜has_partial_u64_map_encoding enc (size m) mdone⌝ ∗
+      "Hl" ∷ l ↦[slice.T byteT] (slice_val s) ∗
+      "Hs" ∷ is_slice s byteT 1 enc
+    )%I with "Hmap [Hl Hs]").
+  { iExists _, _. iFrame. iPureIntro.
+    rewrite right_id_L. split; first done.
+    rewrite /has_partial_u64_map_encoding.
+    split; first set_solver.
+    exists [].
+    split; first by apply NoDup_nil_2.
+    split; first by apply list_to_map_nil.
+    done. }
+  { (* core loop *)
+     clear Φ s' mptr. iIntros (k v mtodo mdone Φ) "!# [HI %Hk] HΦ". iNamed "HI". wp_pures.
+     wp_load. wp_apply (wp_WriteInt with "Hs"). iIntros (s') "Hs". wp_store. clear s.
+     wp_load. wp_apply (wp_WriteInt with "Hs"). iIntros (s) "Hs". wp_store. clear s'.
+     iApply "HΦ". iModIntro. iExists _, _. iFrame. iPureIntro.
+     split.
+     { rewrite union_delete_insert //. }
+     assert (mdone !! k = None).
+     { apply not_elem_of_dom. apply elem_of_dom_2 in Hk. set_solver. }
+     split; first set_solver.
+     assert (m !! k = Some v) as Hkm.
+     { rewrite Hm lookup_union_l //. }
+     destruct Henc as (ls & Hnodup & Hls & Henc). exists (ls ++ [(k, v)]).
+     assert (k ∉ ls.*1).
+     { intros Hin. eapply (not_elem_of_list_to_map ls). 2:by apply Hin.
+       rewrite Hls. auto. }
+     split; last split.
+     - rewrite fmap_app. apply NoDup_app. split; first done.
+       simpl. split; last by apply NoDup_singleton.
+       intros k' Hk' ->%elem_of_list_singleton. done.
+     - rewrite list_to_map_snoc //. rewrite Hls //.
+     - rewrite Henc. rewrite -!app_assoc. repeat f_equal.
+       rewrite /encode_u64_maplist flat_map_app. f_equal.
+  }
+  iIntros "[Hkvs_map HI]". iNamed "HI".
+  wp_load. iApply "HΦ". iFrame.
+  iPureIntro. split.
+  - rewrite Hmsize. word.
+  - done.
+Qed.
 
 Lemma wp_DecodeMapU64ToU64 m enc_sl enc enc_rest q :
   {{{
@@ -306,6 +366,61 @@ Lemma wp_DecodeMapU64ToU64 m enc_sl enc enc_rest q :
         is_slice_small rest_enc_sl byteT q' enc_rest
   }}}.
 Proof.
-Admitted.
+  iIntros "%Φ H HΦ". iNamed "H". wp_call.
+  wp_apply wp_ref_to; first by val_ty. iIntros (l) "Hl".
+  wp_apply wp_NewMap. iIntros (mptr) "Hm".
+  wp_load.
+  destruct Henc as [Hsz (ls & Hnodup & Hls & Henc)]. subst enc.
+  rewrite -!app_assoc.
+  wp_apply (wp_ReadInt with "Henc_sl"). iIntros (s') "Hs". wp_store. clear enc_sl.
+  wp_apply wp_ref_to; first by val_ty. iIntros (li) "Hli". wp_pures.
+  wp_apply (wp_forUpto (λ i, ∃ s,
+              "Hm" ∷ is_map mptr 1 (list_to_map (take (int.nat i) ls)) ∗
+              "Hl" ∷ l ↦[slice.T byteT] (slice_val s) ∗
+              "Hs" ∷ is_slice_small s byteT q (encode_u64_maplist (drop (int.nat i) ls) ++ enc_rest)
+  )%I with "[] [$Hli Hm Hl Hs]"); first word.
+  2:{ iExists _. iFrame. }
+  { (* core loop *)
+    clear s' Φ. iIntros (i Φ) " !#(I & Hli & %Hi) HΦ". iNamed "I". wp_lam.
+    replace (int.nat (u64_instance.u64.(word.add) i 1)) with (1 + int.nat i)%nat by word.
+    assert (is_Some (ls !! (int.nat i))) as [[k data] Hk].
+    { apply lookup_lt_is_Some_2. rewrite -Map.size_list_to_map //.
+      rewrite Hls. word. }
+    rewrite -(take_drop_middle _ _ _ Hk) in Hls Hnodup.
+    move:Hnodup. clear Hls.
+    erewrite take_S_r by done.
+    rewrite (drop_S _ _ _ Hk).
+    set ls_head := take (int.nat i) ls.
+    set ls_tail := drop (1+int.nat i) ls.
+    intros Hnodup.
+    rewrite encode_u64_maplist_cons -!app_assoc.
+    wp_apply wp_ref_of_zero. { done. } iIntros (lkey) "?".
+    wp_apply wp_ref_of_zero. { done. } iIntros (lval) "?".
+    wp_load.
+    wp_apply (wp_ReadInt with "Hs"). iIntros (s') "Hs".
+    do 2 wp_store.
+    wp_load.
+    wp_apply (wp_ReadInt with "Hs"). iIntros (s'') "Hs".
+    do 2 wp_store.
+    do 2 wp_load.
+    wp_apply (wp_MapInsert _ _ _ _ data with "[$Hm]").
+    { done. }
+    rewrite /map_insert.
+    iIntros "Hm". wp_pures. iApply "HΦ". iModIntro.
+    iFrame "Hli". iExists _. iFrame. iExactEq "Hm". rewrite /named. f_equal.
+    rewrite list_to_map_snoc //.
+    rewrite fmap_app NoDup_app in Hnodup.
+    destruct Hnodup as (_ & Hnin & _). intros Hin. eapply Hnin; first done.
+    eapply elem_of_list_fmap_1_alt.
+    - apply elem_of_list_here.
+    - done. }
+  iIntros "(I & Hli)". iNamed "I". wp_load. wp_pures. iApply "HΦ". iModIntro.
+  rewrite take_ge.
+  2:{ rewrite -Map.size_list_to_map // Hls. word. }
+  rewrite Hls. iFrame "Hm".
+  rewrite drop_ge.
+  2:{ rewrite -Map.size_list_to_map // Hls. word. }
+  done.
+Qed.
 
 End map_marshal_proof.
