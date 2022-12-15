def43b8373d62336ef9960583438035882f9790e
Upamanyu Sharma <upamanyu@mit.edu>
2022-12-15 13:10:39 -0500

    Prove lease_alloc

--- src/program_proof/minlease/proof.v
+++ src/program_proof/minlease/proof.v
@@ -11,6 +11,7 @@ Section proof.
 
 Context `{!heapGS Σ}.
 Context `{!ghost_varG Σ u64}.
+Context `{!ghost_varG Σ ()}.
 Context `{!savedPropG Σ }.
 
 Context {γtime:gname}.
@@ -20,11 +21,11 @@ Notation own_time := (own_time γtime).
 
 (* Q is the lease obligation *)
 Definition underLease (N:namespace) (leaseExpiration:u64) (P:iProp Σ) (Q:iProp Σ) : iProp Σ :=
-  ∃ γprop,
+  ∃ γprop γpostLeastTok,
   saved_prop_own γprop (DfracOwn (1/2)) P ∗
   inv N (∃ P',
     saved_prop_own γprop (DfracOwn (1/2)) P' ∗
-    (P' ∗ (P' -∗ Q) ∨ is_time_lb leaseExpiration)
+    (P' ∗ (P' -∗ Q) ∨ is_time_lb leaseExpiration ∗ ghost_var γpostLeastTok 1 ())
   ).
 
 Lemma underLease_mono P' P Q e N :
@@ -48,7 +49,7 @@ Lemma lease_acc_update N e P Q :
 Proof.
   iIntros "Hlease Hlc Hlc2".
   iIntros (?) "%Hineq Htime".
-  iDestruct "Hlease" as (γprop) "[Hprop #Hinv]".
+  iDestruct "Hlease" as (γprop ?) "[Hprop #Hinv]".
   iInv "Hinv" as "Hi" "Hclose".
   iMod (lc_fupd_elim_later with "Hlc Hi") as "Hi".
   iDestruct "Hi" as (?) "[Hprop2 Hi]".
@@ -56,7 +57,7 @@ Proof.
   iMod (lc_fupd_elim_later with "Hlc2 Hagree1") as "#Hagree".
   replace (↑N ∖ ↑N) with (∅:coPset) by set_solver.
   iModIntro.
-  iDestruct "Hi" as "[[HP HQwand]| Hbad]"; last first.
+  iDestruct "Hi" as "[[HP HQwand]|[Hbad HpostLeastTok]]"; last first.
   {
     iDestruct (mono_nat_lb_own_valid with "Htime Hbad") as %[_ Hbad].
     exfalso.
@@ -78,12 +79,59 @@ Proof.
     iFrame.
   }
   iModIntro.
-  iExists _; iFrame "∗#".
+  iExists _, _; iFrame "∗#".
 Qed.
 
-Definition postLease (N:namespace) (leaseExpiration:u64) (Q:iProp Σ) :=
-  is_time_lb leaseExpiration ={↑N}=∗ Q
+Definition postLease (N:namespace) (leaseExpiration:u64) (Q:iProp Σ) : iProp Σ :=
+  is_time_lb leaseExpiration ={↑N}=∗ ▷ Q
 .
+
+Lemma lease_alloc N e P :
+  P ={↑N}=∗
+  underLease N e P P ∗
+  postLease N e P
+.
+Proof.
+  iIntros "HP".
+  iMod (saved_prop_alloc P (DfracOwn 1)) as (γprop) "[Hprop Hprop2]".
+  { done. }
+  iMod (ghost_var_alloc ()) as (γpostLeaseTok) "HpostLeaseTok".
+  iAssert (|={↑N}=>
+  inv N (∃ P',
+    saved_prop_own γprop (DfracOwn (1/2)) P' ∗
+    (P' ∗ (P' -∗ P) ∨ is_time_lb e ∗ ghost_var γpostLeaseTok 1 ())
+  ))%I with "[HP Hprop2]" as ">#Hinv".
+  {
+    iMod (inv_alloc with "[-]") as "$"; last done.
+    iNext.
+    iExists _; iFrame "Hprop2".
+    iLeft. iFrame "HP". iIntros "$".
+  }
+  iModIntro.
+  iSplitL "Hprop".
+  {
+    iExists _, _; iFrame "∗#".
+  }
+  iIntros "Hlb".
+  iInv "Hinv" as "Hi" "Hclose".
+  iDestruct "Hi" as (?) "[Hprop HP]".
+  iDestruct "HP" as "[HP |[_ >Hbad]]"; last first.
+  {
+    iDestruct (ghost_var_valid_2 with "Hbad HpostLeaseTok") as %[Hbad _].
+    exfalso.
+    done.
+  }
+  iDestruct "HP" as "[HP HPwand]".
+  iDestruct (later_wand with "HPwand") as "HPwand".
+  iMod ("Hclose" with "[Hprop HpostLeaseTok Hlb]").
+  {
+    iExists _; iFrame.
+    iRight.
+    iFrame.
+  }
+  iApply "HPwand".
+  done.
+Qed.
 
 (* P are resources you can access and modify at the moment you check that the
    lease is unexpired.
