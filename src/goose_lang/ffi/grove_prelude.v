a10ea1128c6b23182c4d1860b627eb55fb331a35
Nickolai Zeldovich <nickolai@csail.mit.edu>
2022-12-22 15:12:43 -0500

    do not force specific ffi in Var' coercions
    Without this change, importing [disk_prelude] or [grove_prelude] or
    [async_disk_prelude] would make Var' coerce to a specific ffi.  If
    subsequent code was defined in a [Section] that parametrized over an
    arbitrary ffi (e.g., [Context `{ext_ty: ext_types}]), this would lead
    to a mismatch in the ffi argument of @val.

--- src/goose_lang/ffi/grove_prelude.v
+++ src/goose_lang/ffi/grove_prelude.v
@@ -9,4 +9,7 @@ Existing Instances goose_groveGS goose_groveNodeGS.
 (* Now that the TC parameter is fixed, we can make this a coercion. Sadly,
 [Var'] gets replaced by [Var] on the first substitution, so printing terms still
 prints that [Var] -- but we barely look at those parts of the terms anyway. *)
-Coercion Var' (s: string) := Var s.
+Section coerce.
+  Context {ext: ffi_syntax}.
+  Coercion Var' (s: string) : (@expr ext) := Var s.
+End coerce.
