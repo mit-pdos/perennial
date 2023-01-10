7b9ab52cc3499c5688386e7169975a4868fa7858
Ralf Jung <research@ralfj.de>
2023-01-10 16:14:19 +0100

    Revert "do not force specific ffi in Var' coercions"
    This reverts commit a10ea1128c6b23182c4d1860b627eb55fb331a35.
    These preludes are only meant to be imported in FFI-specific code, and the
    Goose-generated code relies on these coercions.

--- src/goose_lang/ffi/disk_prelude.v
+++ src/goose_lang/ffi/disk_prelude.v
@@ -9,7 +9,4 @@ Existing Instance goose_diskGS.
 (* Now that the TC parameter is fixed, we can make this a coercion. Sadly,
 [Var'] gets replaced by [Var] on the first substitution, so printing terms still
 prints that [Var] -- but we barely look at those parts of the terms anyway. *)
-Section coerce.
-  Context {ext: ffi_syntax}.
-  Coercion Var' (s: string) : (@expr ext) := Var s.
-End coerce.
+Coercion Var' (s: string) := Var s.
