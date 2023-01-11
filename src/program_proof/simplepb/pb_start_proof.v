c9c16006e85348c6dcd1d80bbd7f0815eb740d16
Upamanyu Sharma <upamanyu@mit.edu>
2023-01-11 14:21:39 -0500

    Fix start proof except new unspecified RPCs

--- src/program_proof/simplepb/pb_start_proof.v
+++ src/program_proof/simplepb/pb_start_proof.v
@@ -2,6 +2,7 @@ From Perennial.program_proof Require Import grove_prelude.
 From Goose.github_com.mit_pdos.gokv.simplepb Require Export pb.
 From Perennial.program_proof.grove_shared Require Import urpc_proof urpc_spec.
 From Perennial.program_proof.simplepb Require Import pb_marshal_proof pb_definitions pb_applybackup_proof pb_setstate_proof pb_getstate_proof pb_becomeprimary_proof pb_apply_proof pb_makeclerk_proof.
+From iris.algebra Require Import mono_list.
 
 Section pb_start_proof.
 
@@ -22,12 +23,20 @@ Context `{!heapGS Σ}.
 Context `{!pbG Σ}.
 
 Implicit Type (own_StateMachine: u64 → list OpType → bool → (u64 → list OpType → bool → iProp Σ) → iProp Σ).
-Lemma wp_MakeServer sm_ptr own_StateMachine (epoch:u64) σ (sealed:bool) (nextIndex:u64) γ γsrv :
+Lemma wp_MakeServer sm_ptr own_StateMachine (epoch:u64) opsfull (sealed:bool) (nextIndex:u64) γ γsrv γeph :
   {{{
-        "Hstate" ∷ own_StateMachine epoch σ sealed (own_Server_ghost γ γsrv) ∗
-        "#His_sm" ∷ is_StateMachine sm_ptr own_StateMachine (own_Server_ghost γ γsrv) ∗
+        "Hstate" ∷ own_StateMachine epoch (get_rwops opsfull) sealed (own_Server_ghost γ γsrv γeph) ∗
+        "#His_sm" ∷ is_StateMachine sm_ptr own_StateMachine (own_Server_ghost γ γsrv γeph) ∗
         "#Hsys_inv" ∷ sys_inv γ ∗
-        "%HnextIndex" ∷ ⌜int.nat nextIndex = length σ⌝
+        "%HnextIndex" ∷ ⌜int.nat nextIndex = length (get_rwops opsfull)⌝ ∗
+        "Heph" ∷ own_ephemeral_proposal γeph epoch opsfull ∗
+        "Heph_unused" ∷ own_unused_ephemeral_proposals γeph epoch ∗
+        (* XXX: this is basically a guarantee that the list of ops being
+           implicitly passed in via own_StateMachine has been made durable. It
+           would now be buggy to buffer an op in memory before passing a
+           StateMachine into MakeServer because the Server tracks the
+           durableNextIndex and initializes it here to be nextIndex. *)
+        "#Hacc_lb" ∷ is_accepted_lb γsrv epoch opsfull
   }}}
     pb.MakeServer #sm_ptr #nextIndex #epoch #sealed
   {{{
@@ -55,41 +64,83 @@ Proof.
   wp_storeField.
 
   iAssert (_) with "His_sm" as "His_sm2".
+  iEval (rewrite /is_StateMachine /tc_opaque) in "His_sm2".
   iNamed "His_sm2".
 
   iMod ("HaccP" with "Hlc [] Hstate") as "Hstate".
   {
-    instantiate (1:=is_epoch_lb γsrv epoch).
-    iIntros(???). iIntros "(% & % & H1 & H2)".
-    iDestruct (ghost_get_epoch_lb with "H1") as "#Hlb".
+    instantiate (1:=(is_epoch_lb γsrv epoch ∗ is_proposal_lb γ epoch opsfull)%I).
+    iIntros(???). iIntros "Hghost".
+    iNamed "Hghost".
+    iDestruct (ghost_get_epoch_lb with "Hghost") as "#Hlb".
+    iNamed "Hghost".
+    iAssert (⌜prefix opsfull opsfull0⌝)%I with "[-]" as %Hdone.
+    {
+      destruct sealedold.
+      {
+        iDestruct (own_valid_2 with "Haccepted_ro Hacc_lb") as %Hvalid.
+        iPureIntro.
+        rewrite singleton_op singleton_valid mono_list_both_dfrac_valid_L in Hvalid.
+        destruct Hvalid as [_ H]. done.
+      }
+      {
+        iDestruct (own_valid_2 with "Haccepted Hacc_lb") as %Hvalid.
+        iPureIntro.
+        rewrite singleton_op singleton_valid mono_list_both_dfrac_valid_L in Hvalid.
+        destruct Hvalid as [_ H]. done.
+      }
+    }
     iFrame "Hlb".
-    iExists _; iFrame "∗#%".
-    by iModIntro.
+    iSplitL.
+    { iExists _; iFrame "∗#%". iModIntro. done. }
+    iModIntro.
+    iApply (own_mono with "Hproposal_lb").
+    rewrite singleton_included.
+    right.
+    rewrite csum.Cinr_included.
+    apply mono_list_lb_mono.
+    done.
   }
-  iApply (wpc_wp _ _ _ _ True%I).
-  wpc_apply (wpc_nval_elim with "Hstate").
+  Search wpc_nval.
+  wp_apply (wpc_nval_elim_wp with "Hstate").
   { done. }
   { done. }
-  iApply wp_wpc.
   wp_storeField.
-  iIntros "[Hstate #Hepochlb]".
-  iApply wp_wpc.
+  iIntros "(Hstate & #Hepochlb & #Hprop_lb)".
 
   wp_pures.
+  wp_storeField.
 
   wp_apply (wp_NewMap).
   iIntros (?) "Hmap".
+  wp_storeField.
+
+  wp_loadField.
+  wp_apply (wp_newCond' with "HmuInv").
+  iIntros (durableCond) "[HmuInv #Hdurcond]".
+  wp_storeField.
+
+  wp_loadField.
+  wp_apply (wp_newCond' with "HmuInv").
+  iIntros (roCond) "[HmuInv #Hrocond]".
+  wp_storeField.
+
+  wp_loadField.
+  wp_apply (wp_newCond' with "HmuInv").
+  iIntros (commitCond) "[HmuInv #Hcommitcond]".
   wp_storeField.
 
   iApply "HΦ".
   iMod (readonly_alloc_1 with "mu") as "#Hmu".
   iExists _, _.
   iFrame "Hmu Hsys_inv".
-  iMod (alloc_lock with "HmuInv [-]") as "$"; last done.
+  iExists γeph.
+  iMod (alloc_lock with "HmuInv [-]") as "$"; last first.
+  { time done. } (* PERF: why is this slow? *)
 
   (* now just need to establish lock invariant *)
   iNext.
-  do 9 iExists _.
+  repeat iExists _.
   iFrame "∗".
   iSplitL "clerks".
   {
@@ -97,14 +148,16 @@ Proof.
     instantiate (1:=Slice.nil).
     iExact "clerks".
   }
-  iSplitR; first iExact "His_sm".
-
   iFrame "#".
-  iSplitL; first done.
-  iSplitL; last done.
-  iApply big_sepM_empty.
-  done.
-Qed.
+  iSplitR.
+  { iApply big_sepM_empty. done. }
+  iFrame "%".
+  iSplitR.
+  { admit. } (* FIXME: prove that is_proposal_valid is downwards closed. *)
+  iSplitR.
+  { iPureIntro. word. }
+  { iPureIntro. word. }
+Admitted.
 
 Lemma wp_Server__Serve s host γ γsrv :
   {{{
@@ -123,6 +176,16 @@ Proof.
 
   wp_apply (map.wp_NewMap).
   iIntros (handlers) "Hhandlers".
+
+  wp_pures.
+  wp_apply (map.wp_MapInsert with "Hhandlers").
+  iIntros "Hhandlers".
+  wp_pures.
+
+  wp_pures.
+  wp_apply (map.wp_MapInsert with "Hhandlers").
+  iIntros "Hhandlers".
+  wp_pures.
 
   wp_pures.
   wp_apply (map.wp_MapInsert with "Hhandlers").
@@ -168,8 +231,13 @@ Proof.
     {
       iExactEq "Hhandlers".
       f_equal.
-      set_solver.
+      admit. (* FIXME: more RPC specs *)
     }
+
+    iApply (big_sepM_insert_2 with "").
+    { admit. }
+    iApply (big_sepM_insert_2 with "").
+    { admit. }
 
     iApply (big_sepM_insert_2 with "").
     {
@@ -304,6 +372,6 @@ Proof.
   }
   wp_pures.
   by iApply "HΦ".
-Qed.
+Admitted.
 
 End pb_start_proof.
