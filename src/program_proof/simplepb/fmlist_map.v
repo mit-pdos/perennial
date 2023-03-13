0e4f591e54811058573dcc247de7855e4515048e
Upamanyu Sharma <upamanyu@mit.edu>
2023-03-13 14:56:27 -0400

    Fix fmlist_map

--- src/program_proof/simplepb/fmlist_map.v
+++ src/program_proof/simplepb/fmlist_map.v
@@ -7,15 +7,16 @@ From Perennial.program_proof Require Import proof_prelude.
 
 Section definitions.
 Class fmlist_mapG Σ K V `{Countable K} :=
-   { fmlist_map_inG :> inG Σ (gmapR K (mono_listR V)) }.
+   { fmlist_map_inG :> inG Σ (gmapR K (mono_listR (leibnizO V))) }.
 
-Definition fmlist_mapΣ K V `{Countable K} := #[GFunctor (gmapUR K (mono_listR V))].
+Definition fmlist_mapΣ K V `{Countable K} := #[GFunctor (gmapUR K (mono_listR (leibnizO V)))].
 
 Global Instance subG_fmlist_mapΣ K V `{Countable K} {Σ} :
   subG (fmlist_mapΣ K V) Σ → fmlist_mapG Σ K V.
 Proof. solve_inG. Qed.
 
-Definition fmlist_ptsto_def `{fmlist_mapG Σ K V} γ (k:K) dq v :=
+Context `{fmlist_mapG Σ K V}.
+Definition fmlist_ptsto_def γ (k:K) dq v :=
   own γ {[ k := ●ML{dq} v]}.
 Definition fmlist_ptsto_aux : seal (@fmlist_ptsto_def). Proof. by eexists. Qed.
 Definition fmlist_ptsto := fmlist_ptsto_aux.(unseal).
@@ -29,6 +30,7 @@ Definition fmlist_ptsto_lb_eq : @fmlist_ptsto_lb = @fmlist_ptsto_lb_def := fmlis
 
 End definitions.
 
+(** * Notation *)
 Notation "k ⤳l[ γ ]{ dq } v " := (fmlist_ptsto γ k dq v)
 (at level 20, format "k  ⤳l[ γ ]{ dq }  v") : bi_scope.
 Notation "k ⤳l[ γ ]{# q } v " := (k ⤳l[γ]{DfracOwn q} v)%I
@@ -46,82 +48,87 @@ Local Ltac unseal := rewrite
 
 Section lemmas.
   Context `{fmlist_mapG Σ K V}.
-  Implicit Types (k : K) (v : V) (dq : dfrac) (q : Qp).
+  Implicit Types (k : K) (l : list V) (dq : dfrac) (q : Qp).
 
   (** * Lemmas about the fmlist_ptsto and fmlist_ptsto_lb *)
-  About fmlist_ptsto.
-  Global Instance ghost_map_points_to_timeless k γ dq v : Timeless (k ⤳l[γ]{dq} v).
+  Global Instance fmlist_ptsto_timeless k γ dq l : Timeless (k ⤳l[γ]{dq} l).
   Proof. unseal. apply _. Qed.
-  Global Instance ghost_map_points_to_persistent k γ v : Persistent (k ⤳l[γ]□ v).
+  Global Instance fmlist_ptsto_persistent k γ l : Persistent (k ⤳l[γ]□ l).
   Proof. unseal. apply _. Qed.
-  Global Instance ghost_map_points_to_fractional k γ v : Fractional (λ q, k ⤳[γ]{#q} v)%I.
-  Proof. unseal. intros p q. rewrite -own_op singleton_op -dfrac_agree_op. done. Qed.
+  Global Instance fmlist_ptsto_fractional k γ l : Fractional (λ q, k ⤳l[γ]{#q} l)%I.
+  Proof. unseal. intros p q. rewrite -own_op singleton_op -mono_list_auth_dfrac_op. done. Qed.
 
-  Global Instance ghost_map_points_to_as_fractional k γ q v :
-    AsFractional (k ⤳[γ]{#q} v) (λ q, k ⤳[γ]{#q} v)%I q.
+  Global Instance fmlist_ptsto_as_fractional k γ q v :
+    AsFractional (k ⤳l[γ]{#q} v) (λ q, k ⤳l[γ]{#q} v)%I q.
   Proof. split; first done. apply _. Qed.
 
-  Lemma ghost_map_points_to_valid k γ dq v : k ⤳[γ]{dq} v -∗ ⌜✓ dq⌝.
+  Lemma fmlist_ptsto_valid k γ dq v : k ⤳l[γ]{dq} v -∗ ⌜✓ dq⌝.
   Proof.
     unseal. iIntros "Hpoints_to".
     iDestruct (own_valid with "Hpoints_to") as %Hvalid%singleton_valid.
-    rewrite /to_dfrac_agree in Hvalid. by destruct Hvalid.
+    rewrite mono_list_auth_dfrac_valid in Hvalid. done.
   Qed.
-  Lemma ghost_map_points_to_valid_2 k γ dq1 dq2 v1 v2 :
-    k ⤳[γ]{dq1} v1 -∗ k ⤳[γ]{dq2} v2 -∗ ⌜✓ (dq1 ⋅ dq2) ∧ v1 = v2⌝.
+
+  Lemma fmlist_ptsto_valid_2 k γ dq1 dq2 v1 v2 :
+    k ⤳l[γ]{dq1} v1 -∗ k ⤳l[γ]{dq2} v2 -∗ ⌜✓ (dq1 ⋅ dq2) ∧ v1 = v2⌝.
   Proof.
     unseal. iIntros "H1 H2".
     iDestruct (own_valid_2 with "H1 H2") as %Hvalid.
-    rewrite singleton_op singleton_valid dfrac_agree_op_valid in Hvalid.
-    done.
-  Qed.
-  Lemma ghost_map_points_to_agree k γ dq1 dq2 v1 v2 :
-    k ⤳[γ]{dq1} v1 -∗ k ⤳[γ]{dq2} v2 -∗ ⌜v1 = v2⌝.
-  Proof.
-    iIntros "Hpoints_to1 Hpoints_to2".
-    iDestruct (ghost_map_points_to_valid_2 with "Hpoints_to1 Hpoints_to2") as %[_ ?].
+    rewrite singleton_op singleton_valid mono_list_auth_dfrac_op_valid_L in Hvalid.
     done.
   Qed.
 
-  Lemma ghost_map_points_to_combine k γ dq1 dq2 v1 v2 :
-    k ⤳[γ]{dq1} v1 -∗ k ⤳[γ]{dq2} v2 -∗ k ⤳[γ]{dq1 ⋅ dq2} v1 ∗ ⌜v1 = v2⌝.
+  Lemma fmlist_ptsto_agree k γ dq1 dq2 v1 v2 :
+    k ⤳l[γ]{dq1} v1 -∗ k ⤳l[γ]{dq2} v2 -∗ ⌜v1 = v2⌝.
   Proof.
-    iIntros "Hl1 Hl2". iDestruct (ghost_map_points_to_agree with "Hl1 Hl2") as %->.
+    iIntros "Hpoints_to1 Hpoints_to2".
+    iDestruct (fmlist_ptsto_valid_2 with "Hpoints_to1 Hpoints_to2") as %[_ ?].
+    done.
+  Qed.
+
+  Lemma fmlist_ptsto_combine k γ dq1 dq2 v1 v2 :
+    k ⤳l[γ]{dq1} v1 -∗ k ⤳l[γ]{dq2} v2 -∗ k ⤳l[γ]{dq1 ⋅ dq2} v1 ∗ ⌜v1 = v2⌝.
+  Proof.
+    iIntros "Hl1 Hl2". iDestruct (fmlist_ptsto_agree with "Hl1 Hl2") as %->.
     unseal. iCombine "Hl1 Hl2" as "Hl".
-    rewrite -dfrac_agree_op.
     iFrame.
     done.
   Qed.
 
-  Lemma ghost_map_points_to_frac_ne γ k1 k2 dq1 dq2 v1 v2 :
-    ¬ ✓ (dq1 ⋅ dq2) → k1 ⤳[γ]{dq1} v1 -∗ k2 ⤳[γ]{dq2} v2 -∗ ⌜k1 ≠ k2⌝.
+  Lemma fmlist_ptsto_frac_ne γ k1 k2 dq1 dq2 v1 v2 :
+    ¬ ✓ (dq1 ⋅ dq2) → k1 ⤳l[γ]{dq1} v1 -∗ k2 ⤳l[γ]{dq2} v2 -∗ ⌜k1 ≠ k2⌝.
   Proof.
     iIntros (?) "H1 H2"; iIntros (->).
-    by iDestruct (ghost_map_points_to_valid_2 with "H1 H2") as %[??].
+    by iDestruct (fmlist_ptsto_valid_2 with "H1 H2") as %[??].
   Qed.
-  Lemma ghost_map_points_to_ne γ k1 k2 dq2 v1 v2 :
-    k1 ⤳[γ] v1 -∗ k2 ⤳[γ]{dq2} v2 -∗ ⌜k1 ≠ k2⌝.
-  Proof. apply ghost_map_points_to_frac_ne. apply: exclusive_l. Qed.
+
+  Lemma fmlist_ptsto_ne γ k1 k2 dq2 v1 v2 :
+    k1 ⤳l[γ] v1 -∗ k2 ⤳l[γ]{dq2} v2 -∗ ⌜k1 ≠ k2⌝.
+  Proof. apply fmlist_ptsto_frac_ne. apply: exclusive_l. Qed.
 
   (** Make an points_toent read-only. *)
-  Lemma ghost_map_points_to_persist k γ dq v :
-    k ⤳[γ]{dq} v ==∗ k ⤳[γ]□ v.
+  Lemma fmlist_ptsto_persist k γ dq v :
+    k ⤳l[γ]{dq} v ==∗ k ⤳l[γ]□ v.
   Proof. unseal. iApply own_update. apply singleton_update.
-         apply dfrac_agree_persist. Qed.
+         apply mono_list_auth_persist. Qed.
 
   (** * Lemmas about [ghost_map_auth] *)
-  Lemma ghost_map_alloc_strong P (m:gmap K V) :
+  Lemma ghost_map_alloc_strong P (m:gmap K (list V)) :
     pred_infinite P →
-    ⊢ |==> ∃ γ, ⌜P γ⌝ ∗ [∗ map] k ↦ v ∈ m, k ⤳[γ] v.
+    ⊢ |==> ∃ γ, ⌜P γ⌝ ∗ [∗ map] k ↦ v ∈ m, k ⤳l[γ] v.
   Proof.
     unseal. intros.
-    iMod (own_alloc_strong (A:= (gmapR K (dfrac_agreeR (leibnizO V))))
-                              (fmap (λ v, to_dfrac_agree (DfracOwn 1) (v:leibnizO V)) m) P)
+    iMod (own_alloc_strong (A:= (gmapUR K (mono_listR (leibnizO V))))
+                              (fmap (λ v, (●ML (v:list (leibnizO V)))) m) P)
       as (γ) "Hauth".
     { done. }
-    { intros k.
+    {
+      intros k.
       rewrite lookup_fmap.
-      by destruct (m !! k).
+      simpl.
+      destruct (@lookup _ _ _ _ _).
+      { simpl. apply mono_list_auth_valid. }
+      { done. }
     }
     iExists γ.
     iDestruct "Hauth" as "[$ Hauth]".
@@ -134,10 +141,10 @@ Section lemmas.
     iFrame.
   Qed.
 
-  Lemma ghost_map_alloc_fin (v:V) `{!finite.Finite K} :
-    ⊢ |==> ∃ γ, [∗ set] k ∈ (fin_to_set K), k ⤳[γ] v.
+  Lemma ghost_map_alloc_fin (l:list V) `{!finite.Finite K} :
+    ⊢ |==> ∃ γ, [∗ set] k ∈ (fin_to_set K), k ⤳l[γ] l.
   Proof.
-    set (m:=gset_to_gmap (K:=K) (A:=V) v (fin_to_set K)).
+    set (m:=gset_to_gmap (K:=K) (A:=(list V)) l (fin_to_set K)).
     iStartProof.
     iMod (ghost_map_alloc_strong (λ _, True) m) as (?) "[_ H]".
     { apply pred_infinite_True. }
@@ -155,13 +162,15 @@ Section lemmas.
     naive_solver.
   Qed.
 
-  Lemma ghost_map_points_to_update {γ k v} w :
-    k ⤳[γ] v ==∗ k ⤳[γ] w.
+  Lemma fmlist_ptsto_update {γ k v} w :
+    prefix v w →
+    k ⤳l[γ] v ==∗ k ⤳l[γ] w.
   Proof.
+    intros.
     unseal. apply own_update.
     apply singleton_update.
-    apply cmra_update_exclusive.
-    by constructor.
+    apply mono_list_update.
+    done.
   Qed.
 
 End lemmas.
