8ff4fe0fdd13a059b317bd5583307ba774d803de
Upamanyu Sharma <upamanyu@mit.edu>
2023-04-01 17:27:27 -0400

    Prove start read step of preread protocol

--- src/program_proof/simplepb/pb_preread_protocol.v
+++ src/program_proof/simplepb/pb_preread_protocol.v
@@ -19,16 +19,20 @@ Context `{Countable (list EntryType → iProp Σ)}.
 
 (* This is the key ghost state, keeping track of RO ops that have been
    pre-applied before state is committed. *)
-Implicit Types ros:list (gset (list EntryType → iProp Σ)).
+Implicit Types ros:gmap nat (gset (list EntryType → iProp Σ)).
 
-Definition own_proposed_reads (γ:gname) (ros:list (gset (list EntryType → iProp Σ))) : iProp Σ.
+Definition own_proposed_reads (γ:gname) (ros:gmap nat (gset (list EntryType → iProp Σ))) : iProp Σ.
 Admitted.
 
 Definition is_proposed_read (γ:gname) (idx:nat) (Q:(list EntryType → iProp Σ)) : iProp Σ.
 Admitted.
 
-Definition own_log (γlog:gname) (σ:list EntryType) : iProp Σ.
+Instance is_proposed_read_pers γ idx Q : Persistent (is_proposed_read γ idx Q).
 Admitted.
+
+(* This is a 1/2 fraction *)
+Definition own_log (γlog:gname) (σ:list EntryType) : iProp Σ :=
+  own γlog (●ML{#1/2} (σ : list (leibnizO EntryType))).
 
 Definition is_log_lb (γlog:gname) (σ:list EntryType) : iProp Σ.
 Admitted.
@@ -36,14 +40,13 @@ Admitted.
 (* Maybe make the fupds non-persistent, and track things more carefully in the
    proof. *)
 Definition have_proposed_reads_fupds γlog ros : iProp Σ :=
-  [∗ list] idx ↦ rosAtIdx ∈ ros, [∗ set] Q ∈ rosAtIdx,
+  [∗ map] idx ↦ rosAtIdx ∈ ros, [∗ set] Q ∈ rosAtIdx,
     □(|={⊤,∅}=> ∃ σ, own_log γlog σ ∗ (own_log γlog σ ={∅,⊤}=∗ □ Q σ))
 .
 
 (* Maybe make Q not persistent, and escrow it back to the caller *)
 Definition have_completed_reads_Qs ros σ : iProp Σ :=
-  let completed_ros := take (length σ) ros in
-  [∗ list] idx ↦ rosAtIdx ∈ completed_ros, [∗ set] Q ∈ rosAtIdx,
+  [∗ map] idx ↦ rosAtIdx ∈ (filter (λ '(idx, _), idx < length σ) ros), [∗ set] Q ∈ rosAtIdx,
     □ Q (take idx σ)
 .
 
@@ -52,18 +55,46 @@ Definition prereadN := nroot .@ "preread".
 Definition preread_inv γ γlog γreads : iProp Σ :=
   inv prereadN (
   ∃ σ ros,
-  own_ghost γ σ ∗
-  own_log γlog σ ∗
+  "HpbLog" ∷ own_ghost γ σ ∗
+  "Hlog" ∷ own_log γlog σ ∗
   (* For all i < length(σ), the read-op fupds for
    *)
-  own_proposed_reads γreads ros ∗
-  have_proposed_reads_fupds γlog ros ∗
-  have_completed_reads_Qs ros σ
+  "HownRos" ∷ own_proposed_reads γreads ros ∗
+  "#HreadUpds" ∷ have_proposed_reads_fupds γlog ros ∗
+  "#HcompletedRead" ∷ have_completed_reads_Qs ros σ
   )
 .
 
+Lemma update_map_fmset idx Q ros γreads :
+  own_proposed_reads γreads ros ==∗
+  own_proposed_reads γreads (<[ idx := {[ Q ]} ∪ default ∅ (ros !! idx) ]> ros)
+.
+Proof.
+Admitted.
+
+Lemma map_fmset_get_elem idx Q ros γreads :
+  Q ∈ default ∅ (ros !! idx) →
+  own_proposed_reads γreads ros -∗
+  is_proposed_read γreads idx Q
+.
+Proof.
+Admitted.
+
+Lemma own_log_agree γlog σ σ' :
+  own_log γlog σ -∗
+  own_log γlog σ' -∗
+  ⌜σ = σ'⌝.
+Proof.
+  iIntros "H1 H2".
+  iDestruct (own_valid_2 with "H1 H2") as %Hvalid.
+  iPureIntro.
+  rewrite mono_list_auth_dfrac_op_valid_L in Hvalid.
+  by destruct Hvalid as [_ ?].
+Qed.
+
 Lemma start_read_step Q γ γlog γreads idx σ :
-  idx < length σ →
+  idx > length σ →
+  £ 1 -∗
   preread_inv γ γlog γreads -∗
   □(|={⊤,∅}=> ∃ σ, own_log γlog σ ∗ (own_log γlog σ ={∅,⊤}=∗ □ Q σ)) -∗
   own_log γlog σ
@@ -71,7 +102,45 @@ Lemma start_read_step Q γ γlog γreads idx σ :
   is_proposed_read γreads idx Q ∗ own_log γlog σ
 .
 Proof.
-Admitted.
+  intros Hidx.
+  iIntros "Hlc #Hinv #Hupd Hlog_in".
+  iInv "Hinv" as "Hown" "Hclose".
+  iMod (lc_fupd_elim_later with "Hlc Hown") as "Hown".
+  iNamed "Hown".
+  iDestruct (own_log_agree with "Hlog_in Hlog") as %?.
+  subst.
+  iFrame "Hlog_in".
+  iMod (update_map_fmset idx Q with "HownRos") as "HownRos".
+  iDestruct (map_fmset_get_elem idx Q with "HownRos") as "#His_read".
+  { rewrite lookup_insert. set_solver. }
+  iMod ("Hclose" with "[-]"); last done.
+  iNext.
+  iExists _, _.
+  iFrame.
+  iSplit.
+  {
+    iApply (big_sepM_insert_2 with "[] HreadUpds").
+    Search "big_sepS_insert".
+    iApply (big_sepS_insert_2 with "[] []").
+    { done. }
+    (* the rest of these fupds follow straightforwardly old have_proposed_reads_fupds *)
+    destruct (ros !! idx) as [] eqn:Hlookup.
+    {
+      iDestruct (big_sepM_lookup with "HreadUpds") as "Hupds".
+      { done. }
+      iFrame "Hupds".
+    }
+    by iApply big_sepS_empty.
+  }
+  (* FIXME: there's no point in using filter... *)
+  iApply big_sepM_filter.
+  iDestruct (big_sepM_filter with "HcompletedRead") as "HcompletedRead2".
+  simpl.
+  iApply (big_sepM_insert_2 with "[] HcompletedRead2").
+  iIntros (?).
+  exfalso.
+  lia.
+Qed.
 
 Lemma finish_read_step Q γ γlog γreads idx σ :
   length σ = idx →
