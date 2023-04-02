e5df694f33203f9d3af556c686ee49f1d403acc7
Upamanyu Sharma <upamanyu@mit.edu>
2023-04-02 16:52:56 -0400

    Add support for renewing with an extra layer of indirection for expiration time.
    Later will add lemma for doing the renewing itself.

--- src/program_proof/simplepb/renewable_lease.v
+++ src/program_proof/simplepb/renewable_lease.v
@@ -13,26 +13,42 @@ Context `{!ghost_varG Σ ()}.
 Notation is_time_lb := (is_time_lb γtime).
 Notation own_time := (own_time γtime).
 
-Definition isLease N (expirationTime:u64) (R:iProp Σ) : iProp Σ :=
-  ∃ γtok,
-  inv N (R ∨ is_time_lb expirationTime ∗ (R ∨ ghost_var γtok 1 ())).
-
-Definition postLease N (expirationTime:u64) (R:iProp Σ) : iProp Σ :=
-  is_time_lb expirationTime ={↑N}=∗ ▷ R
+Definition is_lease_valid_lb γl (t:u64) :=
+  mono_nat_lb_own γl (int.nat t)
 .
 
-Lemma lease_acc N e R t :
+Definition own_lease_expiration γl (t:u64) :=
+  mono_nat_auth_own γl (1/2) (int.nat t)
+.
+
+Definition isLease N γl (R:iProp Σ) : iProp Σ :=
+  ∃ γtok,
+  inv N (∃ expirationTime, own_lease_expiration γl expirationTime ∗
+        (R ∨ is_time_lb expirationTime ∗ (R ∨ ghost_var γtok 1 ())))
+.
+
+Definition postLease N γl (R:iProp Σ) : iProp Σ :=
+  ∀ expirationTime,
+  own_lease_expiration γl expirationTime -∗
+  is_time_lb expirationTime ={↑N}=∗
+  ▷ R
+.
+
+Lemma lease_acc N e R γl t :
   int.nat t < int.nat e →
-  isLease N e R -∗
+  is_lease_valid_lb γl e -∗
+  isLease N γl R -∗
   £ 1 -∗
   own_time t ={↑N,∅}=∗
   R ∗ (R ={∅,↑N}=∗ own_time t)
 .
 Proof.
-  iIntros (HnotExpired) "#Hlease Hlc Htime".
+  iIntros (HnotExpired) "#Hlease_lb #Hlease Hlc Htime".
   iDestruct "Hlease" as (γtok) "#Hinv".
   iInv "Hinv" as "Hi" "Hclose".
   iMod (lc_fupd_elim_later with "Hlc Hi") as "Hi".
+  iDestruct "Hi" as (?) "[Hexpiration Hi]".
+  iDestruct (mono_nat_lb_own_valid with "Hexpiration Hlease_lb") as %[_ Hineq].
   iDestruct "Hi" as "[HR | [Hbad _]]"; last first.
   {
     iDestruct (mono_nat_lb_own_valid with "Htime Hbad") as %[_ Hbad].
@@ -44,31 +60,43 @@ Proof.
   iIntros "HR".
   iFrame "Htime".
   iMod ("Hclose" with "[-]"); last done.
-  by iLeft.
+  iExists _; iFrame.
 Qed.
 
 Lemma lease_alloc e N R :
   R ={↑N}=∗
-  isLease N e R ∗
-  postLease N e R
+  ∃ γl,
+  isLease N γl R ∗
+  postLease N γl R ∗
+  own_lease_expiration γl e
 .
 Proof.
   iIntros "HR".
   iMod (ghost_var_alloc ()) as (γtok) "Htok".
-  iMod (inv_alloc with "[HR]") as "#Hinv".
+  iMod (mono_nat_own_alloc (int.nat e)) as (γl) "[Hexp _]".
+  iDestruct "Hexp" as "[Hexp Hexp2]".
+  iExists γl.
+  iFrame "Hexp".
+  iMod (inv_alloc with "[HR Hexp2]") as "#Hinv".
   2: {
     iSplitR "Htok".
     { iModIntro. iExists γtok. done. }
     iModIntro.
-    iIntros "Hlb".
+    iIntros (?) "Hexp Hlb".
     iInv "Hinv" as "Hi" "Hclose".
+    iDestruct "Hi" as (?) "(>Hexp2 & Hi)".
+    iDestruct (mono_nat_auth_own_agree with "Hexp Hexp2") as %[_ Hagree].
+    assert (expirationTime = expirationTime0) by word.
+    subst.
     iDestruct "Hi" as "[HR | (_ & [HR | >Hbad])]".
     {
       iFrame "HR". iMod ("Hclose" with "[-]"); last done.
+      iExists _; iFrame.
       iRight. iFrame.
     }
     {
       iFrame. iMod ("Hclose" with "[-]"); last done.
+      iExists _; iFrame.
       iRight. iFrame.
     }
     {
@@ -77,7 +105,7 @@ Proof.
       done.
     }
   }
-  { by iLeft. }
+  { iExists _. iFrame. }
 Qed.
 
 End renewable_lease_protocol.
