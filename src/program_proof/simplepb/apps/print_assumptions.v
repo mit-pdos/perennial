28e710308975585b42e647f5e4d3465120c798ca
Upamanyu Sharma <upamanyu@mit.edu>
2023-07-27 00:13:56 -0400

    Tweak print_assumptions

--- src/program_proof/simplepb/apps/print_assumptions.v
+++ src/program_proof/simplepb/apps/print_assumptions.v
@@ -1,14 +1,14 @@
 From Perennial.program_proof.simplepb Require config_proof admin_proof.
 From Perennial.program_proof.simplepb.apps Require kv_proof kvee_proof closed_proof.
 
-(* FIXME this list is probably incomplete *)
 Definition lemmas :=
-  (@config_proof.wp_MakeServer, @config_proof.wp_Server__Serve,
-  @admin_proof.wp_Reconfig,
-  @kvee_proof.wp_Start,
-  @kvee_proof.wp_MakeClerk,
-  @kvee_proof.wp_Clerk__Put,
-  @kvee_proof.wp_Clerk__Get).
+  (@config_proof.wp_MakeServer,
+   @config_proof.wp_Server__Serve,
+   @admin_proof.wp_Reconfig,
+   @kvee_proof.wp_Start,
+   @kvee_proof.wp_MakeClerk,
+   @kvee_proof.wp_Clerk__Put,
+   @kvee_proof.wp_Clerk__Get).
 
 Print Assumptions lemmas.
 
