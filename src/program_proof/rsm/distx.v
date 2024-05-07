c902c076cd40f31baa612eb81a6b950e5838659e
Yun-Sheng Chang <yschang@mit.edu>
2024-05-07 17:36:55 -0400

    Checkpoint distx invariant

---
+++ src/program_proof/rsm/distx.v
@@ -0,0 +1,223 @@
+From Perennial.program_proof Require Export grove_prelude.
+
+Definition dbkey := string.
+Definition dbval := option string.
+Definition dbtpl := (list dbval * nat)%type.
+Canonical Structure dbvalO := leibnizO dbval.
+Notation Nil := (None : dbval).
+Notation Value x := (Some x : dbval).
+Definition dbmap := gmap dbkey dbval.
+
+Inductive command :=
+| CmdPrep (tid : nat) (wrs : dbmap)
+| CmdCmt (tid : nat)
+| CmdAbt (tid : nat)
+| CmdRead (tid : nat) (key : dbkey).
+
+#[local]
+Instance cmd_eq_decision :
+  EqDecision command.
+Proof. solve_decision. Qed.
+
+(* Transaction status *)
+Inductive txnst :=
+| Prepared (wrs : dbmap)
+| Committed
+| Aborted.
+
+(* Replica state *)
+Inductive rpst :=
+| State (txns : gmap nat txnst) (tpls : gmap dbkey dbtpl)
+| Stuck.
+
+Inductive acquiring :=
+| Acquired (tpls : gmap dbkey dbtpl)
+| NotAcquired.
+
+Definition validate_key (tid : nat) (wr : option dbval) (tpl : option dbtpl) :=
+  match wr, tpl with
+  (* TODO: check if [<] is the right thing to do. *)
+  | Some _, Some (vs, tsprep) => Some (bool_decide (tsprep = O ∧ length vs < tid)%nat)
+  | _, _ => None
+  end.
+
+Definition validate (tid : nat) (wrs : dbmap) (tpls : gmap dbkey dbtpl) :=
+  map_fold (λ _, andb) true (merge (validate_key tid) wrs tpls).
+
+Definition acquire_key (tid : nat) (wr : option dbval) (tpl : option dbtpl) :=
+  match wr, tpl with
+  | None, Some (vs, tsprep) => Some (vs, tsprep)
+  | Some _, Some (vs, _) => Some (vs, tid)
+  | _, _ => None
+  end.
+
+Definition acquire (tid : nat) (wrs : dbmap) (tpls : gmap dbkey dbtpl) :=
+  merge (acquire_key tid) wrs tpls.
+
+Definition try_acquire (tid : nat) (wrs : dbmap) (tpls : gmap dbkey dbtpl) :=
+  if validate tid wrs tpls then Acquired (acquire tid wrs tpls) else NotAcquired.
+
+Definition apply_prepare st (tid : nat) (wrs : dbmap) :=
+  match st with
+  | State txns tpls =>
+      match txns !! tid with
+      | Some _ => st
+      | None =>  match try_acquire tid wrs tpls with
+                | Acquired tpls' => State (<[ tid := Prepared wrs ]> txns) tpls'
+                | NotAcquired => State (<[ tid := Aborted ]> txns) tpls
+                end
+      end
+  | Stuck => Stuck
+  end.
+
+(* TODO: reorder [x] and [n]. *)
+Definition extend {X} (x : X) (n : nat) (l : list X) :=
+    l ++ replicate (n - length l) x.
+
+Definition multiwrite_key (tid : nat) (wr : option dbval) (tpl : option dbtpl) :=
+  match wr, tpl with
+  | None, Some (vs, tsprep) => Some (vs, tsprep)
+  | Some v, Some (vs, _) => Some (extend v tid vs, O)
+  | _, _ => None
+  end.
+
+Definition multiwrite (tid : nat) (wrs : dbmap) (tpls : gmap dbkey dbtpl) :=
+  merge (multiwrite_key tid) wrs tpls.
+
+Definition apply_commit st (tid : nat) :=
+  match st with
+  | State txns tpls =>
+      match txns !! tid with
+      | Some Committed => st
+      | Some (Prepared wrs) => State (<[ tid := Committed ]> txns) (multiwrite tid wrs tpls)
+      | _ => Stuck
+      end
+  | Stuck => Stuck
+  end.
+
+Definition release_key (tid : nat) (wr : option dbval) (tpl : option dbtpl) :=
+  match wr, tpl with
+  | None, Some (vs, tsprep) => Some (vs, tsprep)
+  | Some _, Some (vs, _) => Some (vs, O)
+  | _, _ => None
+  end.
+
+Definition release (tid : nat) (wrs : dbmap) (tpls : gmap dbkey dbtpl) :=
+  merge (release_key tid) wrs tpls.
+
+Definition apply_abort st (tid : nat) :=
+  match st with
+  | State txns tpls =>
+      match txns !! tid with
+      | Some Aborted => st
+      | Some (Prepared wrs) => State (<[ tid := Aborted ]> txns) (release tid wrs tpls)
+      | None => State (<[ tid := Aborted ]> txns) tpls
+      | _ => Stuck
+      end
+  | Stuck => Stuck
+  end.
+
+(* TODO *)
+Definition last_extend {A} (n : nat) (l : list A) := l.
+
+Definition read (tid : nat) (vs : list dbval) (tsprep : nat) :=
+  if decide (tsprep = 0 ∨ tid < tsprep)%nat
+  then (last_extend tid vs, tsprep)
+  else (vs, tsprep).
+
+Definition apply_read st (tid : nat) (key : dbkey) :=
+  match st with
+  | State txns tpls =>
+      match tpls !! key with
+      | Some (vs, tsprep) => State txns (<[ key := (read tid vs tsprep) ]> tpls)
+      | None => st
+      end
+  | Stuck => Stuck
+  end.
+
+Definition apply_cmd st (cmd : command) :=
+  match cmd with
+  | CmdPrep tid wrs => apply_prepare st tid wrs
+  | CmdCmt tid => apply_commit st tid
+  | CmdAbt tid => apply_abort st tid
+  | CmdRead tid key => apply_read st tid key
+  end.
+
+(* TODO: should initial tuple state be ∅? *)
+Definition apply_cmds (cmds : list command) :=
+  foldl apply_cmd (State ∅ ∅) cmds.
+
+Inductive action :=
+| ActCmt (tid : nat) (wrs : dbmap)
+| ActRead (tid : nat) (key : dbkey).
+
+Definition diff_by_ongoing (repl cmtd : list dbval) (kcmt : option (nat * dbval)) :=
+  match kcmt with
+  | Some (ts, v) => cmtd = last_extend ts repl ++ [v]
+  | None => ∃ ts', repl = last_extend ts' cmtd
+  end.
+
+Definition diff_by_linearized (cmtd lnrz : list dbval) (txns : gmap nat dbmap) : Prop.
+Admitted.
+
+Definition conflict_free (acts : list action) (txns : gmap nat dbmap) : Prop.
+Admitted.
+
+Definition conflict_past (acts_future acts_past : list action) (txns : gmap nat dbmap) : Prop.
+Admitted.
+
+Definition exec_eq (key : dbkey) (tpls : gmap dbkey dbtpl) (repl : list dbval) :=
+  ∃ ts, tpls !! key = Some (repl, ts).
+
+Definition per_key_inv (key : dbkey) (db : dbmap) (kcmts : gmap dbkey (nat * dbval)) txns tpls :=
+  ∃ repl cmtd lnrz,
+    db !! key = last lnrz ∧
+    exec_eq key tpls repl ∧
+    prefix cmtd lnrz ∧
+    diff_by_ongoing repl cmtd (kcmts !! key) ∧
+    diff_by_linearized cmtd lnrz txns.
+
+Definition exclusive
+  (key : dbkey) (acts : list action) (cmds : list command)
+  (kcmts : gmap dbkey (nat * dbval)) :=
+  match kcmts !! key with
+  | Some (ts, _) => (∃ wrs, ActCmt ts wrs ∈ acts) ∧ CmdCmt ts ∉ cmds
+  | _ => True
+  end.
+
+Definition repl_impl_cmtd (acts : list action) (cmds : list command) :=
+  ∀ ts, CmdCmt ts ∈ cmds → ∃ wrs, ActCmt ts wrs ∈ acts.
+
+Definition has_prepared ts wrs cmds :=
+  ∃ cmdsp, prefix cmdsp cmds ∧
+           match apply_cmds cmdsp with
+           | State txnst _ => txnst !! ts = Some (Prepared wrs)
+           | _ => False
+           end.
+
+Definition safe_commit (acts : list action) (cmds : list command) :=
+  ∀ ts wrs, ActCmt ts wrs ∈ acts → has_prepared ts wrs cmds.
+
+Definition has_extended ts key cmds :=
+  ∃ cmdsp, prefix cmdsp cmds ∧
+           match apply_cmds cmdsp with
+           | State _ tpls => match tpls !! key with
+                            | Some (vs, _) => (ts < length vs)%nat
+                            | _ => False
+                            end
+           | _ => False
+           end.
+
+Definition safe_read (acts : list action) (cmds : list command) :=
+  ∀ ts key, ActRead ts key ∈ acts → has_extended ts key cmds.
+
+Definition distx_inv :=
+  ∃ txnst tpls acts_future acts_past cmds_paxos kcmts_ongoing db txns_cmt txns_abt,
+    (∀ key, per_key_inv key db kcmts_ongoing txns_cmt tpls) ∧
+    (∀ key, exclusive key acts_past cmds_paxos kcmts_ongoing) ∧
+    repl_impl_cmtd acts_past cmds_paxos ∧
+    conflict_free acts_future txns_cmt ∧
+    conflict_past acts_future acts_past txns_abt ∧
+    safe_commit acts_past cmds_paxos ∧
+    safe_read acts_past cmds_paxos ∧
+    apply_cmds cmds_paxos = State txnst tpls.
