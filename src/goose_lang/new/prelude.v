83f9f3ad3cff04092aa3af4c92c9d60baf6b7454
Upamanyu Sharma <upamanyu@mit.edu>
2024-05-28 23:39:09 -0400

    Add variadic noop

--- src/goose_lang/new/prelude.v
+++ src/goose_lang/new/prelude.v
@@ -4,6 +4,7 @@ From Perennial.goose_lang.lib Require Export
      typed_mem.impl struct.impl
      encoding.impl map.impl slice.impl
      time.impl rand.impl string.impl channel.impl
+     noop.impl
 .
 
 Open Scope heap_types.
