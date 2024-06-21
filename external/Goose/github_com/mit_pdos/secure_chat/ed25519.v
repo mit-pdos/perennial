1a5a74bef5ec4d31641399b0f9e4d431ff4d0ec8
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-06-21 15:00:36 -0400

    rm pav files from secure_chat

--- external/Goose/github_com/mit_pdos/secure_chat/ed25519.v
+++
@@ -1,9 +0,0 @@
-From Perennial.goose_lang Require Import prelude.
-
-Section code.
-Context `{ext_ty: ext_types}.
-Local Coercion Var' s: expr := Var s.
-
-Definition PrivateKey: ty := slice.T byteT.
-Definition PublicKey: ty := slice.T byteT.
-End code.
