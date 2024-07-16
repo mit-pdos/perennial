8a263e237e48ad84936f68e1f423418151b0d859
Upamanyu Sharma <upamanyu@mit.edu>
2024-07-15 22:09:25 -0400

    Prove one case of vmap.Insert; tricky reasoning for map_fold

--- new/golang/theory/vmap.v
+++ new/golang/theory/vmap.v
@@ -28,40 +28,53 @@ Proof.
     { exfalso. subst l.
       rewrite /vmap.vmap.val_list_def /= in Hl.
       apply fmap_nil_inv in Hl.
-      Require Import Coq.Program.Equality.
-      revert Hl.
-      pattern (<[i:=x]> m).
-      pattern (map_fold
-(λ (k0 v0 : val) (kvs : list (val * val)),
-       vmap.insert_replace_sorted (λ '(a, _) '(b, _), val_le a b) (k0, v0) kvs)
-                 [] (<[i:=x]> m)).
-      set P:=
-        (λ g : gmap val val,
-           (λ l : list (val * val), l = [] → False)
-             (map_fold
-                (λ (k0 v0 : val) (kvs : list (val * val)),
-                   vmap.insert_replace_sorted (λ '(a, _) '(b, _), val_le a b) (k0, v0) kvs) [] g)).
-      apply (map_fold_ind (K:=val) (A:=val) (B:=list (val * val)) P).
-
-      set P:=(λ l m, l = [] → False).
-      (λ '(a, b), (a, b)%V) <$>
-        map_fold
-        (λ (k0 v0 : val) (kvs : list (val * val)),
-           vmap.insert_replace_sorted (λ '(a, _) '(b, _), val_le a b) (k0, v0) kvs) []
-        (<[i:=x]> m) = [] → False
-
-      pattern (map_fold
-(λ (k0 v0 : val) (kvs : list (val * val)),
-       vmap.insert_replace_sorted (λ '(a, _) '(b, _), val_le a b) (k0, v0) kvs)
-                 [] (<[i:=x]> m)).
-      pattern m.
-      apply (map_fold_ind (K:=val) (A:=val) (B:=list (val * val))).
-      set (f:=(map_fold _ [] (<[i:=x]> m))) in Hl.
-      About map_fold_ind.
-      induction f using map_fold_ind.
-      unfold map_fold in Hl.
-      Search gmap_fold.
-
-Qed.
+      rewrite map_fold_insert_L in Hl; last done.
+      {
+        apply (f_equal length) in Hl.
+        rewrite /vmap.insert_replace_sorted /= in Hl.
+        set y:=(map_fold _ _ _) in Hl.
+        destruct y.
+        { done. }
+        { destruct p, (val_le i v0), (val_le v0 i); done. }
+      }
+      intros.
+      induction y.
+      {
+        simpl. destruct (val_le j1 j2) eqn:Hy, (val_le j2 j1) eqn:Hx.
+        - exfalso. unfold val_le in *.
+          rewrite !Pos.leb_le in Hx, Hy.
+          pose proof (Pos.le_antisym _ _ Hx Hy).
+          apply (f_equal (decode (A:=val))) in H3.
+          rewrite !decode_encode in H3.
+          inversion H3. subst. done.
+        - done.
+        - done.
+        - exfalso.
+          rewrite !Positive_as_OT.leb_nle in Hx Hy.
+          pose proof (Pos.le_total (encode j1) (encode j2)) as [|].
+          + by apply Hx.
+          + by apply Hy.
+      }
+      {
+        destruct a. simpl in *.
+        destruct (val_le j1 j2) eqn:Hy, (val_le j2 j1) eqn:Hx,
+                   (val_le j1 v0) eqn:?, (val_le j2 v0) eqn:?,
+                     (val_le v0 j1) eqn:?, (val_le v0 j2) eqn:?;
+        unfold val_le in *; simpl in *;
+          repeat lazymatch goal with
+          | [ H : context [Pos.leb] |- _ ] => rewrite ?H; rewrite ?Pos.leb_le ?Pos.leb_gt in H
+          end;
+          repeat lazymatch goal with
+            | [ H1 : Pos.le ?a ?b , H2 : Pos.le ?b ?a |- _ ] =>
+                let Htmp := fresh in
+                pose proof (Pos.le_antisym _ _ H1 H2) as Htmp; clear H1 H2;
+                apply (f_equal (decode (A:=val))) in Htmp;
+                rewrite !decode_encode in Htmp; injection Htmp as ->
+            end.
+        all: try (reflexivity || lia || by exfalso).
+      }
+    }
+    admit.
+Admitted.
 
 End wps.
