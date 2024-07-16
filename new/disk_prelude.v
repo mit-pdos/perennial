9dac378119d8c88c7773f4eca078cbace443fd78
Tej Chajed <chajed@wisc.edu>
2024-07-16 11:17:50 -0500

    Fix new disk prelude

--- new/disk_prelude.v
+++ new/disk_prelude.v
@@ -1,5 +1,5 @@
 From Perennial.goose_lang Require Import lang.
-From Perennial.goose_lang Require Import ffi.disk_ffi.impl.
+From Perennial.goose_lang Require Export ffi.disk_ffi.impl.
 #[global]
 Existing Instances disk.disk_op disk.disk_model.
 Coercion Var' (s: string) := Var s.
