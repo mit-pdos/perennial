23f14ae900fc4ab1c1134fcfa7b95d870bff90fc
Upamanyu Sharma <upamanyu@mit.edu>
2024-07-16 10:39:52 -0400

    More simplification, prove another case of vmap.Insert

--- new/golang/theory/vmap.v
+++ new/golang/theory/vmap.v
@@ -36,30 +36,31 @@ Lemma wp_vmap_Insert (k v : val) (m : gmap val val) :
     vmap.Insert k v (vmap.val m)
   {{{ RET (vmap.val (<[k := v]> m)); True }}}.
 Proof.
-  iInduction m as [] "IH" using map_ind; rewrite vmap.val_unseal.
+  assert (∃ l, vmap.val_list_def m = l) as [l Hl].
+  { eexists _. done. }
+  iInduction l as [] "IH" forall (m Hl); rewrite vmap.val_unseal.
   - (* empty map *)
     iIntros (?) "_ HΦ".
     wp_rec. wp_pures.
     wp_apply (wp_list_Match).
+
+    rewrite /vmap.val_def /vmap.vmap.val_list_def /= in Hl.
+    apply fmap_nil_inv, Permutation_nil_r in Hl.
+    rewrite merge_sort_Permutation in Hl.
+    apply Permutation_nil_r, (f_equal length) in Hl.
+    rewrite map_to_list_length /= in Hl.
+    rewrite map_size_empty_iff in Hl. subst.
+    rewrite /vmap.val_def /vmap.vmap.val_list_def map_to_list_empty
+      insert_empty map_to_list_singleton /=.
+
     wp_pures.
-    rewrite /vmap.val_def /vmap.vmap.val_list_def insert_empty map_to_list_singleton /=.
     by iApply "HΦ".
   - iIntros (?) "_ HΦ".
     wp_rec. wp_pures.
     wp_apply (wp_list_Match).
-    wp_pures.
-    set (l:=vmap.vmap.val_list_def (_)).
-    destruct l eqn:Hl.
-    { exfalso. subst l.
-      rewrite /vmap.vmap.val_list_def /= in Hl.
-      apply fmap_nil_inv, (f_equal length) in Hl.
-      erewrite (Permutation_length ltac:(eapply merge_sort_Permutation)) in Hl.
-      by rewrite map_to_list_length map_size_insert H /= in Hl.
-    }
-    wp_pures.
-    subst l.
-    rewrite /vmap.val_list_def /= in Hl.
-    apply fmap_cons_inv in Hl as [[] [? [-> [ -> Hl]]]]. subst.
+    rewrite Hl.
+    rewrite /vmap.val_def /vmap.vmap.val_list_def /= in Hl.
+    apply fmap_cons_inv in Hl as [[k' v'] [? [-> [-> Hl]]]].
     wp_pures.
     wp_if_destruct.
     {
@@ -69,7 +70,7 @@ Proof.
         pose proof (Pos.le_antisym _ _ Heqb Heqb0) as Heq.
         clear Heqb Heqb0.
         apply (f_equal (decode (A:=val))) in Heq.
-        rewrite !decode_encode in Heq. injection Heq as ->.
+        rewrite !decode_encode in Heq. injection Heq as <-.
         wp_pures.
         iModIntro.
         iSpecialize ("HΦ" with "[//]").
@@ -77,32 +78,38 @@ Proof.
         rewrite /vmap.val_def /=.
         repeat f_equal.
         rewrite /vmap.val_list_def /=.
-        change (v1, v)%V with ((λ x2 : val * val, let (a, b) := x2 in (a, b)%V) (v1, v)).
+        change (k, v)%V with ((λ x2 : val * val, let (a, b) := x2 in (a, b)%V) (k, v)).
         rewrite -fmap_cons. f_equal.
         apply Sorted_unique.
         { setoid_rewrite (merge_sort_Permutation). apply NoDup_fst_map_to_list. }
         {
-          change (NoDup ((v1, v) :: x1).*1) with (NoDup ((v1, v2) :: x1).*1).
+          change (NoDup ((k, v) :: x0).*1) with (NoDup ((k, v') :: x0).*1).
           rewrite -Hl. setoid_rewrite (merge_sort_Permutation). apply NoDup_fst_map_to_list. }
         { apply Sorted_merge_sort. apply _. }
         {
           lazymatch goal with [ H : merge_sort ?c ?l = ?l2 |- _ ] =>
                                 (pose proof Sorted_merge_sort (H:=(@vmap.cmp_dec ext)) c l) end.
-          rewrite Hl in H0.
-          inversion H0. subst.
-          econstructor.
-          { done. }
-          { inversion H4; econstructor. done. }
+          rewrite Hl in H. inversion H; inversion H3; subst;
+          econstructor; try done; econstructor; done.
         }
         {
-          lazymatch goal with [ H : merge_sort ?c ?l = ?l2 |- _ ] =>
-                                (pose proof Sorted_merge_sort (H:=(@vmap.cmp_dec ext)) c l as Hs) end.
-          rewrite Hl in Hs.
-          setoid_rewrite merge_sort_Permutation.
-
+          apply (reflexive_eq_dom_reflexive (R':=(≡ₚ)) id) in Hl.
+          rewrite /= !merge_sort_Permutation in Hl |- *.
+          assert (m = (insert k v' (delete k m))).
+          {
+            symmetry. apply insert_delete.
+            apply elem_of_map_to_list.
+            rewrite Hl.
+            constructor.
+          }
+          rewrite H in Hl |- *.
+          rewrite insert_insert.
+          rewrite !map_to_list_insert in Hl |- *; try by apply lookup_delete.
+          apply Permutation_cons_inv in Hl. naive_solver.
         }
+      - admit.
     }
-
+    admit.
 Admitted.
 
 End wps.
