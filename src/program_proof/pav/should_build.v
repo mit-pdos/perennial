45863d67a954496d0e3451e4300dc42259589a87
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-07-22 12:58:40 -0400

    tmp rm should_build

--- src/program_proof/pav/should_build.v
+++ src/program_proof/pav/should_build.v
@@ -1,3 +1,4 @@
+(*
 From Perennial.program_proof.pav Require Export
   common
   crypto_model
@@ -6,4 +7,6 @@ From Perennial.program_proof.pav Require Export
   evidence
   merkle
   merkle_internal
-  server.
+  server
+  rpc.
+*)
