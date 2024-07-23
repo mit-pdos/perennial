cb4ff2c5f46f6330bfe331a85c9271c7417a7d1a
Tej Chajed <chajed@wisc.edu>
2024-07-23 12:00:10 -0500

    Use Diaframe in the memkv closed proof

--- src/goose_lang/automation/marshal_specs.v
+++ src/goose_lang/automation/marshal_specs.v
@@ -2,15 +2,64 @@ From Perennial.goose_lang.automation Require Import goose_lang_auto std_specs.
 From Perennial.program_proof Require Import proof_prelude.
 From Perennial.program_proof Require Import marshal_proof marshal_stateless_proof.
 
+#[global] Opaque marshal.NewEnc.
+#[global] Opaque marshal.Enc__PutInt.
+#[global] Opaque marshal.Enc__PutBytes.
+#[global] Opaque marshal.Enc__Finish.
 #[global] Opaque marshal.WriteInt.
 #[global] Opaque marshal.ReadInt.
 #[global] Opaque marshal.WriteBytes.
 #[global] Opaque marshal.ReadBytes.
 #[global] Opaque u64_le.
+#[global] Opaque is_enc.
 
 Section proofs.
   Context `{ffi_sem: ffi_semantics} `{!ffi_interp ffi} `{!heapGS Σ}.
   Context {ext_ty: ext_types ext}.
+
+  #[global] Instance NewEnc_spec (sz: w64) :
+   SPEC
+   {{ True }}
+    marshal.NewEnc #sz
+  {{ (enc_v:val), RET enc_v;
+     is_enc enc_v (uint.Z sz) [] (uint.Z sz) }}.
+  Proof.
+    iSteps.
+    wp_apply (wp_new_enc); auto.
+  Qed.
+
+  #[global] Instance Enc__PutInt_spec enc_v sz r (x: w64) remaining :
+    SPEC
+    {{ is_enc enc_v sz r remaining ∗ ⌜8 ≤ remaining⌝ }}
+      marshal.Enc__PutInt enc_v #x
+    {{ RET #(); is_enc enc_v sz (r ++ [EncUInt64 x]) (remaining - 8) }}.
+  Proof.
+    iSteps.
+    wp_apply (wp_Enc__PutInt with "[$]"); auto.
+  Qed.
+
+  #[global] Instance Enc__PutBytes_spec enc_v r sz remaining b_s q bs :
+    SPEC
+  {{ is_enc enc_v sz r remaining ∗ own_slice_small b_s byteT q bs ∗ ⌜Z.of_nat (length bs) ≤ remaining⌝ }}
+    marshal.Enc__PutBytes enc_v (slice_val b_s)
+  {{ RET #(); is_enc enc_v sz (r ++ [EncBytes bs]) (remaining - Z.of_nat (length bs)) ∗
+               own_slice_small b_s byteT q bs }}.
+  Proof.
+    iSteps.
+    wp_apply (wp_Enc__PutBytes with "[$]"); auto.
+  Qed.
+
+  #[global] Instance Enc__Finish_spec enc_v r sz remaining :
+      SPEC
+    {{ is_enc enc_v sz r remaining }}
+      marshal.Enc__Finish enc_v
+    {{ s data, RET slice_val s; ⌜has_encoding data r⌝ ∗
+                                ⌜length data = Z.to_nat sz⌝ ∗
+                                own_slice s byteT (DfracOwn 1) data }}.
+  Proof.
+    iStep.
+    wp_apply (wp_Enc__Finish with "[$]"); auto.
+  Qed.
 
   #[global] Instance WriteInt_spec s vs (x: w64) :
     SPEC
