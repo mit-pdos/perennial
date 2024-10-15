8936f88951227843f47c263925c71fb2d3eb5cf6
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-10-15 14:24:16 -0400

    reduce client spec. shouldn't need to know about prior keys

--- src/program_proof/pav/client.v
+++ src/program_proof/pav/client.v
@@ -3,6 +3,7 @@ From Goose.github_com.mit_pdos.pav Require Import kt.
 
 From RecordUpdate Require Import RecordSet.
 From Perennial.base_logic.lib Require Import ghost_map.
+From iris.unstable.base_logic Require Import mono_list.
 From Perennial.program_proof.pav Require Import auditor core cryptoffi merkle serde server.
 
 (* TODO: unify these with core as soon as core uses w64's instead of nat's. *)
@@ -28,11 +29,11 @@ Record t :=
     serv_sig_pk: list w8;
     serv_vrf_pk: loc;
   }.
-Global Instance eta_cli : Settable _ :=
+Global Instance eta : Settable _ :=
   settable! mk <γ; uid; next_ver; serv_cli; serv_γ; serv_sig_pk; serv_vrf_pk>.
 
 Section defs.
-Context `{!heapGS Σ, !ghost_mapG Σ epoch_ty (dig_ty * gname)%type, !ghost_mapG Σ map_label_ty map_val_ty}.
+Context `{!heapGS Σ, !mono_listG (option (dig_ty * gname)) Σ, !ghost_mapG Σ map_label_ty map_val_ty}.
 
 Definition have_merkle (dig : list w8) (m : cli_map_ty) : iProp Σ :=
   ([∗ map] k ↦ x ∈ m,
@@ -46,14 +47,15 @@ Definition have_merkle (dig : list w8) (m : cli_map_ty) : iProp Σ :=
     end).
 
 Definition own (ptr : loc) (obj : t) : iProp Σ :=
-  ∃ (epoch_maps : gmap epoch_ty (dig_ty * gname)) sl_serv_sig_pk,
+  ∃ (seen_maps : list (option (dig_ty * gname))) sl_serv_sig_pk,
   (* TODO: vrf. *)
   (* key maps. *)
-  "Hepoch_maps" ∷ ghost_map_auth obj.(γ) 1 epoch_maps ∗
-  "Hsubmaps" ∷ ([∗ map] x ∈ epoch_maps,
-    ∃ (submap : cli_map_ty),
-    ghost_map_auth x.2 1 submap ∗
-    have_merkle x.1 submap) ∗
+  "Hseen_maps" ∷ mono_list_auth_own obj.(γ) 1 seen_maps ∗
+  "Hsubmaps" ∷ ([∗ list] x ∈ seen_maps,
+    match x with
+    | None => True
+    | Some y => ∃ submap, ghost_map_auth y.2 1 submap ∗ have_merkle y.1 submap
+    end) ∗
   (* uid and next_ver. *)
   "Hptr_uid" ∷ ptr ↦[Client :: "uid"] #obj.(uid) ∗
   "Hptr_nextVer" ∷ ptr ↦[Client :: "nextVer"] #obj.(next_ver) ∗
@@ -65,55 +67,25 @@ End defs.
 End Client.
 
 Section specs.
-Context `{!heapGS Σ, !ghost_mapG Σ epoch_ty (dig_ty * gname)%type, !ghost_mapG Σ map_label_ty map_val_ty}.
+Context `{!heapGS Σ, !mono_listG (option (dig_ty * gname)) Σ, !ghost_mapG Σ map_label_ty map_val_ty}.
 
-(* timeseries. the general abstraction is having a key view at every epoch.
-timeseries is a concrete impl. *)
-Record ts_ty :=
-  mk_ts {
-    entries: list (epoch_ty * comm_ty);
-    bound: epoch_ty;
-  }.
-
-Definition ts_epoch_mono (ts : ts_ty) :=
-  ∀ i j ep_i val_i ep_j val_j,
-  ts.(entries) !! i = Some (ep_i, val_i) →
-  ts.(entries) !! j = Some (ep_j, val_j) →
-  i < j →
-  uint.nat ep_i < uint.nat ep_j.
-
-Definition ts_bound_ok (ts : ts_ty) :=
-  ∀ i ep val,
-  ts.(entries) !! i = Some (ep, val) →
-  uint.nat ep ≤ uint.nat ts.(bound).
-
-Definition is_key_entry cli_γ uid (ver : nat) e : iProp Σ :=
+Definition is_key cli_γ uid ver ep comm : iProp Σ :=
   ∃ dig sm_γ,
-  "#Hsubmap" ∷ e.1 ↪[cli_γ]□ (dig, sm_γ) ∗
-  "#Hlatest" ∷ (uid, (W64 ver)) ↪[sm_γ]□ (Some e) ∗
-  "#Hbound" ∷ (uid, W64 (S ver)) ↪[sm_γ]□ None.
+  "#Hsubmap" ∷ mono_list_idx_own cli_γ (uint.nat ep) (Some (dig, sm_γ)) ∗
+  "#Hlatest" ∷ (uid, ver) ↪[sm_γ]□ (Some (ep, comm)) ∗
+  "#Hbound" ∷ (uid, word.add (W64 1) ver) ↪[sm_γ]□ None.
 
-Definition is_key_bound cli_γ uid (ver : nat) ep : iProp Σ :=
+Definition is_bound cli_γ uid ver (ep : w64) : iProp Σ :=
   ∃ dig sm_γ,
-  "#Hsubmap" ∷ ep ↪[cli_γ]□ (dig, sm_γ) ∗
-  "#Hbound" ∷ (uid, W64 (ver)) ↪[sm_γ]□ None.
+  "#Hsubmap" ∷ mono_list_idx_own cli_γ (uint.nat ep) (Some (dig, sm_γ)) ∗
+  "#Hbound" ∷ (uid, ver) ↪[sm_γ]□ None.
 
-(* two of the invariants remain from the pure version,
-while the rest have RA analogs. *)
-Definition is_my_keys γ uid (ts : ts_ty) : iProp Σ :=
-  "#Hentries" ∷ ([∗ list] ver ↦ x ∈ ts.(entries),
-    is_key_entry γ uid ver x) ∗
-  "#Hbound" ∷ is_key_bound γ uid (length ts.(entries)) ts.(bound) ∗
-  "%Hinv_epoch_mono" ∷ ⌜ ts_epoch_mono ts ⌝ ∗
-  "%Hinv_bound_ok" ∷ ⌜ ts_bound_ok ts ⌝.
-
+(* an opening exists that binds pk to comm. *)
 Definition is_comm (pk : pk_ty) (comm : comm_ty) : iProp Σ. Admitted.
 
-Lemma wp_Client__Put ptr_c c ts sl_pk d0 (pk : list w8) :
+Lemma wp_Client__Put ptr_c c sl_pk d0 (pk : list w8) :
   {{{
     "Hown_cli" ∷ Client.own ptr_c c ∗
-    "#Hmy_keys" ∷ is_my_keys c.(Client.γ) c.(Client.uid) ts ∗
-    "%Hlatest_keys" ∷ ⌜ length ts.(entries) = uint.nat c.(Client.next_ver) ⌝ ∗
     "Hsl_pk" ∷ own_slice_small sl_pk byteT d0 pk
   }}}
   Client__Put #ptr_c (slice_val sl_pk)
@@ -124,10 +96,23 @@ Lemma wp_Client__Put ptr_c c ts sl_pk d0 (pk : list w8) :
       ∃ comm,
       let new_c := set Client.next_ver (word.add (W64 1)) c in
       "Hown_cli" ∷ Client.own ptr_c new_c ∗
-      let new_keys := (mk_ts (ts.(entries) ++ [(ep, comm)]) ep) in
-      "#Hmy_keys" ∷ is_my_keys c.(Client.γ) c.(Client.uid) new_keys ∗
+      "#His_key" ∷ is_key c.(Client.γ) c.(Client.uid) c.(Client.next_ver) ep comm ∗
       "#Hcomm" ∷ is_comm pk comm
     else "Hown_cli" ∷ Client.own ptr_c c
+  }}}.
+Proof. Admitted.
+
+Lemma wp_Client__SelfMon ptr_c c :
+  {{{
+    "Hown_cli" ∷ Client.own ptr_c c
+  }}}
+  Client__SelfMon #ptr_c
+  {{{
+    (ep : w64) (ptr_evid : loc) (err : bool), RET (#ep, #ptr_evid, #err);
+    "Hown_cli" ∷ Client.own ptr_c c ∗
+    if negb err then
+      "#His_bound" ∷ is_bound c.(Client.γ) c.(Client.uid) c.(Client.next_ver) ep
+    else True
   }}}.
 Proof. Admitted.
 
