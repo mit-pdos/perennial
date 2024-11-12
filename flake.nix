799b72d0a554146c5d1c30ec7b59f28ea2caacd0
Matt Schwennesen <mjschwenne@gmail.com>
2024-11-12 11:35:42 -0600

    setup perennial for nix development

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Flake for Applying Grackle to gokv";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      grackle = pkgs.buildGoModule {
+        name = "grackle";
+        src = pkgs.fetchFromGitHub {
+          owner = "mjschwenne";
+          repo = "grackle";
+          rev = "18dbbd313c299d74895f566aae028eeba62a699a";
+          hash = "sha256-GDo3c3VbRl0FR7sJ2Cvmdau+rBRcxwb/sGAEwV5seFQ=";
+        };
+        vendorHash = "sha256-Wk2v0HSAkrzxHJvCfbw6xOn0OQ1xukvYjDxk3c2LmH8=";
+        checkPhase = false;
+      };
+    in
+      pkgs.mkShell {
+        # create an environment with the required coq libraries
+        packages = with pkgs; [
+          # coq deps
+          coqc
+
+          # grackle
+          grackle
+        ];
+
+        shellHook = ''
+        '';
+      };
+  };
+}
