1d6747dee6188f08e427fb6c577d40c5acc3ed8d
alanwang67 <alanwang599@gmail.com>
2024-12-18 19:39:47 -0500

    maxTS files

---
+++ external/Goose/github_com/session/goFiles/maxTS.go
@@ -0,0 +1,20 @@
+package maxTS
+
+func max(x uint64, y uint64) uint64 {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func maxTS(t1 []uint64, t2 []uint64) []uint64 {
+	var i = uint64(0)
+	var length = uint64(len(t1))
+	var output = make([]uint64, len(t1))
+	for i < length {
+		output[i] = max(t1[i], t2[i])
+		i += 1
+	}
+	return output
+}
