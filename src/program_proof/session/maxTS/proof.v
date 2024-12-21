1b0b4392276f5216289ca2c0178f628953932090
Alan Wang <alanwang599@gmail.com>
2024-12-21 09:26:53 -0500

    updated loop invariant

--- src/program_proof/session/maxTS/proof.v
+++ src/program_proof/session/maxTS/proof.v
@@ -52,7 +52,6 @@ Section heap.
       auto.
   Qed.
 
-  Search "own_slice".
   (* own_slice_small_agree *)
   Lemma getMaxTsEquiv (x: Slice.t) (xs: list w64) (y: Slice.t) (ys: list w64) :
     {{{
@@ -91,8 +90,17 @@ Section heap.
                       index ↦[uint64T] #i ∗
                       len ↦[uint64T] #(length xs) ∗
                       slice ↦[slice.T uint64T] s' ∗ (* this is wrong *)
-                      ⌜continue = false -> uint.nat i >= (length xs)⌝ ∗
-                                                          ⌜(length l) = (length xs)⌝ 
+                      (* Something that says
+                         one is greater than the other
+                       *)
+                      ⌜continue = false -> uint.nat i = (length xs)⌝ ∗
+                                                           ⌜(length l) = (length xs)⌝ ∗
+                                                                           ⌜∀ (i': nat),
+                                                                             i' < uint.nat i <= length xs ->
+                                                                           forall (x y z: w64), l !! i' = Some x ->
+                                                                                          xs !! i' = Some y ->
+                                                                                          ys !! i' = Some z ->
+                                                                                          x = (coqMax y z)⌝
                 )%I
                with "[] [H H1 index len s' slice]").
     - iIntros (?). iModIntro. iIntros "H1 H2".
@@ -112,7 +120,7 @@ Section heap.
         * iIntros "Hy". wp_load. wp_apply (wp_SliceGet with "[$Hx]").
           -- iPureIntro. apply H0.
           -- iIntros "Hx".
-             wp_apply (max_equiv). iIntros (r) "H10".
+             wp_apply (max_equiv). iIntros (r) "%H10".
              wp_load. wp_load.
              wp_apply (wp_SliceSet with "[$Hs]").
              ++ iFrame. iPureIntro.
@@ -122,9 +130,35 @@ Section heap.
                 iApply "H2". iExists (w64_word_instance.(word.add) i (W64 1)).
                 iExists (<[uint.nat i:=r]> l)%E. iFrame. iSplit.
                 ** unfold own_slice_small. auto.
-                ** iPureIntro. rewrite <- H9. apply length_insert.
+                ** iSplitR.
+                   { iPureIntro. destruct H9. rewrite <- H1. apply length_insert. }
+                   { iPureIntro. intros.
+                     -  destruct H9. (* there are two cases either i = i' or i = i *)
+                        destruct (decide (i' = uint.nat i)).
+                        + subst. 
+                          rewrite list_lookup_insert_Some in H2.
+                          destruct H2.
+                         * destruct H2. destruct H9. rewrite <- H9.
+                           rewrite H in H6. injection H6 as ?. rewrite H6. 
+                           rewrite H0 in H5. injection H5 as ?. rewrite H5. auto. 
+                         * destruct H2. assert (uint.nat i = uint.nat i) by word. unfold not in H2.
+                           apply H2 in H10. inversion H10.
+                       + destruct H1.
+                         rewrite Z.lt_le_pred in H1.
+                         assert ((Z.pred (uint.nat (w64_word_instance.(word.add) i (W64 1)))) = uint.nat i) by word.
+                         rewrite H11 in H1.
+                         assert (i' < length l)%nat by word.
+                         assert (i' < uint.nat i <= length xs) by word.
+                         assert(uint.nat i ≠ i') by word.
+                         apply (list_lookup_insert_ne l (uint.nat i) (i') r) in H14.
+                         rewrite H14 in H2. 
+                         eapply H8 in H2; auto.
+                         * apply H2.
+                         * eassumption.
+                         * auto.
+                   }
       + iModIntro. iApply "H2". iExists i. iExists l. iFrame. iPureIntro. split.
-        * intros. word.
+        * intros. word. (* look at old proof to see how we did this? *)
         * auto.
     - assert (zero_val uint64T = #(W64 0)). { unfold zero_val. simpl. auto. }
       rewrite H. iExists (W64 0).
@@ -137,16 +171,23 @@ Section heap.
             f_equal. rewrite w64_to_nat_id. auto. } rewrite H0. iFrame.
         * iPureIntro. split.
           -- intros.  inversion H0.
-          -- rewrite length_replicate. auto.
-    - iIntros "H". wp_pures. iNamed "H". iDestruct "H" as "(H1 & H3 & H4 & H5 & H6 & H7 & H8 & %H9)".
-      wp_load. iModIntro. iApply "H2". iFrame. unfold maxTSCoq. iInduction xs as [| hd1 tl1].
-      + simpl in H9. apply nil_length_inv in H9. rewrite H9. iFrame.
-      + iInduction ys as [| hd2 tl2].
-        * rewrite H3 in H9. apply nil_length_inv in H9. rewrite H9. iFrame.
-        * wp_pures. destruct (decide (uint.Z hd1 >? uint.Z hd2 = true)).
-          { rewrite e. simpl. 
-
-    
-    
-    
+          -- rewrite length_replicate. split; auto. intros.
+             word.
+    - iIntros "H". wp_pures. iNamed "H". iDestruct "H" as "(H1 & H3 & H4 & H5 & H6 & H7 & %H8 & %H9 & %H10)".
+      wp_load. iModIntro. iApply "H2". iFrame.
+      assert (false = false) by auto.
+      apply H8 in H.
+      assert (maxTSCoq xs ys = l). {
+        generalize dependent ys. generalize dependent l.
+        induction xs.
+        + intros. inversion H9. apply nil_length_inv in H1. rewrite H1. auto.
+        + induction ys.
+          * intros. inversion H3.
+          * intros. simpl. destruct (decide (uint.Z a >? uint.Z a0 = true)).
+            -- assert (0 < uint.nat i <= length (a :: xs)) by word.
+            -- admit.
+      }
+      rewrite H. iFrame.
+  Qed.
+  
 End heap.
