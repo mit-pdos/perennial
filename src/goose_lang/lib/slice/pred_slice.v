d7f0f9b47887308398ef45aa58f2400d5d67d908
Matt Schwennesen <mjschwenne@gmail.com>
2025-01-13 12:24:20 -0600

    begin updating pred_slice for grackle use

--- src/goose_lang/lib/slice/pred_slice.v
+++ src/goose_lang/lib/slice/pred_slice.v
@@ -33,40 +33,42 @@ Context `{ffi_sem: ffi_semantics} `{!ffi_interp ffi} `{!heapGS Σ}.
 Context {ext_ty: ext_types ext}.
 
 Context {A: Type}.
-Context `{!IntoVal V}.
-Context `{!IntoValForType V t}.
-Context (Ψ: V → A → iProp Σ).
+Context `{!IntoVal val}.
+Context `{!IntoValForType val t}.
+Context (Ψ: val → A → dfrac -> iProp Σ).
 
 Implicit Types (s: Slice.t) (t: ty) (l: list A).
 (* V is the type that represents the values in the slice (through IntoVal),
 while A is the type that represents the larger predicates at each address *)
-Implicit Types (v: V) (x: A).
+Implicit Types (v: val) (x: A).
 
 Definition is_pred_slice s q l: iProp Σ :=
-  ∃ (vs: list V), own_slice_small s t q (to_val <$> vs) ∗
-                  [∗ list] v;x ∈ vs;l, Ψ v x.
+  ∃ (vs: list val), own_slice_small s t q vs ∗
+                  [∗ list] v;x ∈ vs;l, Ψ v x q.
 
 Theorem wp_SliceGet {stk E} s q l (i: u64) (x: A) :
   l !! uint.nat i = Some x →
   {{{ is_pred_slice s q l }}}
     SliceGet t (slice_val s) #i @ stk; E
-  {{{ (v:V), RET (to_val v); Ψ v x ∗ (Ψ v x -∗ is_pred_slice s q l) }}}.
+  {{{ (v:val), RET v; Ψ v x q ∗ (Ψ v x q -∗ is_pred_slice s q l) }}}.
 Proof.
   iIntros (Hlookup Φ) "Hs HΦ".
   iDestruct "Hs" as (vs) "[Hs Hxs]".
   iDestruct (big_sepL2_lookup_2 (uint.nat i) with "Hxs") as (v) "%Hlookup1"; eauto.
   iDestruct (big_sepL2_lookup_acc with "Hxs") as "[Hx Hxs]"; eauto.
   wp_apply (slice.wp_SliceGet with "[$Hs]").
-  { iPureIntro.
-    rewrite list_lookup_fmap.
-    apply fmap_Some_2; eauto. }
+  { iPureIntro. done. }
   iIntros "[Hs %Hty]".
   iApply "HΦ"; iFrame.
   iIntros "HΨ". iFrame. iApply ("Hxs" with "HΨ").
 Qed.
 
+(* FIXME: Remove the to_val in the precondition and call to SliceAppend.
+   I don't know how to do this, since that causes `apply to_val_ty` 12 lines
+   below to not work since the goal is `impl.val_ty v t`, not
+   `impl.val_ty (to_val v) t` *)
 Theorem wp_SliceAppend {stk E} s l v x :
-  {{{ is_pred_slice s (DfracOwn 1) l ∗ own_slice_cap s t ∗ Ψ v x }}}
+  {{{ is_pred_slice s (DfracOwn 1) l ∗ own_slice_cap s t ∗ Ψ (to_val v) x (DfracOwn 1) }}}
     SliceAppend t (slice_val s) (to_val v) @ stk; E
   {{{ s', RET slice_val s'; is_pred_slice s' (DfracOwn 1) (l ++ [x]) ∗ own_slice_cap s' t }}}.
 Proof using IntoValForType0.
@@ -79,8 +81,7 @@ Proof using IntoValForType0.
   iIntros (s') "Hs".
   iApply "HΦ".
   iDestruct "Hs" as "[Hs $]".
-  iExists (vs ++ [v]).
-  rewrite fmap_app //.
+  iExists (vs ++ [to_val v]).
   iFrame "Hs".
   iApply (big_sepL2_app with "Hxs").
   simpl; iFrame.
@@ -88,9 +89,9 @@ Qed.
 
 Theorem wp_forSlice {stk E} (I: u64 → iProp Σ) s q xs (body: val) :
   (∀ (i: u64) v x,
-      {{{ I i ∗ Ψ v x }}}
-        body #i (to_val v) @ stk; E
-      {{{ RET #(); I (word.add i (W64 1)) ∗ Ψ v x }}}) -∗
+      {{{ I i ∗ Ψ v x q }}}
+        body #i v @ stk; E
+      {{{ RET #(); I (word.add i (W64 1)) ∗ Ψ v x q }}}) -∗
     {{{ I (W64 0) ∗ is_pred_slice s q xs }}}
       forSlice t body (slice_val s) @ stk; E
     {{{ RET #(); I s.(Slice.sz) ∗ is_pred_slice s q xs }}}.
@@ -98,14 +99,12 @@ Proof.
   iIntros "#Hwp".
   iIntros "!>" (Φ) "(HI0 & Hs) HΦ".
   iDestruct "Hs" as (vs) "[Hs Hxs]".
-  wp_apply (slice.wp_forSlice (λ i, I i ∗ [∗ list] v;x ∈ vs;xs, Ψ v x)%I with
+  wp_apply (slice.wp_forSlice (λ i, I i ∗ [∗ list] v;x ∈ vs;xs, Ψ v x q)%I with
                 "[] [$HI0 $Hs $Hxs]").
   { clear Φ.
     iIntros (i x) "!>".
     iIntros (Φ) "((Hi&Hxs)&%Hbound&%Hlookup) HΦ".
-    rewrite list_lookup_fmap in Hlookup.
-    apply fmap_Some_1 in Hlookup as [v [Hlookup ->]].
-    iDestruct (big_sepL2_lookup_1 with "Hxs") as (x) "%Hlookup2"; eauto.
+    iDestruct (big_sepL2_lookup_1 with "Hxs") as (y) "%Hlookup2"; eauto.
     iDestruct (big_sepL2_lookup_acc with "Hxs") as "[Hx Hxs]"; eauto.
     wp_apply ("Hwp" with "[$Hi $Hx]").
     iIntros "[HI Hx]".
