2516252708695da83faf8911c9235dae725f9047
Upamanyu Sharma <upamanyu@mit.edu>
2025-02-25 15:36:38 -0500

    Use `ecomp etcdE` even for SpontaneousTransition; cleanup

--- new/proof/go_etcd_io/etcd/client/v3.v
+++ new/proof/go_etcd_io/etcd/client/v3.v
@@ -3,6 +3,27 @@ Require Import New.generatedproof.go_etcd_io.etcd.client.v3.
 Require Import New.proof.proof_prelude.
 From Perennial.Helpers Require Import Transitions.
 
+Inductive ecomp (E : Type → Type) : Type → Type :=
+| Ret {A} (a : A) : ecomp E A
+| Effect {A} (e : E A) : ecomp E A
+| Bind {A B} (k : A → ecomp E B) (e : ecomp E A) : ecomp E B.
+
+Arguments Ret {_ _} (_).
+Arguments Effect {_ _} (_).
+Arguments Bind {_ _ _} (_ _).
+
+Instance ecomp_MBind E : MBind (ecomp E) := (@Bind E).
+Instance ecomp_MRet E : MRet (ecomp E) := (@Ret E).
+
+Fixpoint denote `{MBind M} `{!MRet M} {E R} (handler : ∀ A (e : E A), M A) (e : ecomp E R) : M R :=
+  match e with
+  | Ret a => mret a
+  | Effect e => (handler _ e)
+  | Bind k e =>
+      v ← (denote handler e);
+      denote handler (k v)
+  end.
+
 Existing Instance fallback_genPred.
 Existing Instances r_mbind r_mret r_fmap.
 
@@ -44,6 +65,39 @@ Global Instance settable : Settable _ :=
   settable! mk < revision; compact_revision; key_values; used_lease_ids; lease_expiration>.
 End EtcdState.
 
+(** Effects for etcd specification. *)
+Inductive etcdE : Type → Type :=
+| SuchThat {A} (pred : A → Prop) : etcdE A
+| GetState : etcdE EtcdState.t
+| SetState (σ' : EtcdState.t) : etcdE unit
+| GetTime : etcdE w64
+| Assume (b : Prop) : etcdE unit
+| Assert (b : Prop) : etcdE unit.
+
+(* Establish monadicity of relation.t *)
+Instance relation_mret A : MRet (relation.t A) :=
+  λ {A} a, λ σ σ' a', a = a' ∧ σ' = σ.
+
+Instance relation_mbind A : MBind (relation.t A) :=
+  λ {A B} kmb ma, λ σ σ' b,
+    ∃ a σmiddle,
+      ma σ σmiddle a ∧
+      kmb a σmiddle σ' b.
+
+(* Handle etcd effects as a in the [relation.t EtcdState.t] monad. *)
+Definition handle_etcdE (t : w64) (A : Type) (e : etcdE A) : relation.t EtcdState.t A :=
+  match e with
+  | SuchThat pred => λ σ σ' a, pred a ∧ σ' = σ
+  | GetState => λ σ σ' σret, σ' = σ ∧ σret = σ
+  | SetState σnew => λ σ σ' _, σ' = σnew
+  | GetTime => λ σ σ' tret, tret = t ∧ σ' = σ
+  | Assume P => λ σ σ' tret, P ∧ σ = σ'
+  | _ => λ σ σ' _, False (* XXX: the assert effect should be interpreted when converting to Iris precondition? *)
+  end.
+
+Definition interp {A} (time_of_execution : w64) (e : ecomp etcdE A) : relation.t EtcdState.t A :=
+  denote (handle_etcdE time_of_execution) e.
+
 (** This covers all transitions of the etcd state that are not tied to a client
    API call, e.g. lease expiration happens "in the background". This will be
    called as a prelude by all the client-facing operations, since it is sound to
@@ -51,37 +105,36 @@ End EtcdState.
    client. This relies on [SpontaneousTransition] being monotonic: if a
    transition can happen at time [t'] with [t' > t], then it must be possible at
    [t] as well. The following lemma confirms this. *)
-Definition SingleSpontaneousTransition (time : w64) : transition EtcdState.t () :=
+Definition SingleSpontaneousTransition : ecomp etcdE () :=
   (* expire some lease *)
   (* XXX: this is a "partial" transition: it is not always possible to expire a
      lease. *)
-  lease_id ← suchThat (λ σ l, ∃ exp, σ.(EtcdState.lease_expiration) !! l = Some (Some exp) ∧
+  time ← Effect GetTime;
+  σ ← Effect GetState;
+  lease_id ← Effect $ SuchThat (λ l, ∃ exp, σ.(EtcdState.lease_expiration) !! l = Some (Some exp) ∧
                                      uint.nat time > uint.nat exp);
-  modify (set EtcdState.lease_expiration <[ lease_id := None ]>)
-.
+  Effect $ SetState (set EtcdState.lease_expiration <[ lease_id := None ]> σ).
 
 Lemma SingleSpontaneousTransition_monotonic (time time' : w64) σ σ' :
   uint.nat time < uint.nat time' →
-  relation.denote (SingleSpontaneousTransition time) σ σ' () →
-  relation.denote (SingleSpontaneousTransition time') σ σ' ().
+  interp time SingleSpontaneousTransition σ σ' () →
+  interp time' SingleSpontaneousTransition σ σ' ().
 Proof.
   intros Htime Hstep.
-  unfold SingleSpontaneousTransition in Hstep |- *.
-  simpl in Hstep.
-  monad_inv. destruct H as (exp & ? & ?).
-  simpl. monad_simpl.
-  econstructor.
-  {
-    constructor.
-    eexists _. split; try eassumption. word.
-  }
-  constructor. done.
+  rewrite /SingleSpontaneousTransition in Hstep |- *.
+  rewrite /interp /= /mbind /relation_mbind in Hstep.
+  destruct Hstep as (? & ? & [-> ->] & Hstep).
+  destruct Hstep as (? & ? & [-> ->] & Hstep).
+  destruct Hstep as (? & ? & [Hstep ->] & ->).
+  destruct Hstep as (? & Hlookup & Hgt).
+  repeat econstructor; try done.
+  lia.
 Qed.
 
 (** This does a non-deterministic number of spontaneous transitions. *)
-Definition SpontaneousTransition (time : w64) : transition EtcdState.t unit :=
-  num_steps ← any nat;
-  Nat.iter num_steps (λ p, p;; SingleSpontaneousTransition time) (ret ()).
+Definition SpontaneousTransition : ecomp etcdE unit :=
+  num_steps ← Effect $ SuchThat (λ (_ : nat), True);
+  Nat.iter num_steps (λ p, p;; SingleSpontaneousTransition) (mret ()).
 
 Module LeaseGrantRequest.
 Record t :=
@@ -103,21 +156,21 @@ Global Instance settable : Settable _ :=
   settable! mk <TTL; ID>.
 End LeaseGrantResponse.
 
-(* time is *)
-Definition LeaseGrant (time : w64) (req : LeaseGrantRequest.t) : transition EtcdState.t LeaseGrantResponse.t :=
-  (* SpontaneousTransition time;; *)
+Definition LeaseGrant (req : LeaseGrantRequest.t) : ecomp etcdE LeaseGrantResponse.t :=
+  SpontaneousTransition;;
   (* req.TTL is advisory, so it is ignored. *)
-  ttl ← any w64;
+  ttl ← Effect $ SuchThat (λ (_ : w64), True);
+  σ ← Effect GetState;
   lease_id ← (if decide (req.(LeaseGrantRequest.ID) = (W64 0)) then
-                suchThat (λ σ lease_id, lease_id ∉ σ.(EtcdState.used_lease_ids))
+                Effect $ SuchThat (λ lease_id, lease_id ∉ σ.(EtcdState.used_lease_ids))
               else
-                (assert (λ σ, req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
-                 ret req.(LeaseGrantRequest.ID))
-    );
-  modify (set EtcdState.used_lease_ids (λ old, {[lease_id]} ∪ old)) ;;
-  (* XXX: no overflow assumption? *)
-  modify (set EtcdState.lease_expiration <[lease_id := Some (word.add time ttl)]>) ;;
-  ret (LeaseGrantResponse.mk lease_id ttl).
+                (Effect $ Assume (req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
+                 mret req.(LeaseGrantRequest.ID)));
+  time ← Effect GetTime;
+  let σ := (set EtcdState.used_lease_ids (λ old, {[lease_id]} ∪ old) σ) in
+  let σ := (set EtcdState.lease_expiration <[lease_id := Some (word.add time ttl)]> σ) in
+  Effect (SetState σ);;
+  mret (LeaseGrantResponse.mk lease_id ttl).
 
 Module LeaseKeepAliveRequest.
 Record t :=
@@ -137,137 +190,3 @@ mk {
 Global Instance settable : Settable _ :=
   settable! mk <TTL; ID>.
 End LeaseKeepAliveResponse.
-
-(*
-Definition LeaseKeepAlive (time : w64) (req : LeaseKeepAliveRequest.t) :
-  transition EtcdState.t LeaseKeepAliveResponse.t :=
-  SpontaneousTransition time;;
-.
-*)
-
-Inductive ecomp (E : Type → Type) : Type → Type :=
-| Ret {A} (a : A) : ecomp E A
-| Effect {A} (e : E A) : ecomp E A
-| Bind {A B} (k : A → ecomp E B) (e : ecomp E A) : ecomp E B.
-
-Arguments Ret {_ _} (_).
-Arguments Effect {_ _} (_).
-Arguments Bind {_ _ _} (_ _).
-
-Instance ecomp_MBind E : MBind (ecomp E) := (@Bind E).
-Instance ecomp_MRet E : MRet (ecomp E) := (@Ret E).
-
-Fixpoint denote `{MBind M} `{!MRet M} {E R} (handler : ∀ A (e : E A), M A) (e : ecomp E R) : M R :=
-  match e with
-  | Ret a => mret a
-  | Effect e => (handler _ e)
-  | Bind k e =>
-      v ← (denote handler e);
-      denote handler (k v)
-  end.
-
-Inductive etcdE : Type → Type :=
-| SuchThat {A} (pred : A → Prop) : etcdE A
-| GetState : etcdE EtcdState.t
-| SetState (σ' : EtcdState.t) : etcdE unit
-| TimeGet : etcdE w64
-| Assume (b : Prop) : etcdE unit
-| Assert (b : Prop) : etcdE unit.
-(* XXX: the assert effect should be interpreted when converting to Iris precondition? *)
-
-Definition handle_etcdE (t : w64) (A : Type) (e : etcdE A) : relation.t EtcdState.t A :=
-  match e with
-  | SuchThat pred => λ σ σ' a, pred a ∧ σ' = σ
-  | GetState => λ σ σ' σret, σ' = σ ∧ σret = σ
-  | SetState σnew => λ σ σ' _, σ' = σnew
-  | TimeGet => λ σ σ' tret, tret = t ∧ σ' = σ
-  | Assume P => λ σ σ' tret, P ∧ σ = σ'
-  | _ => λ σ σ' _, False
-  end
-.
-
-Instance relation_mret A : MRet (relation.t A) :=
-  λ {A} a, λ σ σ' a', a = a' ∧ σ' = σ.
-
-Instance relation_mbind A : MBind (relation.t A) :=
-  λ {A B} kmb ma, λ σ σ' b,
-    ∃ a σmiddle,
-      ma σ σmiddle a ∧
-      kmb a σmiddle σ' b.
-
-Definition LeaseGrant2 (req : LeaseGrantRequest.t) : ecomp etcdE LeaseGrantResponse.t :=
-  (* req.TTL is advisory, so it is ignored. *)
-  ttl ← Effect $ SuchThat (λ (_ : w64), True);
-  σ ← Effect GetState;
-  lease_id ← (if decide (req.(LeaseGrantRequest.ID) = (W64 0)) then
-                Effect $ SuchThat (λ lease_id, lease_id ∉ σ.(EtcdState.used_lease_ids))
-              else
-                (Effect $ Assume (req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
-                 mret req.(LeaseGrantRequest.ID)));
-  time ← Effect TimeGet;
-  let σ := (set EtcdState.used_lease_ids (λ old, {[lease_id]} ∪ old) σ) in
-  let σ := (set EtcdState.lease_expiration <[lease_id := Some (word.add time ttl)]> σ) in
-  Effect (SetState σ);;
-  mret (LeaseGrantResponse.mk lease_id ttl).
-
-Lemma test_equivalence req time :
-  ∀ σ σ' resp,
-  relation.denote (LeaseGrant time req) σ σ' resp ↔
-  denote (handle_etcdE time) (LeaseGrant2 req) σ σ' resp.
-Proof.
-  intros.
-  split.
-  {
-    intros Hstep.
-    simpl in *.
-    monad_inv.
-    exists x, σ; split; first by constructor.
-    exists σ, σ; split; first by constructor.
-    destruct decide.
-    {
-      simpl in *.
-      monad_inv.
-      exists lease_id, σ; split; first by constructor.
-      exists time, σ; split; first by constructor.
-      eexists (), _; split; first by constructor.
-      by constructor.
-    }
-    {
-      simpl in *.
-      monad_inv.
-      eexists _, σ; split.
-      { exists (), σ. split; econstructor; done. }
-      exists time, σ; split; first by constructor.
-      eexists (), _; split; first by constructor.
-      by econstructor.
-    }
-  }
-  {
-    intros Hstep.
-    destruct Hstep as [? Hstep].
-    repeat destruct Hstep as [? Hstep].
-    simpl in *.
-    econstructor; first by econstructor.
-    simpl in *. intuition. subst.
-    destruct decide.
-    {
-      simpl in *.
-      intuition. subst.
-      econstructor; try done.
-      econstructor; try done.
-      econstructor; try done.
-    }
-    {
-      simpl in *.
-      rename H1 into Hstep.
-      repeat destruct Hstep as [? Hstep].
-      intuition. subst.
-      econstructor.
-      { econstructor; try done. }
-      Unshelve.
-      2:{ done. }
-      econstructor; try done.
-      econstructor; try done.
-    }
-  }
-Qed.
