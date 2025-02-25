a015f419f8ed7424712463aa6e3cb4293624a599
Upamanyu Sharma <upamanyu@mit.edu>
2025-02-25 16:29:51 -0500

    handler for interpreting etcd effects into iris `Spec`

--- new/proof/go_etcd_io/etcd/client/v3.v
+++ new/proof/go_etcd_io/etcd/client/v3.v
@@ -157,14 +157,15 @@ Global Instance settable : Settable _ :=
 End LeaseGrantResponse.
 
 Definition LeaseGrant (req : LeaseGrantRequest.t) : ecomp etcdE LeaseGrantResponse.t :=
-  SpontaneousTransition;;
+  (* FIXME: add this back *)
+  (* SpontaneousTransition;; *)
   (* req.TTL is advisory, so it is ignored. *)
   ttl ← Effect $ SuchThat (λ (_ : w64), True);
   σ ← Effect GetState;
   lease_id ← (if decide (req.(LeaseGrantRequest.ID) = (W64 0)) then
                 Effect $ SuchThat (λ lease_id, lease_id ∉ σ.(EtcdState.used_lease_ids))
               else
-                (Effect $ Assume (req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
+                (Effect $ Assert (req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
                  mret req.(LeaseGrantRequest.ID)));
   time ← Effect GetTime;
   let σ := (set EtcdState.used_lease_ids (λ old, {[lease_id]} ∪ old) σ) in
@@ -190,3 +191,72 @@ mk {
 Global Instance settable : Settable _ :=
   settable! mk <TTL; ID>.
 End LeaseKeepAliveResponse.
+
+Section spec.
+Context `{hG: heapGS Σ, !ffi_semantics _ _}.
+Definition Spec Resp := (Resp → iProp Σ) → iProp Σ.
+
+Instance Spec_MRet : MRet Spec :=
+  λ {Resp} resp Φ, Φ resp.
+
+Instance Spec_MBind : MBind Spec :=
+  λ {RespA RespB} (kmb : RespA → Spec RespB) (ma : Spec RespA) ΦB,
+    ma (λ respa, kmb respa ΦB).
+
+Context `{!ghost_varG Σ EtcdState.t}.
+
+(* This is only in grove_ffi. *)
+Axiom own_time : w64 → iProp Σ.
+
+(* Handle etcd effects as a in the [relation.t EtcdState.t] monad. *)
+Definition handle_etcdE_spec (γ : gname) (A : Type) (e : etcdE A) : Spec A :=
+  (match e with
+   | GetState => λ Φ, (∃ σ q, ghost_var γ q σ ∗ (ghost_var γ q σ -∗ Φ σ))
+   | SetState σ' => λ Φ, (∃ (_σ : EtcdState.t), ghost_var γ 1%Qp _σ ∗ (ghost_var γ 1%Qp σ' -∗ Φ tt))
+   | GetTime => λ Φ, (∀ time, own_time time -∗ own_time time ∗ Φ time)
+   | Assume P => λ Φ, (⌜ P ⌝ -∗ Φ ())
+   | Assert P => λ Φ, (⌜ P ⌝ ∗ Φ ())
+   | SuchThat pred => λ Φ, ∀ x, ⌜ pred x ⌝ -∗ Φ x
+   end)%I.
+
+Definition GrantSpec req γ := denote (handle_etcdE_spec γ) (LeaseGrant req).
+Lemma test req γ :
+  ⊢ ∀ Φ,
+  (∃ (σ : EtcdState.t), ghost_var γ 1%Qp σ ∗
+        (∀ resp (σ' : EtcdState.t), ghost_var γ 1%Qp σ' -∗ Φ resp)) -∗
+  GrantSpec req γ Φ.
+Proof.
+  iIntros (?) "Hupd".
+  unfold GrantSpec.
+  unfold denote.
+  simpl. unfold mbind, mret.
+  unfold Spec_MBind, Spec_MRet, handle_etcdE_spec.
+  simpl.
+  iIntros "* _".
+  iDestruct "Hupd" as (?) "[Hv Hupd]".
+  repeat iExists _. iFrame.
+  iIntros "Hv".
+  simpl.
+  destruct decide.
+  {
+    iIntros "* %Hnot".
+    iIntros (?) "Htime".
+    iFrame "Htime".
+    iExists _; iFrame.
+    iIntros "Hv".
+    iApply "Hupd".
+    iFrame.
+  }
+  {
+    simpl.
+    iSplitR.
+    { admit. } (* need to prove this assert statement. *)
+    iIntros (?) "Htime".
+    iFrame "Htime".
+    iExists _; iFrame.
+    iIntros "Hv".
+    iApply "Hupd".
+    iFrame.
+  }
+Abort.
+End spec.
