cf7ba1c94adaedad3763102222ecbc2968c835e6
Upamanyu Sharma <upamanyu@mit.edu>
2025-02-24 19:40:58 -0500

    Start handling spontaneous etcd transitions

--- new/proof/go_etcd_io/etcd/client/v3.v
+++ new/proof/go_etcd_io/etcd/client/v3.v
@@ -3,6 +3,9 @@ Require Import New.generatedproof.go_etcd_io.etcd.client.v3.
 Require Import New.proof.proof_prelude.
 From Perennial.Helpers Require Import Transitions.
 
+Existing Instance fallback_genPred.
+Existing Instances r_mbind r_mret r_fmap.
+
 (* https://etcd.io/docs/v3.6/learning/api/ *)
 Module KeyValue.
 Record t :=
@@ -34,11 +37,51 @@ mk {
        incorrectly see its keys still attached with its leaseid.
      *)
     used_lease_ids : gset w64;
+    lease_expiration : gmap w64 (option w64); (* [None] means the lease has been expired. *)
   }.
 
 Global Instance settable : Settable _ :=
-  settable! mk < revision; compact_revision; key_values; used_lease_ids>.
+  settable! mk < revision; compact_revision; key_values; used_lease_ids; lease_expiration>.
 End EtcdState.
+
+(** This covers all transitions of the etcd state that are not tied to a client
+   API call, e.g. lease expiration happens "in the background". This will be
+   called as a prelude by all the client-facing operations, since it is sound to
+   delay running spontaneous transitions until they would actually affect the
+   client. This relies on [SpontaneousTransition] being monotonic: if a
+   transition can happen at time [t'] with [t' > t], then it must be possible at
+   [t] as well. The following lemma confirms this. *)
+Definition SingleSpontaneousTransition (time : w64) : transition EtcdState.t () :=
+  (* expire some lease *)
+  (* XXX: this is a "partial" transition: it is not always possible to expire a
+     lease. *)
+  lease_id ← suchThat (λ σ l, ∃ exp, σ.(EtcdState.lease_expiration) !! l = Some (Some exp) ∧
+                                     uint.nat time > uint.nat exp);
+  modify (set EtcdState.lease_expiration <[ lease_id := None ]>)
+.
+
+Lemma SingleSpontaneousTransition_monotonic (time time' : w64) σ σ' :
+  uint.nat time < uint.nat time' →
+  relation.denote (SingleSpontaneousTransition time) σ σ' () →
+  relation.denote (SingleSpontaneousTransition time') σ σ' ().
+Proof.
+  intros Htime Hstep.
+  unfold SingleSpontaneousTransition in Hstep |- *.
+  simpl in Hstep.
+  monad_inv. destruct H as (exp & ? & ?).
+  simpl. monad_simpl.
+  econstructor.
+  {
+    constructor.
+    eexists _. split; try eassumption. word.
+  }
+  constructor. done.
+Qed.
+
+(** This does a non-deterministic number of spontaneous transitions. *)
+Definition SpontaneousTransition (time : w64) : transition EtcdState.t unit :=
+  num_steps ← any nat;
+  Nat.iter num_steps (λ p, p;; SingleSpontaneousTransition time) (ret ()).
 
 Module LeaseGrantRequest.
 Record t :=
@@ -46,7 +89,7 @@ mk {
     TTL : w64;
     ID : w64;
   }.
-Global Instance settable_EtcdState : Settable _ :=
+Global Instance settable : Settable _ :=
   settable! mk < TTL; ID>.
 End LeaseGrantRequest.
 
@@ -60,17 +103,44 @@ Global Instance settable : Settable _ :=
   settable! mk <TTL; ID>.
 End LeaseGrantResponse.
 
-Existing Instance fallback_genPred.
-Existing Instances r_mbind r_mret r_fmap.
-
-Definition LeaseGrant (req : LeaseGrantRequest.t) : transition EtcdState.t LeaseGrantResponse.t :=
+(* time is *)
+Definition LeaseGrant (time : w64) (req : LeaseGrantRequest.t) : transition EtcdState.t LeaseGrantResponse.t :=
+  SpontaneousTransition time;;
   (* req.TTL is advisory, so it is ignored. *)
-  ttl ← suchThat (λ _ _, True);
-  lease_id ← (if (word.eqb req.(LeaseGrantRequest.ID) (W64 0)) then
+  ttl ← any w64;
+  lease_id ← (if decide (req.(LeaseGrantRequest.ID) = (W64 0)) then
                 suchThat (λ σ lease_id, lease_id ∉ σ.(EtcdState.used_lease_ids))
               else
                 (assert (λ σ, req.(LeaseGrantRequest.ID) ∉ σ.(EtcdState.used_lease_ids));;
                  ret req.(LeaseGrantRequest.ID))
     );
   modify (set EtcdState.used_lease_ids (λ old, {[lease_id]} ∪ old)) ;;
+  (* XXX: no overflow assumption? *)
+  modify (set EtcdState.lease_expiration <[lease_id := Some (word.add time ttl)]>) ;;
   ret (LeaseGrantResponse.mk lease_id ttl).
+
+Module LeaseKeepAliveRequest.
+Record t :=
+mk {
+    ID : w64;
+  }.
+Global Instance settable : Settable _ :=
+  settable! mk < ID>.
+End LeaseKeepAliveRequest.
+
+Module LeaseKeepAliveResponse.
+Record t :=
+mk {
+    TTL : w64;
+    ID : w64;
+  }.
+Global Instance settable : Settable _ :=
+  settable! mk <TTL; ID>.
+End LeaseKeepAliveResponse.
+
+(*
+Definition LeaseKeepAlive (time : w64) (req : LeaseKeepAliveRequest.t) :
+  transition EtcdState.t LeaseKeepAliveResponse.t :=
+  SpontaneousTransition time;;
+.
+*)
