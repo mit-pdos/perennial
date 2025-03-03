0b37e484d15938afae6e2ac36b89764fab980ac0
Levi Redlin <lredlin@wisc.edu>
2025-03-03 11:55:09 -0600

    Fix comments that I forgot to fix in previous commit

--- src/goose_lang/base_prelude.v
+++ src/goose_lang/base_prelude.v
@@ -1,6 +1,5 @@
-(* Gooselang dependencies included in every translated Goose file. These are 
-the minimal dependencies needed for models implemented in Goose compatible 
-Go i.e. the channel model. *)
+(* These are the minimal dependencies needed for models implemented in 
+Goose compatible Go i.e. the channel model. *)
 From Perennial.goose_lang Require Export lang notation typing.
 From Perennial.goose_lang.lib Require Export
      typed_mem.impl struct.impl loop.impl
--- src/goose_lang/prelude.v
+++ src/goose_lang/prelude.v
@@ -1,7 +1,8 @@
+(* Gooselang dependencies included in every translated Goose file. *)
 From Perennial.goose_lang Require Export base_prelude.
 (* Gooselang dependencies that are generated from features modeled as Goose 
-   compatible Go code, such as the channel model, which is implemented as a Go generic 
-   struct. *)
+   compatible Go code, such as the channel model, which is implemented as 
+   a Go generic struct. *)
 From Goose.github_com.goose_lang.goose
 Require Export channel
 .
