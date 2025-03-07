de426ee57adcf85c7f31b49b5558b2e41b76ae55
Upamanyu Sharma <upamanyu@mit.edu>
2025-03-07 01:10:20 -0500

    etcd client specs still cause universe inconsistency wrt something in ShouldBuild

--- src/ShouldBuild.v
+++ src/ShouldBuild.v
@@ -111,8 +111,9 @@ From Perennial.program_proof.cachekv Require proof.
 From New.code.go_etcd_io.raft Require v3.
 From New.code.github_com.goose_lang.goose.testdata.examples Require unittest semantics.
 From New.proof Require asyncfile etcdraft globals_test.
-From New.proof Require go_etcd_io.etcd.client.v3.concurrency.
 
+(* FIXME: keeping this out because of universe inconsistency. *)
+(* From New.proof Require go_etcd_io.etcd.client.v3.concurrency. *)
 
 (* FIXME: add back in. *)
 (* From Perennial.program_proof.pav Require should_build. *)
