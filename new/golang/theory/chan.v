a0142d221489de29c52867f06644b021cdc23b78
Upamanyu Sharma <upamanyu@mit.edu>
2025-03-12 23:31:52 -0400

    Respect `chan` gauge invariance in chan.make and onetime_barrier

--- new/golang/theory/chan.v
+++ new/golang/theory/chan.v
@@ -42,7 +42,7 @@ Implicit Types (s : chanstate.t V).
 Lemma wp_chan_make cap :
   {{{ True }}}
     chan.make t #cap
-  {{{ (c : chan.t), RET #c; own_chan c (chanstate.mk cap false ([] : list V) O) }}}.
+  {{{ (c : chan.t) (init : list V), RET #c; own_chan c (chanstate.mk cap false init (length init)) }}}.
 Proof.
 Admitted.
 
@@ -96,26 +96,67 @@ Admitted.
 
 Section onetime_barrier.
   Context `{!ghost_varG Σ ()}.
-  Definition is_onetime_barrier γsend γrecv (ch : chan.t) Sd Rv : iProp Σ :=
+
+  Record params :=
+    {
+      gauge_init : nat; (* XXX: putting "gauge" in the name because this would
+                         change under a `chan` gauge transformation. *)
+      send_gn : gname;
+      recv_gn : gname;
+    }.
+  Implicit Types γ : params.
+
+  Definition own_send_tok γ := ghost_var γ.(send_gn) 1 ().
+  Definition own_recv_tok γ := ghost_var γ.(recv_gn) 1 ().
+
+  Definition is_onetime_barrier γ (ch : chan.t) Sd Rv : iProp Σ :=
     is_chan ch ∗
     inv nroot (
-        ∃ s, own_chan ch s ∗
-             ⌜ s.(chanstate.cap) = W64 0 ⌝ ∗
-             ⌜ s.(chanstate.closed) = false ⌝ ∗
-             match s.(chanstate.received) with
-             | O => True
-             | S _ => (Rv ∨ ghost_var γsend 1 ())
-             end ∗
-             match s.(chanstate.sent) with
-             | [] => True
-             | _::_ => (Sd ∨ ghost_var γrecv 1 ())
-             end
+        ∃ s,
+          "Hch" ∷ own_chan ch s ∗
+          "%Hcap" ∷ ⌜ s.(chanstate.cap) = W64 0 ⌝ ∗
+          "%Hopen" ∷ ⌜ s.(chanstate.closed) = false ⌝ ∗
+          "%Hoffset_r" ∷ ⌜ γ.(gauge_init) ≤ s.(chanstate.received) ⌝%nat ∗
+          "%Hoffset_s" ∷ ⌜ γ.(gauge_init) ≤ length s.(chanstate.sent) ⌝%nat ∗
+          "Hr" ∷ match (s.(chanstate.received) - γ.(gauge_init))%nat with
+            | O => True
+            | S _ => (Rv ∨ own_send_tok γ)
+            end ∗
+          "Hs" ∷ match (length s.(chanstate.sent) - γ.(gauge_init))%nat with
+            | O => True
+            | S _ => (Sd ∨ own_recv_tok γ)
+            end
       ).
 
-  Lemma wp_onetime_barrier_receive γsend γrecv ch Sd Rv :
+  Lemma start_onetime_barrier Sd Rv ch s:
+    s.(chanstate.cap) = (W64 0) →
+    s.(chanstate.closed) = false →
+    s.(chanstate.received) = length s.(chanstate.sent) →
+    is_chan ch -∗
+    own_chan ch s ={⊤}=∗
+    ∃ γ, is_onetime_barrier γ ch Sd Rv ∗
+         own_recv_tok γ ∗
+         own_send_tok γ.
+  Proof.
+    intros.
+    iIntros "#? Hchan".
+    iMod (ghost_var_alloc ()) as "[% ?]".
+    iMod (ghost_var_alloc ()) as "[% ?]".
+    iExists _.
+    instantiate (1:=(ltac:(econstructor))). simpl.
+    iFrame "#". iFrame. simpl.
+    iApply inv_alloc.
+    iNext. iFrame "∗%".
+    instantiate (1:=s.(chanstate.received)).
+    iSplitR; first word.
+    iSplitR; first word.
+    rewrite H1 Nat.sub_diag //.
+  Qed.
+
+  Lemma wp_onetime_barrier_receive γ ch Sd Rv :
     {{{
-        is_onetime_barrier γsend γrecv ch Sd Rv ∗
-        ghost_var γrecv 1 () ∗
+        is_onetime_barrier γ ch Sd Rv ∗
+        own_recv_tok γ ∗
         Rv
     }}}
       chan.receive #ch
@@ -129,40 +170,41 @@ Section onetime_barrier.
     (* send Rv *)
     iInv "Hinv" as "Hi" "Hclose".
     iApply fupd_mask_intro; [set_solver | iIntros "Hmask"].
-    iNext.
-    iDestruct "Hi" as (?) "(? & % & %Hopen & Hs & Hr)".
+    iNext. iNamed "Hi".
     iExists _; iFrame.
     rewrite Hopen.
     iIntros "Hch".
     iMod "Hmask" as "_".
     iMod ("Hclose" with "[-Htok HΦ]").
-    { iNext. iFrame. destruct (chanstate.received s); iFrame "∗%". }
+    { iNext. iFrame "∗%".
+      simpl. iSplitR; first word.
+      rewrite Nat.sub_succ_l //. iFrame.
+    }
     iModIntro.
 
     (* get Sd *)
     iInv "Hinv" as "Hi" "Hclose".
     iApply fupd_mask_intro; [set_solver | iIntros "Hmask"].
-    iNext.
-    iDestruct "Hi" as (?) "(? & % & % & Hs & Hr)".
+    iNext. iNamed "Hi".
     iExists _; iFrame.
     iIntros "* % Hch".
 
-    destruct chanstate.sent.
-    { simpl in *. done. }
-    iDestruct "Hr" as "[Hr|Hbad]".
+    edestruct (length _ - _)%nat eqn:?.
+    { simpl in *. exfalso. apply lookup_lt_Some in H. lia. }
+    iDestruct "Hs" as "[Hs|Hbad]".
     2:{ iCombine "Hbad Htok" gives %[Hbad _]. done. }
     iMod "Hmask" as "_".
-    iMod ("Hclose" with "[-HΦ Hr]").
-    { iNext. iFrame "∗%". destruct (chanstate.sent s0); iFrame "∗%". }
+    iMod ("Hclose" with "[-HΦ Hs]").
+    { iNext. iFrame "∗%". destruct (_ - _)%nat; iFrame. }
     iModIntro.
     iApply "HΦ".
     iFrame.
   Qed.
 
-  Lemma wp_onetime_barrier_send γsend γrecv ch Sd Rv v :
+  Lemma wp_onetime_barrier_send γ ch Sd Rv v :
     {{{
-        is_onetime_barrier γsend γrecv ch Sd Rv ∗
-        ghost_var γsend 1 () ∗
+        is_onetime_barrier γ ch Sd Rv ∗
+        own_send_tok γ ∗
         Sd
     }}}
       chan.send #ch #v
@@ -176,30 +218,31 @@ Section onetime_barrier.
     (* send Sd *)
     iInv "Hinv" as "Hi" "Hclose".
     iApply fupd_mask_intro; [set_solver | iIntros "Hmask"].
-    iNext.
-    iDestruct "Hi" as (?) "(? & % & % & Hs & Hr)".
+    iNext. iNamed "Hi".
     iExists _; iFrame "∗%".
     iIntros "Hch".
     iMod "Hmask" as "_".
     iMod ("Hclose" with "[-Htok HΦ]").
-    { iNext. iFrame "∗%". destruct (chanstate.sent s); iFrame "∗%". }
+    { iNext. iFrame "∗%". iSplitR.
+      { rewrite length_app /=. iPureIntro. lia. }
+      iClear "Hs". by destruct Nat.sub; iFrame.
+    }
     iModIntro.
 
     (* get Rv *)
     iInv "Hinv" as "Hi" "Hclose".
     iApply fupd_mask_intro; [set_solver | iIntros "Hmask"].
-    iNext.
-    iDestruct "Hi" as (?) "(? & % & % & Hs & Hr)".
+    iNext. iNamed "Hi".
     iExists _; iFrame.
     iIntros "* % Hch".
 
-    destruct chanstate.received.
-    { simpl in *. word. }
-    iDestruct "Hs" as "[Hs|Hbad]".
+    edestruct (chanstate.received s0 - _)%nat eqn:?.
+    { simpl in *. exfalso. word. }
+    iDestruct "Hr" as "[Hr|Hbad]".
     2:{ iCombine "Hbad Htok" gives %[Hbad _]. done. }
     iMod "Hmask" as "_".
-    iMod ("Hclose" with "[-HΦ Hs]").
-    { iNext. iFrame "∗%". destruct (chanstate.received s0); iFrame "∗%". }
+    iMod ("Hclose" with "[-HΦ Hr]").
+    { iNext. iFrame "∗%". by destruct Nat.sub; iFrame. }
     iModIntro.
     iApply "HΦ".
     iFrame.
