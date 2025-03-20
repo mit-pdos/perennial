18f1fff2bfe92e87aeeb46297fba95e818b8f42e
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-03-20 10:43:55 -0400

    qed is_merk_proof_inj. matching on proof simplifies structure. generalize depth for inductive proof

--- src/program_proof/pav/merkle.v
+++ src/program_proof/pav/merkle.v
@@ -1,4 +1,5 @@
 From Perennial.program_proof Require Import grove_prelude.
+From Perennial Require Import base.
 From Goose.github_com.mit_pdos.pav Require Import merkle.
 
 From Perennial.Helpers Require Import bytes.
@@ -14,62 +15,89 @@ Context `{!heapGS Σ}.
 
 (* get_bit returns false if bit n of l is 0. *)
 Definition get_bit (l : list w8) (n : nat) : option bool :=
-  concat (byte_to_bits <$> l) !! n.
+  mjoin (byte_to_bits <$> l) !! n.
 
-Fixpoint is_merk_proof_recur (depth_inv : nat) (label : list w8)
+(* TODO: rm once seal merged in. *)
+Program Definition u64_le_seal := sealed @u64_le.
+Definition u64_le_unseal : u64_le_seal = _ := seal_eq _.
+Lemma u64_le_seal_len x :
+  length $ u64_le_seal x = 8%nat.
+Proof. Admitted.
+
+Fixpoint is_merk_proof_recur (depth : nat) (label : list w8)
     (val : option $ list w8) (proof : list $ list w8)
     (hash : list w8) : iProp Σ :=
-  match depth_inv with
-  | 0%nat =>
+  match proof with
+  | [] =>
     match val with
     (* empty node. *)
     | None => is_hash [empty_node_tag] hash
     (* leaf node. *)
-    | Some val' => is_hash (leaf_node_tag :: label ++ (u64_le $ length val') ++ val') hash
+    | Some val' => is_hash (leaf_node_tag :: label ++ (u64_le_seal $ length val') ++ val') hash
     end
-  | S depth_inv' =>
-    let depth := (length proof - depth_inv)%nat in
+  | sib_hash :: proof' =>
     ∃ next_hash,
     (* next recurrence. *)
-    is_merk_proof_recur depth_inv' label val proof next_hash ∗
-    match proof !! depth with None => False | Some sib_hash =>
+    is_merk_proof_recur (S depth) label val proof' next_hash ∗
+    ⌜ length sib_hash = hash_len ⌝ ∗
     match get_bit label depth with None => False | Some next_pos =>
     let children :=
       match next_pos with
-      | false => (next_hash, sib_hash)
-      | true => (sib_hash, next_hash)
+      | false => next_hash ++ sib_hash
+      | true => sib_hash ++ next_hash
       end in
     (* inner node. *)
-    is_hash (inner_node_tag :: children.1 ++ children.2) hash
-    end
+    is_hash (inner_node_tag :: children) hash
     end
   end.
 
 Definition is_merk_proof (label : list w8) (val : option $ list w8)
     (proof : list $ list w8) (dig : list w8) : iProp Σ :=
-  is_merk_proof_recur (length proof) label val proof dig.
+  is_merk_proof_recur 0 label val proof dig.
+
+Lemma is_merk_proof_recur_len depth label val proof hash :
+  is_merk_proof_recur depth label val proof hash -∗
+  ⌜ length hash = hash_len ⌝.
+Proof.
+  iIntros "H". destruct proof; simpl.
+  - case_match; by iApply is_hash_len.
+  - iDestruct "H" as (?) "(?&?&?)".
+    repeat case_match; try done; by iApply is_hash_len.
+Qed.
+
+Lemma is_merk_proof_recur_inj depth label val0 val1 proof0 proof1 hash :
+  is_merk_proof_recur depth label val0 proof0 hash -∗
+  is_merk_proof_recur depth label val1 proof1 hash -∗
+  ⌜ val0 = val1 ⌝.
+Proof.
+  iIntros "H0 H1".
+  iInduction (proof0) as [|sib_hash0] "IH" forall (depth proof1 hash).
+  { destruct proof1; simpl.
+    - repeat case_match; try done;
+        iDestruct (is_hash_inj with "H0 H1") as %Heq; try naive_solver.
+      iPureIntro. simplify_eq/=.
+      apply app_inj_1 in Heq; [naive_solver|].
+      by rewrite !u64_le_seal_len.
+    - iDestruct "H1" as (?) "(_ & _ & H1)".
+      repeat case_match; try done;
+        iDestruct (is_hash_inj with "H0 H1") as %Heq; naive_solver. }
+  destruct proof1 as [|sib_hash1]; simpl.
+  - iDestruct "H0" as (?) "(_ & % & H0)".
+    repeat case_match; try done;
+      iDestruct (is_hash_inj with "H0 H1") as %?; naive_solver.
+  - iDestruct "H0" as (?) "(HR0 & % & H0)".
+    iDestruct "H1" as (?) "(HR1 & % & H1)".
+    iDestruct (is_merk_proof_recur_len with "HR0") as %?.
+    iDestruct (is_merk_proof_recur_len with "HR1") as %?.
+    repeat case_match; try done;
+      iDestruct (is_hash_inj with "H0 H1") as %?;
+      list_simplifier; iApply ("IH" with "HR0 HR1").
+Qed.
 
 Lemma is_merk_proof_inj label val0 val1 proof0 proof1 dig :
   is_merk_proof label val0 proof0 dig -∗
   is_merk_proof label val1 proof1 dig -∗
   ⌜ val0 = val1 ⌝.
-Proof.
-  (* TODO: figure out how to structure this recursive argument.
-  iInduction (length proof0) as [|d] "IH" forall ((length proof1) dig).
-  *)
-  rewrite /is_merk_proof /is_merk_proof_recur. iIntros "H0 H1".
-  destruct (length proof0), (length proof1); rewrite -/is_merk_proof_recur.
-  (* case: both last nodes. *)
-  - repeat case_match; iDestruct (is_hash_inj with "H0 H1") as %?; naive_solver.
-  (* case: one last and one inner. *)
-  - iDestruct "H1" as (?) "[_ H1]".
-    repeat case_match; try done;
-      iDestruct (is_hash_inj with "H0 H1") as %?; naive_solver.
-  - iDestruct "H0" as (?) "[_ H0]".
-    repeat case_match; try done;
-      iDestruct (is_hash_inj with "H0 H1") as %?; naive_solver.
-  (* case: both inner. *)
-  - admit.
-Admitted.
+Proof. iIntros "H0 H1". iApply (is_merk_proof_recur_inj with "H0 H1"). Qed.
 
 End proof.
