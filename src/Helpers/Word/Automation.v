2840d08fe97b7f0d4ea8efc938a84c7e808218bb
Upamanyu Sharma <upamanyu@mit.edu>
2025-03-25 17:43:34 -0400

    Start more principled and predictable [word] tactic

--- src/Helpers/Word/Automation.v
+++ src/Helpers/Word/Automation.v
@@ -3,6 +3,152 @@ From iris.proofmode Require Import proofmode.
 From coqutil.Word Require Import Interface Properties.
 From Perennial.Helpers.Word Require Import Integers Properties.
 
+Import Ltac2.
+Set Default Proof Mode "Classic".
+
+(*
+  The high-level plan: normalize the entire proof context to be in terms of .
+  The steps this tactic follows
+  - Unfold the types (w64) to expose the underlying [word.rep ...] type. All
+    automation hereafter only needs to concern itself with any word
+    satisfying the word laws spelled out in [word.ok]
+  - Rewrite everything to be in terms of [word.unsigned]; even [word.signed]
+    gets replaced with a sign-wrapped [word.unsigned]. All equalities of
+    [word.rep] are replaced with [Z] equalities.
+  - Eliminate all word ops
+  - zify
+  - compute away Z constants.
+  - lia.
+ *)
+
+Module word.
+
+Local Lemma decision_assume_opposite `{!Decision P} : (¬ P → False) → P.
+Proof. by destruct (decide P). Qed.
+
+Import Printf.
+Ltac2 verbose_logger m := printf "word: %a" (fun () () => m) ().
+Ltac2 handle_goal logger :=
+  first [
+      apply decision_assume_opposite | (* if decidable, assume the opposite; *)
+      logger (fprintf "eliminating goal and trying to prove false instead"); Std.cut 'False
+    ].
+
+Ltac2 unfold_w_whatever () :=
+  unfold W64, W32, (* W16, *) W8, w64, w32, (* w16, *) w8 in *.
+
+Local Lemma word_eq_iff_Z_eq (x y: w64) :
+  x = y ↔ word.unsigned x = word.unsigned y.
+Proof. split; first by intros ->. apply inj. apply _. Qed.
+
+Ltac2 normalize_to_word_unsigned () :=
+  (* eliminate sint.Z *)
+  rewrite -> ?word.signed_eq_swrap_unsigned in *;
+  (* eliminate any (eq (word.)*)
+  rewrite -> ?word_eq_iff_Z_eq in *
+.
+
+Search word.of_Z.
+(* uint.Z (W64 (a + b)). *)
+
+(** Precondition: in all "relevant" hypotheses, all terms of type [word.rep _]
+    are either fed into [uint.Z] or fed into a [word] operation.
+
+   Postcondition: in all "relevant" hypotheses: all terms of type [word.rep _]
+   are directly wrapped in a [uint.Z], with some exceptions explained next.
+
+   Exceptions: if there are [word.rep]s showing up underneath a word-opaque
+   term. E.g. if [x y : w64] and [f : w64 → w64] is some function that's not a
+   standard word op, then [uint.Z [word.add (f x) y]] will turn into
+   [wrap (Z.add uint.Z (f x) (uint.Z y)]], which has the [word.rep] x that is
+   not getting passed into [uint.Z]. In this case, [f x] is treated as opaque
+   and unrelated to [x]. Any relationships should be proven and stated upfront.
+ *)
+Import Interface.word.
+
+Print Ltac2 Signatures.
+
+Ltac2 unsupported_ops () : constr list :=
+  ['mulhss ; 'mulhsu ; 'mulhuu ; 'eqb ; 'ltu ; 'lts ; 'srs; 'divs; 'mods ] .
+
+(* These equalities have no sideconditions, and should always be used to
+   rewrite. *)
+Ltac2 sidegoal_free_word_laws () : constr list :=
+  [ 'unsigned_of_Z; 'of_Z_unsigned; 'unsigned_add; 'unsigned_sub;
+    'unsigned_opp; 'unsigned_or; 'unsigned_and; 'unsigned_xor;
+    'unsigned_not; 'unsigned_ndn; 'unsigned_mul
+    (* 'signed_mulhss; 'signed_mulhsu; 'unsigned_mulhuu; 'unsigned_eqb; *)
+    (* 'unsigned_ltu; 'signed_lts *)
+  ].
+
+Local Lemma unsigned_slu' {width} {word : Interface.word width} {word_ok : word.ok word}
+  (x y : word) :
+  Z.lt (unsigned y) width -> unsigned (slu x y) = wrap ((unsigned x) * (2^(unsigned y))).
+Proof.
+  intros. rewrite unsigned_slu //. pose proof (word.unsigned_range y) as [??].
+  rewrite Z.shiftl_mul_pow2 //.
+Qed.
+
+Local Lemma unsigned_sru' {width} {word : Interface.word width} {word_ok : word.ok word}
+  (x y : word) :
+  Z.lt (unsigned y) width -> unsigned (sru x y) = ((unsigned x) / (2^(unsigned y))).
+Proof.
+  pose proof (word.unsigned_range y) as [??].
+  pose proof (word.unsigned_range x) as [??].
+  intros. rewrite unsigned_sru //. rewrite Z.shiftr_div_pow2 //. rewrite wrap_small //.
+  Z.div_mod_to_equations. nia.
+Qed.
+
+(* These equalities have 1 sidegoal, and the string is a description of what
+   that sidegoal must prove. The string is used in error messages. *)
+Ltac2 word_laws_with_sidecondition () : (constr * string) list := [
+    ('unsigned_slu', "slu: the shift amount must be less than width");
+    ('unsigned_sru', "sru: the shift amount must be less than width");
+    (* signed_srs *)
+    ('word.unsigned_divu_nowrap, "divu: the divisor must be non-zero");
+    (* signed_divs *)
+    ('word.unsigned_modu_nowrap, "modu: the modulus must be non-zero")
+      (* signed_mods *)
+  ].
+
+Ltac2 mutable solve_word () := ().
+
+Ltac2 Type exn ::= [Word_side_goal_failed (string)].
+
+Ltac2 solve_side_goal (parent_hyp : ident) (err_msg : string) :=
+  Std.clear [parent_hyp];
+  first [
+      solve_word () |
+      let m := (fprintf "side goal failed: %s from expression %t" err_msg (Control.hyp parent_hyp)) in
+      Control.zero (Word_side_goal_failed (Message.to_string m))
+    ].
+
+(* TODO: have to rewrite the sidegoals one at a time so we can eliminate only
+   the originating hypothesis. *)
+Ltac2 eliminate_word_ops () :=
+  let tacs :=
+    List.map (fun law () =>
+                let rw := { Std.rew_orient := Some Std.LTR;
+                            Std.rew_repeat := Std.RepeatStar;
+                            Std.rew_equatn := (fun () => (law, Std.NoBindings))  }
+                in
+                rewrite0 false [rw] None None
+      ) (sidegoal_free_word_laws ()) in
+  repeat0 (fun () => (first0 tacs))
+.
+
+Ltac2 all_but_lia () :=
+  handle_goal verbose_logger;
+  unfold_w_whatever ();
+  eliminate_word_ops ()
+  (* TODO: unfold word.wrap *)
+  (* TODO: simplify Z constants *)
+.
+
+Ltac2 Set solve_word as old := fun () => all_but_lia (); ltac1:(lia).
+
+End word.
+
 Lemma unsigned_U64 z : uint.Z (W64 z) = word.wrap (word:=w64_word_instance) z.
 Proof.
   unfold W64; rewrite word.unsigned_of_Z; auto.
@@ -98,12 +244,7 @@ Ltac word_cleanup_core :=
       (* TODO: this isn't the right strategy if the numbers in the goal are used
       signed. [word] can try both via backtracking, but this can't be part of
       "cleanup".  *)
-      | |- @eq u64 _ _ => apply word.unsigned_inj
-      | |- @eq u32 _ _ => apply word.unsigned_inj
-      | |- @eq u8 _ _ => apply word.unsigned_inj
-      | |- not (@eq u64 _ _) => apply (f_not_equal uint.Z)
-      | |- not (@eq u32 _ _) => apply (f_not_equal uint.Z)
-      | |- not (@eq u8 _ _) => apply (f_not_equal uint.Z)
+      | |- context [@eq word.rep _ _] => rewrite word.unsigned_inj_iff
       end;
   simpl_word_constants;
   (* can't replace some of these with [autorewrite], probably because typeclass inference
