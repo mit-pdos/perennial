279533bcebb307e174cdad3e83ed77a29fcaafd3
Tej Chajed <chajed@wisc.edu>
2025-04-05 18:47:38 -0500

    Add a DFractional typeclass (#219)
    This is just a start. I didn't add instances for every use of Fractional, just the most basic ones. This does automatically give `UpdateIntoPersistently` for these definitions, which is nice and one of the original motivations here.
    
    We could also try to fully replace Fractional with DFractional, but this would require a little more experimentation and integration with the IPM - deriving Fractional from DFractional automatically seems tricky due to higher-order unification issues.

--- src/algebra/na_heap.v
+++ src/algebra/na_heap.v
@@ -5,6 +5,7 @@ From iris.algebra Require Export dfrac.
 From iris.algebra Require Import csum excl auth cmra_big_op numbers lib.gmap_view.
 From iris.bi Require Import fractional.
 From Perennial.base_logic Require Export lib.own.
+From Perennial.iris_lib Require Export dfractional.
 From iris.proofmode Require Export tactics.
 From Perennial.algebra Require Export blocks.
 Set Default Proof Using "Type".
@@ -241,19 +242,38 @@ Section na_heap.
     AsFractional (na_heap_pointsto_st WSt l (DfracOwn q) v) (λ q, na_heap_pointsto_st WSt l (DfracOwn q) v)%I q.
   Proof. split; first done. apply _. Qed.
 
-  Global Instance na_heap_pointsto_fractional l v: Fractional (λ q, l ↦{#q} v)%I.
+  Lemma na_heap_pointsto_persist l dq v:
+    l ↦{dq} v ==∗ l ↦□ v.
   Proof.
-    intros p q.
-    rewrite na_heap_pointsto_eq -own_op -gmap_view_frag_add -pair_op.
-    rewrite agree_idemp.
-    rewrite // Cinr_op.
+    rewrite na_heap_pointsto_eq.
+    iApply own_update.
+    apply gmap_view_frag_persist.
   Qed.
-  Global Instance na_heap_pointsto_as_fractional l q v:
-    AsFractional (l ↦{#q} v) (λ q, l ↦{#q} v)%I q.
-  Proof. split; first done. apply _. Qed.
 
   Global Instance na_heap_pointsto_persistent l v : Persistent (l ↦□ v).
   Proof. rewrite na_heap_pointsto_eq. apply _. Qed.
+
+  Global Instance na_heap_pointsto_dfractional l v : DFractional (λ dq, na_heap_pointsto l dq v).
+  Proof.
+    constructor; intros.
+    - rewrite na_heap_pointsto_eq -own_op -gmap_view_frag_op -pair_op.
+      rewrite agree_idemp.
+      rewrite // Cinr_op.
+    - apply _.
+    - iIntros "H". iMod (na_heap_pointsto_persist with "H") as "$". done.
+  Qed.
+
+  Global Instance na_heap_pointsto_as_dfractional l dq v :
+    AsDFractional (na_heap_pointsto l dq v) (λ dq, na_heap_pointsto l dq v) dq.
+  Proof. auto. Qed.
+
+  Global Instance na_heap_pointsto_fractional l v: Fractional (λ q, l ↦{#q} v)%I.
+  Proof.
+    apply (fractional_of_dfractional (λ dq, na_heap_pointsto l dq v)).
+  Qed.
+  Global Instance na_heap_pointsto_as_fractional l q v:
+    AsFractional (l ↦{#q} v) (λ q, l ↦{#q} v)%I q.
+  Proof. auto. Qed.
 
   Global Instance na_heap_pointsto_combine_sep_gives l dq1 dq2 v1 v2 :
     CombineSepGives (l ↦{dq1} v1)%I (l ↦{dq2} v2)%I ⌜ ✓(dq1 ⋅ dq2) ∧ v1 = v2 ⌝%I.
@@ -262,14 +282,6 @@ Section na_heap.
          iDestruct (own_valid_2 with "H1 H2") as %Hvalid.
          iModIntro. iPureIntro. apply gmap_view_frag_op_valid in Hvalid as [? [? ?]].
          split; first done. simpl in *. pose proof (to_agree_op_inv_L _ _ H3). done.
-  Qed.
-
-  Lemma na_heap_pointsto_persist l dq v:
-    l ↦{dq} v ==∗ l ↦□ v.
-  Proof.
-    rewrite na_heap_pointsto_eq.
-    iApply own_update.
-    apply gmap_view_frag_persist.
   Qed.
 
   Lemma na_heap_pointsto_st_agree l st1 st2 q1 q2 v1 v2 :
