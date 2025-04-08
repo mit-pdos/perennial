7eaef92b9800cbb2fb5b843f852e280b4c5efd7e
Tej Chajed <chajed@wisc.edu>
2025-04-08 17:31:58 -0500

    Add shorthands for ArrayT and StructT

--- new/golang/defn/dynamic_typing.v
+++ new/golang/defn/dynamic_typing.v
@@ -23,6 +23,8 @@ The list type used here is the same as the golang/defn/list library, [list a =
 unit + (a * list a)].
 
 *)
+Notation ArrayT n T := (InjRV (InjLV (LitV n, T))) (only parsing).
+Notation StructT decls := (InjRV (InjRV (InjLV decls))) (only parsing).
 Fixpoint type_to_val (t: go_type): val :=
   match t with
   | boolT => InjLV (# (W64 0))
@@ -31,17 +33,19 @@ Fixpoint type_to_val (t: go_type): val :=
   | uint32T => InjLV (# (W64 3))
   | uint64T => InjLV (# (W64 4))
   | stringT => InjLV (# (W64 5))
-  | arrayT n elem => InjRV (InjLV (LitV n, type_to_val elem))
+  | arrayT n elem => ArrayT n (type_to_val elem)
   | sliceT => InjLV (# (W64 6))
   | interfaceT => InjLV (# (W64 7))
   | structT decls =>
+      (* decls' is a list *)
       let decls' := (fix go (decls: list (go_string * go_type)) :=
                        match decls with
                        | [] => InjLV #()
                        | (d, t) :: decls => InjRV ((#d, type_to_val t), go decls)
                        end
                     ) decls in
-      InjRV (InjRV (InjLV decls'))
+      (* wrap this in the right constructor *)
+      StructT decls'
   | ptrT => InjLV (# (W64 8))
   | funcT => InjLV (# (W64 9))
   end.
