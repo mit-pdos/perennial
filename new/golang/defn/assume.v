ddbe7a8e2fb230d3592ee258db2099f1b4aeaa75
Tej Chajed <chajed@wisc.edu>
2025-04-09 15:41:22 -0500

    Implement a check for w64 multiplication overflow

--- new/golang/defn/assume.v
+++ new/golang/defn/assume.v
@@ -20,4 +20,12 @@ Definition sum_assume_no_overflow : val :=
   λ: "a" "b", assume_sum_no_overflow "a" "b";;
               "a" + "b".
 
+Definition mul_overflows : val :=
+  λ: "a" "b", if: ("a" = #(W64 0)) || ("b" = #(W64 0)) then #false
+              else "a" > #(W64 (2^64-1)) `quot` "b".
+
+(** Assume "a" * "b" doesn't overflow (as unsigned 64-bit integers) *)
+Definition assume_mul_no_overflow : val :=
+  λ: "a" "b", assume (~ mul_overflows "a" "b").
+
 End defn.
