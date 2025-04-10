02bfc91eab7e071424938e2c34c5e94f805314d9
Upamanyu Sharma <upamanyu@mit.edu>
2025-04-10 16:44:45 -0400

    Clean up imports in glob.v; checkpoint proof progress

--- src/program_proof/pav/marshalutil.v
+++ src/program_proof/pav/marshalutil.v
@@ -41,4 +41,46 @@ Proof.
     wp_pures. by iApply "HΦ".
 Qed.
 
+(* relational rather than funtional *)
+Definition read_int b0 v b err : Prop :=
+  if decide (8 ≤ length b0) then
+    v = le_to_u64 (take 8 b0) ∧
+    b = drop 8 b0 ∧
+    err = false
+  else
+    v = 0 ∧ b = [] ∧ err = true.
+
+Lemma wp_ReadInt b0_sl dq (b0 : list w8) :
+  {{{ own_slice_small b0_sl byteT dq b0 }}}
+    ReadInt (slice_val b0_sl)
+  {{{ sl v b err, RET (#v, slice_val sl, #err);
+      ⌜ read_int b0 v b err ⌝ ∗
+      own_slice_small sl byteT dq b }}}.
+Proof.
+  iIntros (?) "Hpre HΦ". wp_rec.
+  wp_apply wp_ref_to; [val_ty|]. iIntros (?) "Hb_ptr".
+  wp_pures. wp_load. wp_apply wp_slice_len.
+  iDestruct (own_slice_small_sz with "[$]") as %Hsz.
+  wp_pures. wp_if_destruct.
+  - wp_pures. replace (slice.nil) with (slice_val Slice.nil) by done. iApply "HΦ".
+    iDestruct (own_slice_small_nil) as "$".
+    { done. }
+    iPureIntro. unfold read_int. rewrite decide_False //. word.
+  - wp_load.
+    pose proof (take_drop 8 b0) as Hb.
+    rewrite -[take _ _]le_to_u64_le in Hb.
+    2:{ rewrite length_take. word. } rewrite -Hb.
+    wp_apply (wp_ReadInt with "[$]").
+    iIntros (?) "Hsl". wp_pures. iApply "HΦ". iFrame.
+    iPureIntro. unfold read_int.
+    rewrite -> le_to_u64_le by len.
+    rewrite take_app take_take take_drop length_take.
+    rewrite decide_True //.
+    { split_and!; try done. f_equal.
+      rewrite -> Nat.min_l by word. rewrite -> Nat.min_l by word.
+      simpl. rewrite take_0 app_nil_r //.
+    }
+    word.
+Qed.
+
 End wps.
