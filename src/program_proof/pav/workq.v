07af107865743418a96d78c29e22f9bbe5b7baf3
Upamanyu Sharma <upamanyu@mit.edu>
2025-04-14 22:52:47 -0400

    Prove wp_NewWork

--- src/program_proof/pav/workq.v
+++ src/program_proof/pav/workq.v
@@ -28,7 +28,7 @@ Context `{!heapGS Σ}.
 
 Definition is_Work w Ψ : iProp Σ :=
   ∃ (mu cond : loc),
-  "#mu" ∷ w ↦[Work::"mu"] #mu ∗
+  "#mu" ∷ w ↦[Work::"mu"]□ #mu ∗
   "#Hmu" ∷ is_lock nroot #mu (
       ∃ (done : bool),
         "done" ∷ w ↦[Work::"done"] #done ∗
@@ -39,22 +39,22 @@ Definition is_Work w Ψ : iProp Σ :=
           else
             True
     ) ∗
-  "#cond" ∷ w ↦[Work::"cond"] #cond ∗
+  "#cond" ∷ w ↦[Work::"cond"]□ #cond ∗
   "#Hcond" ∷ is_cond cond #mu.
 
 (* authority to read [Req] and write [Resp] *)
-Definition own_Work (w : loc) (spec : loc → (loc → iProp Σ) → iProp Σ) : iProp Σ :=
-  ∃ Ψ (Req : loc),
-  "#Req" ∷ w ↦[Work::"Req"] #Req ∗
+Definition own_Work (w req : loc) (spec : loc → (loc → iProp Σ) → iProp Σ) : iProp Σ :=
+  ∃ Ψ,
   "Resp" ∷ w ↦[Work::"Resp"] #null ∗
-  "Hspec" ∷ (spec Req Ψ) ∗
+  "#Req" ∷ w ↦[Work::"Req"]□ #req ∗
+  "Hspec" ∷ (spec req Ψ) ∗
   "#His" ∷ is_Work w Ψ.
 
 Definition own_WorkQ wq spec : iProp Σ :=
   ∃ work_sl (work_ptr : list loc),
     "work" ∷ wq ↦[WorkQ::"work"] (slice_val work_sl) ∗
     "Hwork_sl" ∷ own_slice work_sl ptrT (DfracOwn 1) work_ptr ∗
-    "Hwork" ∷ ([∗ list] w ∈ work_ptr, own_Work w spec)
+    "Hwork" ∷ ([∗ list] w ∈ work_ptr, ∃ req, own_Work w req spec)
 .
 
 Definition is_WorkQ wq spec : iProp Σ :=
@@ -63,5 +63,24 @@ Definition is_WorkQ wq spec : iProp Σ :=
   "#Hmu" ∷ is_lock nroot #mu (own_WorkQ wq spec) ∗
   "#condCli" ∷ wq ↦[WorkQ::"cond"] #cond ∗
   "#HcondCli" ∷ is_cond cond #mu.
+
+Lemma wp_NewWork spec (req : loc) Ψ :
+  {{{ spec req Ψ }}}
+    NewWork #req
+  {{{ w, RET #w; own_Work w req spec ∗ is_Work w Ψ }}}.
+Proof.
+  iIntros (?) "Hpre HΦ". wp_rec. wp_apply wp_new_free_lock.
+  iIntros (mu) "Hmu". wp_pures. wp_apply wp_allocStruct; [val_ty|].
+  iIntros (w) "Hw". wp_pures. iDestruct (struct_fields_split with "Hw") as "H".
+  iNamed "H". wp_loadField. wp_apply (wp_newCond' with "[$]").
+  iIntros (?) "[Hmu #Hcond]". wp_storeField.
+  iApply "HΦ".
+  iMod (struct_field_pointsto_persist with "Req") as "#Req".
+  iMod (struct_field_pointsto_persist with "mu") as "#mu".
+  iMod (struct_field_pointsto_persist with "cond") as "#cond".
+  iMod (alloc_lock with "Hmu [done]") as "#Hmu".
+  2:{ iModIntro. iFrame "∗#". }
+  iNext. iFrame.
+Qed.
 
 End proof.
