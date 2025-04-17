8c7c1195170b688752515262830fe06f66229ba4
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-04-17 04:18:12 -0400

    security / correctness specs for Serv.{SelfMon,Get} rpc stubs

--- src/program_proof/pav/rpc.v
+++ src/program_proof/pav/rpc.v
@@ -28,24 +28,6 @@ Lemma wp_NewRpcAuditor ptr_adtr :
   }}}.
 Proof. Admitted.
 
-Lemma wp_CallServPut ptr_cli addr (uid : w64) sl_pk d0 (pk : list w8) :
-  {{{
-    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr false ∗
-    "Hsl_pk" ∷ own_slice_small sl_pk byteT d0 pk
-  }}}
-  CallServPut #ptr_cli #uid (slice_val sl_pk)
-  {{{
-    (err : bool) ptr_dig dig ptr_memb memb ptr_nonmemb nonmemb,
-    RET (#ptr_dig, #ptr_memb, #ptr_nonmemb, #err);
-    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr false ∗
-    "Hsl_pk" ∷ own_slice_small sl_pk byteT d0 pk ∗
-    "Herr" ∷ (if err then True else
-      "Hown_dig" ∷ SigDig.own ptr_dig dig (DfracOwn 1) ∗
-      "Hown_memb" ∷ Memb.own ptr_memb memb (DfracOwn 1) ∗
-      "Hown_nonmemb" ∷ NonMemb.own ptr_nonmemb nonmemb (DfracOwn 1))
-  }}}.
-Proof. Admitted.
-
 (* TODO: this is mostly a copy of the Server__Put spec.
 later, need to properly bind these two. *)
 Lemma wp_CallServPut ptr_cli addr is_good serv uid nVers sl_pk d0 (pk : list w8) cli_next_ep :
@@ -91,17 +73,81 @@ Lemma wp_CallServPut ptr_cli addr is_good serv uid nVers sl_pk d0 (pk : list w8)
   }}}.
 Proof. Admitted.
 
-Lemma wp_CallServSelfMon ptr_cli addr (uid : w64) :
+Lemma wp_CallServSelfMon ptr_cli addr is_good (uid : w64) serv nVers cli_next_ep :
   {{{
-    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr false
+    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr is_good ∗
+    "Hvers" ∷ (if negb is_good then True else uid ↪[serv.(Server.γvers)] nVers) ∗
+    "#Hlb_ep" ∷ (if negb is_good then True else mono_nat_lb_own serv.(Server.γepoch) cli_next_ep)
   }}}
   CallServSelfMon #ptr_cli #uid
   {{{
-    (err : bool) ptr_dig dig ptr_nonmemb nonmemb, RET (#ptr_dig, #ptr_nonmemb, #err);
-    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr false ∗
+    (err : bool) ptr_sigdig sigdig ptr_bound bound,
+    RET (#ptr_sigdig, #ptr_bound, #err);
+    let new_next_ep := word.add sigdig.(SigDig.Epoch) (W64 1) in
+    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr is_good ∗
+    "%Hgenie" ∷ ⌜ is_good = true → err = false ⌝ ∗
+
     "Herr" ∷ (if err then True else
-      "#Hown_dig" ∷ SigDig.own ptr_dig dig (DfracOwn 1) ∗
-      "Hown_nonmemb" ∷ NonMemb.own ptr_nonmemb nonmemb (DfracOwn 1))
+      "#Hsigdig" ∷ SigDig.own ptr_sigdig sigdig DfracDiscarded ∗
+      "Hbound" ∷ NonMemb.own ptr_bound bound (DfracOwn 1)) ∗
+
+    "Hgood" ∷ (if negb is_good then True else
+      "Hvers" ∷ uid ↪[serv.(Server.γvers)] nVers ∗
+
+      "#Hlb_ep" ∷ mono_nat_lb_own serv.(Server.γepoch) (uint.nat new_next_ep) ∗
+      "%Hlt_ep" ∷ ⌜ Z.of_nat cli_next_ep ≤ uint.Z new_next_ep ⌝ ∗
+      "%Hnoof_ep" ∷ ⌜ uint.Z new_next_ep = (uint.Z sigdig.(SigDig.Epoch) + 1)%Z ⌝ ∗
+
+      "#Hsig" ∷ is_sig serv.(Server.sig_pk)
+        (PreSigDig.encodesF $ PreSigDig.mk sigdig.(SigDig.Epoch) sigdig.(SigDig.Dig))
+        sigdig.(SigDig.Sig) ∗
+      "#Hwish_bound" ∷ wish_checkNonMemb serv.(Server.vrf_pk) uid nVers
+        sigdig.(SigDig.Dig) bound)
+  }}}.
+Proof. Admitted.
+
+Lemma wp_CallServGet ptr_cli addr is_good (uid : w64) serv cli_next_ep :
+  {{{
+    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr is_good ∗
+    "#Hlb_ep" ∷ (if negb is_good then True else mono_nat_lb_own serv.(Server.γepoch) cli_next_ep)
+  }}}
+  CallServGet #ptr_cli #uid
+  {{{
+    (err : bool) ptr_sigdig sigdig sl_hist ptr2_hist hist
+    (is_reg : bool) ptr_lat lat ptr_bound bound (nVers : w64),
+    RET (#ptr_sigdig, slice_val sl_hist, #is_reg, #ptr_lat, #ptr_bound, #err);
+    let new_next_ep := word.add sigdig.(SigDig.Epoch) (W64 1) in
+    "Hown_cli" ∷ advrpc.own_Client ptr_cli addr is_good ∗
+    "%Hgenie" ∷ ⌜ is_good = true → err = false ⌝ ∗
+
+    "Herr" ∷ (if err then True else
+      "#Hsigdig" ∷ SigDig.own ptr_sigdig sigdig DfracDiscarded ∗
+      "Hhist" ∷ ([∗ list] l;x ∈ ptr2_hist;hist,
+        MembHide.own l x (DfracOwn 1)) ∗
+      "Hsl_hist" ∷ own_slice_small sl_hist ptrT (DfracOwn 1) ptr2_hist ∗
+      "Hlat" ∷ Memb.own ptr_lat lat (DfracOwn 1) ∗
+      "Hbound" ∷ NonMemb.own ptr_bound bound (DfracOwn 1)) ∗
+
+    "Hgood" ∷ (if negb is_good then True else
+      "#Hlb_ep" ∷ mono_nat_lb_own serv.(Server.γepoch) (uint.nat new_next_ep) ∗
+      "%Hlt_ep" ∷ ⌜ Z.of_nat cli_next_ep ≤ uint.Z new_next_ep ⌝ ∗
+      "%Hnoof_ep" ∷ ⌜ uint.Z new_next_ep = (uint.Z sigdig.(SigDig.Epoch) + 1)%Z ⌝ ∗
+
+      "#Hsig" ∷ is_sig serv.(Server.sig_pk)
+        (PreSigDig.encodesF $ PreSigDig.mk sigdig.(SigDig.Epoch) sigdig.(SigDig.Dig))
+        sigdig.(SigDig.Sig) ∗
+      "%Hlen_hist" ∷ ⌜ length hist = pred (uint.nat nVers) ⌝ ∗
+      "#Hwish_hist" ∷ ([∗ list] ver ↦ x ∈ hist,
+        ∃ label,
+        wish_checkMembHide serv.(Server.vrf_pk) uid (W64 ver)
+          sigdig.(SigDig.Dig) x label) ∗
+      "%Heq_is_reg" ∷ ⌜ is_reg = negb $ bool_decide (nVers = (W64 0)) ⌝ ∗
+      "#Hwish_lat" ∷ (if negb is_reg then True else
+        ∃ label commit,
+        wish_checkMemb serv.(Server.vrf_pk) uid (word.sub nVers (W64 1))
+          sigdig.(SigDig.Dig) lat label commit) ∗
+      "#Hwish_bound" ∷ wish_checkNonMemb serv.(Server.vrf_pk) uid nVers
+        sigdig.(SigDig.Dig) bound)
   }}}.
 Proof. Admitted.
 
