785dea1ba55ea3458a781cf96bac9dffa4c64205
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-05-03 15:56:04 -0700

    fix IntoVal instances for Disk

--- new/manualproof/github_com/goose_lang/primitive/disk.v
+++ new/manualproof/github_com/goose_lang/primitive/disk.v
@@ -9,16 +9,8 @@ End Disk.
 
 Section instances.
 
-Global Instance : IntoVal Disk.t :=
-  {|
-    to_val_def := λ v, #v
-  |}.
-
-Global Program Instance into_val_typed_Disk : IntoValTyped Disk.t disk.Disk :=
-{| default_val := null |}.
-Next Obligation. rewrite to_val_unseal /=; solve_has_go_type. Qed.
-Next Obligation. rewrite zero_val_unseal /=. rewrite to_val_unseal /=. rewrite to_val_unseal /=. done. Qed.
-Next Obligation. rewrite /Inj to_val_unseal /=. rewrite to_val_unseal /=. intros. inversion H. done. Qed.
+Global Instance : IntoVal Disk.t := _.
+Global Program Instance into_val_typed_Disk : IntoValTyped Disk.t disk.Disk := _.
 
 End instances.
 
