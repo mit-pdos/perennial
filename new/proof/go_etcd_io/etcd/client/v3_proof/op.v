b0532f55d79130a8acbf3dbdeb4d41a6fa67c581
Upamanyu Sharma <upamanyu@mit.edu>
2025-07-01 16:44:56 -0400

    Reduce slow `wp_auto` down to a ~70 second `congruence` for constructing
    a `SetterWf` for constructing a `PointsToAccess` for applying `wp_load`
    for a a struct field.

--- new/proof/go_etcd_io/etcd/client/v3_proof/op.v
+++ new/proof/go_etcd_io/etcd/client/v3_proof/op.v
@@ -50,6 +50,37 @@ Qed.
 
 Axiom admit_ax : false.
 
+Local Ltac setter etaT proj :=
+  lazymatch etaT with
+  | context[proj] => idtac
+  | _ => fail 1 proj "is not a field"
+  end;
+  let set :=
+      (match eval pattern proj in etaT with
+       | ?setter _ => constr:(fun f => setter (fun r => f (proj r)))
+       end) in
+  exact set.
+Local Ltac get_setter T proj :=
+  match constr:(mkT T _) with
+  | mkT _ ?updateable =>
+    let updateable := (eval hnf in updateable) in
+    match updateable with
+    | {| mkT := ?mk |} =>
+      setter mk proj
+    end
+  end.
+Local Ltac SetterWfInstance_t :=
+  match goal with
+  | |- @SetterWf ?T _ ?A =>
+    unshelve notypeclasses refine (Build_SetterWf _ _ _);
+    [ get_setter T A |
+      let r := fresh in
+      intros ? r; destruct r; reflexivity |
+      let f := fresh in
+      let r := fresh in
+      intros f r; destruct r; cbv [RecordSet.set]; cbn; congruence ]
+  end.
+
 Lemma wp_OpPut key v :
   {{{ is_pkg_init clientv3 }}}
     clientv3@"OpPut" #key #v #slice.nil
@@ -60,20 +91,82 @@ Lemma wp_OpPut key v :
       )
   }}}.
 Proof.
-  wp_start. wp_auto.
-  wp_apply wp_StringToBytes.
-  iIntros "%key_sl key_sl".
+  wp_start.
+  wp_alloc_auto. progress wp_pures.
+  wp_alloc_auto. progress wp_pures.
+  wp_alloc_auto. progress wp_pures.
+  wp_alloc_auto. progress wp_pures.
+  wp_load. wp_apply wp_StringToBytes.
+  iIntros "%key_sl key_sl". progress wp_pures.
+  wp_load. wp_apply wp_StringToBytes.
+  iIntros "%val_sl val_sl". progress wp_pures.
+  rewrite -!default_val_eq_zero_val. progress wp_pures.
+  wp_store. progress wp_pures. wp_load. progress wp_pures.
+  wp_apply wp_Op__applyOpts. progress wp_pures.
+  wp_bind.
+
+  Time eapply (tac_wp_load_ty []);
+    [ eapply points_to_access_struct_field_ref; tc_solve|
+      iAssumptionCore |
+      ltac2:(ectx_simpl ())]; simpl.
+  Unshelve.
+  2:{
+    unshelve notypeclasses refine (Build_SetterWf _ _ _).
+    { get_setter clientv3.Op.t clientv3.Op.end'.  }
+    { let r := fresh in
+      intros ? r; destruct r; reflexivity. }
+    {
+      let f := fresh in
+      let r := fresh in
+      intros f r; destruct r.
+      simpl. intros ?.
+      cbv [RecordSet.set].
+      cbn. Time congruence.
+      (* Takes 72.858s *)
+Admitted.
+(*
+    }
+
+
+    SetterWfInstance_t.
+    SetterWf
+    tc_solve.
+    (* FIXME: this is what's *really* slow *)
+  }
+  Undo 1.
+  Set Typeclasses Debug.
+  Time eapply (tac_wp_load_ty []);
+    [ ltac2:(tc_solve_many ()) |
+      iAssumptionCore |
+      ltac2:(ectx_simpl ())]; simpl.
+  Unset Typeclasses Debug.
   wp_auto.
-  wp_apply wp_StringToBytes.
-  iIntros "%val_sl val_sl".
-  time wp_auto. wp_apply wp_Op__applyOpts. time wp_auto. (* 73 seconds *)
   rewrite !bool_decide_decide.
   rewrite !(decide_True (P:=slice.nil = slice.nil)) //=.
-  time wp_auto. (* 70 seconds *)
+  wp_pures.
+  Ltac fast_wp_load :=
+    wp_bind;
+    eapply (tac_wp_load_ty []);
+    [ eapply points_to_access_struct_field_ref; tc_solve|
+      iAssumptionCore |
+      ltac2:(ectx_simpl ())]; simpl.
+
+  wp_bind.
+  eapply (tac_wp_load_ty []).
+
+    { unshelve eapply points_to_access_struct_field_ref; try tc_solve.
+      Search SetterWf.
+    }
+    { iAssumptionCore. }
+    ltac2:(ectx_simpl ()); simpl.
+  Unshelve.
+
+  fast_wp_load. wp_pures).
+  wp_load. wp_pures.
   iApply "HΦ".
   destruct admit_ax.
 Time Qed.
-(* 6.51 *)
+(* 6.51 *) *)
 
 (* FIXME: very slow proof, though [Qed] isn't too bad *)
 
