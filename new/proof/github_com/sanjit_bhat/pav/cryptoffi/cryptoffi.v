983b85bd659fee89852b938f4057a17f7dfd4f20
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-07-10 14:17:49 -0400

    WPs for hash cryptoffi

---
+++ new/proof/github_com/sanjit_bhat/pav/cryptoffi/cryptoffi.v
@@ -0,0 +1,81 @@
+From New.proof.github_com.sanjit_bhat.pav Require Import prelude.
+From New.generatedproof.github_com.sanjit_bhat.pav Require Import cryptoffi.
+
+Notation hash_len := 32 (only parsing).
+
+Section proof.
+Context `{hG: heapGS Σ, !ffi_semantics _ _, !goGlobalsGS Σ}.
+Context `{!cryptoffi.GlobalAddrs}.
+
+#[global]
+Program Instance is_pkg_init_cryptoffi : IsPkgInit cryptoffi := ltac2:(build_pkg_init ()).
+#[global] Opaque is_pkg_init_cryptoffi.
+
+(* Hashes. *)
+
+(* is_hash says that [data] will hash to [hash].
+relative to the crypto model, it says the inputs are in the set of hashes. *)
+Definition is_hash (data hash : list w8) : iProp Σ.
+Proof. Admitted.
+
+#[global]
+Instance is_hash_persistent data hash : Persistent (is_hash data hash).
+Proof. Admitted.
+
+#[global]
+Instance is_hash_timeless data hash : Timeless (is_hash data hash).
+Proof. Admitted.
+
+Lemma is_hash_det data hash0 hash1 :
+  is_hash data hash0 -∗ is_hash data hash1 -∗ ⌜ hash0 = hash1 ⌝.
+Proof. Admitted.
+
+Lemma is_hash_inj data0 data1 hash :
+  is_hash data0 hash -∗ is_hash data1 hash -∗ ⌜ data0 = data1⌝.
+Proof. Admitted.
+
+Lemma is_hash_len data hash :
+  is_hash data hash -∗ ⌜ Z.of_nat (length hash) = hash_len ⌝.
+Proof. Admitted.
+
+Definition own_Hasher (ptr : loc) (data : list w8) : iProp Σ. Admitted.
+
+Lemma wp_NewHasher :
+  {{{ is_pkg_init cryptoffi }}}
+  cryptoffi @ "NewHasher" #()
+  {{{
+    ptr_hr, RET #ptr_hr;
+    "Hown_hr" ∷ own_Hasher ptr_hr []
+  }}}.
+Proof. Admitted.
+
+Lemma wp_Hasher__Write hr data sl_b d0 b :
+  {{{
+    is_pkg_init cryptoffi ∗
+    "Hown_hr" ∷ own_Hasher hr data ∗
+    "Hsl_b" ∷ sl_b ↦*{d0} b
+  }}}
+  hr @ cryptoffi @ "Hasher" @ "Write" #sl_b
+  {{{
+    RET #();
+    "Hown_hr" ∷ own_Hasher hr (data ++ b) ∗
+    "Hsl_b" ∷ sl_b ↦*{d0} b
+  }}}.
+Proof. Admitted.
+
+Lemma wp_Hasher__Sum sl_b_in hr data b_in :
+  {{{
+    is_pkg_init cryptoffi ∗
+    "Hown_hr" ∷ own_Hasher hr data ∗
+    "Hsl_b_in" ∷ sl_b_in ↦* b_in
+  }}}
+  hr @ cryptoffi @ "Hasher" @ "Sum" #sl_b_in
+  {{{
+    sl_b_out hash, RET #sl_b_out;
+    "Hown_hr" ∷ own_Hasher hr data ∗
+    "Hsl_b_out" ∷ sl_b_out ↦* (b_in ++ hash) ∗
+    "#His_hash" ∷ is_hash data hash
+  }}}.
+Proof. Admitted.
+
+End proof.
---
+++ new/proof/github_com/sanjit_bhat/pav/prelude.v
@@ -0,0 +1,4 @@
+From iris.base_logic.lib Require Export mono_nat ghost_map.
+From Perennial.base_logic Require Export mono_list.
+From RecordUpdate Require Export RecordSet.
+From Perennial.Helpers Require Export NamedProps.
