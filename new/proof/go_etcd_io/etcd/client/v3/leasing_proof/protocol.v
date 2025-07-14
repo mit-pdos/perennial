7eaad12f548f70037be04c0a88d1066462962aa6
Upamanyu Sharma <upamanyu@mit.edu>
2025-07-14 18:05:00 -0400

    Checkpoint etcd model; have a better understanding of what'll be needed now

--- new/proof/go_etcd_io/etcd/client/v3/leasing_proof/protocol.v
+++ new/proof/go_etcd_io/etcd/client/v3/leasing_proof/protocol.v
@@ -13,46 +13,58 @@ Section protocol.
 
 Context {W : Type} {W_eq_dec : EqDecision W} {W_countable : Countable W}.
 
-Inductive leasedEntryInner :=
-| EntryValid (response : RangeResponse.t)
-| EntryUpdating (waitc : W).
-
-Record leasedEntry :=
-{
-  leased_rev : Z;
-  leased_entry : leasedEntryInner;
-}.
-
-Record cache_state :=
+Record Response :=
   {
-    entries : gmap go_string leasedEntry;
+    resp_value : go_string;
+    resp_mod_rev : w64;
+  }.
+
+(* FIXME: this is the wrong abstraction. Even when the entry is being updated,
+   it might be concurrently read as long as the `waitc` the reader saw at the
+   beginning closes. *)
+Inductive LeasedEntryInner :=
+| entry_valid (resp : option Response)
+| entry_updating (waitc : W).
+
+Record LeasedEntry :=
+  {
+    leased_rev : Z;
+    leased_entry : LeasedEntryInner;
+  }.
+
+Record ServerNames :=
+  {
+
+  }.
+
+Record CacheNames :=
+  {
+
+  }.
+
+Record CacheState :=
+  {
+    entries : gmap go_string LeasedEntry;
     waitc_map : gmap W bool;
   }.
 
-Global Instance settable_cache_state : Settable cache_state :=
-  settable! Build_cache_state < entries; waitc_map >.
-
-Record server_state :=
-  {
-    kvs : gmap go_string go_string;
-    lease_revs : gmap go_string Z; (* revision number *)
-  }.
-
-Global Instance settable_server_state : Settable server_state :=
-  settable! Build_server_state < kvs ; lease_revs >.
+Global Instance settable_CacheState : Settable CacheState :=
+  settable! Build_CacheState < entries; waitc_map >.
 
 Context `{!invGS Σ}.
-Definition own_server_inv (σ : server_state) : iProp Σ.
+
+Definition own_server_inv (γs : ServerNames) (s : EtcdState.t) : iProp Σ.
 Admitted.
 
-Definition own_cache_inv (σ : cache_state) : iProp Σ.
+Definition own_cache_inv (γc : CacheNames) (c : CacheState) : iProp Σ.
 Admitted.
 
 Definition own_Locked (w : W) (rev : Z) : iProp Σ.
 Admitted.
 
-Context (c c' : cache_state).
-Context (s s' : server_state).
+Context (c c' : CacheState).
+Context (s s' : EtcdState.t).
+Context (γc : CacheNames) (γs : ServerNames).
 
 Definition step_Lock (key : go_string) (w : W) (rev : Z) : Prop :=
   ∃ le,
@@ -60,25 +72,29 @@ Definition step_Lock (key : go_string) (w : W) (rev : Z) : Prop :=
   c.(waitc_map) !! w = None ∧
   rev = le.(leased_rev) ∧
   c' = c <| waitc_map := <[ w := false ]> c.(waitc_map) |>
-         <| entries :=  <[key := {| leased_rev := rev; leased_entry := EntryUpdating w |} ]> c.(entries) |>.
+         <| entries :=  <[key := {| leased_rev := rev; leased_entry := entry_updating w |} ]> c.(entries) |>.
 
 Lemma take_step_Lock (key : go_string) w rev :
   step_Lock key w rev →
-  own_cache_inv c ==∗
-  own_cache_inv c' ∗
+  own_cache_inv γc c ==∗
+  own_cache_inv γc c' ∗
   own_Locked w rev.
 Proof.
 Admitted.
 
-Definition step_Commit_Put (rev : Z) (put_req : PutRequest.t) : Prop :=
-  (default 0 (s.(lease_revs) !! put_req.(PutRequest.key))) < rev + 1 ∧
-  s'.(kvs) = <[ put_req.(PutRequest.key) := put_req.(PutRequest.value)]> s.(kvs).
+Definition step_Commit_Put (rev : Z) (put_req : PutRequest.t) resp : Prop :=
+  interp (handle_etcdE 0) (Put put_req) s s' (inr resp).
+
+Definition own_Put_committed (resp : PutResponse.t) : iProp Σ.
+Admitted.
 
 (* FIXME: requires linearization update. *)
-Lemma take_step_Commit_Put rev put_req :
-  step_Commit_Put rev put_req →
-  own_server_inv s ==∗
-  own_server_inv s'.
+Lemma take_step_Commit_Put {w} rev put_req resp :
+  step_Commit_Put rev put_req resp →
+  own_Locked w rev -∗
+  own_server_inv γs s ==∗
+  own_server_inv γs s' ∗
+  own_Put_committed resp.
 Proof.
 Admitted.
 
@@ -88,13 +104,17 @@ Definition step_Evict (key : go_string) : Prop :=
 Lemma take_step_Evict key w rev :
   step_Evict key →
   own_Locked w rev -∗
-  own_cache_inv c ==∗
-  own_cache_inv c' ∗ own_Locked w rev.
+  own_cache_inv γc c ==∗
+  own_cache_inv γc c' ∗ own_Locked w rev.
 Proof.
 Admitted.
 
-
-(* Q: what happens when the KV operation is committed? Really, that should be
-   part of the inv too I guess. *)
+(*
+Definition step_Update key val resp :
+  ∃ le,
+  s.(entries) !! key = le ∧
+  le.(entry) *)
+(* FIXME: need to update the value even if there's a new waitc, but the sum type
+   doesn't have a way of storing both a value and a waitc. *)
 
 End protocol.
