730f2bc8e24ad708d63c5d55aa444a170ffdbc84
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-07-30 17:02:29 -0400

    make dfrac notation 'only parsing', for now. it seems to conflict with the dfrac custom syntax, st pointsto DfracOwn 1 get printed as {1}, instead of empty

--- src/iris_lib/dfrac.v
+++ src/iris_lib/dfrac.v
@@ -4,10 +4,10 @@ Declare Scope dfrac_scope.
 Delimit Scope dfrac_scope with dfrac.
 Bind Scope dfrac_scope with dfrac.
 
-Notation "1" := (DfracOwn 1) : dfrac_scope.
+Notation "1" := (DfracOwn 1) (only parsing) : dfrac_scope.
 (* users need (□) to de-conflict with the reserved persistent notation
 in bi/notation.v, which takes in an extra arg. *)
-Notation "□" := DfracDiscarded (at level 20) : dfrac_scope.
+Notation "□" := DfracDiscarded (at level 20, only parsing) : dfrac_scope.
 (* this currently conflicts with to_val notation.
 maybe we need to restrict that notation's scope? *)
 (* Notation "# q" := (DfracOwn q%Qp) (at level 50) : dfrac_scope. *)
