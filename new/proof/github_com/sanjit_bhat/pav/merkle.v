d5e23714727dfcc6542670fb52186592f7dd7935
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-07-31 11:42:11 -0400

    merkle: copy over own defs

--- new/proof/github_com/sanjit_bhat/pav/merkle.v
+++ new/proof/github_com/sanjit_bhat/pav/merkle.v
@@ -1,10 +1,10 @@
 (* NOTE: "merkle_proof" files themselves don't explicitly define Modules.
 this allows merkle files to not have to write "merkle".
 however, when they're exposed to the outside, they're within this merkle module. *)
-From New.proof.github_com.sanjit_bhat.pav.merkle_proof Require Import base serde theory.
+From New.proof.github_com.sanjit_bhat.pav.merkle_proof Require Import base merkle serde theory.
 
 Module merkle.
-  Export base serde theory.
+  Export base merkle serde theory.
   (* tentative strategy:
   - inside sub-files, don't mark opaque / local.
   those objects need to be visible inside the "pkg".
