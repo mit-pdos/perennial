d3caf50ddcfd25e32d4eec2e64eda8297666481e
Tej Chajed <chajed@wisc.edu>
2025-08-22 16:44:26 -0500

    Add output test infrastructure

---
+++ tests/golang_notation.v
@@ -0,0 +1,14 @@
+From New.proof Require Import proof_prelude.
+From New.proof Require Import grove_prelude.
+
+Section proof.
+Context `{!heapGS Σ}.
+
+Check "pointsto_print".
+Lemma pointsto_print (l1 l2: loc) (v1: bool) (v2: slice.t) dq :
+  l1 ↦ v1 ∗ l2 ↦{dq} v2 ∗ l1 ↦□ v1 ⊢ True.
+Proof.
+  Show.
+Abort.
+
+End proof.
