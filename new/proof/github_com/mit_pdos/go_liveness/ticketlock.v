234c4d42793a66998ae2713b04847f3e78b9383a
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-09-05 10:04:18 -0400

    goose runtime.Gosched axiom

---
+++ new/proof/github_com/mit_pdos/go_liveness/ticketlock.v
@@ -0,0 +1,5 @@
+Require Import New.generatedproof.github_com.mit_pdos.go_liveness.ticketlock.
+Require Import New.proof.proof_prelude.
+
+Set Implicit Arguments.
+
---
+++ new/trusted_code/runtime.v
@@ -0,0 +1,10 @@
+From New.golang Require Import defn.
+
+Section code.
+Context `{ffi_syntax}.
+
+Definition Goschedⁱᵐᵖˡ : val :=
+  λ: <>,
+    #().
+
+End code.
