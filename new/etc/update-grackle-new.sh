4f27b04cde9baa224cef2404e91d042c17b590e0
Matt Schwennesen <mjschwenne@gmail.com>
2025-09-18 11:53:12 -0500

    update grackle to use rocq CLI names

--- new/etc/update-grackle-new.sh
+++ new/etc/update-grackle-new.sh
@@ -23,14 +23,14 @@ perennial_path() {
     echo "$1" | sed -E -e "s/\./-/g" -e "s/-/_/g"
 }
 
-# Computes the coq logical name by
+# Computes the Rocq logical name by
 #
 # - Replacing "." with "-"
 # - Replacing "-" with "_"
 # - Replacing "/" with "."
 #
 # This mirrors grackle/internal/util/util.go :: CleanCoqName function
-coq_logical_name() {
+rocq_logical_name() {
     echo $(perennial_path "$1") | sed -E -e "s/\//\./g"
 }
 
@@ -38,19 +38,19 @@ coq_logical_name() {
 #
 # We will assume that:
 # 1. The proto file is in the directory $2/$1
-# 2. We only want to output coq code
-# 3. The coq code should be output into "new/proof/$1"
-# 4. The desired coq package matches the directory structure
+# 2. We only want to output rocq code
+# 3. The rocq code should be output into "new/proof/$1"
+# 4. The desired rocq package matches the directory structure
 #
 # Parameters
 # - $1 : Name of the go package inside its repo
 # - $2 : Path to the root of the go repo. Go package to translate should be at "$2/$1"
 # - $3 : Prefix to compute the go package name, "$3/$1"
 run_grackle() {
-    $GRACKLE --coq-logical-path "New.proof.$(coq_logical_name "$3/$1")" --coq-physical-path "new/proof/$(perennial_path "$3")/$1" --go-package "$3/$1" "$(realpath "$2/$1")"
+    $GRACKLE --rocq-logical-path "New.proof.$(rocq_logical_name "$3/$1")" --rocq-physical-path "new/proof/$(perennial_path "$3")/$1" --go-package "$3/$1" "$(realpath "$2/$1")"
 }
 
-# Generate Coq files from gokv repo.
+# Generate Rocq files from gokv repo.
 #
 # Parameters
 # - $1 : Path to the gokv repo.
@@ -96,13 +96,13 @@ while [ $# -ge 1 ]; do
 usage: update-grackle.sh [--compile-grackle <grackle repo> | -c <grackle repo>]
                          [--gokv <gokv repo> | -g <gokv repo>] [--help | -h]
 
-Calls grackle on all gokv go modules known to have proto files for grackle usage, generating coq proofs.
+Calls grackle on all gokv go modules known to have proto files for grackle usage, generating rocq proofs.
 
 THIS SCRIPT SHOULD BE RUN FROM THE ROOT OF THE PERENNIAL REPO.
 
 --compile-grackle [-c] : Takes the path to the grackle repository, recompiles and installs grackle
 
---gokv [-g] : Regenerate Coq proofs for the gokv project, takes path to gokv as argument
+--gokv [-g] : Regenerate Rocq proofs for the gokv project, takes path to gokv as argument
 EOF
         shift
         exit 0
