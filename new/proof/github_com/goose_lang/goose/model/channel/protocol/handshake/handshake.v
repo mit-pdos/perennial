30a5b3a8af28dad81980621d63d80f9712b76583
Levi Redlin <lredlin@wisc.edu>
2025-09-24 09:15:08 -0500

    Add protocol files.

---
+++ new/proof/github_com/goose_lang/goose/model/channel/protocol/handshake/handshake.v
@@ -0,0 +1,133 @@
+Require Import New.proof.proof_prelude.
+From New.proof.github_com.goose_lang.goose.model.channel Require Export chan_au_send chan_au_recv chan_au_base chan_init.
+
+Section handshake.
+Context `{hG: heapGS Σ, !ffi_semantics _ _}.
+Context `{!ghost_varG Σ ()}.
+Context `{!chanGhostStateG Σ ()}.
+Context `{!globalsGS Σ} {go_ctx : GoContext}.
+
+Implicit Types (s : chan_rep.t ()).
+
+(*----------------------------------------------------------------------------
+  Invariant for a simple handshake on an unbuffered channel with unit payloads.
+
+  - When the channel has an in-flight *send* (SndWait/SndDone), predicate [P]
+    must hold (producer-side obligation).
+  - When the channel has an in-flight *receive* (RcvWait/RcvDone), predicate [Q]
+    must hold (consumer-side obligation).
+  - Buffered channels are intentionally disallowed.
+  - Closing is also disallowed in this protocol ([_ => False]).
+
+  ---------------------------------------------------------------------------*)
+Definition is_handshake γ (ch : loc) P Q : iProp Σ :=
+  is_channel ch 0 γ  ∗
+  inv nroot (
+      ∃ s,
+        "Hch" ∷ own_channel ch s 0 γ ∗
+    (match s with
+     | chan_rep.Idle =>
+        True
+     | chan_rep.SndWait _ | chan_rep.SndDone _ =>
+         P
+     | chan_rep.RcvWait | chan_rep.RcvDone =>
+         Q
+     (* Can't use buffered channel and we don't close here. *)
+     | _ => False
+     end
+    )).
+
+Lemma start_handshake P Q ch s γ:
+  is_channel ch 0 γ  -∗
+  own_channel ch chan_rep.Idle 0 γ ={⊤}=∗
+  is_handshake γ ch P Q .
+Proof.
+    intros.
+  iIntros "#? Hchan".
+  iFrame "#". iFrame. simpl.
+  iApply inv_alloc.
+ iExists chan_rep.Idle.
+ iFrame "∗%#".
+Qed.
+
+Lemma wp_handshake_receive γ ch P Q :
+  {{{
+         £ 1 ∗  £ 1 ∗  £ 1 ∗
+      is_pkg_init channel ∗
+      is_handshake γ ch P Q  ∗
+      Q
+  }}}
+    ch @ (ptrT.id channel.Channel.id) @ "Receive" #(structT []) #()
+  {{{
+      RET (#(), #true); P
+  }}}.
+Proof.
+  iIntros (?) "(Hlc1 & Hlc2 & Hlc3 & Hpc & (#Hchan & #Hinv) & HQ) HΦ".
+  iApply ((wp_Receive ch 0  γ Φ  ) with "[$Hpc $Hchan]").
+  iModIntro. unfold chan_receive_atomic_update.
+  iFrame "#".
+  iInv "Hinv" as "Hi" "Hclose".
+   iMod (lc_fupd_elim_later with "[$] Hi") as "Hi".
+   iNamed "Hi".
+   iApply fupd_mask_intro; [ solve_ndisj | iIntros "Hmask"].
+  iNext. iNamed "Hi". iFrame.
+   destruct s. all:try done.
+   -   iIntros "H".
+    iMod "Hmask" as "_". iMod ("Hclose" with "[-HΦ Hlc2]").
+    +  iModIntro. iExists chan_rep.RcvWait.  iFrame.
+    + iModIntro.  iInv "Hinv" as "Hi" "Hclose".
+      iMod (lc_fupd_elim_later with "[$] Hi") as "Hi".
+   iNamed "Hi".  iApply fupd_mask_intro; [ solve_ndisj | iIntros "Hmask"].
+   iModIntro. iExists s. iFrame. destruct s. all: try done.
+   { iMod "Hmask" as "_". iIntros "Hid". iMod ("Hclose" with "[-HΦ Hi]").
+     { iModIntro.  iFrame. }
+     iModIntro. replace v with (). { iApply "HΦ". done. }
+     destruct v.
+      done.
+   }
+   -  iIntros "H".
+    iMod "Hmask" as "_". iMod ("Hclose" with "[-HΦ Hlc2 Hi]").
+    + iModIntro. iFrame.
+    + iModIntro. destruct v. iApply "HΦ". done.
+Qed.
+
+Lemma wp_handshake_send γ ch P Q :
+  {{{
+         £ 1 ∗  £ 1 ∗  £ 1 ∗
+      is_pkg_init channel ∗
+      is_handshake γ ch P Q ∗
+      P
+  }}}
+    ch @ (ptrT.id channel.Channel.id) @ "Send" #(structT []) #()
+  {{{
+      RET (#()); Q
+  }}}.
+Proof.
+  iIntros (?) "(Hlc1 & Hlc2 & Hlc3 & Hpc & (#Hchan & #Hinv) & HP) HΦ".
+  iApply ((wp_Send ch 0 ()  γ Φ  ) with "[$Hpc $Hchan]").
+  iModIntro. unfold chan_receive_atomic_update.
+  iFrame "#".
+  iInv "Hinv" as "Hi" "Hclose".
+   iMod (lc_fupd_elim_later with "[$] Hi") as "Hi".
+   iNamed "Hi".
+   iApply fupd_mask_intro; [ solve_ndisj | iIntros "Hmask"].
+  iNext. iNamed "Hi". iFrame.
+   destruct s. all:try done.
+   -   iIntros "H".
+    iMod "Hmask" as "_". iMod ("Hclose" with "[-HΦ Hlc2]").
+    +  iModIntro. iExists (chan_rep.SndWait ()).  iFrame.
+    + iModIntro.  iInv "Hinv" as "Hi" "Hclose".
+      iMod (lc_fupd_elim_later with "[$] Hi") as "Hi".
+   iNamed "Hi".  iApply fupd_mask_intro; [ solve_ndisj | iIntros "Hmask"].
+   iModIntro. iExists s. iFrame. destruct s. all: try done.
+   { iMod "Hmask" as "_". iIntros "Hid". iMod ("Hclose" with "[-HΦ Hi]").
+     { iModIntro.  iFrame. }
+     iModIntro.  iApply "HΦ". done.
+   }
+   - iIntros "Hsd".
+    iMod "Hmask" as "_". iMod ("Hclose" with "[-HΦ Hlc2 Hi]").
+    + iModIntro.  iFrame.
+    + iModIntro.  iApply "HΦ". done.
+Qed.
+
+End handshake.
