48406a0471225accad3912523bfa69e2b38527c7
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-10-16 20:23:39 -0400

    upd pav experiments to import new goose

--- new/proof/github_com/sanjit_bhat/pav/should_build.v
+++ new/proof/github_com/sanjit_bhat/pav/should_build.v
@@ -1,4 +1,4 @@
 From New.generatedproof.github_com.sanjit_bhat.pav Require alicebob.
 From New.proof.github_com.sanjit_bhat.pav Require hashchain merkle.
 From New.proof.github_com.sanjit_bhat.pav.experiments Require
-  crypto_model genie_equiv hash_proph.
+  crypto_semantics genie_equiv hash_proph.
