07caa8c024ca32f54482b3b06c0a9c37386d5fdb
Tej Chajed <chajed@wisc.edu>
2025-10-29 09:03:21 -0500

    regoose SearchReplace

--- new/generatedproof/github_com/goose_lang/goose/testdata/examples/channel.v
+++ new/generatedproof/github_com/goose_lang/goose/testdata/examples/channel.v
@@ -3,7 +3,6 @@ Require Export New.proof.proof_prelude.
 Require Export New.generatedproof.strings.
 Require Export New.generatedproof.time.
 Require Export New.generatedproof.sync.
-Require Export New.generatedproof.github_com.goose_lang.goose.model.channel.
 Require Export New.golang.theory.
 
 Require Export New.code.github_com.goose_lang.goose.testdata.examples.channel.
@@ -25,8 +24,7 @@ Global Instance is_pkg_defined_pure_chan_spec_raw_examples : IsPkgDefinedPure ch
       is_pkg_defined_pure_single chan_spec_raw_examples ∧
       is_pkg_defined_pure code.strings.strings ∧
       is_pkg_defined_pure code.time.time ∧
-      is_pkg_defined_pure code.sync.sync ∧
-      is_pkg_defined_pure code.github_com.goose_lang.goose.model.channel.channel;
+      is_pkg_defined_pure code.sync.sync;
   |}.
 
 #[local] Transparent is_pkg_defined_single is_pkg_defined_pure_single.
@@ -36,8 +34,7 @@ Global Program Instance is_pkg_defined_chan_spec_raw_examples : IsPkgDefined cha
       (is_pkg_defined_single chan_spec_raw_examples ∗
        is_pkg_defined code.strings.strings ∗
        is_pkg_defined code.time.time ∗
-       is_pkg_defined code.sync.sync ∗
-       is_pkg_defined code.github_com.goose_lang.goose.model.channel.channel)%I
+       is_pkg_defined code.sync.sync)%I
   |}.
 Final Obligation. iIntros. iFrame "#%". Qed.
 #[local] Opaque is_pkg_defined_single is_pkg_defined_pure_single.
