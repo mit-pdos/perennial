be8ba3060e63c6d5e656b72f3eb144c7406fc38d
Sanjit Bhat <sanjit.bhat@gmail.com>
2025-10-30 23:27:33 -0400

    switch to abbreviation for `DfracDiscarded`

--- src/iris_lib/dfrac.v
+++ src/iris_lib/dfrac.v
@@ -5,9 +5,10 @@ Delimit Scope dfrac_scope with dfrac.
 Bind Scope dfrac_scope with dfrac.
 
 Notation "1" := (DfracOwn 1) (only parsing) : dfrac_scope.
-(* users need (□) to de-conflict with the reserved persistent notation
-in bi/notation.v, which takes in an extra arg. *)
-Notation "□" := DfracDiscarded (at level 20, only parsing) : dfrac_scope.
+(* use abbreviation instead of "□" to avoid parsing ambiguities
+with reserved persistent notation in bi/notation.v,
+which takes in an extra arg. *)
+Notation dd := DfracDiscarded (only parsing).
 (* this currently conflicts with to_val notation.
 maybe we need to restrict that notation's scope? *)
 (* Notation "# q" := (DfracOwn q%Qp) (at level 50) : dfrac_scope. *)
