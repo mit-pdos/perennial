ff7b32f4f7006d7641a797da3d89efe57b047f04
Tej Chajed <chajed@wisc.edu>
2025-11-07 07:36:13 -0600

    Add a trusted model and spec for time.After

--- new/trusted_code/time.v
+++ new/trusted_code/time.v
@@ -17,7 +17,20 @@ Definition syncTimerⁱᵐᵖˡ : val :=
      if: ArbitraryInt = #(W64 0) then "c"
      else #chan.nil.
 
-Axiom Afterⁱᵐᵖˡ : val.
+(* TODO: awkward to construct a time.Time from trusted_code *)
+Axiom arbitraryTime : val.
+#[local] Definition __Time : go_type := structT [
+  "wall" :: uint64T;
+  "ext" :: int64T;
+  "loc" :: ptrT
+].
+
+Definition Afterⁱᵐᵖˡ : val :=
+  λ: "d",
+    let: "ch" := chan.make #__Time #(W64 0) in
+    (* delay is modeled as a no-op *)
+    Fork (chan.send #__Time "ch" (arbitraryTime #()));;
+    "ch".
 
 Definition Sleepⁱᵐᵖˡ : val :=
   λ: "d", #().
