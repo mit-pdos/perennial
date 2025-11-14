1af32500253691aaea69acfdf4de95d43b7adb77
Tej Chajed <chajed@wisc.edu>
2025-11-14 16:39:13 -0600

    Translate CmpFunc-based sorting code

---
+++ new/proof/slices_proof/sort.v
@@ -0,0 +1,8 @@
+From New.generatedproof Require Import slices.
+From New.proof Require Import proof_prelude.
+From New.proof.slices_proof Require Import slices_init.
+
+Section proof.
+Context  `{hG: heapGS Σ, !ffi_semantics _ _} `{!globalsGS Σ} {go_ctx : GoContext}.
+
+End proof.
