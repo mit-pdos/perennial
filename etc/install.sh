81ac460e4516de44c33875389726fc7b08b2a9d9
Tej Chajed <chajed@wisc.edu>
2025-11-25 13:44:44 -0600

    Add ability to install dependencies of one file

--- etc/install.sh
+++ etc/install.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
+set -eu
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+target=""
+
+if [ $# -gt 0 ]; then
+	target="$1"
+fi
+
 function gen_rocq_project() {
-  cat _RocqProject
-  find src new -name "*.v"
+	cat _RocqProject
+	if [ -z "$target" ]; then
+		# get all files
+		find src new -name "*.v"
+	else
+		"$DIR"/get-deps.py "$target"
+	fi
 }
 
 proj_file=_RocqProject.install
@@ -15,5 +30,5 @@ make -j1 -f "$makefile" install
 
 ## cleanup
 rm -f "$proj_file" \
-  "$makefile" "$makefile.conf" "$makefile.local" ".${makefile}.d" \
-  .filestoinstall
+	"$makefile" "$makefile.conf" "$makefile.local" ".${makefile}.d" \
+	.filestoinstall
