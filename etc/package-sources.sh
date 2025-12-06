ac2e8dca7bd22448e264cc43bcedc7124948dfbe
Tej Chajed <chajed@wisc.edu>
2025-12-05 19:50:30 -0600

    Add an opam package for old goose (#519)
    This can be a dependency for old program proofs that we want to keep
    working, albeit with a frozen version of perennial.

--- etc/package-sources.sh
+++ etc/package-sources.sh
@@ -3,7 +3,7 @@
 set -eu
 
 function show_help() {
-	cat <<EOF
+  cat <<EOF
 Usage: $0 PACKAGE
 
 List source files for a given package.
@@ -16,44 +16,66 @@ OPTIONS:
   --help     Show this help message
 
 EOF
-	exit 0
+  exit 0
 }
 
 if [ $# -eq 0 ]; then
-	echo "Error: No package specified" 1>&2
-	echo "Use --help for usage information" 1>&2
-	exit 1
+  echo "Error: No package specified" 1>&2
+  echo "Use --help for usage information" 1>&2
+  exit 1
 fi
 
 if [ "$1" = "--help" ]; then
-	show_help
+  show_help
 fi
 
 package="$1"
 
+function find_build() {
+  find "$@" -not -name "*__nobuild.v" -name "*.v"
+}
+
+function goose_common() {
+  ls -1 src/base.v
+  # TODO: program_logic adds a lot of extra files
+  find_build src/Helpers src/algebra src/base_logic src/iris_lib \
+    src/program_logic
+}
+
 if [ "$package" = "all" ]; then
-	# get all files
-	find src new -not -name "*__nobuild.v" -name "*.v"
+  # get all files
+  find src new -not -name "*__nobuild.v" -name "*.v"
 elif [ "$package" = "new-goose" ]; then
-	ls src/base.v
-	# TODO: program_logic adds a lot of extra files
-	find src/Helpers src/algebra src/base_logic src/iris_lib \
-		src/program_logic \
-		new/golang/{defn/,defn.v} \
-		new/golang/{theory/,theory.v} \
-		new/proof/sync_proof \
-		new/proof/{sync/,sync.v} \
-		new/proof/github_com/goose_lang/std.v \
-		-not -name "*__nobuild.v" \
-		-name "*.v"
-	ls -1 src/goose_lang/{lang,lifting,locations,notation,ipersist}.v
-	ls -1 new/code/github_com/goose_lang/goose/model/channel.v \
-		new/{code,trusted_code}/github_com/goose_lang/primitive.v \
-		new/code/internal/race.v \
-		new/{code,trusted_code}/sync.v \
-		new/{code,trusted_code}/sync/atomic.v
-	ls -1 new/proof/{proof_prelude,disk_prelude,grove_prelude}.v
+  goose_common
+  ls -1 \
+    src/goose_lang/{lang,lifting,locations,notation,ipersist}.v
+  find_build \
+    new/golang/{defn/,defn.v} \
+    new/golang/{theory/,theory.v} \
+    new/proof/sync_proof \
+    new/proof/{sync/,sync.v} \
+    new/proof/github_com/goose_lang/std.v
+  ls -1 new/code/github_com/goose_lang/goose/model/channel.v \
+    new/{code,trusted_code}/github_com/goose_lang/primitive.v \
+    new/code/internal/race.v \
+    new/{code,trusted_code}/sync.v \
+    new/{code,trusted_code}/sync/atomic.v
+  ls -1 new/proof/{proof_prelude,disk_prelude,grove_prelude}.v
+elif [ "$package" = "old-goose" ]; then
+  goose_common
+  find_build src/goose_lang
+  #ls -1 \
+  #  src/goose_lang/{prelude,base_prelude,proofmode,tactics,wpc_proofmode}.v \
+  #  src/goose_lang/{adequacy,array,typing,crash_modality}.v
+  #find_build src/goose_lang/ffi/ src/goose_lang/lib/
+  find_build external/Goose/github_com/goose_lang
+  ls -1 external/Goose/github_com/tchajed/marshal.v
+  ls -1 \
+    src/program_proof/unittest.v \
+    src/program_proof/{proof_prelude,disk_lib,disk_prelude}.v \
+    src/program_proof/marshal*.v \
+    src/program_proof/std_proof.v
 else
-	echo "unknown package $package" 1>&2
-	exit 1
+  echo "unknown package $package" 1>&2
+  exit 1
 fi
