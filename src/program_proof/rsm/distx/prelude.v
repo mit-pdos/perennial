05b509ff08acca4f8ba18d6f8adbf337e2df1e07
Tej Chajed <chajed@wisc.edu>
2025-12-06 12:11:19 -0600

    Remove tulip proofs
    These are the files that have been moved to https://github.com/mit-pdos/tulip-proof

--- src/program_proof/rsm/distx/prelude.v
+++
@@ -1,7 +0,0 @@
-From Perennial.program_proof Require Export grove_prelude.
-From Perennial.program_logic Require Export atomic. (* prefer the ncfupd atomics *)
-From Perennial.program_proof.rsm Require Export big_sep.
-From Perennial.program_proof.rsm.pure Require Export
-  dual_lookup extend fin_maps largest_before list misc nat nonexpanding_merge sets vslice word.
-From Perennial.program_proof.rsm.distx Require Export
-  action base cmd res inv.
