4a1f17bce74346ca18dc918b3f9d459f181b2fd0
Tej Chajed <chajed@wisc.edu>
2025-12-07 15:46:37 -0600

    Remove files that no longer build

--- new/should_build.v
+++ new/should_build.v
@@ -9,7 +9,7 @@ From New.proof Require Import bytes chan context sort slices runtime.
 (* required just to make sure this demo works *)
 From New.golang.theory Require static_mem.
 From New.proof Require go_etcd_io.raft.v3.
-From New.proof.github_com.mit_pdos.gokv Require gokv cachekv bank.
+From New.proof.github_com.mit_pdos.gokv Require gokv.
 From New.proof.github_com.goose_lang.goose.testdata.examples
   Require unittest unittest.generics.
 From New.proof.github_com.goose_lang.goose.testdata.examples
