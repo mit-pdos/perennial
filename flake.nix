8fabac45e7d152fba6126f2a6d565a215d23d06d
Matt Schwennesen <mjschwenne@gmail.com>
2025-12-09 21:01:29 -0600

    finish nix update to opam, check nix build in CI if flakes change

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
     grackle.url = "github:mjschwenne/grackle";
     opam-nix.url = "github:tweag/opam-nix";
-    opam-repository.url = "github:ocaml/opam-repository";
-    opam-repository.flake = false;
+    opam-repository = {
+      url = "github:ocaml/opam-repository";
+      flake = false;
+    };
+    opam-rocq-repo = {
+      url = "github:rocq-prover/opam";
+      flake = false;
+    };
   };
 
   outputs = {
@@ -16,6 +22,7 @@
     grackle,
     opam-nix,
     opam-repository,
+    opam-rocq-repo,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -23,49 +30,61 @@
         pkgs = import nixpkgs {
           inherit system;
         };
-        opam-rocq-repo = pkgs.fetchFromGitHub {
-          owner = "rocq-prover";
-          repo = "opam";
-          rev = "21386fecc599280c73801a6a270111a1fabca62b";
-          hash = "sha256-AeSR497sH7JftvtGPpRZzfaJ/L7asuL/hlaaMZDx7/k=";
-        };
-        inherit
-          (opam-nix.lib.${system}.buildOpamProject {
-              repos = ["${opam-repository}" "${opam-rocq-repo}/released"];
-            } "perennial"
-            ./. {})
-          perennial
-          ;
-        perennial-final = perennial.overrideAttrs (finalAttrs: previousAttrs: {
+        inherit (opam-nix.lib.${system}) buildOpamProject;
+        perennialPkgs =
+          buildOpamProject {
+            repos = ["${opam-repository}" "${opam-rocq-repo}/released"];
+          } "perennial"
+          ./. {};
+        perennial = perennialPkgs.perennial.overrideAttrs (finalAttrs: previousAttrs: {
           nativeBuildInputs = with pkgs; [python3] ++ previousAttrs.nativeBuildInputs;
+          preBuild = ''
+            # swap ROCQPATH for COQPATH, avoiding overriding the complex configurationPhase
+            export ROCQPATH=$COQPATH
+            unset COQPATH
+          '';
         });
       in {
         packages = {
-          inherit perennial-final;
-          default = perennial-final;
+          # remove the perennial package from perennialPkgs since it won't build
+          # without python
+          inherit (removeAttrs perennialPkgs ["perennial"]) perennial;
+          default = perennial;
         };
         devShells.default = with pkgs;
           mkShell {
-            buildInputs = [
-              opam
+            buildInputs =
+              [
+                opam
+                python3
 
-              go
-              grackle.packages.${system}.default
-              grackle.packages.${system}.goose
-              protobuf
+                go
+                grackle.packages.${system}.default
+                grackle.packages.${system}.goose
+                protobuf
 
-              # nix helpers
-              nix-update
+                # nix helpers
+                nix-update
 
-              # opam related system dependencies
-              pkg-config
-              gmp
-              findutils
-            ];
+                # opam related system dependencies
+                pkg-config
+                gmp
+                findutils
+              ]
+              ++ (with perennialPkgs; [
+                rocq-runtime
+                rocq-stdlib
+                coq-coqutil
+                coq-record-update
+                rocq-stdpp
+                rocq-iris
+                iris-named-props
+              ]);
 
             shellHook = ''
+              # swap ROCQPATH for COQPATH
+              export ROCQPATH=$COQPATH
               unset COQPATH
-              eval $(opam env)
             '';
           };
       }
