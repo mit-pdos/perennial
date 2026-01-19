24d39044a8f09634f3c61bc9f541d209302db6b3
Upamanyu Sharma <upamanyu@mit.edu>
2026-01-19 15:46:09 -0500

    Primitive semantics for byte to string conversion

--- new/golang/defn/string.v
+++ new/golang/defn/string.v
@@ -10,6 +10,8 @@ Class StringSemantics `{!GoSemanticsFunctions} :=
 {
   (* FIXME: convert semantics *)
   #[global] package_sem :: strings.Assumptions;
+  #[global] convert_byte_to_string (c : w8)
+   `{!from ↓u go.byte} `{!to ↓u go.string} :: go.GoExprEq (Convert from to #c) #([c]);
 }.
 End defs.
 End go.
