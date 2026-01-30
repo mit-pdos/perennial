c95588beb2e3d861ff715da6206a048585d96f26
Upamanyu Sharma <upamanyu@mit.edu>
2026-01-30 18:20:58 -0500

    Delete more files

--- src/goose_lang/examples/nfs_spec/symtest/rfc1057/__init__.py
+++
@@ -1,5 +0,0 @@
-import const
-import types
-import pack
-import client
-import server
--- src/program_proof/mvcc/goose.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../../../
-./etc/update-goose.py --compile --goose ../goose/ --mvcc ../vmvcc/
