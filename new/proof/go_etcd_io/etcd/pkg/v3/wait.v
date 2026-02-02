a8c8d76a5258e152df0eb89620b5075ddb8c42cd
Upamanyu Sharma <upamanyu@mit.edu>
2026-02-02 12:35:21 -0500

    Try to fix wait

--- new/proof/go_etcd_io/etcd/pkg/v3/wait.v
+++ new/proof/go_etcd_io/etcd/pkg/v3/wait.v
@@ -1,45 +1,53 @@
 Require Export New.generatedproof.go_etcd_io.etcd.pkg.v3.wait.
 Require Export New.proof.proof_prelude.
 From New.proof Require Import log sync.
+From New.proof.github_com.goose_lang.goose.model.channel
+  Require Import logatom.chan_au_base idiom.handoff.handoff.
+
+Class waitG `{ffi_syntax} Σ :=
+  {
+    #[local] wait_chanG :: chanG Σ any.t;
+  }.
 
 Section wps.
 Context `{hG: heapGS Σ, !ffi_semantics _ _}.
-Context `{!globalsGS Σ} {go_ctx : GoContext}.
+Context {sem : go.Semantics} {package_sem : wait.Assumptions}.
+Context `{!waitG Σ}.
+Local Set Default Proof Using "All".
 
-#[global] Instance : IsPkgInit wait := define_is_pkg_init True%I.
+#[global] Instance : IsPkgInit (iProp Σ) wait := define_is_pkg_init True%I.
+#[global] Instance : GetIsPkgInitWf (iProp Σ) wait := build_get_is_pkg_init_wf.
 
 Record wait_params :=
   {
     I : iProp Σ;
     own_unregistered_id : w64 → iProp Σ;
   }.
+
+Local Notation interface_call i m := #(methods i.(interface.ty) m i.(interface.v)).
+
 (* This is non-duplicable so that ownership of the internal [I] can rule out
    overflow. This also permits non-concurrent implementations. *)
-Definition own_Wait γ (w : wait.Wait.t) R : iProp Σ :=
+Definition own_Wait γ w R : iProp Σ :=
     "HI" ∷ I γ ∗
     "#Register" ∷
       (∀ (id' : w64),
          {{{ I γ ∗ own_unregistered_id γ id' }}}
-           interface.get #"Register" #w #id'
+           interface_call w "Register" #id'
          {{{ ch, RET #ch;
              I γ ∗
-             |={⊤,∅}=>
-               ∃ (st : chanstate.t interface.t),
-               own_chan ch st ∗
-               (∀ v, ⌜ v ∈ drop st.(chanstate.received) st.(chanstate.sent) ⌝ → R id' v) ∗
-               (∀ r, own_chan ch (set chanstate.received (Nat.add r) st) ={⊤,∅}=∗ True)
-         }}}
-      ) ∗
+             (∀ Φ, (∀ v, R id' v -∗ Φ v true) -∗ recv_au ch any.t Φ)
+         }}}) ∗
     "#Trigger" ∷
       (∀ (id' : w64) (x : interface.t),
          {{{ I γ ∗ R id' x }}}
-           interface.get #"Trigger" #w #id' #x
+           interface_call w "Trigger" #id' #x
          {{{ RET #(); I γ }}}
       ) ∗
     "#IsRegistered" ∷
       (∀ (id' : w64),
          {{{ I γ }}}
-           interface.get #"IsRegistered" #w #id'
+           interface_call w "IsRegistered" #id'
          {{{ (reg : bool), RET #reg; I γ }}}
       ).
 #[global] Opaque own_Wait.
@@ -47,14 +55,10 @@ Definition own_Wait γ (w : wait.Wait.t) R : iProp Σ :=
 
 Lemma wp_Wait__Register γ w (id' : w64) R :
   {{{ is_pkg_init wait ∗ own_Wait γ w R ∗ own_unregistered_id γ id' }}}
-    interface.get #"Register" #w #id'
+    interface_call w "Register" #id'
   {{{ ch, RET #ch;
       own_Wait γ w R ∗
-      |={⊤,∅}=>
-        ∃ (st : chanstate.t interface.t),
-        own_chan ch st ∗
-        (∀ v, ⌜ v ∈ drop st.(chanstate.received) st.(chanstate.sent) ⌝ → R id' v) ∗
-        (∀ r, own_chan ch (set chanstate.received (Nat.add r) st) ={⊤,∅}=∗ True)
+      (∀ Φ, (∀ v, R id' v -∗ Φ v true) -∗ recv_au ch any.t Φ)
   }}}.
 Proof.
   wp_start as "[Hw Hid]". iNamed "Hw".
@@ -63,7 +67,7 @@ Qed.
 
 Lemma wp_Wait__Trigger γ w (id' : w64) (x : interface.t) R :
   {{{ is_pkg_init wait ∗ own_Wait γ w R ∗ R id' x }}}
-    interface.get #"Trigger" #w #id' #x
+    interface_call w "Trigger" #id' #x
   {{{ RET #(); own_Wait γ w R }}}.
 Proof.
   wp_start as "[Hw HR]". iNamed "Hw".
@@ -72,7 +76,7 @@ Qed.
 
 Lemma wp_Wait__IsRegistered γ w (id' : w64) R :
   {{{ is_pkg_init wait ∗ own_Wait γ w R }}}
-    interface.get #"IsRegistered" #w #id'
+    interface_call w "IsRegistered" #id'
   {{{ (reg : bool), RET #reg; own_Wait γ w R }}}.
 Proof.
   wp_start as "Hw". iNamed "Hw".
