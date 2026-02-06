1a756fbf4131ca6386738973ba431c76a17a539c
Upamanyu Sharma <upamanyu@mit.edu>
2026-02-06 13:46:18 -0500

    unittest/generics proofs is easy now (no more shelved typeclasses or impossible proofs)

--- new/proof/github_com/goose_lang/goose/testdata/examples/unittest/generics.v
+++ new/proof/github_com/goose_lang/goose/testdata/examples/unittest/generics.v
@@ -4,57 +4,48 @@ From New.generatedproof.github_com.goose_lang.goose.testdata.examples
 
 Section wps.
 Context `{hG: heapGS Σ, !ffi_semantics _ _}.
-Context `{!globalsGS Σ} {go_ctx : GoContext}.
+Context {sem : go.Semantics} {package_sem : generics.Assumptions}.
+Local Set Default Proof Using "All".
 
-#[global] Instance : IsPkgInit helpers := define_is_pkg_init True%I.
-#[global] Instance : GetIsPkgInitWf helpers := build_get_is_pkg_init_wf.
+#[global] Instance : IsPkgInit (iProp Σ) helpers := define_is_pkg_init True%I.
+#[global] Instance : GetIsPkgInitWf (iProp Σ) helpers := build_get_is_pkg_init_wf.
 
-#[global] Instance : IsPkgInit generics := define_is_pkg_init True%I.
-#[global] Instance : GetIsPkgInitWf generics := build_get_is_pkg_init_wf.
+#[global] Instance : IsPkgInit (iProp Σ) generics := define_is_pkg_init True%I.
+#[global] Instance : GetIsPkgInitWf (iProp Σ) generics := build_get_is_pkg_init_wf.
 
 Section generic_proofs.
-Context `{!IntoVal T'} `{!IntoValTyped T' T} `{Hbounded: BoundedTypeSize T}.
+Context `[!ZeroVal T'] `[!TypedPointsto T'] `[!IntoValTyped T' T].
 
 Lemma wp_BoxGet (b: generics.Box.t T') :
   {{{ is_pkg_init generics }}}
-    @! generics.BoxGet #T #b
+    #(functions generics.BoxGet [T]) #b
   {{{ RET #(generics.Box.Value' b); True }}}.
-Proof using Hbounded.
-  wp_start as "_"; try wp_auto.
-  iApply struct_fields_split in "b"; iNamed "b".
-  wp_auto.
-  iApply "HΦ"; done.
+Proof.
+  wp_start as "_". wp_auto. wp_end.
 Qed.
 
 Lemma wp_Box__Get (b: generics.Box.t T') :
   {{{ is_pkg_init generics }}}
-    b @ generics.Box.id @ "Get" #T #()
+    b @! (generics.Box T) @! "Get" #()
   {{{ RET #(generics.Box.Value' b); True }}}.
-Proof using Hbounded.
-  wp_start as "_"; try wp_auto.
-  iApply struct_fields_split in "b"; iNamed "b".
-  wp_auto.
-  iApply "HΦ"; done.
-Qed.
-
-Lemma wp_Box__Get' (b: generics.Box.t T') :
-  {{{ is_pkg_init generics }}}
-    b @ (ptrT.id generics.Box.id) @ "Get" #T #()
-  {{{ RET #(generics.Box.Value' b); True }}}.
-Proof using Hbounded.
-  wp_start as "_"; try wp_auto. wp_method_call.
-  iApply struct_fields_split in "b"; iNamed "b".
-  wp_auto.
-  iApply "HΦ"; done.
-Qed.
-
-Lemma wp_makeGenericBox (value: T') :
-  {{{ is_pkg_init generics }}}
-    @! generics.makeGenericBox #T #value
-  {{{ RET #(generics.Box.mk value); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  iApply "HΦ"; done.
+  wp_start as "_". wp_auto. wp_end.
+Qed.
+
+Lemma wp_Box__Get' l (b: generics.Box.t T') :
+  {{{ is_pkg_init generics ∗ l ↦ b }}}
+    l @! (go.PointerType $ generics.Box T) @! "Get" #()
+  {{{ RET #(generics.Box.Value' b); True }}}.
+Proof.
+  wp_start. wp_auto. wp_apply wp_Box__Get. wp_end.
+Qed.
+
+Lemma wp_makeGenericBox (value : T') :
+  {{{ is_pkg_init generics }}}
+    #(functions generics.makeGenericBox [T]) #value
+  {{{ RET #(generics.Box.mk T' value); True }}}.
+Proof.
+  wp_start. wp_auto. wp_end.
 Qed.
 End generic_proofs.
 
@@ -63,19 +54,15 @@ Lemma wp_BoxGet2 (b: generics.Box.t w64) :
     @! generics.BoxGet2 #b
   {{{ RET #(generics.Box.Value' b); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  iApply struct_fields_split in "b"; iNamed "b".
-  wp_auto.
-  iApply "HΦ"; done.
+  wp_start. wp_auto. wp_end.
 Qed.
 
 Lemma wp_makeBox :
   {{{ is_pkg_init generics }}}
     @! generics.makeBox #()
-  {{{ RET #(generics.Box.mk (W64 42)); True }}}.
+  {{{ RET #(generics.Box.mk _ (W64 42)); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  iApply "HΦ"; done.
+  wp_start. wp_end.
 Qed.
 
 Lemma wp_useBoxGet :
@@ -83,23 +70,7 @@ Lemma wp_useBoxGet :
     @! generics.useBoxGet #()
   {{{ RET #(W64 42); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  (* TODO: how to fix automation that does this? *)
-  rewrite <- (default_val_eq_zero_val (V:=generics.Box.t w64)).
-  wp_auto.
-  (* TODO: why does this get shelved? *)
-  (* About IntoValTyped. *)
-  Global Hint Mode IntoValTyped ! - - - : typeclass_instances.
-  unshelve wp_apply wp_makeGenericBox --no-auto. { apply _. }
-  (* TODO: it's a [wp_load] inside the auto causing the typeclass goal. *)
-  wp_auto.
-  wp_bind.
-  wp_method_call.
-  wp_auto.
-  wp_auto.
-  wp_apply wp_Box__Get.
-  unshelve wp_apply wp_Box__Get --no-auto. { apply _. }
-  wp_auto. iApply "HΦ"; done.
+  wp_start. wp_auto. wp_apply wp_makeGenericBox. wp_apply wp_Box__Get. wp_end.
 Qed.
 
 Lemma wp_useContainer :
@@ -107,40 +78,33 @@ Lemma wp_useContainer :
     @! generics.useContainer #()
   {{{ RET #(); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  rewrite <- (default_val_eq_zero_val (V:=generics.Container.t w64)).
-  wp_auto.
-  (* NOTE: there's no way to reduce the pair here to a #x to use the list wp
-  theorems, and anyway there's no wp for map literals yet *)
-Abort.
+  wp_start. wp_auto.
+  wp_bind. wp_apply wp_map_make1 as "% Hm".
+  wp_apply (wp_map_insert with "Hm") as "Hm".
+  wp_alloc Z_ptr as "Z". wp_auto.
+  wp_apply (wp_map_insert with "Hm") as "Hm".
+  wp_alloc Z'_ptr as "Z'". wp_auto.
+  wp_end.
+Qed.
 
 Lemma wp_useMultiParam :
   {{{ is_pkg_init generics }}}
     @! generics.useMultiParam #()
   {{{ RET #(); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  rewrite <- (default_val_eq_zero_val (V:=generics.MultiParam.t w64 bool)).
-  wp_auto.
-  iApply struct_fields_split in "mp"; iNamed "mp".
-  with_strategy opaque [is_pkg_init] cbn.
-  wp_auto.
-  iApply "HΦ"; done.
+  wp_start. wp_auto. wp_end.
 Qed.
 
-Lemma wp_multiParamFunc `{!IntoVal A'} `{!IntoValTyped A' A}
-`{!IntoVal B'} `{!IntoValTyped B' B}
+Lemma wp_multiParamFunc `[!ZeroVal A'] `[!TypedPointsto A'] `[!IntoValTyped A' A]
+`[!ZeroVal B'] `[!TypedPointsto B'] `[!IntoValTyped B' B]
   (x: A') (y: B') :
   {{{ is_pkg_init generics }}}
-    @! generics.multiParamFunc #A #B #x #y
+    #(functions generics.multiParamFunc [A; B]) #x #y
   {{{ s, RET #s; s ↦* [y] }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  unshelve wp_apply wp_slice_literal.
-  { apply _. }
-  iIntros (sl) "[H _]".
-  wp_auto.
-  iApply "HΦ". iFrame.
+  wp_start. wp_auto. iPersist "b". wp_apply wp_slice_literal as "% Hsl".
+  { iIntros. wp_auto. iFrame. }
+  wp_end.
 Qed.
 
 Lemma wp_useMultiParamFunc :
@@ -148,13 +112,7 @@ Lemma wp_useMultiParamFunc :
     @! generics.useMultiParamFunc #()
   {{{ RET #(); True }}}.
 Proof.
-  wp_start as "_"; try wp_auto.
-  unshelve wp_apply wp_multiParamFunc.
-  { apply _. }
-  { apply _. }
-  iIntros (s) "H".
-  wp_auto.
-  iApply "HΦ"; done.
+  wp_start. wp_apply wp_multiParamFunc as "% H". wp_end.
 Qed.
 
 End wps.
