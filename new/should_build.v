45b7f8c14b53a7cca9c3b1a8e15b1cc1cb27544d
Sanjit Bhat <sanjit.bhat@gmail.com>
2026-02-21 17:48:58 -0500

    should_build: organize and add some missing

--- new/should_build.v
+++ new/should_build.v
@@ -1,9 +1,3 @@
-From New.generatedproof.github_com.goose_lang.goose.testdata.examples
-  Require unittest semantics unittest.externalglobals.
-From New.generatedproof Require math.rand testing unsafe.
-
-From New.generatedproof.github_com.stretchr.testify Require assert.
-
 (* stdlib. *)
 From New.proof Require Import
   bytes context errors fmt io log math runtime slices sort strings
@@ -12,19 +6,31 @@ From New.proof.crypto Require Import ed25519.
 From New.proof.encoding Require Import binary.
 From New.proof.internal Require Import race synctest.
 From New.proof.math Require Import bits.
+From New.proof.github_com.goose_lang.goose.model.channel.idiom Require idioms.
 
-From New.proof.github_com.goose_lang.goose.testdata.examples
-  Require unittest unittest.generics semantics.
-From New.proof.github_com.goose_lang.goose.testdata.examples
-  Require channel channel_search_replace elimination_stack.
+From New.generatedproof Require math.rand testing.
 
-From New.proof.github_com.goose_lang.goose.model.channel.idiom
-  Require idioms.
+(* common external pkgs. *)
+From New.proof.github_com.tchajed Require Import marshal.
+From New.proof.github_com.goose_lang Require Import primitive std.
+From New.proof.github_com.goose_lang.primitive Require Import disk.
 
+(* testdata. *)
+From New.proof.github_com.goose_lang.goose.testdata.examples Require
+  channel channel_search_replace elimination_stack.
+From New.proof.github_com.goose_lang.goose.testdata.examples Require
+  unittest semantics.
+From New.proof.github_com.goose_lang.goose.testdata.examples.unittest Require
+  generics.
+From New.generatedproof.github_com.goose_lang.goose.testdata.examples.unittest Require
+  externalglobals.
+
+(* misc. *)
+From New Require ghost.
+From New.generatedproof.github_com.stretchr.testify Require assert.
+From New.proof Require Import inG_problem.
+
+(* etcd. *)
 From New.proof Require go_etcd_io.raft.v3.
 From New.proof Require go_etcd_io.etcd.client.v3.leasing leasing_proof.protocol.
 Require New.proof.go_etcd_io.etcd.server.v3.etcdserver.
-
-From New.proof Require Import inG_problem.
-
-From New.ghost Require all.
